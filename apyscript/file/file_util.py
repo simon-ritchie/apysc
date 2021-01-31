88e7fd3e742688b414b0a6675d5b690f084f568b
simon-ritchie <antisocial.sid2@gmail.com>
2021-01-31 21:43:32 +0900

    Add module for files' common utilities implementation.

---
+++ apyscript/file/file_util.py
@@ -0,0 +1,19 @@
+"""Files' common utilities implementation.
+"""
+
+import shutil
+import os
+
+
+def empty_directory(directory_path: str) -> None:
+    """
+    Empty specified directory.
+
+    Parameters
+    ----------
+    directory_path : str
+        Directory path to empty. This folder itself will not be
+        removed.
+    """
+    shutil.rmtree(directory_path, ignore_errors=True)
+    os.makedirs(directory_path, exist_ok=True)
---
+++ tests/file/test_file_util.py
@@ -0,0 +1,14 @@
+import os
+
+from apyscript.file import file_util
+from tests import testing_helper
+
+
+def test_empty_directory() -> None:
+    tmp_dir_path: str = '../.tmp_apyscript/'
+    os.makedirs(tmp_dir_path, exist_ok=True)
+    test_file_path: str = os.path.join(tmp_dir_path, 'test.txt')
+    testing_helper.make_blank_file(file_path=test_file_path)
+    file_util.empty_directory(directory_path=tmp_dir_path)
+    assert os.path.isdir(tmp_dir_path)
+    assert len(os.listdir(tmp_dir_path)) == 0

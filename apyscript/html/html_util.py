0bb55482d75de886bd7ad160146a7d4cbe846999
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-02 21:36:21 +0900

    Add implementation to add html string to another string.

--- apyscript/html/html_util.py
+++ apyscript/html/html_util.py
@@ -19,3 +19,31 @@ def remove_first_selector_symbol_char(str_val: str) -> str:
     if str_val.startswith('.') or str_val.startswith('#'):
         str_val = str_val[1:]
     return str_val
+
+
+def add_html_to_str(
+        to_append_html: str, dest_html: str, indent_num: int) -> str:
+    """
+    Add html string to another string with line break and specified
+    number's indentation.
+
+    Parameters
+    ----------
+    to_append_html : str
+        Html string to append.
+    dest_html : str
+        `to_append_html` will be appended to this string.
+    indent_num : int
+        Indentation's number. The spaces that multiplied this
+        number by 2 will be added.
+
+    Returns
+    -------
+    result : str
+        Html appended string.
+    """
+    result: str = dest_html
+    result += '\n'
+    result += ' ' * (indent_num * 2)
+    result += to_append_html
+    return result
--- tests/html/test_html_util.py
+++ tests/html/test_html_util.py
@@ -13,3 +13,15 @@ def test_remove_first_selector_symbol_char() -> None:
     str_val = html_util.remove_first_selector_symbol_char(
         str_val='line-graph')
     assert str_val == 'line-graph'
+
+
+def test_add_html_to_str() -> None:
+    result: str = html_util.add_html_to_str(
+        to_append_html='<body>',
+        dest_html='<html>',
+        indent_num=1)
+    expected_str: str = (
+        '<html>'
+        '\n  <body>'
+    )
+    assert result == expected_str

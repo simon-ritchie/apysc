21870603b81bbfe1499b71dbd7f68a106719a189
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-07 12:33:49 +0900

    Fix broken tests.

--- tests/html/test_exporter.py
+++ tests/html/test_exporter.py
@@ -48,29 +48,23 @@ def test__append_head_to_html_str() -> None:
 @retry(stop_max_attempt_number=5, wait_fixed=300)
 def test__append_each_expression_to_html_str() -> None:
     html_str: str = '<html>\n<body>'
-    stage: Stage = Stage()
+    Stage(stage_elem_id='test_stage')
     html_str = exporter._append_each_expression_to_html_str(
         html_str=html_str)
-    expected_expresion: str = stage._make_constructor_expression()
-    expected_expresion = html_util.append_indent_to_each_line(
-        html=expected_expresion, indent_num=1)
-    assert expected_expresion in html_str
+    assert 'id="test_stage"' in html_str
 
 
 @retry(stop_max_attempt_number=5, wait_fixed=300)
 def test_save_expressions_overall_html() -> None:
     tmp_dir_path: str = '../.tmp_action_py_script_test_exporter/'
     shutil.rmtree(tmp_dir_path, ignore_errors=True)
-    stage: Stage = Stage()
+    Stage(stage_elem_id='test_stage')
     exporter.save_expressions_overall_html(dest_dir_path=tmp_dir_path)
     expected_index_file_path: str = os.path.join(tmp_dir_path, 'index.html')
     assert os.path.isfile(expected_index_file_path)
     html_str: str = file_util.read_txt(file_path=expected_index_file_path)
     assert html_str.startswith('<html>\n<head>')
     assert html_str.endswith('</body>\n</html>')
-    expected_expresion: str = stage._make_constructor_expression()
-    expected_expresion = html_util.append_indent_to_each_line(
-        html=expected_expresion, indent_num=1)
-    assert expected_expresion in html_str
+    assert 'id="test_stage"' in html_str
 
     shutil.rmtree(tmp_dir_path, ignore_errors=True)

2e12ff9f1c683f1b4e6ccda630b81e9726d3dd5f
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-07 21:41:36 +0900

    Add module for type related common implementations.

---
+++ apyscript/type/type_util.py
@@ -0,0 +1,33 @@
+"""Type related common implementations.
+"""
+
+from typing import Any, Type
+
+
+def is_same_class_instance(cls: Type, instance: Any):
+    """
+    Get a boolean value whether specified class and instance's class
+    are same or not.
+
+    Notes
+    -----
+    If instance is subclass of `cls` argument, differ from `isinstace`,
+    then False will be returned.
+
+    Parameters
+    ----------
+    cls : Type
+        Expected class.
+    instance : *
+        Intance to check it's class.
+
+    Returns
+    -------
+    result : bool
+        If a specified class and instance's class are same, then True
+        will be set.
+    """
+    instance_type: Type = type(instance)
+    if instance_type == cls:
+        return True
+    return False
---
+++ tests/type/test_type_util.py
@@ -0,0 +1,9 @@
+from apyscript.type import type_util
+
+
+def test_is_same_class_instance() -> None:
+    result: bool = type_util.is_same_class_instance(cls=int, instance=1)
+    assert not result
+
+    result = type_util.is_same_class_instance(cls=int, instance=1)
+    assert result

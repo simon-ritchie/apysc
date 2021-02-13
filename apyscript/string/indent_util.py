ba36c0c3c4783f817198a5a9e573ed4cbdd0573f
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-13 16:48:53 +0900

    #1 Add validations to `make_spaces_for_html` interface.

--- apyscript/string/indent_util.py
+++ apyscript/string/indent_util.py
@@ -1,6 +1,8 @@
 """Common indentation related utility implementations.
 """
 
+from apyscript.validation import number_validation
+
 
 def make_spaces_for_html(indent_num: int) -> str:
     """
@@ -16,5 +18,7 @@ def make_spaces_for_html(indent_num: int) -> str:
     spaces : str
         Result spaces string.
     """
+    number_validation.validate_integer(integer=indent_num)
+    number_validation.validate_num_is_gte_zero(num=indent_num)
     spaces: str = ' ' * (indent_num * 2)
     return spaces
--- tests/string/test_indent_util.py
+++ tests/string/test_indent_util.py
@@ -2,7 +2,9 @@ from apyscript.string import indent_util
 
 
 def test_make_spaces_for_html() -> None:
-    spaces: str = indent_util.make_spaces_for_html(indent_num=1)
+    spaces: str = indent_util.make_spaces_for_html(indent_num=0)
+    assert spaces == ''
+    spaces = indent_util.make_spaces_for_html(indent_num=1)
     assert spaces == '  '
     spaces = indent_util.make_spaces_for_html(indent_num=2)
     assert spaces == '    '

c29f73e605bdad54b65d61b7ca5a973769e60f34
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-13 10:25:46 +0900

    Add each lints command interface.

---
+++ apply_lints.py
@@ -0,0 +1,84 @@
+"""Apply each lints to all modules.
+
+Command example:
+$ python apply_lints.py
+"""
+
+import subprocess as sp
+from logging import Logger
+from typing import List
+
+from typing_extensions import TypedDict
+
+from apyscript.logging import loggers
+
+logger: Logger = loggers.get_info_logger()
+
+
+class LintCommand(TypedDict):
+    command: str
+    lint_name: str
+
+
+lint_commands: List[LintCommand] = [
+    {
+        'command':
+        'autoflake --in-place --remove-unused-variables '
+        '--remove-all-unused-imports -r .',
+        'lint_name': 'autoflake',
+    }, {
+        'command': 'isort --force-single-line-imports .',
+        'lint_name': 'isort',
+    }, {
+        'command':
+        'autopep8 --in-place --aggressive --aggressive -r --ignore=E402 .',
+        'lint_name': 'autopep8',
+    }, {
+        'command': 'flake8 --ignore E402,W503 ./',
+        'lint_name': 'flake8',
+    }, {
+        'command': 'numdoclint -p ./ -r',
+        'lint_name': 'numdoclint',
+    }, {
+        'command':
+        'mypy --ignore-missing-imports --follow-imports skip '
+        '--disallow-untyped-calls --disallow-untyped-defs '
+        '--strict-optional --strict-equality ./',
+        'lint_name': 'mypy',
+    },
+]
+
+
+def _main() -> None:
+    """Entry point of this command.
+    """
+    for lint_command in lint_commands:
+        _run_lint_command(lint_command=lint_command)
+
+
+def _run_lint_command(lint_command: LintCommand) -> str:
+    """
+    Run single lint command.
+
+    Parameters
+    ----------
+    lint_command : str
+        Target lint command settng.
+
+    Returns
+    -------
+    stdout : str
+        Command result stdout.
+    """
+    print('-' * 20)
+    logger.info(msg=f'{lint_command["lint_name"]} command started.')
+    complete_process: sp.CompletedProcess = sp.run(
+        lint_command['command'], shell=True,
+        stdout=sp.PIPE, stderr=sp.STDOUT)
+    stdout: str = complete_process.stdout.decode('utf-8')
+    print(stdout)
+    return stdout
+
+
+if __name__ == '__main__':
+    _main()

b6b04dcf04e02c6dccd2e76120e0852cdbf6e740
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-20 20:17:11 +0900

    #9 Rename `acceptable_arg_types` module name to `acceptable_arg_and_ret_types`, and split types get function.

---
+++ apyscript/expression/acceptable_arg_and_ret_types.py
@@ -0,0 +1,52 @@
+"""Definition of callable's acceptable arguments and return value(s).
+"""
+
+
+from typing import List, Type
+
+from apyscript.display.variable_name_interface import VariableNameInterface
+
+
+def get_common_acceptable_types() -> List[Type[VariableNameInterface]]:
+    """
+    Get argument and return value(s) common acceptable types.
+
+    Notes
+    -----
+    To avoid recursive import, each modules are imported in
+    this function (not on top-level scope).
+
+    Returns
+    -------
+    acceptable_types : list of type
+        Common acceptable types.
+    """
+    from apyscript.display.stage import Stage
+    from apyscript.display.sprite import DisplayObject
+    from apyscript.display.graphics import Graphics
+    from apyscript.display.graphic_base import GraphicBase
+    acceptable_types: List[Type] = [
+        Stage,
+        DisplayObject,
+        Graphics,
+        GraphicBase,
+    ]
+    return acceptable_types
+
+
+def get_acceptable_arg_types() -> List[Type[VariableNameInterface]]:
+    """
+    Get expression callable's acceptable argument types.
+
+    Notes
+    -----
+    To avoid recursive import, each modules are imported in
+    this function (not on top-level scope).
+
+    Returns
+    -------
+    arg_types : list of type
+        Acceptable argument types.
+    """
+    arg_types: List[Type] = get_common_acceptable_types()
+    return arg_types
---
+++ tests/expression/test_acceptable_arg_and_ret_types.py
@@ -0,0 +1,26 @@
+from typing import List, Type
+from apyscript.expression import acceptable_arg_and_ret_types
+from apyscript.display.stage import Stage
+from apyscript.display.sprite import DisplayObject
+from apyscript.display.variable_name_interface import VariableNameInterface
+
+
+def test_get_acceptable_arg_types() -> None:
+    arg_types: List[Type[VariableNameInterface]] = \
+        acceptable_arg_and_ret_types.get_acceptable_arg_types()
+    assert Stage in arg_types
+    assert DisplayObject in arg_types
+
+
+def test_acceptable_arg_types_have_variable_name_interface() -> None:
+    arg_types: List[Type[VariableNameInterface]] = \
+        acceptable_arg_and_ret_types.get_acceptable_arg_types()
+    for arg_type in arg_types:
+        assert issubclass(arg_type, VariableNameInterface)
+
+
+def test_get_common_acceptable_types() -> None:
+    acceptable_types: List[Type[VariableNameInterface]] = \
+        acceptable_arg_and_ret_types.get_common_acceptable_types()
+    assert Stage in acceptable_types
+    assert DisplayObject in acceptable_types

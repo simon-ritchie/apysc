a179172e05e5eb0c28766be8e0b528cba00d5a4b
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-23 14:14:21 +0900

    #7 Add `remove_child` interface.

--- apyscript/display/child_interface.py
+++ apyscript/display/child_interface.py
@@ -15,12 +15,12 @@ class ChildInterface:
 
     def add_child(self, child: DisplayObject) -> None:
         """
-        Add display object child to this object.
+        Add display object child to this instance.
 
         Parameters
         ----------
         child : DisplayObject
-            Child object to add.
+            Child instance to add.
         """
         display_validation.validate_display_object(display_object=child)
         self._childs.append(child)
@@ -42,3 +42,27 @@ class ChildInterface:
         expression = html_util.wrap_expression_by_script_tag(
             expression=expression)
         expression_file_util.append_expression(expression=expression)
+
+    def remove_child(self, child: DisplayObject) -> None:
+        """
+        Remove display object child from this instance.
+
+        Parameters
+        ----------
+        child : DisplayObject
+            Child instance to remove.
+
+        Raises
+        ------
+        ValueError
+            If specified child not found in this instance's child list.
+        """
+        for child_ in self._childs:
+            if child_ != child:
+                continue
+            self._childs.remove(child)
+            return
+        raise ValueError(
+            'Specified child not found in this instance\'s child list.'
+            f'\nChild type: {type(child)}'
+            f'\nChild variable name: {child.variable_name}')
--- tests/display/test_child_interface.py
+++ tests/display/test_child_interface.py
@@ -2,10 +2,10 @@ from random import randint
 
 from retrying import retry
 
-from apyscript.display.child_interface import ChildInterface
 from apyscript.display.sprite import Sprite
 from apyscript.display.stage import Stage
 from apyscript.expression import expression_file_util
+from tests import testing_helper
 
 
 class TestChildInterface:
@@ -32,3 +32,16 @@ class TestChildInterface:
         )
         expression: str = expression_file_util.get_current_expression()
         assert expected in expression
+
+    @retry(stop_max_attempt_number=5, wait_fixed=randint(100, 1000))
+    def test_remove_child(self) -> None:
+        stage: Stage = Stage()
+        sprite: Sprite = Sprite(stage=stage)
+        stage.add_child(child=sprite)
+        stage.remove_child(child=sprite)
+        assert stage._childs == []
+
+        testing_helper.assert_raises(
+            expected_error_class=ValueError,
+            func_or_method=stage.remove_child,
+            kwargs={'child': sprite})

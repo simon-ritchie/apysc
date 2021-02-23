dd6ab9a239667894edb4ea435ebd7b5e515d69b7
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-23 20:22:24 +0900

    #11 Add module for `trace` (console.log expression) interface implementations.

---
+++ apyscript/logging/trace.py
@@ -0,0 +1,32 @@
+"""`trace` (console.log expression) interface implementations
+"""
+
+from typing import List
+
+from apyscript.display.variable_name_interface import VariableNameInterface
+from apyscript.expression import expression_file_util
+from apyscript.html import html_util
+
+
+def trace(*args) -> None:
+    """
+    Display arguments information to console. This function will
+    save js `console.log` expression.
+
+    Parameters
+    ----------
+    *args : list
+        Any arguments to display to console.
+    """
+    expression: str = 'console.log('
+    arg_strs: List[str] = []
+    for arg in args:
+        if isinstance(arg, VariableNameInterface):
+            arg_strs.append(f'{arg.variable_name}')
+            continue
+        arg_strs.append(f'"{str(arg)}"')
+    expression += ', '.join(arg_strs)
+    expression += ');'
+    expression = html_util.wrap_expression_by_script_tag(
+        expression=expression)
+    expression_file_util.append_expression(expression=expression)
---
+++ tests/logger/test_trace.py
@@ -0,0 +1,18 @@
+from random import randint
+
+from retrying import retry
+
+from apyscript.logging import trace
+from apyscript.display.stage import Stage
+from apyscript.expression import expression_file_util
+
+
+@retry(stop_max_attempt_number=5, wait_fixed=randint(100, 1000))
+def test_trace() -> None:
+    stage: Stage = Stage()
+    trace.trace(stage, 100, 'Hello!')
+    expression: str = expression_file_util.get_current_expression()
+    expected: str = (
+        f'console.log({stage.variable_name}, "100", "Hello!");'
+    )
+    assert expected in expression

a9c63297221a0da597c3b7433223f6dd29ffa7bb
simon-ritchie <antisocial.sid2@gmail.com>
2021-02-28 16:29:46 +0900

    #19 Add module for js assertion interface.

---
+++ apyscript/console/assertion.py
@@ -0,0 +1,41 @@
+"""Each js assertion (console.assert) interface implementations.
+"""
+
+from typing import Any
+
+from apyscript.expression import expression_file_util
+from apyscript.console.trace import trace
+from apyscript.type import value_util
+
+
+def assert_equal(expected: Any, actual: Any, msg: str = '') -> None:
+    """
+    Assertion interface for equal condition.
+
+    Parameters
+    ----------
+    expected : *
+        Expected value.
+    actual : *
+        Actual value.
+    msg : str
+        Message to display when assertion failed.
+    """
+    from apyscript.type.variable_name_interface import VariableNameInterface
+    info: str = ''
+    if isinstance(expected, VariableNameInterface):
+        info += f'Expected variable name: {expected.variable_name}'
+    if isinstance(actual, VariableNameInterface):
+        if info != '':
+            info += f'\nActual variable name: {actual.variable_name}'
+    trace(info, '\nExpected:', expected, 'actual:', actual)
+
+    expected_str: str = value_util.get_value_str_for_expression(
+        value=expected)
+    actual_str: str = value_util.get_value_str_for_expression(value=actual)
+
+    expression: str = (
+        f'console.assert({expected_str} === {actual_str}, "{msg}");'
+    )
+    expression_file_util.wrap_by_script_tag_and_append_expression(
+        expression=expression)
---
+++ tests/console/test_assertion.py
@@ -0,0 +1,32 @@
+from random import randint
+
+from retrying import retry
+
+from apyscript.console import assertion
+from apyscript.expression import expression_file_util
+from apyscript.type.int import Int
+
+
+@retry(stop_max_attempt_number=5, wait_fixed=randint(100, 1000))
+def test_assert_equal() -> None:
+    expression_file_util.remove_expression_file()
+    int_1: Int = Int(10)
+    int_2: Int = Int(20)
+    assertion.assert_equal(
+        expected=int_1, actual=int_2,
+        msg='Invalid int values.')
+
+    expression: str = expression_file_util.get_current_expression()
+    expected: str = (
+        f'Expected variable name: {int_1.variable_name}'
+    )
+    assert expected in expression
+    expected = (
+        f'Actual variable name: {int_2.variable_name}'
+    )
+    assert expected in expression
+    expected = (
+        f'console.assert({int_1.variable_name} === {int_2.variable_name}, '
+        '"Invalid int values.");'
+    )
+    assert expected in expression

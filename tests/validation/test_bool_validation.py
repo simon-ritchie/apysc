f6c32e453ca0fe41e5a4462c2b48f98d82f9fbb2
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-02 21:47:42 +0900

    #20 Add module for boolean value's validation implementations.

---
+++ tests/validation/test_bool_validation.py
@@ -0,0 +1,10 @@
+from apyscript.validation import bool_validation
+from tests import testing_helper
+
+
+def test_validate_bool() -> None:
+    bool_validation.validate_bool(value=True)
+    testing_helper.assert_raises(
+        expected_error_class=ValueError,
+        func_or_method=bool_validation.validate_bool,
+        kwargs={'value': 1})

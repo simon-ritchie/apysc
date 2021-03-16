c96a8555a7e1781ccd4fdb5b3ee295c92a7c9169
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-16 20:33:47 +0900

    #34 Add test project for assert_defined interface.

---
+++ test_projects/assert_defined/main.py
@@ -0,0 +1,43 @@
+"""Test project for `assert_defined` and `assert_undefined` interfaces.
+
+Command examples:
+$ python test_projects/assert_defined/main.py
+$ python assert_defined/main.py
+"""
+
+import sys
+
+sys.path.append('./')
+
+import os
+from types import ModuleType
+
+from apyscript.console.assertion import assert_defined
+from apyscript.display.stage import Stage
+from apyscript.file import file_util
+from apyscript.html import exporter
+from apyscript.type import Array, Int
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module),
+    'test_output/'
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    _: Stage = Stage(background_color='#333')
+
+    int_1: Int = Int(3)
+    assert_defined(actual=int_1)
+
+    exporter.save_expressions_overall_html(
+        dest_dir_path=_DEST_DIR_PATH)
+
+
+if __name__ == '__main__':
+    main()

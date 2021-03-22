70ee9828aec1bf8fad3d2530b23a35438674121d
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-22 20:52:47 +0900

    #40 Adjust GraphicsClearInterface branch instruction to include BeginFillInterface and LineStyleInterface.

--- apyscript/display/graphics_clear_interface.py
+++ apyscript/display/graphics_clear_interface.py
@@ -26,18 +26,26 @@ class GraphicsClearInterface:
         from apyscript.display.line_color_interface import LineColorInterface
         from apyscript.display.line_thickness_interface import \
             LineThicknessInterface
-        if isinstance(self, FillColorInterface):
+        from apyscript.display.line_alpha_interface import LineAlphaInterface
+        from apyscript.display.child_interface import ChildInterface
+        from apyscript.display.begin_fill_interface import BeginFillInterface
+        from apyscript.display.line_style_interface import LineStyleInterface
+        if isinstance(self, (FillColorInterface, BeginFillInterface)):
             self._initialize_fill_color_if_not_initialized()
         self._fill_color.value = ''
-        if isinstance(self, FillAlphaInterface):
+        if isinstance(self, (FillAlphaInterface, BeginFillInterface)):
             self._initialize_fill_alpha_if_not_initialized()
         self._fill_alpha.value = 1.0
-        if isinstance(self, LineColorInterface):
+        if isinstance(self, (LineColorInterface, LineStyleInterface)):
             self._initialize_line_color_if_not_initialized()
         self._line_color.value = ''
-        if isinstance(self, LineThicknessInterface):
+        if isinstance(self, (LineThicknessInterface, LineStyleInterface)):
             self._initialize_line_thickness_if_not_initialized()
         self._line_thickness.value = 1
+        if isinstance(self, (LineAlphaInterface, LineStyleInterface)):
+            self._initialize_line_alpha_if_not_initialized()
         self._line_alpha.value = 1.0
+        if isinstance(self, ChildInterface):
+            self._initialize_childs_if_not_initialized()
         while self._childs:
             self._childs[0].remove_from_parent()

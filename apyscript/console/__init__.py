8de20334ade67e37a19c001650d4466e9a8a84ab
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-23 21:42:47 +0900

    #44 Shorten assert_true import.

--- apyscript/console/__init__.py
+++ apyscript/console/__init__.py
@@ -1,3 +1,4 @@
 from apyscript.console._trace import trace  # isort:skip # noqa
 from apyscript.console.assertion import assert_equal  # isort:skip # noqa
 from apyscript.console.assertion import assert_not_equal # isort:skip # noqa
+from apyscript.console.assertion import assert_true # isort:skip # noqa
--- test_projects/Boolean/main.py
+++ test_projects/Boolean/main.py
@@ -13,7 +13,7 @@ import os
 from types import ModuleType
 
 from apyscript.console.assertion import assert_false
-from apyscript.console.assertion import assert_true
+from apyscript.console import assert_true
 from apyscript.display import Stage
 from apyscript.file import file_util
 from apyscript.html import exporter

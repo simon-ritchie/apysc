81e8efe81cd39c9e1b39b0dee98ccfa7997bb4f2
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-23 21:04:14 +0900

    #39 Add module that defines constants of expression's variable names.

---
+++ apyscript/expression/var_names.py
@@ -0,0 +1,15 @@
+"""A module that defines constants of expression's variable names.
+"""
+
+from typing_extensions import Final
+
+DISPLAY_OBJECT: Final[str] = 'display_object'
+GRAPHICS: Final[str] = 'graphics'
+RECTANGLE: Final[str] = 'rectangle'
+SPRITE: Final[str] = 'sprite'
+ARRAY: Final[str] = 'array'
+INDEX: Final[str] = 'index'
+BOOLEAN: Final[str] = 'boolean'
+INT: Final[str] = 'int'
+NUMBER: Final[str] = 'number'
+STRING: Final[str] = 'string'
---
+++ tests/expression/test_var_names.py
@@ -0,0 +1,19 @@
+import inspect
+from typing import Any, List, Tuple
+
+from apyscript.expression import var_names
+
+
+def test_const_values_not_duplicated() -> None:
+    members: List[Tuple[str, Any]] = inspect.getmembers(var_names)
+    constants: List[str] = []
+    for member_name, member_val in members:
+        if not isinstance(member_val, str):
+            continue
+        if not member_name.isupper():
+            continue
+        constants.append(member_val)
+
+    original_len: int = len(constants)
+    unique_len: int = len(set(constants))
+    assert original_len == unique_len

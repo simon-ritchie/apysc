645bd2a8046da7f8da1ace20824f40cfc9db814c
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-24 12:01:41 +0900

    #44 Apply each lints.

--- apyscript/console/__init__.py
+++ apyscript/console/__init__.py
@@ -1,9 +1,9 @@
 from apyscript.console._trace import trace  # isort:skip # noqa
 from apyscript.console.assertion import assert_equal  # isort:skip # noqa
-from apyscript.console.assertion import assert_not_equal # isort:skip # noqa
-from apyscript.console.assertion import assert_true # isort:skip # noqa
-from apyscript.console.assertion import assert_false # isort:skip # noqa
-from apyscript.console.assertion import assert_arrays_equal # isort:skip # noqa
-from apyscript.console.assertion import assert_arrays_not_equal # isort:skip # noqa
-from apyscript.console.assertion import assert_defined # isort:skip # noqa
-from apyscript.console.assertion import assert_undefined # isort:skip # noqa
+from apyscript.console.assertion import assert_not_equal  # isort:skip # noqa
+from apyscript.console.assertion import assert_true  # isort:skip # noqa
+from apyscript.console.assertion import assert_false  # isort:skip # noqa
+from apyscript.console.assertion import assert_arrays_equal  # isort:skip # noqa
+from apyscript.console.assertion import assert_arrays_not_equal  # isort:skip # noqa
+from apyscript.console.assertion import assert_defined  # isort:skip # noqa
+from apyscript.console.assertion import assert_undefined  # isort:skip # noqa

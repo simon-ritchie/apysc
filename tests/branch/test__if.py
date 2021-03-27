539d0770e6fe90d51d4a5293d6580498b1c6ce22
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-27 20:42:16 +0900

    #46 Add __enter__ method test.

--- tests/branch/test__if.py
+++ tests/branch/test__if.py
@@ -20,3 +20,15 @@ class TestIf:
                 '_globals': globals_,
             },
             any_obj=if_)
+
+    def test___enter__(self) -> None:
+        int_1: Int = Int(10)
+        int_2: Int = Int(20)
+        locals_: Dict[str, Any] = {'value1': int_1}
+        globals_: Dict[str, Any] = {'value2': int_2}
+        boolean_1: Boolean = Boolean(True)
+        with If(condition=boolean_1, locals_=locals_, globals_=globals_):
+            pass
+        snapshot_name: str = list(int_1._value_snapshots.keys())[0]
+        assert int_1._value_snapshots[snapshot_name] == 10
+        assert int_2._value_snapshots[snapshot_name] == 20

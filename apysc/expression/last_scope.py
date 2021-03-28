c02e4facee3db1660ad6da6faaa9ae50a6d8ad18
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-28 19:03:48 +0900

    #48 Add getter and setter function interface to last_scope module.

--- apysc/expression/last_scope.py
+++ apysc/expression/last_scope.py
@@ -1,6 +1,7 @@
 """Interfaces and definitions for last expression's scope information.
 """
 
+import os
 from enum import Enum
 
 
@@ -19,3 +20,40 @@ def reset() -> None:
     from apysc.expression.expression_file_util import LAST_SCOPE_FILE_PATH
     from apysc.file import file_util
     file_util.remove_file_if_exists(file_path=LAST_SCOPE_FILE_PATH)
+
+
+def get_last_scope() -> LastScope:
+    """
+    Get last scope value.
+
+    Returns
+    -------
+    last_scope : LastScope
+        Last scope value. If there is no last scope's value, then
+        LastScope.NORMAL will be returned.
+    """
+    from apysc.expression.expression_file_util import LAST_SCOPE_FILE_PATH
+    from apysc.file import file_util
+    if not os.path.isfile(LAST_SCOPE_FILE_PATH):
+        return LastScope.NORMAL
+    last_scope_str: str = file_util.read_txt(file_path=LAST_SCOPE_FILE_PATH)
+    if last_scope_str == '':
+        return LastScope.NORMAL
+    last_scope: LastScope = LastScope(int(last_scope_str))
+    return last_scope
+
+
+def set_last_scope(value: LastScope) -> None:
+    """
+    Set last scope value.
+
+    Parameters
+    ----------
+    value : LastScope
+        Last scope value to set.
+    """
+    from apysc.expression.expression_file_util import LAST_SCOPE_FILE_PATH
+    from apysc.file import file_util
+    file_util.save_plain_txt(
+        txt=str(value.value),
+        file_path=LAST_SCOPE_FILE_PATH)
--- tests/expression/test_last_scope.py
+++ tests/expression/test_last_scope.py
@@ -4,6 +4,7 @@ import os
 from retrying import retry
 
 from apysc.expression import last_scope
+from apysc.expression.last_scope import LastScope
 from apysc.file import file_util
 from apysc.expression.expression_file_util import LAST_SCOPE_FILE_PATH
 
@@ -14,3 +15,27 @@ def test_reset() -> None:
         txt='', file_path=LAST_SCOPE_FILE_PATH)
     last_scope.reset()
     assert not os.path.exists(LAST_SCOPE_FILE_PATH)
+
+
+@retry(stop_max_attempt_number=10, wait_fixed=randint(100, 1000))
+def test_get_last_scope() -> None:
+    last_scope.reset()
+    last_scope_: LastScope = last_scope.get_last_scope()
+    assert last_scope_ == last_scope.LastScope.NORMAL
+
+    file_util.save_plain_txt(
+        txt='', file_path=LAST_SCOPE_FILE_PATH)
+    last_scope_ = last_scope.get_last_scope()
+    assert last_scope_ == last_scope.LastScope.NORMAL
+
+    last_scope.set_last_scope(value=last_scope.LastScope.IF)
+    last_scope_ = last_scope.get_last_scope()
+    assert last_scope_ == last_scope.LastScope.IF
+
+
+@retry(stop_max_attempt_number=10, wait_fixed=randint(100, 1000))
+def test_set_last_scope() -> None:
+    last_scope.reset()
+    last_scope.set_last_scope(value=last_scope.LastScope.IF)
+    last_scope_: LastScope = last_scope.get_last_scope()
+    assert last_scope_ == last_scope.LastScope.IF

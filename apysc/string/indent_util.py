8fd7bd4776af183b16dabac66959259d8b49b8dc
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-28 12:56:09 +0900

    #46 Add function to append spaces to a expression string.

--- apysc/string/indent_util.py
+++ apysc/string/indent_util.py
@@ -1,5 +1,14 @@
 """Common indentation related utility implementations.
+
+Mainly following interfaces are defined:
+
+- make_spaces_for_html: Make spaces that multiplied 2 to
+    specified indentation number.
+- append_spaces_to_expression: Append spaces to a expression string.
 """
+
+
+from typing import List
 
 
 def make_spaces_for_html(indent_num: int) -> str:
@@ -21,3 +30,29 @@ def make_spaces_for_html(indent_num: int) -> str:
     number_validation.validate_num_is_gte_zero(num=indent_num)
     spaces: str = ' ' * (indent_num * 2)
     return spaces
+
+
+def append_spaces_to_expression(expression: str, indent_num: int) -> str:
+    """
+    Append spaces to a expression string.
+
+    Parameters
+    ----------
+    expression : str
+        Expression string to add spaces to.
+    indent_num : int
+        Indentation number. If 1 is specified, then spaces will be 2.
+
+    Returns
+    -------
+    expression : str
+        Expression string after adding spaces.
+    """
+    if indent_num == 0:
+        return expression
+    lines: List[str] = expression.splitlines()
+    spaces: str = make_spaces_for_html(indent_num=indent_num)
+    for i, line in enumerate(lines):
+        lines[i] = f'{spaces}{line}'
+    expression = '\n'.join(lines)
+    return expression
--- tests/string/test_indent_util.py
+++ tests/string/test_indent_util.py
@@ -8,3 +8,21 @@ def test_make_spaces_for_html() -> None:
     assert spaces == '  '
     spaces = indent_util.make_spaces_for_html(indent_num=2)
     assert spaces == '    '
+
+
+def test_append_spaces_to_expression() -> None:
+    expression: str = (
+        'console.log("Hello!");'
+        '\nconsole.log("World!");'
+    )
+    result_expression: str = indent_util.append_spaces_to_expression(
+        expression=expression, indent_num=0)
+    assert result_expression == expression
+
+    result_expression = indent_util.append_spaces_to_expression(
+        expression=expression, indent_num=2)
+    expected: str = (
+        '    console.log("Hello!");'
+        '\n    console.log("World!");'
+    )
+    assert result_expression == expected

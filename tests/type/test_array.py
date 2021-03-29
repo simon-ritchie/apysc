f5e2134e313f7994ef8b7496355efe0c8b3b2f5b
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-29 21:33:44 +0900

    #50 Add type test pattern to Array's __eq__ method.

--- tests/type/test_array.py
+++ tests/type/test_array.py
@@ -582,6 +582,9 @@ class TestArray:
         array_4: Array = Array([1, 2, 3])
         assert array_1 != array_4
 
+        result = array_4 == 10
+        assert isinstance(result, Boolean)
+
     @retry(stop_max_attempt_number=10, wait_fixed=randint(100, 1000))
     def test___bool__(self) -> None:
         array_1: Array = Array([])

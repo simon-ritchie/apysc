e01a59e76e1236ed54b864a62a278be0b8f9b3a0
simon-ritchie <antisocial.sid2@gmail.com>
2021-03-31 21:03:28 +0900

    #52 Add __gt__ method expression to NumberValueInterface class.

--- apysc/type/number_value_interface.py
+++ apysc/type/number_value_interface.py
@@ -672,8 +672,33 @@ class NumberValueInterface(CopyInterface, RevertInterface):
         """
         from apysc.type import Boolean
         if isinstance(other, NumberValueInterface):
-            return Boolean(self.value > other.value)
-        return Boolean(self.value > other)
+            result: Boolean = Boolean(self.value > other.value)
+        else:
+            result = Boolean(self.value > other)
+        if isinstance(other, NumberValueInterface):
+            self._append_gt_expression(result=result, other=other)
+        return result
+
+    def _append_gt_expression(
+            self, result: VariableNameInterface,
+            other: VariableNameInterface) -> None:
+        """
+        Append __gt__ expression to file.
+
+        Parameters
+        ----------
+        result : Boolean
+            Result boolean value.
+        other : VariableNameInterface
+            Other value to compare.
+        """
+        from apysc.expression import expression_file_util
+        expression: str = (
+            f'{result.variable_name} = '
+            f'{self.variable_name} > {other.variable_name};'
+        )
+        expression_file_util.wrap_by_script_tag_and_append_expression(
+            expression=expression)
 
     def __ge__(self, other: Any) -> Any:
         """

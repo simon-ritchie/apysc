5b627337800f8f62dbf84a47a06c00d914c9c5fe
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-04 14:20:26 +0900

    #22 Add unit test for `append_js_expression` function.

--- tests/expression/test_expression_file_util.py
+++ tests/expression/test_expression_file_util.py
@@ -1,5 +1,7 @@
 import os
 from random import randint
+import re
+from typing import Match, Optional
 
 from retrying import retry
 
@@ -54,3 +56,28 @@ def test_wrap_by_script_tag_and_append_expression() -> None:
     expression: str = expression_file_util.get_current_expression()
     expected: str = 'var num = 100;'
     assert expected in expression
+    assert html_const.SCRIPT_START_TAG in expression
+    assert html_const.SCRIPT_END_TAG in expression
+
+
+@retry(stop_max_attempt_number=10, wait_fixed=randint(100, 1000))
+def test_append_js_expression() -> None:
+    indent_num.reset()
+    expression_file_util.remove_expression_file()
+    expression_file_util.append_js_expression(expression='var num = 100;')
+    expression: str = expression_file_util.get_current_expression()
+    match: Optional[Match] = re.search(
+        pattern=r'^var num = 100;',
+        string=expression,
+        flags=re.MULTILINE)
+    assert match is not None
+
+    expression_file_util.remove_expression_file()
+    indent_num.increment()
+    expression_file_util.append_js_expression(
+        expression='var num_1 = 100;\nvar num_2 = 200;\nvar num_3 = 300;')
+    expression = expression_file_util.get_current_expression()
+    match: Optional[Match] = re.search(
+        pattern=r'^  var num_2 = 200;',
+        string=expression,
+        flags=re.MULTILINE)

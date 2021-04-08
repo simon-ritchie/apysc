e286ccd517c3472cc40df526b4d12278ec359743
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-08 20:39:41 +0900

    #59 Add test for _append_constructor_expression method.

--- tests/type/test_any_value.py
+++ tests/type/test_any_value.py
@@ -2,7 +2,7 @@ from random import randint
 
 from retrying import retry
 
-from apysc import AnyValue
+from apysc import AnyValue, Int
 from apysc.expression import expression_file_util
 from apysc.expression import var_names
 
@@ -14,3 +14,21 @@ class TestAnyValue:
         any_value: AnyValue = AnyValue(100)
         assert any_value._value == 100
         assert any_value.variable_name.startswith(var_names.ANY)
+
+    @retry(stop_max_attempt_number=10, wait_fixed=randint(100, 1000))
+    def test__append_constructor_expression(self) -> None:
+        expression_file_util.remove_expression_file()
+        any_value_1: AnyValue = AnyValue(100)
+        expression: str = expression_file_util.get_current_expression()
+        expected: str = (
+            f'{any_value_1.variable_name} = 100;'
+        )
+        assert expected in expression
+
+        int_1: Int = Int(10)
+        any_value_2: AnyValue = AnyValue(int_1)
+        expression = expression_file_util.get_current_expression()
+        expected = (
+            f'{any_value_2.variable_name} = {int_1.variable_name};'
+        )
+        assert expected in expression

17df5c9e8f937076ec3eed438fec970423d1318f
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-11 20:40:31 +0900

    #60 Add js test project for ClickInterface class.

---
+++ test_projects/ClickInterface/main.py
@@ -0,0 +1,69 @@
+"""Test project for ClickInterface class.
+
+Command examples:
+$ python test_projects/ClickInterface/main.py
+$ python ClickInterface/main.py
+"""
+
+import sys
+from typing import Any, Dict
+
+sys.path.append('./')
+
+import os
+from types import ModuleType
+
+from apysc import Int, String
+from apysc import Number
+from apysc import Rectangle
+from apysc import Sprite
+from apysc import Stage
+from apysc import String
+from apysc import assert_not_equal, assert_equal
+from apysc.file import file_util
+from apysc.html import exporter
+from apysc import Event, trace
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module),
+    'test_output/'
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    stage: Stage = Stage(
+        background_color='#111',
+        stage_width=1000, stage_height=500)
+
+    sprite_1: Sprite = Sprite(stage=stage)
+    sprite_1.graphics.begin_fill(color='#0af')
+    sprite_1.graphics.draw_rect(x=50, y=50, width=50, height=50)
+    msg: String = String('Hello!')
+    sprite_1.click(on_sprite_1_clicked, kwargs={'msg': msg})
+
+    exporter.save_expressions_overall_html(
+        dest_dir_path=_DEST_DIR_PATH)
+
+
+def on_sprite_1_clicked(e: Event, kwargs: Dict[str, Any]) -> None:
+    """
+    Test handler that called when sprite_1 is clicked.
+
+    Parameters
+    ----------
+    e : Event
+        Event object.
+    kwargs : dict
+        Specified keyword arguments.
+    """
+    trace('Sprite 1 is clicked!')
+    assert_equal(expected='Hello!', actual=kwargs['msg'])
+
+
+if __name__ == '__main__':
+    main()

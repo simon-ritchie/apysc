096a387ae3f41d17afa072b8eeaa395acde6258f
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-15 21:02:43 +0900

    #67 Add validation for event type.

--- apysc/validation/event_validation.py
+++ apysc/validation/event_validation.py
@@ -8,12 +8,12 @@ Mainly following interfaces are defined.
 
 from typing import Any
 
-from apysc import Event
+from apysc import Event, EventType
 
 
 def validate_event(e: Any) -> Event:
     """
-    Validate specified instance is Event.
+    Validate whether specified instance is Event or not.
 
     Parameters
     ----------
@@ -30,8 +30,22 @@ def validate_event(e: Any) -> Event:
     e : Event
         Event instance.
     """
-    from apysc import Event
     if isinstance(e, Event):
         return e
     raise ValueError(
         f'Specified instance is not Event type: {type(e)}')
+
+
+def validate_event_type(event_type: Any) -> EventType:
+    """
+    Validate whether specified value is EventType one or not.
+
+    Parameters
+    ----------
+    event_type : EventType
+        EventType value to check.
+    """
+    if isinstance(event_type, EventType):
+        return event_type
+    raise ValueError(
+        f'Specified value is not EventType: {type(event_type)}')
--- tests/validation/test_event_validation.py
+++ tests/validation/test_event_validation.py
@@ -1,4 +1,4 @@
-from apysc import Event
+from apysc import Event, EventType
 from apysc import Int
 from apysc.validation import event_validation
 from tests import testing_helper
@@ -14,3 +14,14 @@ def test_validate_event() -> None:
     e: Event = Event(this=int_1)
     e = event_validation.validate_event(e=e)
     assert isinstance(e, Event)
+
+
+def test_validate_event_type() -> None:
+    testing_helper.assert_raises(
+        expected_error_class=ValueError,
+        func_or_method=event_validation.validate_event_type,
+        kwargs={'event_type': 100})
+
+    event_type: EventType = event_validation.validate_event_type(
+        event_type=EventType.CLICK)
+    assert isinstance(event_type, EventType)

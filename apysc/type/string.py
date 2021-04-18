d5f50c967a9f56bd25110368e408e5ffaa9fff75
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-18 18:10:42 +0900

    #71 Change String __le__ method expression to accept not VariableNameInterface value.

--- apysc/type/string.py
+++ apysc/type/string.py
@@ -442,6 +442,7 @@ class String(CopyInterface, RevertInterface):
         string_validation.validate_string_type(string=other)
         value: str = self._get_str_value(value=other)
         result: Boolean = Boolean(self._value <= value)
+        other = self._convert_other_val_to_string(other=other)
         if isinstance(other, VariableNameInterface):
             self._append_le_expression(result=result, other=other)
         return result

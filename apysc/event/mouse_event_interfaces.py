99672f97f02afa40a79db05a1d982c0663d63109
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-24 20:48:18 +0900

    #79 Add MouseOverInterface inheritance to MouseEventInterfaces class.

--- apysc/event/mouse_event_interfaces.py
+++ apysc/event/mouse_event_interfaces.py
@@ -5,10 +5,11 @@ from apysc.event.click_interface import ClickInterface
 from apysc.event.double_click_interface import DoubleClickInterface
 from apysc.event.mouse_down_interface import MouseDownInterface
 from apysc.event.mouse_up_interface import MouseUpInterface
+from apysc.event.mouse_over_interface import MouseOverInterface
 
 
 class MouseEventInterfaces(
         ClickInterface, DoubleClickInterface, MouseDownInterface,
-        MouseUpInterface):
+        MouseUpInterface, MouseOverInterface):
     """Class implementation for inheritance of each mouse event interfaces.
     """

e8ff41ea1cfd7278922dda9aff6aa1c248acd681
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-24 20:56:19 +0900

    #79 Add js test project for MouseOverInterface class.

---
+++ test_projects/MouseOverInterface/main.py
@@ -0,0 +1,65 @@
+"""Test project for MouseOverInterface class.
+
+Command examples:
+$ python test_projects/MouseOverInterface/main.py
+$ python MouseOverInterface/main.py
+"""
+
+import sys
+from typing import Any
+from typing import Dict
+
+sys.path.append('./')
+
+import os
+from types import ModuleType
+
+from apysc import MouseEvent, Sprite
+from apysc import Stage
+from apysc import assert_equal
+from apysc import trace
+from apysc.file import file_util
+from apysc.html import exporter
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module),
+    'test_output/'
+)
+
+
+def main() -> None:
+    """Entry point of this test project.
+    """
+    stage: Stage = Stage(
+        background_color='#111',
+        stage_width=1000, stage_height=500)
+    sprite_1: Sprite = Sprite(stage=stage)
+    sprite_1.graphics.begin_fill(color='#0af')
+    sprite_1.graphics.draw_rect(
+        x=50, y=50, width=50, height=50)
+    sprite_1.mouseover(
+        handler=on_sprite_1_mouse_over, options={'msg': 'Hello!'})
+
+    exporter.save_expressions_overall_html(
+        dest_dir_path=_DEST_DIR_PATH)
+
+
+def on_sprite_1_mouse_over(e: MouseEvent, options: Dict[str, Any]) -> None:
+    """
+    Test handler that called when sprite 1 is mouse overed.
+
+    Parameters
+    ----------
+    e : MouseEvent
+        Event object.
+    options : dict
+        Optional arguments dictionary.
+    """
+    trace('Sprite 1 is mouse overed!')
+    assert_equal(expected='Hello!', actual=options['msg'])
+
+
+if __name__ == '__main__':
+    main()

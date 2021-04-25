10228d066232a21f77988c0d361fc0a9c945ba41
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-25 16:24:48 +0900

    #84 Add js unbinding test pattern to MouseOverInterface class.

--- test_projects/MouseOverInterface/main.py
+++ test_projects/MouseOverInterface/main.py
@@ -14,7 +14,7 @@ sys.path.append('./')
 import os
 from types import ModuleType
 
-from apysc import MouseEvent
+from apysc import MouseEvent, Rectangle
 from apysc import Sprite
 from apysc import Stage
 from apysc import assert_equal
@@ -38,18 +38,29 @@ def main() -> None:
         stage_width=1000, stage_height=500)
     sprite_1: Sprite = Sprite(stage=stage)
     sprite_1.graphics.begin_fill(color='#0af')
-    sprite_1.graphics.draw_rect(
+
+    rectangle_1: Rectangle = sprite_1.graphics.draw_rect(
         x=50, y=50, width=50, height=50)
-    sprite_1.mouseover(
-        handler=on_sprite_1_mouse_over, options={'msg': 'Hello!'})
+    rectangle_1.mouseover(
+        handler=on_rectangle_1_mouse_over, options={'msg': 'Hello!'})
+
+    rectangle_2: Rectangle = sprite_1.graphics.draw_rect(
+        x=150, y=50, width=50, height=50)
+    rectangle_2.mouseover(handler=on_rectangle_2_mouse_over)
+    rectangle_2.unbind_mouseover(handler=on_rectangle_2_mouse_over)
+
+    rectangle_3: Rectangle = sprite_1.graphics.draw_rect(
+        x=250, y=50, width=50, height=50)
+    rectangle_3.mouseover(handler=on_rectangle_3_mouse_over)
+    rectangle_3.unbind_mouseover_all()
 
     exporter.save_expressions_overall_html(
         dest_dir_path=_DEST_DIR_PATH)
 
 
-def on_sprite_1_mouse_over(e: MouseEvent, options: Dict[str, Any]) -> None:
+def on_rectangle_1_mouse_over(e: MouseEvent, options: Dict[str, Any]) -> None:
     """
-    Test handler that called when sprite 1 is mouse overed.
+    Test handler that called when rectangle 1 is mouse overed.
 
     Parameters
     ----------
@@ -58,8 +69,40 @@ def on_sprite_1_mouse_over(e: MouseEvent, options: Dict[str, Any]) -> None:
     options : dict
         Optional arguments dictionary.
     """
-    trace('Sprite 1 is mouse overed!')
+    trace('Rectangle 1 is mouse overed!')
     assert_equal(expected='Hello!', actual=options['msg'])
+
+
+def on_rectangle_2_mouse_over(e: MouseEvent, options: Dict[str, Any]) -> None:
+    """
+    Test handler that called when rectangle 2 is mouse overed.
+
+    Parameters
+    ----------
+    e : MouseEvent
+        Event object.
+    options : dict
+        Optional arguments dictionary.
+    """
+    trace(
+        'Rectangle 2 is mouse overed. Probably unbind_mouseover '
+        'interface not working correctly.')
+
+
+def on_rectangle_3_mouse_over(e: MouseEvent, options: Dict[str, Any]) -> None:
+    """
+    Test handler that called when rectangle 3 is mouse overed.
+
+    Parameters
+    ----------
+    e : MouseEvent
+        Event object.
+    options : dict
+        Optional arguments dictionary.
+    """
+    trace(
+        'Rectangle 3 is mouse overed. Probably unbind_mouseover_all '
+        'interface not working correctly.')
 
 
 if __name__ == '__main__':

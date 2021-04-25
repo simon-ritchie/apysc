fd7de3edb0fb55f86f14efb8490a71f7d5d37f18
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-25 15:33:21 +0900

    #84 Add unbinding js testing pattern to DoubleClickInterface class.

--- test_projects/DoubleClickInterface/main.py
+++ test_projects/DoubleClickInterface/main.py
@@ -38,17 +38,22 @@ def main() -> None:
     stage: Stage = Stage(
         background_color='#111',
         stage_width=1000, stage_height=500)
-    stage.dblclick(
-        handler=on_stage_dblclick,
-        options={'msg': String('Hello!')})
-
     sprite_1: Sprite = Sprite(stage=stage)
-    sprite_1.dblclick(handler=on_sprite_1_dblclick)
 
     sprite_1.graphics.begin_fill(color='#0af')
     rectangle_1: Rectangle = sprite_1.graphics.draw_rect(
         x=50, y=50, width=50, height=50)
     rectangle_1.dblclick(handler=on_rectangle_1_dblclick)
+
+    rectangle_2: Rectangle = sprite_1.graphics.draw_rect(
+        x=150, y=50, width=50, height=50)
+    rectangle_2.dblclick(handler=on_rectangle_2_dblclick)
+    rectangle_2.unbind_dblclick(handler=on_rectangle_2_dblclick)
+
+    rectangle_3: Rectangle = sprite_1.graphics.draw_rect(
+        x=250, y=50, width=50, height=50)
+    rectangle_3.dblclick(handler=on_rectangle_3_dblclick)
+    rectangle_3.unbind_dblclick_all()
 
     exporter.save_expressions_overall_html(
         dest_dir_path=_DEST_DIR_PATH)
@@ -95,6 +100,38 @@ def on_rectangle_1_dblclick(e: MouseEvent, options: Dict[str, Any]) -> None:
         Optional arguments dictionary.
     """
     trace('Rectangle 1 is double clicked!')
+
+
+def on_rectangle_2_dblclick(e: MouseEvent, options: Dict[str, Any]) -> None:
+    """
+    Test handler that called when rectangle 2 is double clicked.
+
+    Parameters
+    ----------
+    e : MouseEvent
+        Event object.
+    options : dict
+        Optional arguments dictionary.
+    """
+    trace(
+        'Rectangle 2 is double clicked. Probably unbind_dblclick '
+        'interface not working correctly.')
+
+
+def on_rectangle_3_dblclick(e: MouseEvent, options: Dict[str, Any]) -> None:
+    """
+    Test handler that called when rectangle 3 is double clicked.
+
+    Parameters
+    ----------
+    e : MouseEvent
+        Event object.
+    options : dict
+        Optional arguments dictionary.
+    """
+    trace(
+        'Rectangle 3 is double clicked. Probably unbind_dblclick_all '
+        'interface not working correctly.')
 
 
 if __name__ == '__main__':

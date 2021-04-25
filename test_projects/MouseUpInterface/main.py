aa7f2737e847afc956da8488919c0a6e3418d406
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-25 16:38:31 +0900

    #84 Add unbinding js test pattern for MouseUpInterface class.

--- test_projects/MouseUpInterface/main.py
+++ test_projects/MouseUpInterface/main.py
@@ -14,7 +14,7 @@ sys.path.append('./')
 import os
 from types import ModuleType
 
-from apysc import MouseEvent
+from apysc import MouseEvent, Sprite, Rectangle
 from apysc import Stage
 from apysc import assert_equal
 from apysc import trace
@@ -35,15 +35,30 @@ def main() -> None:
     stage: Stage = Stage(
         background_color='#111',
         stage_width=1000, stage_height=500)
-    stage.mouseup(handler=on_stage_mouse_up, options={'msg': 'Hello!'})
+    sprite_1: Sprite = Sprite(stage=stage)
+    sprite_1.graphics.begin_fill(color='#0af')
+
+    rectangle_1: Rectangle = sprite_1.graphics.draw_rect(
+        x=50, y=50, width=50, height=50)
+    rectangle_1.mouseup(on_rectangle_1_mouse_up, options={'msg': 'Hello!'})
+
+    rectangle_2: Rectangle = sprite_1.graphics.draw_rect(
+        x=150, y=50, width=50, height=50)
+    rectangle_2.mouseup(on_rectangle_2_mouse_up)
+    rectangle_2.unbind_mouseup(handler=on_rectangle_2_mouse_up)
+
+    rectangle_3: Rectangle = sprite_1.graphics.draw_rect(
+        x=250, y=50, width=50, height=50)
+    rectangle_3.mouseup(on_rectangle_3_mouse_up)
+    rectangle_3.unbind_mouseup_all()
 
     exporter.save_expressions_overall_html(
         dest_dir_path=_DEST_DIR_PATH)
 
 
-def on_stage_mouse_up(e: MouseEvent, options: Dict[str, Any]) -> None:
+def on_rectangle_1_mouse_up(e: MouseEvent, options: Dict[str, Any]) -> None:
     """
-    Test handler that called when stage is mouse upped.
+    Test handler that called when rectangle 1 is mouse upped.
 
     Parameters
     ----------
@@ -52,8 +67,40 @@ def on_stage_mouse_up(e: MouseEvent, options: Dict[str, Any]) -> None:
     options : dict
         Optional arguments dictionary.
     """
-    trace('Stage is mouse upped!')
+    trace('Rectangle 1 is mouse upped!')
     assert_equal(expected='Hello!', actual=options['msg'])
+
+
+def on_rectangle_2_mouse_up(e: MouseEvent, options: Dict[str, Any]) -> None:
+    """
+    Test handler that called when rectangle 2 is mouse upped.
+
+    Parameters
+    ----------
+    e : MouseEvent
+        Event object.
+    options : dict
+        Optional arguments dictionary.
+    """
+    trace(
+        'Rectangle 2 is mouse upped. Probably unbind_mouseup '
+        'interface not working correctly.')
+
+
+def on_rectangle_3_mouse_up(e: MouseEvent, options: Dict[str, Any]) -> None:
+    """
+    Test handler that called when rectangle 3 is mouse upped.
+
+    Parameters
+    ----------
+    e : MouseEvent
+        Event object.
+    options : dict
+        Optional arguments dictionary.
+    """
+    trace(
+        'Rectangle 3 is mouse upped. Probably unbind_mouseup_all '
+        'interface not working correctly.')
 
 
 if __name__ == '__main__':

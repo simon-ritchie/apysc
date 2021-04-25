8be9ffe10f80fff9da7ba8a978f75fb3cb213a98
simon-ritchie <antisocial.sid2@gmail.com>
2021-04-25 13:43:30 +0900

    #80 Add MouseOutInterface inheritance to bundle class.

--- apysc/event/mouse_event_interfaces.py
+++ apysc/event/mouse_event_interfaces.py
@@ -6,10 +6,11 @@ from apysc.event.double_click_interface import DoubleClickInterface
 from apysc.event.mouse_down_interface import MouseDownInterface
 from apysc.event.mouse_over_interface import MouseOverInterface
 from apysc.event.mouse_up_interface import MouseUpInterface
+from apysc.event.mouse_out_interface import MouseOutInterface
 
 
 class MouseEventInterfaces(
         ClickInterface, DoubleClickInterface, MouseDownInterface,
-        MouseUpInterface, MouseOverInterface):
+        MouseUpInterface, MouseOverInterface, MouseOutInterface):
     """Class implementation for inheritance of each mouse event interfaces.
     """

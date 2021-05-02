3e3afd79adbc9fb334839c62786fdfcdb9052cd8
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-02 11:26:23 +0900

    #91 Add logger to lint script.

--- run_lints_for_deploy.py
+++ run_lints_for_deploy.py
@@ -4,11 +4,17 @@ Command example:
 $ python apply_lints.py
 """
 
+from logging import Logger
+
+from apysc.console import loggers
+
+logger: Logger = loggers.get_info_logger()
+
 
 def _main() -> None:
     """Entry point of this command.
     """
-    print('Command executed.')
+    logger.info("Lint script started.")
 
 
 if __name__ == '__main__':

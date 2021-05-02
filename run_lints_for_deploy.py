5e82e42d7ed45ed8df2677b42e6f43abb0ec8639
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-02 11:05:06 +0900

    #91 Add script for lints command.

---
+++ run_lints_for_deploy.py
@@ -0,0 +1,15 @@
+"""Lint script for deploying (GitHub Actions).
+
+Command example:
+$ python apply_lints.py
+"""
+
+
+def _main() -> None:
+    """Entry point of this command.
+    """
+    print('Command executed.')
+
+
+if __name__ == '__main__':
+    _main()

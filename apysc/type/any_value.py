46fd2d695ee8558e855a7dd9e2d4eadbc0c937c8
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-03 21:29:19 +0900

    #97 Add dunder method for less than equal comparison.

--- apysc/type/any_value.py
+++ apysc/type/any_value.py
@@ -397,3 +397,22 @@ class AnyValue(CopyInterface):
         result: Boolean = self._append_comparison_expression(
             comparison_operator='<', other=other)
         return result
+
+    def __le__(self, other: Any) -> Boolean:
+        """
+        Less than equal comparison method.
+
+        Parameters
+        ----------
+        other : Any
+            Other value to compare.
+
+        Returns
+        -------
+        result : Boolean
+            Comparison result. This will always be False on Python
+            since correct comparison is not possible.
+        """
+        result: Boolean = self._append_comparison_expression(
+            comparison_operator='<=', other=other)
+        return result

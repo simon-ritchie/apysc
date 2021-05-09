8e2d61a75fd31ae72777b930b8e03a31e2098422
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-09 16:23:12 +0900

    #111 Add dot_size getter method.

--- apysc/display/line_dot_setting.py
+++ apysc/display/line_dot_setting.py
@@ -22,5 +22,19 @@ class LineDotSetting:
         from apysc.validation import number_validation
         number_validation.validate_integer(integer=dot_size)
         if isinstance(dot_size, int):
-            dot_size = Int(dot_size)
-        self._dot_size = dot_size
+            dot_size_: Int = Int(dot_size)
+        else:
+            dot_size_ = dot_size._copy()
+        self._dot_size = dot_size_
+
+    @property
+    def dot_size(self) -> Int:
+        """
+        Get a dot size setting.
+
+        Returns
+        -------
+        dot_size : Int
+            Dot size setting.
+        """
+        return self._dot_size
--- tests/display/test_line_dot_setting.py
+++ tests/display/test_line_dot_setting.py
@@ -13,3 +13,12 @@ class TestLineDotSetting:
         setting: LineDotSetting = LineDotSetting(dot_size=5)
         assert isinstance(setting._dot_size, Int)
         assert setting._dot_size == 5
+
+        setting = LineDotSetting(dot_size=Int(10))
+        assert setting._dot_size == 10
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_dot_size(self) -> None:
+        setting: LineDotSetting = LineDotSetting(dot_size=5)
+        assert setting.dot_size == 5
+        assert isinstance(setting.dot_size, Int)

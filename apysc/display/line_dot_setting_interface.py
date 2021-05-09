47ad8a6380b9c0df04becb0e1ca7a11710a468ed
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-09 20:32:34 +0900

    #111 Add method to update line dot setting and skip appending expression to file.

--- apysc/display/line_dot_setting_interface.py
+++ apysc/display/line_dot_setting_interface.py
@@ -36,6 +36,23 @@ class LineDotSettingInterface(VariableNameInterface, RevertInterface):
         self._initialize_line_dot_setting_if_not_initialized()
         return self._line_dot_setting
 
+    def _update_line_dot_setting_and_skip_appending_exp(
+            self, value: Optional[LineDotSetting]) -> None:
+        """
+        Update line dot setting and skip appending expression to file.
+
+        Parameters
+        ----------
+        value : LineDotSetting or None
+            Line dot setting to set.
+        """
+        if value is not None and not isinstance(value, LineDotSetting):
+            raise TypeError(
+                'Not supported line_dot_setting type specified: '
+                f'{type(value)}'
+                '\nAcceptable ones are: LineDotSetting or None.')
+        self._line_dot_setting = value
+
     def _append_line_dot_setting_update_expression(self) -> None:
         """
         Append line dot setting updating expression to file.
--- tests/display/test_line_dot_setting_interface.py
+++ tests/display/test_line_dot_setting_interface.py
@@ -7,6 +7,7 @@ from retrying import retry
 from apysc.display.line_dot_setting_interface import LineDotSettingInterface
 from apysc.expression import expression_file_util, var_names
 from apysc import LineDotSetting
+from tests.testing_helper import assert_raises
 
 
 class TestLineDotSettingInterface:
@@ -56,3 +57,27 @@ class TestLineDotSettingInterface:
             ),
             string=expression, flags=re.MULTILINE)
         assert match is not None
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__update_line_dot_setting_and_skip_appending_exp(self) -> None:
+        expression_file_util.remove_expression_file()
+        interface: LineDotSettingInterface = LineDotSettingInterface()
+        interface._update_line_dot_setting_and_skip_appending_exp(
+            value=None)
+        line_dot_setting: Optional[LineDotSetting] = \
+            interface.line_dot_setting
+        assert line_dot_setting is None
+        expression: str = expression_file_util.get_current_expression()
+        assert '.css' not in expression
+
+        interface._update_line_dot_setting_and_skip_appending_exp(
+            value=LineDotSetting(dot_size=10))
+        line_dot_setting = interface.line_dot_setting
+        assert line_dot_setting.dot_size == 10
+
+        assert_raises(
+            expected_error_class=TypeError,
+            func_or_method=interface.
+            _update_line_dot_setting_and_skip_appending_exp,
+            kwargs={'value': 10},
+            match='Not supported line_dot_setting type specified: ')

f5ea4325a3c89a14f1d6e18695c7a8711c16107f
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-23 20:32:55 +0900

    #120 Add test for _replace_static_path_recursively function.

---
+++ tests/test_build_docs.py
@@ -0,0 +1,33 @@
+import os, shutil
+from typing import List
+
+import build_docs
+
+
+def test__replace_static_path_recursively() -> None:
+    tmp_dir_1: str = '../.tmp_test_build_docs/'
+    shutil.rmtree(tmp_dir_1, ignore_errors=True)
+    tmp_dir_2: str = os.path.join(tmp_dir_1, 'subdir/')
+    os.makedirs(tmp_dir_2, exist_ok=True)
+
+    html_path: str = os.path.join(tmp_dir_1, 'test.html')
+    with open(html_path, 'w') as f:
+        f.write(
+            '<link rel="stylesheet" type="text/css" '
+            'href="_static/groundwork.css" />')
+    js_path: str = os.path.join(tmp_dir_2, 'test.js')
+    with open(js_path, 'w') as f:
+        f.write('""_static/groundwork.css""')
+    pkl_path: str = os.path.join(tmp_dir_1, 'test.pkl')
+    with open(pkl_path, 'w') as f:
+        f.write('')
+
+    build_docs._replace_static_path_recursively(dir_path=tmp_dir_1)
+    target_file_paths: List[str] = [html_path, js_path]
+    for target_file_path in target_file_paths:
+        with open(target_file_path) as f:
+            file_txt: str = f.read()
+        assert '_static' not in file_txt
+        assert 'static' in file_txt
+
+    shutil.rmtree(tmp_dir_1, ignore_errors=True)

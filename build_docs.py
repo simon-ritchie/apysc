a9fc8da8c21dcca1d2850a4133bac5acbc422037
simon-ritchie <antisocial.sid2@gmail.com>
2021-05-23 14:51:54 +0900

    #120 Add documentation build script.

---
+++ build_docs.py
@@ -0,0 +1,37 @@
+"""Documentations build script.
+
+Command example:
+$ python build_docs.py
+"""
+
+from logging import Logger
+import subprocess as sp
+import shutil
+import os
+
+from apysc.console import loggers
+
+logger: Logger = loggers.get_info_logger()
+
+
+def _main() -> None:
+    """Entry point of this command.
+    """
+    logger.info(msg='Sphix build command started...')
+    os.chdir('./docs_src/')
+    complete_process: sp.CompletedProcess = sp.run(
+        'make html', shell=True,
+        stdout=sp.PIPE, stderr=sp.STDOUT)
+    stdout: str = complete_process.stdout.decode('utf-8')
+    print(stdout)
+
+    logger.info(msg='Moving documentation to docs directory...')
+    os.chdir('../')
+    shutil.rmtree('./docs/', ignore_errors=True)
+    shutil.copytree(src='./docs_src/build/html/', dst='./docs/')
+
+    logger.info(msg='Build completed!')
+
+
+if __name__ == '__main__':
+    _main()

8c5603a3b8133119fa2c7dc9646c24017a019997
simon-ritchie <antisocial.sid2@gmail.com>
2021-06-05 10:23:07 +0900

    #105 Add radius getter interface.

--- tests/display/test_radius_interface.py
+++ tests/display/test_radius_interface.py
@@ -16,3 +16,11 @@ class TestRadiusInterface:
         interface._radius.value = 10
         interface._initialize_radius_if_not_initialized()
         assert interface._radius == 10
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_radius(self) -> None:
+        interface: RadiusInterface = RadiusInterface()
+        assert interface.radius == 0
+
+        interface._radius.value = 10
+        assert interface.radius == 10

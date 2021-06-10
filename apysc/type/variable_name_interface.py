62c80c9a26ab6eed510a84831c927d7c37764182
simon-ritchie <antisocial.sid2@gmail.com>
2021-06-10 21:47:45 +0900

    #157 Add the VariableNameInterface method to get a previous variable name.

--- apysc/type/variable_name_interface.py
+++ apysc/type/variable_name_interface.py
@@ -37,3 +37,19 @@ class VariableNameInterface:
             self._variable_name_history = []
         self._variable_name = variable_name
         self._variable_name_history.append(variable_name)
+
+    def _get_previous_variable_name(self) -> str:
+        """
+        Get a previous variable name.
+
+        Returns
+        -------
+        previous_variable_name : str
+            A previous variable name of this instance. If that value
+            is not existing, then a blank string will be returned.
+        """
+        if not hasattr(self, '_variable_name_history'):
+            return ''
+        if len(self._variable_name_history) <= 1:
+            return ''
+        return self._variable_name_history[-2]
--- tests/type/test_variable_name_interface.py
+++ tests/type/test_variable_name_interface.py
@@ -1,8 +1,13 @@
+from random import randint
+
+from retrying import retry
+
 from apysc.type.variable_name_interface import VariableNameInterface
 
 
 class TestVariableNameInterface:
 
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
     def test_variable_name(self) -> None:
         interface: VariableNameInterface = VariableNameInterface()
         interface.variable_name = 'test_interface_1'
@@ -13,3 +18,17 @@ class TestVariableNameInterface:
         assert interface.variable_name == 'test_interface_2'
         assert interface._variable_name_history == [
             'test_interface_1', 'test_interface_2']
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__get_previous_variable_name(self) -> None:
+        interface: VariableNameInterface = VariableNameInterface()
+        previous_variable_name: str = interface._get_previous_variable_name()
+        assert previous_variable_name == ''
+
+        interface.variable_name = 'test_interface_1'
+        previous_variable_name = interface._get_previous_variable_name()
+        assert previous_variable_name == ''
+
+        interface.variable_name = 'test_interface_2'
+        previous_variable_name = interface._get_previous_variable_name()
+        assert previous_variable_name == 'test_interface_1'

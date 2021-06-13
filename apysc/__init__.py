e73ab6c4cbb689ebf703ac30ac12fde3b686b615
simon-ritchie <antisocial.sid2@gmail.com>
2021-06-13 10:29:31 +0900

    #160 Increment the version number to 0.14.3.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -49,4 +49,4 @@ from apysc.event.document_mouse_wheel_interface import \
 
 from apysc.html.exporter import save_overall_html  # isort:skip # noqa
 
-__version__: str = '0.14.2'
+__version__: str = '0.14.3'

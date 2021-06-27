34ffbb66b15f4784c0c254ff6da17271bcb61dac
simon-ritchie <antisocial.sid2@gmail.com>
2021-06-27 13:13:04 +0900

    #200 Split the Jupyter and Colaboratory interfaces.

--- apysc/_jupyter/jupyter_util.py
+++ apysc/_jupyter/jupyter_util.py
@@ -1,11 +1,46 @@
 """
 The module that is implemented each Jupyter interface and definition.
+
+Mainly the following interfaces are defined:
+
+- display_on_jupyter
+    Save the overall HTML and display it on the Jupyter.
+- display_on_colaboratory
+    Save the overall HTML and display it on the Google Colaboratory.
 """
 
 import os
 
+from apysc._display.stage import Stage
+
+
+def _save_overall_html(
+        html_file_name: str, dest_dir_path: str,
+        minify: bool) -> None:
+    """
+    Save the overall HTML file.
+
+    Parameters
+    ----------
+    html_file_name : str, default 'index.html'
+        The output HTML file name.
+    dest_dir_path : str
+        Destination directory path to save HTML.
+    minify : bool, default True
+        Boolean value whether minify a HTML or not.
+    """
+    from apysc import save_overall_html
+    save_overall_html(
+        dest_dir_path=dest_dir_path,
+        html_file_name=html_file_name,
+        minify=minify,
+        skip_js_lib_exporting=True,
+        embed_js_libs=True,
+        verbose=0)
+
 
 def display_on_jupyter(
+        stage: Stage,
         html_file_name: str,
         dest_dir_path: str = './',
         minify: bool = True) -> None:
@@ -14,6 +49,8 @@ def display_on_jupyter(
 
     Parameters
     ----------
+    stage : Stage
+        Target stage instance.
     html_file_name : str, default 'index.html'
         The output HTML file name.
     dest_dir_path : str
@@ -26,18 +63,41 @@ def display_on_jupyter(
     -----
     This interface requires the Jupyter library (e.g., `notebook` package).
     """
+    from IPython.display import IFrame
+    from IPython.display import display
+
+    _save_overall_html(
+        html_file_name=html_file_name, dest_dir_path=dest_dir_path,
+        minify=minify)
+    display(
+        IFrame(src=f'./{html_file_name}',
+               width=stage._width._value, height=stage._height._value))
+
+
+def display_on_colaboratory(
+        html_file_name: str,
+        dest_dir_path: str = './',
+        minify: bool = True) -> None:
+    """
+    Save the overall HTML and display it on the Google Colaboratory.
+
+    Parameters
+    ----------
+    html_file_name : str, default 'index.html'
+        The output HTML file name.
+    dest_dir_path : str
+        Destination directory path to save HTML.
+    minify : bool, default True
+        Boolean value whether minify a HTML or not.
+        False setting is useful when debugging.
+    """
     from IPython.display import HTML
     from IPython.display import display
 
-    from apysc import save_overall_html
     from apysc._file import file_util
-    save_overall_html(
-        dest_dir_path=dest_dir_path,
-        html_file_name=html_file_name,
-        minify=minify,
-        skip_js_lib_exporting=True,
-        embed_js_libs=True,
-        verbose=0)
+    _save_overall_html(
+        html_file_name=html_file_name, dest_dir_path=dest_dir_path,
+        minify=minify)
     file_path: str = os.path.join(dest_dir_path, html_file_name)
     html_str: str = file_util.read_txt(file_path=file_path)
     display(HTML(html_str))
--- tests/_jupyter/test_jupyter_util.py
+++ tests/_jupyter/test_jupyter_util.py
@@ -10,13 +10,46 @@ from apysc._jupyter import jupyter_util
 
 @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
 def test_display_on_jupyter() -> None:
-    tmp_dest_dir_path: str = '../tmp_test_jupyter_util/'
+    tmp_dest_dir_path: str = '../tmp_test_jupyter_util_1/'
     shutil.rmtree(tmp_dest_dir_path, ignore_errors=True)
-    _: Stage = Stage()
+    stage: Stage = Stage()
 
     jupyter_util.display_on_jupyter(
+        stage=stage,
         html_file_name='test_file.html',
         dest_dir_path=tmp_dest_dir_path)
+    expected_file_path: str = os.path.join(
+        tmp_dest_dir_path, 'test_file.html')
+    assert os.path.isfile(expected_file_path)
+
+    shutil.rmtree(tmp_dest_dir_path, ignore_errors=True)
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__save_overall_html() -> None:
+    tmp_dest_dir_path: str = '../tmp_test_jupyter_util_2/'
+    shutil.rmtree(tmp_dest_dir_path, ignore_errors=True)
+
+    _: Stage = Stage()
+    jupyter_util._save_overall_html(
+        html_file_name='test_file.html',
+        dest_dir_path=tmp_dest_dir_path,
+        minify=True)
+    expected_file_path: str = os.path.join(
+        tmp_dest_dir_path, 'test_file.html')
+    assert os.path.isfile(expected_file_path)
+
+    shutil.rmtree(tmp_dest_dir_path, ignore_errors=True)
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_display_on_colaboratory() -> None:
+    tmp_dest_dir_path: str = '../tmp_test_jupyter_util_3/'
+    shutil.rmtree(tmp_dest_dir_path, ignore_errors=True)
+
+    _: Stage = Stage()
+    jupyter_util.display_on_colaboratory(
+        html_file_name='test_file.html', dest_dir_path=tmp_dest_dir_path)
     expected_file_path: str = os.path.join(
         tmp_dest_dir_path, 'test_file.html')
     assert os.path.isfile(expected_file_path)

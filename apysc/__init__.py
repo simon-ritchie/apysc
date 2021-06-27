5f7641367a41d86b4b60ef5997cd61ea487b632b
simon-ritchie <antisocial.sid2@gmail.com>
2021-06-27 20:15:23 +0900

    #204 Increment the minor version number to 0.25.0.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -50,4 +50,4 @@ from apysc._expression.expression_file_util import append_js_expression  # isort
 from apysc._jupyter.jupyter_util import display_on_jupyter  # isort:skip # noqa
 from apysc._jupyter.jupyter_util import display_on_colaboratory  # isort:skip # noqa
 
-__version__: str = '0.24.4'
+__version__: str = '0.25.0'

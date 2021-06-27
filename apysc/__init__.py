5724053cf863f129e605e072d09b690891c92b0c
simon-ritchie <antisocial.sid2@gmail.com>
2021-06-27 11:16:03 +0900

    #180 Increment the version number to 0.21.15.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -49,4 +49,4 @@ from apysc._html.exporter import save_overall_html  # isort:skip # noqa
 from apysc._expression.expression_file_util import append_js_expression  # isort:skip # noqa
 from apysc._jupyter.jupyter_util import display_on_jupyter  # isort:skip # noqa
 
-__version__: str = '0.21.14'
+__version__: str = '0.21.15'

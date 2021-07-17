84b573abdc8313386aada89670732fe6c74e0180
simon-ritchie <antisocial.sid2@gmail.com>
2021-07-17 20:45:10 +0900

    #246 Add the helper interface to get a copied String value from a Python built-in str.

--- apysc/_converter/to_apysc_val_from_builtin.py
+++ apysc/_converter/to_apysc_val_from_builtin.py
@@ -1,4 +1,4 @@
-"""Each interfaces to get apysc values from Python built-in ones.
+"""Each interface to get apysc values from Python built-in ones.
 """
 
 from typing import Union
@@ -9,7 +9,7 @@ import apysc as ap
 def get_copied_int_from_builtin_val(
         integer: Union[int, ap.Int]) -> ap.Int:
     """
-    Get a copied Int value from Python built-in int.
+    Get a copied Int value from a Python built-in int.
 
     Parameters
     ----------
@@ -25,4 +25,26 @@ def get_copied_int_from_builtin_val(
         copied: ap.Int = ap.Int(integer)
     else:
         copied = integer._copy()
+    return copied
+
+
+def get_copied_string_from_builtin_val(
+        string: Union[str, ap.String]) -> ap.String:
+    """
+    Get a copied String value from a Python built-in str.
+
+    Parameters
+    ----------
+    string : str or String
+        Target string value.
+
+    Returns
+    -------
+    copied : String
+        Copied String value.
+    """
+    if isinstance(string, str):
+        copied: ap.String = ap.String(string)
+    else:
+        copied = string._copy()
     return copied
--- tests/_converter/test_to_apysc_val_from_builtin.py
+++ tests/_converter/test_to_apysc_val_from_builtin.py
@@ -19,3 +19,18 @@ def test_get_copied_int_from_builtin_val() -> None:
     assert copied == 20
     assert isinstance(copied, ap.Int)
     assert int_1.variable_name != copied.variable_name
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_get_copied_string_from_builtin_val() -> None:
+    copied: ap.String = to_apysc_val_from_builtin.\
+        get_copied_string_from_builtin_val(string='Hello')
+    assert copied == 'Hello'
+    assert isinstance(copied, ap.String)
+
+    string: ap.String = ap.String('World')
+    copied = to_apysc_val_from_builtin.get_copied_string_from_builtin_val(
+        string=string)
+    assert copied == 'World'
+    assert isinstance(copied, ap.String)
+    assert string.variable_name != copied.variable_name

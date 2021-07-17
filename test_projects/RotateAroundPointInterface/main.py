64c160b709498e76790e42f112ddfe14abb6604a
simon-ritchie <antisocial.sid2@gmail.com>
2021-07-17 13:38:17 +0900

    #245 Add the js test pattern for the case of using the `Sprite` container.

--- test_projects/RotateAroundPointInterface/main.py
+++ test_projects/RotateAroundPointInterface/main.py
@@ -33,25 +33,36 @@ def main() -> None:
         background_color='#333',
         stage_width=1000, stage_height=500)
 
-    sprite: ap.Sprite = ap.Sprite(stage=stage)
-    sprite.graphics.begin_fill(color='#0af', alpha=0.5)
-    rectangle_1: ap.Rectangle = sprite.graphics.draw_rect(
+    sprite_1: ap.Sprite = ap.Sprite(stage=stage)
+    sprite_1.graphics.begin_fill(color='#0af', alpha=0.5)
+    rectangle_1: ap.Rectangle = sprite_1.graphics.draw_rect(
         x=50, y=50, width=50, height=50)
 
-    rectangle_2: ap.Rectangle = sprite.graphics.draw_rect(
+    rectangle_2: ap.Rectangle = sprite_1.graphics.draw_rect(
         x=50, y=50, width=50, height=50)
 
-    timer: ap.Timer = ap.Timer(
-        handler=on_timer, delay=ap.FPS.FPS_60,
+    timer_1: ap.Timer = ap.Timer(
+        handler=on_timer_1, delay=ap.FPS.FPS_60,
         options={'rectangle_1': rectangle_1, 'rectangle_2': rectangle_2})
-    timer.start()
+    timer_1.start()
 
-    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH)
+    sprite_2: ap.Sprite = ap.Sprite(stage=stage)
+    sprite_2.graphics.begin_fill(color='#f0a', alpha=0.5)
+    sprite_2.x = ap.Int(50)
+    sprite_2.y = ap.Int(50)
+    rectangle_3: ap.Rectangle = sprite_2.graphics.draw_rect(
+        x=50, y=50, width=50, height=50)
+    timer_2: ap.Timer = ap.Timer(
+        handler=on_timer_2, delay=ap.FPS.FPS_60,
+        options={'rectangle_3': rectangle_3})
+    timer_2.start()
+
+    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH, minify=False)
 
 
-def on_timer(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+def on_timer_1(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
     """
-    The handler would be called from the timer.
+    The handler would be called from the first timer.
 
     Parameters
     ----------
@@ -65,6 +76,21 @@ def on_timer(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
 
     rectangle_2: ap.Rectangle = options['rectangle_2']
     rectangle_2.rotate_around_point(additional_rotation=1, x=100, y=100)
+
+
+def on_timer_2(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler would be called from the second timer.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    rectangle_3: ap.Rectangle = options['rectangle_3']
+    rectangle_3.rotate_around_point(additional_rotation=1, x=50, y=50)
 
 
 if __name__ == '__main__':

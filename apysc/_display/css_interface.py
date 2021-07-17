2788127266f33f8f73ab5e5eaef28fd253f1473f
simon-ritchie <antisocial.sid2@gmail.com>
2021-07-17 21:16:54 +0900

    #246 Add the method to append a css getter expression string to the file.

--- apysc/_display/css_interface.py
+++ apysc/_display/css_interface.py
@@ -40,10 +40,33 @@ class CssInterface(VariableNameInterface, RevertInterface):
         else:
             name_ = name
         if name_ in self._css:
-            css: ap.String = self._css[name_]
+            css: ap.String = self._css[name_]._copy()
         else:
             css = ap.String('')
+        self._append_get_css_expresion(name=name, css=css)
         return css
+
+    def _append_get_css_expresion(
+            self, name: Union[str, ap.String], css: ap.String) -> None:
+        """
+        Append a css getter expression string to the file.
+
+        Parameters
+        ----------
+        name : str or String
+            CSS name (e.g., 'display').
+        css : String
+            CSS value.
+        """
+        from apysc._type import value_util
+        name_value_str: str = value_util.get_value_str_for_expression(
+            value=name)
+        css_value_str: str = value_util.get_value_str_for_expression(
+            value=css)
+        expression: str = (
+            f'{css_value_str} = {self.variable_name}.css({name_value_str});'
+        )
+        ap.append_js_expression(expression=expression)
 
     def _make_snapshot(self, snapshot_name: str) -> None:
         pass
--- tests/_display/test_css_interface.py
+++ tests/_display/test_css_interface.py
@@ -4,6 +4,15 @@ from retrying import retry
 
 import apysc as ap
 from apysc._display.css_interface import CssInterface
+from apysc._expression import expression_file_util
+
+
+class _TestInterface(CssInterface):
+
+    def __init__(self) -> None:
+        """The class for testing.
+        """
+        self.variable_name = 'test_css_interface'
 
 
 class TestCssInterface:
@@ -27,3 +36,15 @@ class TestCssInterface:
         interface._css['display'] = ap.String('none')
         css = interface.get_css(name='display')
         assert css == 'none'
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__append_get_css_expresion(self) -> None:
+        expression_file_util.empty_expression_dir()
+        interface: _TestInterface = _TestInterface()
+        name: ap.String = ap.String('display')
+        css: ap.String = interface.get_css(name=name)
+        expression: str = expression_file_util.get_current_expression()
+        expected: str = (
+            f'{css.variable_name} = {interface}.css({name.variable_name});'
+        )
+        assert expected in expression

65bb808c7354043c2bf30325d7355f72b5c22e7c
simon-ritchie <antisocial.sid2@gmail.com>
2021-07-17 20:55:56 +0900

    #246 Add the method to get a CSS value string.

--- apysc/_display/css_interface.py
+++ apysc/_display/css_interface.py
@@ -1,7 +1,7 @@
 """Class implementation for the css interface.
 """
 
-from typing import Dict, Union
+from typing import Any, Dict, Union
 
 import apysc as ap
 from apysc._type.revert_interface import RevertInterface
@@ -10,7 +10,7 @@ from apysc._type.variable_name_interface import VariableNameInterface
 
 class CssInterface(VariableNameInterface, RevertInterface):
 
-    _css: ap.Dictionary
+    _css: Dict[str, ap.String]
 
     def _initialize_css_if_not_initialized(self) -> None:
         """
@@ -18,7 +18,32 @@ class CssInterface(VariableNameInterface, RevertInterface):
         """
         if hasattr(self, '_css'):
             return
-        self._css = ap.Dictionary({})
+        self._css = {}
+
+    def get_css(self, name: Union[str, ap.String]) -> ap.String:
+        """
+        Get a CSS value string.
+
+        Parameters
+        ----------
+        name : str or String
+            CSS name (e.g., 'display').
+
+        Returns
+        -------
+        css : ap.String
+            CSS value.
+        """
+        self._initialize_css_if_not_initialized()
+        if isinstance(name, ap.String):
+            name_: str = name._value
+        else:
+            name_ = name
+        if name_ in self._css:
+            css: ap.String = self._css[name_]
+        else:
+            css = ap.String('')
+        return css
 
     def _make_snapshot(self, snapshot_name: str) -> None:
         pass
--- tests/_display/test_css_interface.py
+++ tests/_display/test_css_interface.py
@@ -1,13 +1,29 @@
+from random import randint
+
+from retrying import retry
+
+import apysc as ap
 from apysc._display.css_interface import CssInterface
 
 
 class TestCssInterface:
 
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
     def test__initialize_css_if_not_initialized(self) -> None:
         interface: CssInterface = CssInterface()
         interface._initialize_css_if_not_initialized()
-        assert interface._css._value == {}
+        assert interface._css == {}
 
-        interface._css['display'] = 'none'
+        interface._css['display'] = ap.String('none')
         interface._initialize_css_if_not_initialized()
-        assert interface._css._value == {'display': 'none'}
+        assert interface._css == {'display': 'none'}
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_get_css(self) -> None:
+        interface: CssInterface = CssInterface()
+        css: ap.String = interface.get_css(name='display')
+        assert css == ''
+
+        interface._css['display'] = ap.String('none')
+        css = interface.get_css(name='display')
+        assert css == 'none'

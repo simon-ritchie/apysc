211d6f5dd973239391c9018169fcebadaa2cf816
simon-ritchie <antisocial.sid2@gmail.com>
2021-07-23 21:30:25 +0900

    #274 Add the method to get a boolean value whether the current debug mode is enabled or not.

--- apysc/_html/debug_mode.py
+++ apysc/_html/debug_mode.py
@@ -2,6 +2,8 @@
 and JavaScript.
 """
 
+import os
+
 
 def set_debug_mode() -> None:
     """
@@ -14,3 +16,23 @@ def set_debug_mode() -> None:
     file_path: str = expression_file_util.DEBUG_MODE_SETTING_FILE_PATH
     with open(file_path, 'w') as f:
         f.write('1')
+
+
+def is_debug_mode() -> bool:
+    """
+    Get a boolean value whether the current debug mode is enabled or not.
+
+    Returns
+    -------
+    result : bool
+        If the current debug mode is enabled, True will be returned.
+    """
+    from apysc._expression import expression_file_util
+    file_path: str = expression_file_util.DEBUG_MODE_SETTING_FILE_PATH
+    if not os.path.isfile(file_path):
+        return False
+    with open(file_path) as f:
+        txt: str = f.read()
+    if txt == '1':
+        return True
+    return False
--- tests/_html/test_debug_mode.py
+++ tests/_html/test_debug_mode.py
@@ -4,6 +4,7 @@ from retrying import retry
 
 import apysc as ap
 from apysc._expression import expression_file_util
+from apysc._file import file_util
 
 
 @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
@@ -13,3 +14,20 @@ def test_set_debug_mode() -> None:
     with open(expression_file_util.DEBUG_MODE_SETTING_FILE_PATH) as f:
         txt: str = f.read()
     assert txt == '1'
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_is_debug_mode() -> None:
+    expression_file_util.empty_expression_dir()
+    result: bool = ap.is_debug_mode()
+    assert not result
+
+    file_util.append_plain_txt(
+        txt='',
+        file_path=expression_file_util.DEBUG_MODE_SETTING_FILE_PATH)
+    result = ap.is_debug_mode()
+    assert not result
+
+    ap.set_debug_mode()
+    result = ap.is_debug_mode()
+    assert result

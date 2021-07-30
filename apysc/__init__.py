8ea6cfcb98fc314de883b213e4490bc3ce5653cb
simon-ritchie <antisocial.sid2@gmail.com>
2021-07-30 14:36:26 +0900

    #279 Increment the version number to 0.32.2.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -57,4 +57,4 @@ from apysc._html.debug_mode import set_debug_mode  # isort:skip # noqa
 from apysc._html.debug_mode import is_debug_mode  # isort:skip # noqa
 from apysc._html.debug_mode import DebugInfo  # isort:skip # noqa
 
-__version__: str = '0.32.1'
+__version__: str = '0.32.2'

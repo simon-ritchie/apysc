daa7c1559a9a80a4fd9a7577f479426e31aa418e
simon-ritchie <antisocial.sid2@gmail.com>
2021-08-07 19:43:29 +0900

    #304 Add the `_rotate_around_center` property initializing method.

--- apysc/_display/rotate_around_center_interface.py
+++ apysc/_display/rotate_around_center_interface.py
@@ -4,45 +4,77 @@
 from typing import Union
 
 import apysc as ap
+from apysc._type.revert_interface import RevertInterface
 from apysc._type.variable_name_interface import VariableNameInterface
 
 
-class RotateAroundCenterInterface(VariableNameInterface):
+class RotateAroundCenterInterface(VariableNameInterface, RevertInterface):
 
-    def rotate_around_center(
-            self, additional_rotation: Union[int, ap.Int]) -> None:
+    _rotate_around_center: ap.Int
+
+    def _initialize_rotate_around_center_if_not_initialized(self) -> None:
         """
-        Add a rotation value around the center of this instance.
+        Initialize the `_rotate_around_center` attribute if if hasn't
+        been initialized yet.
+        """
+        if hasattr(self, '_rotate_around_center'):
+            return
+        self._rotate_around_center = ap.Int(0)
 
-        Notes
-        -----
-        - This interface value is a relative value, not a absolute value,
-            so if you call this value multiple times, total rotation will
-            be cumulative.
-        - This interface is not supported by the container instance, such
-            as the `Sprite` class due to the HTML (SVG) specification.
+    # def rotate_around_center(
+    #         self, additional_rotation: Union[int, ap.Int]) -> None:
+    #     """
+    #     Add a rotation value around the center of this instance.
+
+    #     Notes
+    #     -----
+    #     - This interface value is a relative value, not a absolute value,
+    #         so if you call this value multiple times, total rotation will
+    #         be cumulative.
+    #     - This interface is not supported by the container instance, such
+    #         as the `Sprite` class due to the HTML (SVG) specification.
+
+    #     Parameters
+    #     ----------
+    #     additional_rotation : int or Int
+    #         A value to add.
+
+    #     References
+    #     ----------
+    #     - GraphicsBase rotate_around_center interface document
+    #         - https://bit.ly/3hP6d12
+    #     """
+    #     import apysc as ap
+    #     with ap.DebugInfo(
+    #             callable_=self.rotate_around_center, locals_=locals(),
+    #             module_name=__name__, class_=RotateAroundCenterInterface):
+    #         from apysc._type import value_util
+    #         from apysc._validation import number_validation
+    #         number_validation.validate_integer(
+    #             integer=additional_rotation)
+    #         value_str: str = value_util.get_value_str_for_expression(
+    #             value=additional_rotation)
+    #         expression: str = (
+    #             f'{self.variable_name}.rotate({value_str});'
+    #         )
+    #         ap.append_js_expression(expression=expression)
+
+    def _make_snapshot(self, snapshot_name: str) -> None:
+        """
+        Make a value's snapshot.
 
         Parameters
         ----------
-        additional_rotation : int or Int
-            A value to add.
-
-        References
-        ----------
-        - GraphicsBase rotate_around_center interface document
-            - https://bit.ly/3hP6d12
+        snapshot_name : str
+            Target snapshot name.
         """
-        import apysc as ap
-        with ap.DebugInfo(
-                callable_=self.rotate_around_center, locals_=locals(),
-                module_name=__name__, class_=RotateAroundCenterInterface):
-            from apysc._type import value_util
-            from apysc._validation import number_validation
-            number_validation.validate_integer(
-                integer=additional_rotation)
-            value_str: str = value_util.get_value_str_for_expression(
-                value=additional_rotation)
-            expression: str = (
-                f'{self.variable_name}.rotate({value_str});'
-            )
-            ap.append_js_expression(expression=expression)
+
+    def _revert(self, snapshot_name: str) -> None:
+        """
+        Revert a value if snapshot exists.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
--- tests/_display/test_rotate_around_center_interface.py
+++ tests/_display/test_rotate_around_center_interface.py
@@ -10,14 +10,11 @@ from apysc._expression import expression_file_util
 class TestRotateAroundCenterInterface:
 
     @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
-    def test_rotate_around_center(self) -> None:
-        expression_file_util.empty_expression_dir()
+    def test__initialize_rotate_around_center_if_not_initialized(
+            self) -> None:
         interface: RotateAroundCenterInterface = RotateAroundCenterInterface()
-        interface.variable_name = 'test_rotate_around_center_interface'
-
-        interface.rotate_around_center(additional_rotation=10)
-        expression: str = expression_file_util.get_current_expression()
-        expected: str = (
-            'test_rotate_around_center_interface.rotate(10);'
-        )
-        assert expected in expression
+        interface._initialize_rotate_around_center_if_not_initialized()
+        assert interface._rotate_around_center == 0
+        interface._rotate_around_center._value = 10
+        interface._initialize_rotate_around_center_if_not_initialized()
+        assert interface._rotate_around_center == 10

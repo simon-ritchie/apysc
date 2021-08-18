a52e63e7292b235956cac88bbd7e7b534aa1a83b
simon-ritchie <antisocial.sid2@gmail.com>
2021-08-18 21:40:59 +0900

    #320 Drop the old rotation interface.

--- tests/_display/test_rotation_around_point_interface.py
+++ tests/_display/test_rotation_around_point_interface.py
@@ -25,23 +25,6 @@ class _TestInterface(RotationAroundPointInterface):
 class TestRotationAroundPointInterface:
 
     @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
-    def test_rotate_around_point(self) -> None:
-        expression_file_util.empty_expression_dir()
-        interface: _TestInterface = _TestInterface()
-        additional_rotation: ap.Int = ap.Int(10)
-        x: ap.Int = ap.Int(20)
-        y: ap.Int = ap.Int(30)
-        interface.rotate_around_point(
-            additional_rotation=additional_rotation, x=x, y=y)
-        expression: str = expression_file_util.get_current_expression()
-        expected: str = (
-            f'{interface.variable_name}.rotate('
-            f'{additional_rotation.variable_name}, '
-            f'{x.variable_name}, {y.variable_name});'
-        )
-        assert expected in expression
-
-    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
     def test__initialize_rotation_around_point_if_not_initialized(
             self) -> None:
         interface: _TestInterface = _TestInterface()

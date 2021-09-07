c4c458e607d388e13cebdc21c8f4e6fdb2f8da3e
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-07 21:45:29 +0900

    #368 Add the method to append the expression of applying new attribute value to each stacked value.

--- apysc/_type/attr_linking_interface.py
+++ apysc/_type/attr_linking_interface.py
@@ -36,3 +36,28 @@ class AttrLinkingInterface(Generic[_T]):
         """
         self._initialize_attr_linking_stack()
         self._attr_linking_stack.append(attr)
+
+    def _append_applying_new_attr_val_exp(self, new_attr: _T) -> None:
+        """
+        Append the expression of applying new attribute value to each
+        stacked value.
+
+        Parameters
+        ----------
+        new_attr : Int or Number or String
+            New attribute value.
+        """
+        self._initialize_attr_linking_stack()
+        if not self._attr_linking_stack:
+            return
+        new_attr_name: str = new_attr.variable_name
+        expression: str = ''
+        for stacked_value in self._attr_linking_stack:
+            if stacked_value.variable_name == new_attr_name:
+                continue
+            if expression != '':
+                expression += '\n'
+            expression += (
+                f'{stacked_value.variable_name} = {new_attr_name};'
+            )
+        ap.append_js_expression(expression=expression)
--- tests/_type/test_attr_linking_interface.py
+++ tests/_type/test_attr_linking_interface.py
@@ -1,9 +1,11 @@
 from random import randint
+from typing import List
 
 from retrying import retry
 
 import apysc as ap
 from apysc._type.attr_linking_interface import AttrLinkingInterface
+from apysc._expression import expression_data_util
 
 
 class TestAttrLinkingInterface:
@@ -22,3 +24,23 @@ class TestAttrLinkingInterface:
         interface: AttrLinkingInterface = AttrLinkingInterface()
         interface._append_attr_to_linking_stack(attr=ap.Int(10))
         assert interface._attr_linking_stack == [ap.Int(10)]
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__append_applying_new_attr_val_exp(self) -> None:
+        expression_data_util.empty_expression()
+        interface: AttrLinkingInterface = AttrLinkingInterface()
+        attr_1: ap.Int = ap.Int(10)
+        attr_2: ap.Int = ap.Int(20)
+        new_attr: ap.Int = ap.Int(30)
+        interface._append_attr_to_linking_stack(attr=attr_1)
+        interface._append_attr_to_linking_stack(attr=attr_2)
+        interface._append_attr_to_linking_stack(attr=new_attr)
+        interface._append_applying_new_attr_val_exp(
+            new_attr=new_attr)
+        expression: str = expression_data_util.get_current_expression()
+        assert f'{attr_1.variable_name} = {new_attr.variable_name};' \
+            in expression
+        assert f'{attr_2.variable_name} = {new_attr.variable_name};' \
+            in expression
+        assert f'{new_attr.variable_name} = {new_attr.variable_name};' \
+            not in expression

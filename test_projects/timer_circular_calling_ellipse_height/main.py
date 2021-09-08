601058784417349b026635ee480f27638a2a807b
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-08 21:07:32 +0900

    #368 Add the js test project for the timer event circular calling and updating ellipse height value.

---
+++ test_projects/timer_circular_calling_ellipse_height/main.py
@@ -0,0 +1,124 @@
+"""The test project for the timer event circular calling and
+updating ellipse height interface.
+
+Command examples:
+$ python test_projects/timer_circular_calling_ellipse_height/main.py
+$ python timer_circular_calling_ellipse_height/main.py
+"""
+
+import sys
+
+sys.path.append('./')
+
+import os
+from types import ModuleType
+from typing import Any
+from typing import Dict
+
+import apysc as ap
+from apysc._file import file_util
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module),
+    'test_output/'
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    stage: ap.Stage = ap.Stage(
+        background_color='#333',
+        stage_width=1000, stage_height=500, stage_elem_id='stage')
+    sprite: ap.Sprite = ap.Sprite(stage=stage)
+    sprite.graphics.begin_fill(color='#00aaff')
+
+    rectangle: ap.Rectangle = sprite.graphics.draw_round_rect(
+        x=50, y=50, width=400, height=400,
+        ellipse_width=100, ellipse_height=100)
+    timer_1: ap.Timer = ap.Timer(
+        on_timer_1, delay=ap.FPS.FPS_60, repeat_count=100,
+        options={'rectangle': rectangle})
+    timer_1.timer_complete(
+        on_timer_complete_1, options={'rectangle': rectangle})
+    timer_1.start()
+
+    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH, minify=False)
+
+
+def on_timer_1(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called from a timer.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    rectangle: ap.Rectangle = options['rectangle']
+    rectangle.ellipse_height += 1
+
+
+def on_timer_complete_1(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called when a timer has completed.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    rectangle: ap.Rectangle = options['rectangle']
+    timer_2: ap.Timer = ap.Timer(
+        on_timer_2, delay=ap.FPS.FPS_60, repeat_count=100,
+        options={'rectangle': rectangle})
+    timer_2.timer_complete(
+        on_timer_complete_2,
+        options={'rectangle': rectangle})
+    timer_2.start()
+
+
+def on_timer_2(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called from a timer.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    rectangle: ap.Rectangle = options['rectangle']
+    rectangle.ellipse_height -= 1
+
+
+def on_timer_complete_2(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called when a timer has completed.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    rectangle: ap.Rectangle = options['rectangle']
+    timer_3: ap.Timer = ap.Timer(
+        on_timer_1, delay=ap.FPS.FPS_60, repeat_count=100,
+        options={'rectangle': rectangle})
+    timer_3.timer_complete(
+        on_timer_complete_1, options={'rectangle': rectangle})
+    timer_3.start()
+
+
+if __name__ == '__main__':
+    main()

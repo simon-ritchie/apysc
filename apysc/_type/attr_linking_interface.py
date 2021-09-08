1af9fc2de2fc3d2b1089a77da4c35d2d7f19ec01
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-08 20:32:32 +0900

    #368 Change the stacking attribute value type from the `list` to `dict`.

--- apysc/_type/attr_linking_interface.py
+++ apysc/_type/attr_linking_interface.py
@@ -4,28 +4,36 @@ This interface is used for updating an old property value to
 achieve consistency in the handler functions.
 """
 
-from typing import Generic, TypeVar
-from typing import List
+from typing import Dict, List, Union
 
 import apysc as ap
 
-_T = TypeVar('_T', ap.Int, ap.Number, ap.String)
+_AttrName = str
+_Attr = Union[ap.Int, ap.Number, ap.String]
 
 
-class AttrLinkingInterface(Generic[_T]):
+class AttrLinkingInterface:
 
-    _attr_linking_stack: List[_T]
+    _attr_linking_stack: Dict[_AttrName, List[_Attr]]
 
-    def _initialize_attr_linking_stack(self) -> None:
+    def _initialize_attr_linking_stack(self, attr_name: str) -> None:
         """
         Initialize the _attr_linking_stack attribute if it hasn't been
         initialized yet.
-        """
-        if hasattr(self, '_attr_linking_stack'):
-            return
-        self._attr_linking_stack = []
 
-    def _append_attr_to_linking_stack(self, attr: _T) -> None:
+        Parameters
+        ----------
+        attr_name : str
+            Target attribute name.
+        """
+        if not hasattr(self, '_attr_linking_stack'):
+            self._attr_linking_stack = {}
+        if attr_name in self._attr_linking_stack:
+            return
+        self._attr_linking_stack[attr_name] = []
+
+    def _append_attr_to_linking_stack(
+            self, attr: _Attr, attr_name: str) -> None:
         """
         Append an attribute to the linking attribute stack.
 
@@ -33,11 +41,14 @@ class AttrLinkingInterface(Generic[_T]):
         ----------
         attr : Int or Number or String
             Target attribute to be appended.
+        attr_name : str
+            Target attribute name.
         """
-        self._initialize_attr_linking_stack()
-        self._attr_linking_stack.append(attr)
+        self._initialize_attr_linking_stack(attr_name=attr_name)
+        self._attr_linking_stack[attr_name].append(attr)
 
-    def _append_applying_new_attr_val_exp(self, new_attr: _T) -> None:
+    def _append_applying_new_attr_val_exp(
+            self, new_attr: _Attr, attr_name: str) -> None:
         """
         Append the expression of applying new attribute value to each
         stacked value.
@@ -46,13 +57,16 @@ class AttrLinkingInterface(Generic[_T]):
         ----------
         new_attr : Int or Number or String
             New attribute value.
+
+        attr_name : str
+            Target attribute name.
         """
-        self._initialize_attr_linking_stack()
+        self._initialize_attr_linking_stack(attr_name=attr_name)
         if not self._attr_linking_stack:
             return
         new_attr_name: str = new_attr.variable_name
         expression: str = ''
-        for stacked_value in self._attr_linking_stack:
+        for stacked_value in self._attr_linking_stack[attr_name]:
             if stacked_value.variable_name == new_attr_name:
                 continue
             if expression != '':
--- tests/_type/test_attr_linking_interface.py
+++ tests/_type/test_attr_linking_interface.py
@@ -13,17 +13,18 @@ class TestAttrLinkingInterface:
     @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
     def test__initialize_attr_linking_stack(self) -> None:
         interface: AttrLinkingInterface = AttrLinkingInterface()
-        interface._initialize_attr_linking_stack()
-        assert interface._attr_linking_stack == []
-        interface._attr_linking_stack.append(ap.Int(10))
-        interface._initialize_attr_linking_stack()
-        assert interface._attr_linking_stack == [ap.Int(10)]
+        interface._initialize_attr_linking_stack(attr_name='x')
+        assert interface._attr_linking_stack == {'x': []}
+        interface._attr_linking_stack['x'].append(ap.Int(10))
+        interface._initialize_attr_linking_stack(attr_name='x')
+        assert interface._attr_linking_stack == {'x': [ap.Int(10)]}
 
     @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
     def test__append_attr_to_linking_stack(self) -> None:
         interface: AttrLinkingInterface = AttrLinkingInterface()
-        interface._append_attr_to_linking_stack(attr=ap.Int(10))
-        assert interface._attr_linking_stack == [ap.Int(10)]
+        interface._append_attr_to_linking_stack(
+            attr=ap.Int(10), attr_name='x')
+        assert interface._attr_linking_stack == {'x': [ap.Int(10)]}
 
     @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
     def test__append_applying_new_attr_val_exp(self) -> None:
@@ -32,11 +33,11 @@ class TestAttrLinkingInterface:
         attr_1: ap.Int = ap.Int(10)
         attr_2: ap.Int = ap.Int(20)
         new_attr: ap.Int = ap.Int(30)
-        interface._append_attr_to_linking_stack(attr=attr_1)
-        interface._append_attr_to_linking_stack(attr=attr_2)
-        interface._append_attr_to_linking_stack(attr=new_attr)
+        interface._append_attr_to_linking_stack(attr=attr_1, attr_name='x')
+        interface._append_attr_to_linking_stack(attr=attr_2, attr_name='x')
+        interface._append_attr_to_linking_stack(attr=new_attr, attr_name='x')
         interface._append_applying_new_attr_val_exp(
-            new_attr=new_attr)
+            new_attr=new_attr, attr_name='x')
         expression: str = expression_data_util.get_current_expression()
         assert f'{attr_1.variable_name} = {new_attr.variable_name};' \
             in expression

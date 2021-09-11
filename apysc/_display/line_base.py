78e2bc025e61bf1f0ffb1353ff153ff8dd53f779
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-11 13:24:21 +0900

    #368 Add the `AttrLinkingInterface` interfaces setting of the line color value to the `LineBase` class.

--- apysc/_display/line_base.py
+++ apysc/_display/line_base.py
@@ -111,6 +111,11 @@ class LineBase(
                 attr=self._fill_color, attr_name='fill_color')
 
             self._append_applying_new_attr_val_exp(
+                new_attr=self._line_color, attr_name='line_color')
+            self._append_attr_to_linking_stack(
+                attr=self._line_color, attr_name='line_color')
+
+            self._append_applying_new_attr_val_exp(
                 new_attr=self._line_alpha, attr_name='line_alpha')
             self._append_attr_to_linking_stack(
                 attr=self._line_alpha, attr_name='line_alpha')

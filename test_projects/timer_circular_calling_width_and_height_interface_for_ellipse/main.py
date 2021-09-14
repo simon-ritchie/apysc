7b84acf1aeda69f0da59437500884b838a971fd4
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-14 21:06:45 +0900

    #368 Add the js test project for the timer event circular calling and updating width and height interface values for the ellipse class.

---
+++ test_projects/timer_circular_calling_width_and_height_interface_for_ellipse/main.py
@@ -0,0 +1,126 @@
+"""The test project for the timer event circular calling and
+updating width and height interface values for the ellipse class.
+
+Command examples:
+$ python test_projects/\
+timer_circular_calling_width_and_height_interface_for_ellipse/main.py
+$ python timer_circular_calling_width_and_height_interface_for_ellipse/main.py
+"""
+
+import sys
+
+sys.path.append('./')
+
+import os
+from types import ModuleType
+from typing import Any
+from typing import Dict
+
+import apysc as ap
+from apysc._file import file_util
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module),
+    'test_output/'
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    stage: ap.Stage = ap.Stage(
+        background_color='#333',
+        stage_width=1000, stage_height=500, stage_elem_id='stage')
+    sprite: ap.Sprite = ap.Sprite(stage=stage)
+    sprite.graphics.begin_fill(color='#00aaff')
+
+    ellipse: ap.Ellipse = sprite.graphics.draw_ellipse(
+        x=200, y=200, width=150, height=150)
+    timer_1: ap.Timer = ap.Timer(
+        on_timer_1, delay=ap.FPS.FPS_60, repeat_count=50,
+        options={'ellipse': ellipse})
+    timer_1.timer_complete(
+        on_timer_complete_1, options={'ellipse': ellipse})
+    timer_1.start()
+
+    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH, minify=False)
+
+
+def on_timer_1(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called from a timer.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    ellipse: ap.Ellipse = options['ellipse']
+    ellipse.width += 1
+    ellipse.height -= 1
+
+
+def on_timer_complete_1(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called when a timer has completed.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    ellipse: ap.Ellipse = options['ellipse']
+    timer_2: ap.Timer = ap.Timer(
+        on_timer_2, delay=ap.FPS.FPS_60, repeat_count=100,
+        options={'ellipse': ellipse})
+    timer_2.timer_complete(
+        on_timer_complete_2,
+        options={'ellipse': ellipse})
+    timer_2.start()
+
+
+def on_timer_2(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called from a timer.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    ellipse: ap.Ellipse = options['ellipse']
+    ellipse.width -= 1
+    ellipse.height += 1
+
+
+def on_timer_complete_2(e: ap.TimerEvent, options: Dict[str, Any]) -> None:
+    """
+    The handler will be called when a timer has completed.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    ellipse: ap.Ellipse = options['ellipse']
+    timer_3: ap.Timer = ap.Timer(
+        on_timer_1, delay=ap.FPS.FPS_60, repeat_count=100,
+        options={'ellipse': ellipse})
+    timer_3.timer_complete(
+        on_timer_complete_1, options={'ellipse': ellipse})
+    timer_3.start()
+
+
+if __name__ == '__main__':
+    main()

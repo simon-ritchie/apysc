2765aaafd231b5ff0bafa225451b412c63379c56
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-16 16:21:52 +0900

    #371 Add the function to get a handler's argument(s) data list.

--- apysc/_validation/options_validation.py
+++ apysc/_validation/options_validation.py
@@ -3,7 +3,9 @@
 
 import inspect
 from inspect import Signature
-from typing import Any, Dict
+from typing import Any, Dict, List
+
+from typing_extensions import TypedDict
 
 from apysc._event.handler import Handler
 
@@ -22,7 +24,38 @@ def validate_options(handler: Handler, options: Any) -> None:
     if options is None:
         return
     _validate_dict_type(options=options)
+    handler_arg_data_list: List[_ArgData] = _get_handler_arg_data_list(
+        handler=handler)
     pass
+
+
+class _ArgData(TypedDict):
+    arg_name: str
+    annotation: Any
+
+
+def _get_handler_arg_data_list(handler: Handler) -> List[_ArgData]:
+    """
+    Get a handler's argument(s) data list.
+
+    Parameters
+    ----------
+    handler : Handler
+        Target handler.
+
+    Returns
+    -------
+    handler_arg_data_list : list of _ArgData
+        Argument(s) data list.
+    """
+    signature: Signature = inspect.signature(handler)
+    handler_arg_data_list: List[_ArgData] = []
+    for arg_name, parameter in signature.parameters.items():
+        handler_arg_data_list.append({
+            'arg_name': arg_name,
+            'annotation': parameter.annotation,
+        })
+    return handler_arg_data_list
 
 
 def _validate_dict_type(options: Any) -> None:
--- tests/_validation/test_options_validation.py
+++ tests/_validation/test_options_validation.py
@@ -1,9 +1,13 @@
 from random import randint
+import inspect
+from typing import Any, Dict, List
 
 from retrying import retry
 from typing_extensions import TypedDict
 
+import apysc as ap
 from apysc._validation import options_validation
+from apysc._validation.options_validation import _ArgData
 from tests.testing_helper import assert_raises
 
 
@@ -27,3 +31,29 @@ def test__validate_dict_type() -> None:
         func_or_method=options_validation._validate_dict_type,
         kwargs={'options': [1, 2]},
     )
+
+
+def _test_handler_1(e: ap.Event, options: Dict[str, Any]) -> None:
+    """
+    The handler function for the testing.
+
+    Parameters
+    ----------
+    e : Event
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__get_handler_arg_data_list() -> None:
+    handler_arg_data_list: List[_ArgData] = options_validation.\
+        _get_handler_arg_data_list(handler=_test_handler_1)
+    arg_names: List[str] = [
+        arg_data['arg_name'] for arg_data in handler_arg_data_list]
+    assert arg_names == ['e', 'options']
+
+    annotations: List[Any] = [
+        arg_data['annotation'] for arg_data in handler_arg_data_list]
+    assert annotations == [ap.Event, Dict[str, Any]]

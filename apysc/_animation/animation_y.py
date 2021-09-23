67d8c3098f876bd79a16658269d5b993972ff02d
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-23 19:01:17 +0900

    #387 Add the class implementation for the y animation value.

--- apysc/_animation/animation_y.py
+++ apysc/_animation/animation_y.py
@@ -1,4 +1,4 @@
-"""Class implementation for the x animation value.
+"""Class implementation for the y animation value.
 """
 
 from typing import Dict
@@ -14,30 +14,30 @@ from apysc._type.variable_name_interface import VariableNameInterface
 _T = TypeVar('_T', bound=VariableNameInterface)
 
 
-class AnimationX(AnimationBase[_T], Generic[_T]):
+class AnimationY(AnimationBase[_T], Generic[_T]):
     """
-    The animation class for a x-coordinate.
+    The animation class for a y-coordinate.
     """
 
-    _x: ap.Int
+    _y: ap.Int
 
     def __init__(
             self,
             target: _T,
-            x: Union[int, ap.Int],
+            y: Union[int, ap.Int],
             duration: Union[int, ap.Int] = 3000,
             delay: Union[int, ap.Int] = 0,
             easing: Easing = Easing.LINEAR) -> None:
         """
-        The animation class for a x-coordinate.
+        The animation class for a y-coordinate.
 
         Parameters
         ----------
         target : VariableNameInterface
             A target instance of the animation target
             (e.g., `DisplayObject` instance).
-        x : int or Int
-            Destination of the x-coordinate.
+        y : int or Int
+            Destination of the y-coordinate.
         duration : int or Int, default 3000
             Milliseconds before an animation ends.
         delay : int or Int, default 0
@@ -47,20 +47,20 @@ class AnimationX(AnimationBase[_T], Generic[_T]):
         """
         with ap.DebugInfo(
                 callable_='__init__', locals_=locals(),
-                module_name=__name__, class_=AnimationX):
+                module_name=__name__, class_=AnimationY):
             from apysc._converter import to_apysc_val_from_builtin
             from apysc._expression import expression_variables_util
             from apysc._expression import var_names
             variable_name: str = expression_variables_util.\
-                get_next_variable_name(type_name=var_names.ANIMATION_X)
-            self._x = to_apysc_val_from_builtin.\
-                get_copied_int_from_builtin_val(integer=x)
+                get_next_variable_name(type_name=var_names.ANIMATION_Y)
+            self._y = to_apysc_val_from_builtin.\
+                get_copied_int_from_builtin_val(integer=y)
             self._set_basic_animation_settings(
                 target=target,
                 duration=duration,
                 delay=delay,
                 easing=easing)
-            super(AnimationX, self).__init__(variable_name=variable_name)
+            super(AnimationY, self).__init__(variable_name=variable_name)
 
     def _get_animation_func_expression(self) -> str:
         """
@@ -71,36 +71,3 @@ class AnimationX(AnimationBase[_T], Generic[_T]):
         expression : str
             Animation function expression.
         """
-        from apysc._type import value_util
-        x_str: str = value_util.get_value_str_for_expression(value=self._x)
-        return f'\n  .x({x_str});'
-
-    _x_snapshots: Dict[str, int]
-
-    def _make_snapshot(self, snapshot_name: str) -> None:
-        """
-        Make a value's snapshot.
-
-        Parameters
-        ----------
-        snapshot_name : str
-            Target snapshot name.
-        """
-        if not hasattr(self, '_x_snapshots'):
-            self._x_snapshots = {}
-        if self._snapshot_exists(snapshot_name=snapshot_name):
-            return
-        self._x_snapshots[snapshot_name] = int(self._x._value)
-
-    def _revert(self, snapshot_name: str) -> None:
-        """
-        Revert value if a snapshot exists.
-
-        Parameters
-        ----------
-        snapshot_name : str
-            Target snapshot name.
-        """
-        if not self._snapshot_exists(snapshot_name=snapshot_name):
-            return
-        self._x._value = self._x_snapshots[snapshot_name]
---
+++ tests/_animation/test_animation_y.py
@@ -0,0 +1,28 @@
+from random import randint
+
+from retrying import retry
+
+import apysc as ap
+from apysc._animation.animation_y import AnimationY
+from apysc._type.variable_name_interface import VariableNameInterface
+from tests.testing_helper import assert_attrs
+
+
+class TestAnimationY:
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test___init__(self) -> None:
+        target: VariableNameInterface = VariableNameInterface()
+        target.variable_name = 'test_animation_y'
+        animation_y: AnimationY = AnimationY(
+            target=target, y=100, duration=2000, delay=1000,
+            easing=ap.Easing.EASE_OUT_QUINT)
+        assert_attrs(
+            expected_attrs={
+                '_target': target,
+                '_y': 100,
+                '_duration': 2000,
+                '_delay': 1000,
+                '_easing': ap.Easing.EASE_OUT_QUINT,
+            },
+            any_obj=animation_y)

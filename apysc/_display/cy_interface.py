39ead046f16d392d7492aa68a19e96581c7f190c
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-25 16:45:52 +0900

    #394 Add the `AnimationCyInterface` inheritance to the `CyInterface`.

--- apysc/_display/cy_interface.py
+++ apysc/_display/cy_interface.py
@@ -4,13 +4,13 @@
 from typing import Dict
 
 import apysc as ap
+from apysc._animation.animation_cy_interface import AnimationCyInterface
 from apysc._type.attr_linking_interface import AttrLinkingInterface
 from apysc._type.revert_interface import RevertInterface
-from apysc._type.variable_name_interface import VariableNameInterface
 
 
 class CyInterface(
-        VariableNameInterface, RevertInterface, AttrLinkingInterface):
+        AnimationCyInterface, RevertInterface, AttrLinkingInterface):
 
     _cy: ap.Int
 

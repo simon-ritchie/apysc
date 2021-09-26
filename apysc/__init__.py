828d1f5915a6588d5b7b468b940f09e1d2dd9ee5
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-26 20:40:07 +0900

    #401 Increment the version number to 0.50.7.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -68,4 +68,4 @@ from apysc._animation.animation_cx import AnimationCx  # isort:skip # noqa
 from apysc._animation.animation_cy import AnimationCy  # isort:skip # noqa
 from apysc._animation.animation_width_for_ellipse import AnimationWidthForEllipse  # isort:skip # noqa
 
-__version__: str = '0.50.6'
+__version__: str = '0.50.7'

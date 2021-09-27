d21ac9359b0fc8f78657383ad1d7407f649a78a2
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-27 21:48:13 +0900

    #399 Add the class implementation for the animation_height (for ellipse) interface.

---
+++ apysc/_animation/animation_height_for_ellipse_interface.py
@@ -0,0 +1,56 @@
+"""Class implementation for the animation_height (for ellipse) interface.
+"""
+
+from typing import Union
+
+import apysc as ap
+from apysc._animation.animation_interface_base import AnimationInterfaceBase
+from apysc._animation.animation_height_for_ellipse import \
+    AnimationHeightForEllipse
+from apysc._animation.easing import Easing
+
+
+class AnimationHeightForEllipseInterface(AnimationInterfaceBase):
+
+    def animation_height(
+            self,
+            height: Union[int, ap.Int],
+            duration: Union[int, ap.Int] = 3000,
+            delay: Union[int, ap.Int] = 0,
+            easing: Easing = Easing.LINEAR) -> AnimationHeightForEllipse:
+        """
+        Set the ellipse-height animation setting.
+
+        Parameters
+        ----------
+        height : int or Int
+            The final ellipse-height of the animation.
+        duration : int or Int, default 3000
+            Milliseconds before an animation ends.
+        delay : int or Int, default 0
+            Milliseconds before an animation starts.
+        easing : Easing, default Easing.LINEAR
+            Easing setting.
+
+        Returns
+        -------
+        animation_height_for_ellipse : AnimationHeightForEllipse
+            Created animation setting instance.
+
+        References
+        ----------
+        - Animation interfaces duration setting
+            - https://simon-ritchie.github.io/apysc/animation_duration.html
+        - Each animation interface return value document
+            - https://bit.ly/2XOoa8w
+        - Easing enum document
+            - https://simon-ritchie.github.io/apysc/easing_enum.html
+        """
+        animation_height_for_ellipse: AnimationHeightForEllipse = \
+            AnimationHeightForEllipse(
+                target=self,
+                height=height,
+                duration=duration,
+                delay=delay,
+                easing=easing)
+        return animation_height_for_ellipse
---
+++ tests/_animation/test_animation_height_for_ellipse_interface.py
@@ -0,0 +1,31 @@
+from random import randint
+
+from retrying import retry
+
+import apysc as ap
+from apysc._animation.animation_height_for_ellipse_interface import \
+    AnimationHeightForEllipseInterface
+from tests.testing_helper import assert_attrs
+
+
+class TestAnimationHeightForEllipseInterface:
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_animation_height(self) -> None:
+        interface: AnimationHeightForEllipseInterface = \
+            AnimationHeightForEllipseInterface()
+        interface.variable_name = \
+            'test_animation_height_for_ellipse_interface'
+        animation_height_for_ellipse: ap.AnimationHeightForEllipse = \
+            interface.animation_height(
+                height=100, duration=1000, delay=500,
+                easing=ap.Easing.EASE_OUT_QUINT)
+        assert_attrs(
+            expected_attrs={
+                '_target': interface,
+                '_height': 100,
+                '_duration': 1000,
+                '_delay': 500,
+                '_easing': ap.Easing.EASE_OUT_QUINT,
+            },
+            any_obj=animation_height_for_ellipse)

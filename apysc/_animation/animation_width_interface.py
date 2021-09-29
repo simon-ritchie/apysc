69b92acba8798a18686509543688f795b9ea5b48
simon-ritchie <antisocial.sid2@gmail.com>
2021-09-29 20:47:11 +0900

    #403 Add the class implementation for the animation_width interface.

---
+++ apysc/_animation/animation_width_interface.py
@@ -0,0 +1,54 @@
+"""Class implementation for the animation_width interface.
+"""
+
+from typing import Union
+
+import apysc as ap
+from apysc._animation.animation_interface_base import AnimationInterfaceBase
+from apysc._animation.animation_width import AnimationWidth
+from apysc._animation.easing import Easing
+
+
+class AnimationWidthInterface(AnimationInterfaceBase):
+
+    def animation_width(
+            self,
+            width: Union[int, ap.Int],
+            duration: Union[int, ap.Int] = 3000,
+            delay: Union[int, ap.Int] = 0,
+            easing: Easing = Easing.LINEAR) -> AnimationWidth:
+        """
+        Set the width animation setting.
+
+        Parameters
+        ----------
+        width : int or Int
+            The final width of the animation.
+        duration : int or Int, default 3000
+            Milliseconds before an animation ends.
+        delay : int or Int, default 0
+            Milliseconds before an animation starts.
+        easing : Easing, default Easing.LINEAR
+            Easing setting.
+
+        Returns
+        -------
+        animation_width : AnimationWidth
+            Created animation setting instance.
+
+        References
+        ----------
+        - Animation interfaces duration setting
+            - https://simon-ritchie.github.io/apysc/animation_duration.html
+        - Each animation interface return value document
+            - https://bit.ly/2XOoa8w
+        - Easing enum document
+            - https://simon-ritchie.github.io/apysc/easing_enum.html
+        """
+        animation_width: AnimationWidth = AnimationWidth(
+            target=self,
+            width=width,
+            duration=duration,
+            delay=delay,
+            easing=easing)
+        return animation_width
---
+++ tests/_animation/test_animation_width_interface.py
@@ -0,0 +1,27 @@
+from random import randint
+
+from retrying import retry
+
+import apysc as ap
+from apysc._animation.animation_width_interface import AnimationWidthInterface
+from tests.testing_helper import assert_attrs
+
+
+class TestAnimationWidthInterface:
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_animation_width(self) -> None:
+        interface: AnimationWidthInterface = AnimationWidthInterface()
+        interface.variable_name = 'test_animation_width_interface'
+        animation_width: ap.AnimationWidth = interface.animation_width(
+            width=100, duration=1000, delay=500,
+            easing=ap.Easing.EASE_OUT_QUINT)
+        assert_attrs(
+            expected_attrs={
+                '_target': interface,
+                '_width': 100,
+                '_duration': 1000,
+                '_delay': 500,
+                '_easing': ap.Easing.EASE_OUT_QUINT,
+            },
+            any_obj=animation_width)

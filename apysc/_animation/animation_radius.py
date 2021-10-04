c8105ff60a8162c7e6cde35934dadc6f0c50dc2a
simon-ritchie <antisocial.sid2@gmail.com>
2021-10-04 21:37:10 +0900

    #418 Add the method to make a value's snapshot.

--- apysc/_animation/animation_radius.py
+++ apysc/_animation/animation_radius.py
@@ -96,3 +96,20 @@ class AnimationRadius(AnimationBase[_T], Generic[_T]):
                 f'{self._radius.variable_name};'
             )
         return expression
+
+    _radius_snapshots: Dict[str, int]
+
+    def _make_snapshot(self, snapshot_name: str) -> None:
+        """
+        Make a value's snapshot.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not hasattr(self, '_radius_snapshots'):
+            self._radius_snapshots = {}
+        if self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._radius_snapshots[snapshot_name] = int(self._radius._value)

54f783a98854956683afb6871abd996b4f4342be
simon-ritchie <antisocial.sid2@gmail.com>
2021-10-04 17:23:51 +0900

    #416 Add the `AnimationLineThicknessInterface` inheritance to the`LineThicknessInterface` class.

--- apysc/_display/line_thickness_interface.py
+++ apysc/_display/line_thickness_interface.py
@@ -5,13 +5,14 @@ from typing import Dict
 from typing import Union
 
 import apysc as ap
+from apysc._animation.animation_line_thickness_interface import \
+    AnimationLineThicknessInterface
 from apysc._type.attr_linking_interface import AttrLinkingInterface
 from apysc._type.revert_interface import RevertInterface
-from apysc._type.variable_name_interface import VariableNameInterface
 
 
 class LineThicknessInterface(
-        VariableNameInterface, RevertInterface, AttrLinkingInterface):
+        AnimationLineThicknessInterface, RevertInterface, AttrLinkingInterface):
 
     _line_thickness: ap.Int
 

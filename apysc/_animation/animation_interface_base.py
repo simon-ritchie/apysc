c0c8002064822498f5b8cabd361e1d0dd8d740c2
simon-ritchie <antisocial.sid2@gmail.com>
2021-10-16 10:45:46 +0900

    #359 Add the `AnimationFinishInterface` inheritance to the `AnimationInterfaceBase` class.

--- apysc/_animation/animation_interface_base.py
+++ apysc/_animation/animation_interface_base.py
@@ -4,10 +4,12 @@
 from apysc._animation.animation_pause_interface import AnimationPauseInterface
 from apysc._animation.animation_play_interface import AnimationPlayInterface
 from apysc._animation.animation_reset_interface import AnimationResetInterface
+from apysc._animation.animation_finish_interface import \
+    AnimationFinishInterface
 
 
 class AnimationInterfaceBase(
         AnimationPauseInterface, AnimationPlayInterface,
-        AnimationResetInterface):
+        AnimationResetInterface, AnimationFinishInterface):
     """Base class for each animation interface.
     """

e6389fe8789ee453639fecf68dbe4b4c2ec1162e
simon-ritchie <antisocial.sid2@gmail.com>
2021-10-16 11:25:17 +0900

    #359 Apply each lint.

--- apysc/_animation/animation_interface_base.py
+++ apysc/_animation/animation_interface_base.py
@@ -1,11 +1,11 @@
 """Base class for each animation interface.
 """
 
+from apysc._animation.animation_finish_interface import \
+    AnimationFinishInterface
 from apysc._animation.animation_pause_interface import AnimationPauseInterface
 from apysc._animation.animation_play_interface import AnimationPlayInterface
 from apysc._animation.animation_reset_interface import AnimationResetInterface
-from apysc._animation.animation_finish_interface import \
-    AnimationFinishInterface
 
 
 class AnimationInterfaceBase(

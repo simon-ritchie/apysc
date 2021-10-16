69c4c5182720b9661eb87807158de6b4d67fb168
simon-ritchie <antisocial.sid2@gmail.com>
2021-10-16 13:16:45 +0900

    #361 Add the `AnimationReverseInterface` inheritance to the `AnimationInterfaceBase` class.

--- apysc/_animation/animation_interface_base.py
+++ apysc/_animation/animation_interface_base.py
@@ -6,10 +6,13 @@ from apysc._animation.animation_finish_interface import \
 from apysc._animation.animation_pause_interface import AnimationPauseInterface
 from apysc._animation.animation_play_interface import AnimationPlayInterface
 from apysc._animation.animation_reset_interface import AnimationResetInterface
+from apysc._animation.animation_reverse_interface import \
+    AnimationReverseInterface
 
 
 class AnimationInterfaceBase(
         AnimationPauseInterface, AnimationPlayInterface,
-        AnimationResetInterface, AnimationFinishInterface):
+        AnimationResetInterface, AnimationFinishInterface,
+        AnimationReverseInterface):
     """Base class for each animation interface.
     """

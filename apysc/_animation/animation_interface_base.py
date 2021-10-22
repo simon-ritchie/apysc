bea51a0e58e2773218a914866122d067084acfff
simon-ritchie <antisocial.sid2@gmail.com>
2021-10-22 21:34:02 +0900

    #363 Append the `AnimationTimeInterface` inheritance to the `AnimationInterfaceBase` class.

--- apysc/_animation/animation_interface_base.py
+++ apysc/_animation/animation_interface_base.py
@@ -8,11 +8,12 @@ from apysc._animation.animation_play_interface import AnimationPlayInterface
 from apysc._animation.animation_reset_interface import AnimationResetInterface
 from apysc._animation.animation_reverse_interface import \
     AnimationReverseInterface
+from apysc._animation.animation_time_interface import AnimationTimeInterface
 
 
 class AnimationInterfaceBase(
         AnimationPauseInterface, AnimationPlayInterface,
         AnimationResetInterface, AnimationFinishInterface,
-        AnimationReverseInterface):
+        AnimationReverseInterface, AnimationTimeInterface):
     """Base class for each animation interface.
     """

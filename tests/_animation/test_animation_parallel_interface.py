1adbf2bc6822a516b3ee699d9c0bd8a0f39548ee
simon-ritchie <antisocial.sid2@gmail.com>
2021-10-27 20:51:05 +0900

    #447 Rename the `animation_parallel` module to `animation_parallel_interface`.

---
+++ tests/_animation/test_animation_parallel_interface.py
@@ -0,0 +1,13 @@
+from random import randint
+from typing import List
+
+from retrying import retry
+
+import apysc as ap
+from apysc._expression import var_names
+from apysc._animation.animation_base import AnimationBase
+from apysc._expression import expression_data_util
+
+
+class TestAnimationParallel:
+    pass

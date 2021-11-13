67a580486723d4660092a2d25f1f35ef79bb2d3f
simon-ritchie <antisocial.sid2@gmail.com>
2021-11-13 13:35:07 +0900

    #478 Fix the `Reading basic expression file...` exporting log to `Reading basic expression data...`.

--- apysc/_html/exporter.py
+++ apysc/_html/exporter.py
@@ -302,7 +302,7 @@ def _append_expression_to_html_str(html_str: str, verbose: int) -> str:
     from apysc._expression import expression_data_util
     from apysc._html import html_const
     from apysc._html import html_util
-    _display_info(msg='Reading basic expression file...', verbose=verbose)
+    _display_info(msg='Reading basic expression data...', verbose=verbose)
     expression: str = expression_data_util.get_current_expression()
     _display_info(msg='Appending common js functions...', verbose=verbose)
     expression = _append_common_js_functions(expression=expression)

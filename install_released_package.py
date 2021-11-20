b76d3471c2b31d49113129f221e8b91edf4866b3
simon-ritchie <antisocial.sid2@gmail.com>
2021-11-20 11:40:35 +0900

    #480 Add the released package installing tests script.

---
+++ install_released_package.py
@@ -0,0 +1,54 @@
+"""The script to install the released (latest) apysc package from PyPI.
+Also, removing checked out directories and additional minimum tests
+will be run.
+
+This is used by the deploying job mainly.
+"""
+
+import time
+import shutil
+import os
+from typing import List
+
+import apysc as ap
+import command_util
+
+_PIP_COMMAND: str = f'pip install apysc=={ap.__version__}'
+_APYSC_TEST_CODE: str = (
+    'import apysc as ap;'
+    'stage: ap.Stage = ap.Stage();'
+    "ap.save_overall_html(dest_dir_path='./');"
+)
+
+
+def _main() -> None:
+    """
+    Install the released (latest) apysc package from PyPI.
+    Also, removing checked out directories and additional minimum tests
+    will be run.
+    """
+    count: int = 0
+    while True:
+        stdout: str = command_util.run_command(command=_PIP_COMMAND)
+        if 'ERROR: Could not find a version' not in stdout:
+            break
+
+        if count >= 10:
+            raise Exception('apysc package installing is failed.')
+        count += 1
+        time.sleep(10)
+
+    file_or_dir_names: List[str] = os.listdir('./')
+    for file_or_dir_name in file_or_dir_names:
+        if os.path.isfile(file_or_dir_name):
+            os.remove(file_or_dir_name)
+            continue
+        if os.path.isdir(file_or_dir_name):
+            shutil.rmtree(file_or_dir_name, ignore_errors=True)
+
+    os.system(f'python -c "{_APYSC_TEST_CODE}"')
+    assert os.path.exists('./index.html')
+
+
+if __name__ == '__main__':
+    _main()

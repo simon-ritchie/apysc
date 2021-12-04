763b01216f5fe61ceb65753e2ee496311d426718
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-04 10:51:46 +0900

    #487 Add the overriding of the `_get_svg_str` method to the `PathLineTo` class.

--- tests/_geom/test_path_line_to.py
+++ tests/_geom/test_path_line_to.py
@@ -1,8 +1,11 @@
 from random import randint
+import re
+from typing import Match, Optional
 
 from retrying import retry
 
 import apysc as ap
+from apysc._expression import var_names
 from tests.testing_helper import assert_attrs
 
 
@@ -23,3 +26,13 @@ class TestPathLineTo:
         )
         assert isinstance(path_line_to._x, ap.Int)
         assert isinstance(path_line_to._y, ap.Int)
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__get_svg_str(self) -> None:
+        path_line_to: ap.PathLineTo = ap.PathLineTo(x=50, y=100)
+        svg_str: str = path_line_to._get_svg_str()
+        print(svg_str)
+        match: Optional[Match] = re.match(
+            pattern=rf'L {var_names.INT}_\d+? {var_names.INT}_\d+?$',
+            string=svg_str)
+        assert match is not None

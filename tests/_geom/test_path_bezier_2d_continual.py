e9ab7a41a905a9e041907c1eef01d20b3d6fc79c
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-04 12:21:33 +0900

    #487 Add the overriding of the `_get_svg_str` method to the `PathBezier2DContinual` class.

--- tests/_geom/test_path_bezier_2d_continual.py
+++ tests/_geom/test_path_bezier_2d_continual.py
@@ -1,8 +1,11 @@
 from random import randint
+import re
+from typing import Match, Optional
 
 from retrying import retry
 
 import apysc as ap
+from apysc._expression import var_names
 from tests.testing_helper import assert_attrs
 
 
@@ -22,3 +25,13 @@ class TestPathBezier2DContinual:
             any_obj=path_bezier_2d_continual)
         assert isinstance(path_bezier_2d_continual._x, ap.Int)
         assert isinstance(path_bezier_2d_continual._y, ap.Int)
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__get_svg_str(self) -> None:
+        path_bezier_2d_continual: ap.PathBezier2DContinual = \
+            ap.PathBezier2DContinual(x=10, y=20)
+        svg_str: str = path_bezier_2d_continual._get_svg_str()
+        match: Optional[Match] = re.match(
+            pattern=rf'T {var_names.INT}_\d+? {var_names.INT}_\d+?$',
+            string=svg_str)
+        assert match is not None

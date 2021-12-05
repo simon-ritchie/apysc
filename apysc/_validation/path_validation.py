c0d7983e47935fc1609ef4e82fb1a51a62a3068c
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-05 14:03:39 +0900

    #487 Add the function to validate a preceding data of `PathBezier3DContinual` instance in list is a `PathBezier3D` or `PathBezier3DContinual`.

--- apysc/_validation/path_validation.py
+++ apysc/_validation/path_validation.py
@@ -33,7 +33,44 @@ def validate_path_data_list(
         raise ValueError(
             '`path_data_list` argument can not be empty.')
     _validate_bezier_2d_continual_pre_data(path_data_list=path_data_list)
-    pass
+    _validate_bezier_3d_continual_pre_data(path_data_list=path_data_list)
+
+
+def _validate_bezier_3d_continual_pre_data(
+        path_data_list: List[PathDataBase]) -> None:
+    """
+    Validate a preceding data of `PathBezier3DContinual` instance
+    in list is a `PathBezier3D` or `PathBezier3DContinual`.
+
+    Parameters
+    ----------
+    path_data_list : list of PathDataBase
+        Target path data settings, such as the ap.PathData.MoveTo.
+
+    Raises
+    ------
+    ValueError
+        If a preceding data of `PathBezier3DContinual` instance is
+        not a `PathBezier3D` or `PathBezier3DContinual` one.
+    """
+    for i, path_data in enumerate(path_data_list):
+        if i == 0 and isinstance(path_data, PathBezier3DContinual):
+            raise ValueError(
+                '`PathBezier3DContinual` instance can not be set '
+                'at the first position of `path_data_list`.')
+        if i == 0:
+            continue
+        if not isinstance(path_data, PathBezier3DContinual):
+            continue
+        pre_data: PathDataBase = path_data_list[i - 1]
+        if (not isinstance(pre_data, PathBezier3D)
+                and not isinstance(pre_data, PathBezier3DContinual)):
+            raise ValueError(
+                '`PathBezier3DContinual` instance in a '
+                '`path_data_list` argument can set only after a '
+                '`PathBezier3D` or `PathBezier3DContinual` one.'
+                f'\nActual: {type(pre_data)}'
+                f'\nIndex: {i}')
 
 
 def _validate_bezier_2d_continual_pre_data(
--- tests/_validation/test_path_validation.py
+++ tests/_validation/test_path_validation.py
@@ -37,3 +37,36 @@ def test__validate_bezier_2d_continual_pre_data() -> None:
             bezier_2d_continual,
             bezier_2d_continual,
         ])
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__validate_bezier_3d_continual_pre_data() -> None:
+    move_to: ap.PathMoveTo = ap.PathMoveTo(x=0, y=50)
+    bezier_3d: ap.PathBezier3D = ap.PathBezier3D(
+        control_x1=100, control_y1=0, control_x2=150, control_y2=0,
+        dest_x=100, dest_y=100)
+    bezier_3d_continual: ap.PathBezier3DContinual = ap.PathBezier3DContinual(
+        control_x=200, control_y=200, dest_x=250, dest_y=100)
+
+    assert_raises(
+        expected_error_class=ValueError,
+        func_or_method=path_validation._validate_bezier_3d_continual_pre_data,
+        kwargs={'path_data_list': [bezier_3d_continual]},
+        match='`PathBezier3DContinual` instance can not be set '
+              'at the first position of `path_data_list`.')
+
+    assert_raises(
+        expected_error_class=ValueError,
+        func_or_method=path_validation._validate_bezier_3d_continual_pre_data,
+        kwargs={'path_data_list': [move_to, bezier_3d_continual]},
+        match='`PathBezier3DContinual` instance in a '
+              '`path_data_list` argument can set only after a '
+              '`PathBezier3D` or `PathBezier3DContinual` one.')
+
+    path_validation._validate_bezier_3d_continual_pre_data(
+        path_data_list=[
+            move_to,
+            bezier_3d,
+            bezier_3d_continual,
+            bezier_3d_continual,
+        ])

063d7ca5333dbfa47ce0cbc75b1f336aef423ede
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-07 21:18:41 +0900

    #496 Adjust the `_get_svg_str` of the `PathBezier2D` class to use `String` type value.

--- apysc/_geom/path_bezier_2d.py
+++ apysc/_geom/path_bezier_2d.py
@@ -8,6 +8,7 @@ from apysc._converter.to_apysc_val_from_builtin import \
 from apysc._geom.path_data_base import PathDataBase
 from apysc._type.int import Int
 from apysc._type.boolean import Boolean
+from apysc._type.string import String
 
 
 class PathBezier2D(PathDataBase):
@@ -64,7 +65,9 @@ class PathBezier2D(PathDataBase):
             A path's SVG string created with the current setting.
         """
         from apysc._type import value_util
-        svg_char: str = self._get_svg_char()
+        svg_char: String = self._get_svg_char()
+        svg_char_str: str = value_util.get_value_str_for_expression(
+            value=svg_char)
         control_x_str: str = value_util.get_value_str_for_expression(
             value=self._control_x)
         control_y_str: str = value_util.get_value_str_for_expression(
@@ -74,7 +77,7 @@ class PathBezier2D(PathDataBase):
         dest_y_str: str = value_util.get_value_str_for_expression(
             value=self._dest_y)
         svg_str: str = (
-            f'"{svg_char} " + String({control_x_str}) + " " '
+            f'{svg_char_str} + String({control_x_str}) + " " '
             f'+ String({control_y_str}) + " " '
             f'+ String({dest_x_str}) + " " '
             f'+ String({dest_y_str})'
--- tests/_geom/test_path_bezier_2d.py
+++ tests/_geom/test_path_bezier_2d.py
@@ -1,9 +1,12 @@
 from random import randint
+import re
+from typing import Optional, Match
 
 from retrying import retry
 
 import apysc as ap
 from tests.testing_helper import assert_attrs
+from apysc._expression import var_names
 
 
 class TestPathBezier2D:
@@ -34,10 +37,16 @@ class TestPathBezier2D:
         path_bezier_2d: ap.PathBezier2D = ap.PathBezier2D(
             control_x=10, control_y=20, dest_x=30, dest_y=40)
         svg_str: str = path_bezier_2d._get_svg_str()
-        expected: str = (
-            f'"Q " + String({path_bezier_2d._control_x.variable_name}) + " " '
-            f'+ String({path_bezier_2d._control_y.variable_name}) + " " '
-            f'+ String({path_bezier_2d._dest_x.variable_name}) + " " '
-            f'+ String({path_bezier_2d._dest_y.variable_name})'
-        )
-        assert svg_str == expected
+        match: Optional[Match] = re.match(
+            pattern=(
+                rf'{var_names.STRING}_\d+? \+ '
+                rf'String\({path_bezier_2d._control_x.variable_name}\)'
+                r' \+ " " \+ '
+                rf'String\({path_bezier_2d._control_y.variable_name}\)'
+                r' \+ " " \+ '
+                rf'String\({path_bezier_2d._dest_x.variable_name}\)'
+                r' \+ " " \+ '
+                rf'String\({path_bezier_2d._dest_y.variable_name}\)'
+            ),
+            string=svg_str)
+        assert match is not None

3ab9ce4baf466c54fcc5b58a1ad5da736e06da3a
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-12 14:27:50 +0900

    #512 Apply each lint.

--- tests/_geom/test_path_control_x_interface.py
+++ tests/_geom/test_path_control_x_interface.py
@@ -2,8 +2,8 @@ from random import randint
 
 from retrying import retry
 
-from apysc._geom.path_control_x_interface import PathControlXInterface
 import apysc as ap
+from apysc._geom.path_control_x_interface import PathControlXInterface
 
 
 class TestPathControlXInterface:

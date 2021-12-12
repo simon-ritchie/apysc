e59f8fe6e0bb8b0e49290378dfa4bed5baf8efbc
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-12 14:17:49 +0900

    #512 Add the interface class implementation for the path control x data.

---
+++ apysc/_geom/path_control_x_interface.py
@@ -0,0 +1,33 @@
+"""Interface class implementation for the path control x data.
+"""
+
+from apysc._type.int import Int
+
+
+class PathControlXInterface:
+
+    _control_x: Int
+
+    @property
+    def control_x(self) -> Int:
+        """
+        Get a X-coordinate of the bezier's control point.
+
+        Returns
+        -------
+        control_x : Int
+            X-coordinate of the bezier's control point.
+        """
+        return self._control_x
+
+    @control_x.setter
+    def control_x(self, value: Int) -> None:
+        """
+        Set a X-coordinate of the bezier's control point.
+
+        Parameters
+        ----------
+        value : Int
+            X-coordinate of the bezier's control point.
+        """
+        self._control_x.value = value
---
+++ tests/_geom/test_path_control_x_interface.py
@@ -0,0 +1,16 @@
+from random import randint
+
+from retrying import retry
+
+from apysc._geom.path_control_x_interface import PathControlXInterface
+import apysc as ap
+
+
+class TestPathControlXInterface:
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_control_x(self) -> None:
+        interface: PathControlXInterface = PathControlXInterface()
+        interface._control_x = ap.Int(0)
+        interface.control_x = ap.Int(10)
+        assert interface.control_x == 10

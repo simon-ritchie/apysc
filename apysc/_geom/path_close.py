c1c5b25c821f7358efa64c77955dc573621d8b05
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-14 21:35:04 +0900

    #520 Add the `DebugInfo` with statement to the `PathClose` class interfaces.

--- apysc/_geom/path_close.py
+++ apysc/_geom/path_close.py
@@ -14,10 +14,14 @@ class PathClose(PathDataBase):
         """
         Path data class for the svg's `close path` (Z).
         """
-        from apysc._geom.path_label import PathLabel
-        super(PathClose, self).__init__(
-            path_label=PathLabel.CLOSE,
-            relative=False)
+        import apysc as ap
+        with ap.DebugInfo(
+                callable_='__init__', locals_=locals(),
+                module_name=__name__, class_=PathClose):
+            from apysc._geom.path_label import PathLabel
+            super(PathClose, self).__init__(
+                path_label=PathLabel.CLOSE,
+                relative=False)
 
     def _get_svg_str(self) -> str:
         """
@@ -28,8 +32,12 @@ class PathClose(PathDataBase):
         svg_str : str
             A path's SVG string created with the current setting.
         """
-        from apysc._type import value_util
-        svg_char: String = self._get_svg_char()
-        svg_str: str = value_util.get_value_str_for_expression(
-            value=svg_char)
-        return svg_str
+        import apysc as ap
+        with ap.DebugInfo(
+                callable_=self._get_svg_str, locals_=locals(),
+                module_name=__name__, class_=PathClose):
+            from apysc._type import value_util
+            svg_char: String = self._get_svg_char()
+            svg_str: str = value_util.get_value_str_for_expression(
+                value=svg_char)
+            return svg_str

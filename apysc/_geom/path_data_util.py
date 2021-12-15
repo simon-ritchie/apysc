3c42cdbc3670d56689ae725ce41eaf34b58b2b8e
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-15 20:34:31 +0900

    #520 Add the `DebugInfo` with statement to the `path_data_util` module interface.

--- apysc/_geom/path_data_util.py
+++ apysc/_geom/path_data_util.py
@@ -21,9 +21,13 @@ def make_paths_expression_from_list(
     expression : str
         Created expression string.
     """
-    expression: str = ''
-    for path_data in path_data_list:
-        if expression != '':
-            expression += ' + " " + '
-        expression += path_data._get_svg_str()
-    return expression
+    import apysc as ap
+    with ap.DebugInfo(
+            callable_=make_paths_expression_from_list,
+            locals_=locals(), module_name=__name__):
+        expression: str = ''
+        for path_data in path_data_list:
+            if expression != '':
+                expression += ' + " " + '
+            expression += path_data._get_svg_str()
+        return expression

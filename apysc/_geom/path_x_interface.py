ee053a315b3d83085759450c3f63447ad7ea81cc
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-18 15:03:27 +0900

    #521 Add the `RevertInterface` inheritance to the `PathXInterface` class.

--- apysc/_geom/path_x_interface.py
+++ apysc/_geom/path_x_interface.py
@@ -1,10 +1,12 @@
 """Interface class implementation for the x path data.
 """
 
+from typing import Dict
 from apysc._type.int import Int
+from apysc._type.revert_interface import RevertInterface
 
 
-class PathXInterface:
+class PathXInterface(RevertInterface):
 
     _x: Int
 
@@ -39,3 +41,31 @@ class PathXInterface:
                 callable_='x', locals_=locals(),
                 module_name=__name__, class_=PathXInterface):
             self._x.value = value
+
+    _x_snapshots: Dict[str, int]
+
+    def _make_snapshot(self, snapshot_name: str) -> None:
+        """
+        Make a value's snapshot.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        self._set_single_snapshot_val_to_dict(
+            dict_name='_x_snapshots',
+            value=int(self._x._value), snapshot_name=snapshot_name)
+
+    def _revert(self, snapshot_name: str) -> None:
+        """
+        Revert a value if snapshot exists.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._x._value = self._x_snapshots[snapshot_name]
--- tests/_geom/test_path_x_interface.py
+++ tests/_geom/test_path_x_interface.py
@@ -16,3 +16,24 @@ class TestPathXInterface:
 
         interface.x = ap.Int(20)
         assert interface.x == 20
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__make_snapshot(self) -> None:
+        interface: PathXInterface = PathXInterface()
+        interface._x = ap.Int(10)
+        snapshot_name: str = interface._get_next_snapshot_name()
+        interface._run_all_make_snapshot_methods(snapshot_name=snapshot_name)
+        assert interface._x_snapshots[snapshot_name] == 10
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__revert(self) -> None:
+        interface: PathXInterface = PathXInterface()
+        interface._x = ap.Int(10)
+        snapshot_name: str = interface._get_next_snapshot_name()
+        interface._run_all_revert_methods(snapshot_name=snapshot_name)
+        assert interface.x == 10
+
+        interface._run_all_make_snapshot_methods(snapshot_name=snapshot_name)
+        interface._x = ap.Int(20)
+        interface._run_all_revert_methods(snapshot_name=snapshot_name)
+        assert interface.x == 10

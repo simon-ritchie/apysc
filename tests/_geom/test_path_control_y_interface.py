49c3edfaec784bee68568d76f5cc27790c25285b
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-18 11:48:11 +0900

    #521 Add the `RevertInterface` inheritance to the `PathControlYInterface` class.

--- tests/_geom/test_path_control_y_interface.py
+++ tests/_geom/test_path_control_y_interface.py
@@ -14,3 +14,24 @@ class TestPathControlYInterface:
         interface._control_y = ap.Int(0)
         interface.control_y = ap.Int(10)
         assert interface.control_y == 10
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__make_snapshot(self) -> None:
+        interface: PathControlYInterface = PathControlYInterface()
+        interface._control_y = ap.Int(10)
+        snapshot_name: str = interface._get_next_snapshot_name()
+        interface._run_all_make_snapshot_methods(snapshot_name=snapshot_name)
+        assert interface._control_y_snapshots[snapshot_name] == 10
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__revert(self) -> None:
+        interface: PathControlYInterface = PathControlYInterface()
+        interface._control_y = ap.Int(10)
+        snapshot_name: str = interface._get_next_snapshot_name()
+        interface._run_all_revert_methods(snapshot_name=snapshot_name)
+        assert interface.control_y == 10
+
+        interface._run_all_make_snapshot_methods(snapshot_name=snapshot_name)
+        interface._control_y = ap.Int(20)
+        interface._run_all_revert_methods(snapshot_name=snapshot_name)
+        assert interface.control_y == 10

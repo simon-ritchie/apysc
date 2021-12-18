f79da7057349447f9e19ab8a7f0ad4cc7409964e
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-18 12:40:29 +0900

    #521 Add the `RevertInterface` inheritance to the `PathControlY1Interface` class.

--- apysc/_geom/path_control_y1_interface.py
+++ apysc/_geom/path_control_y1_interface.py
@@ -1,10 +1,12 @@
 """Interface class implementation for the first control y path data.
 """
 
+from typing import Dict
 from apysc._type.int import Int
+from apysc._type.revert_interface import RevertInterface
 
 
-class PathControlY1Interface:
+class PathControlY1Interface(RevertInterface):
 
     _control_y1: Int
 
@@ -39,3 +41,31 @@ class PathControlY1Interface:
                 callable_='control_y1', locals_=locals(),
                 module_name=__name__, class_=PathControlY1Interface):
             self._control_y1.value = value
+
+    _control_y1_snapshots: Dict[str, int]
+
+    def _make_snapshot(self, snapshot_name: str) -> None:
+        """
+        Make a value's snapshot.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        self._set_single_snapshot_val_to_dict(
+            dict_name='_control_y1_snapshots',
+            value=int(self._control_y1._value), snapshot_name=snapshot_name)
+
+    def _revert(self, snapshot_name: str) -> None:
+        """
+        Revert a value if snapshot exists.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._control_y1._value = self._control_y1_snapshots[snapshot_name]
--- tests/_geom/test_path_control_y1_interface.py
+++ tests/_geom/test_path_control_y1_interface.py
@@ -14,3 +14,24 @@ class TestPathControlY1Interface:
         interface._control_y1 = ap.Int(0)
         interface.control_y1 = ap.Int(10)
         assert interface.control_y1 == 10
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test__make_snapshot(self) -> None:
+        interface: PathControlY1Interface = PathControlY1Interface()
+        interface._control_y1 = ap.Int(10)
+        snapshot_name: str = interface._get_next_snapshot_name()
+        interface._run_all_make_snapshot_methods(snapshot_name=snapshot_name)
+        assert interface._control_y1_snapshots[snapshot_name] == 10
+
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_test__revert(self) -> None:
+        interface: PathControlY1Interface = PathControlY1Interface()
+        interface._control_y1 = ap.Int(10)
+        snapshot_name: str = interface._get_next_snapshot_name()
+        interface._run_all_revert_methods(snapshot_name=snapshot_name)
+        assert interface.control_y1 == 10
+
+        interface._run_all_make_snapshot_methods(snapshot_name=snapshot_name)
+        interface._control_y1 = ap.Int(20)
+        interface._run_all_revert_methods(snapshot_name=snapshot_name)
+        assert interface.control_y1 == 10

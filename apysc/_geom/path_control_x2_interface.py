75e91b90662b1366b59891db57e4f038fd7b2bf3
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-18 11:25:09 +0900

    #521 Add the `RevertInterface` inheritance to the `PathControlX2Interface` class.

--- apysc/_geom/path_control_x2_interface.py
+++ apysc/_geom/path_control_x2_interface.py
@@ -1,10 +1,12 @@
 """Interface class implementation for the second control x path data.
 """
 
+from typing import Dict
 from apysc._type.int import Int
+from apysc._type.revert_interface import RevertInterface
 
 
-class PathControlX2Interface:
+class PathControlX2Interface(RevertInterface):
 
     _control_x2: Int
 
@@ -39,3 +41,31 @@ class PathControlX2Interface:
                 callable_='control_x2', locals_=locals(),
                 module_name=__name__, class_=PathControlX2Interface):
             self._control_x2.value = value
+
+    _control_x2_snapshots: Dict[str, int]
+
+    def _make_snapshot(self, snapshot_name: str) -> None:
+        """
+        Make a value's snapshot.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        self._set_single_snapshot_val_to_dict(
+            dict_name='_control_x2_snapshots',
+            value=int(self._control_x2._value), snapshot_name=snapshot_name)
+
+    def _revert(self, snapshot_name: str) -> None:
+        """
+        Revert a value if snapshot exists.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._control_x2._value = self._control_x2_snapshots[snapshot_name]

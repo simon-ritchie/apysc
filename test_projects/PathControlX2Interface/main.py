6c214e7170eae8d5fa982b3173360625dc946f20
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-25 14:08:44 +0900

    #529 Add the test project for the PathControlX2Interface class.

---
+++ test_projects/PathControlX2Interface/main.py
@@ -0,0 +1,83 @@
+"""The test project for the PathControlX2Interface class.
+
+Command examples:
+$ python test_projects/PathControlX2Interface/main.py
+"""
+
+import sys
+
+sys.path.append('./')
+
+import os
+from types import ModuleType
+
+from typing_extensions import TypedDict
+
+import apysc as ap
+from apysc._file import file_util
+from apysc._geom.path_control_x2_interface import PathControlX2Interface
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module),
+    'test_output/'
+)
+
+
+class _InterfaceOptions(TypedDict):
+    interface: PathControlX2Interface
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    _ = ap.Stage(
+        background_color='#333',
+        stage_width=1000, stage_height=500, stage_elem_id='stage')
+    interface: PathControlX2Interface = PathControlX2Interface()
+    interface.control_x2 = ap.Int(10)
+    options: _InterfaceOptions = {'interface': interface}
+    ap.Timer(
+        on_timer_1, delay=1000, repeat_count=1, options=options).start()
+
+    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH)
+
+
+def on_timer_1(e: ap.TimerEvent, options: _InterfaceOptions) -> None:
+    """
+    The handler will be called from a timer.
+
+    Parameters
+    ----------
+    e : TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    ap.assert_equal(10, options['interface'].control_x2)
+    options['interface'].control_x2 = ap.Int(20)
+    ap.Timer(
+        on_timer_2, delay=1000, repeat_count=1, options=options).start()
+
+
+def on_timer_2(e: ap.TimerEvent, options: _InterfaceOptions) -> None:
+    """
+    The handler will be called from a timer.
+
+    Parameters
+    ----------
+    e : TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    ap.assert_equal(20, options['interface'].control_x2)
+    options['interface'].control_x2 = ap.Int(10)
+    ap.Timer(
+        on_timer_1, delay=1000, repeat_count=1, options=options).start()
+
+
+if __name__ == '__main__':
+    main()

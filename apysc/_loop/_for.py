f077b59b8d7e998183d667c2d48c26cd3d2af456
simon-ritchie <antisocial.sid2@gmail.com>
2021-12-26 15:09:50 +0900

    #530 Adjust not to use the top-level importing of the `import apysc as ap` in the `_loop` package modules.

--- apysc/_loop/_for.py
+++ apysc/_loop/_for.py
@@ -8,10 +8,14 @@ from typing import Optional
 from typing import TypeVar
 from typing import Union
 
-import apysc as ap
+from apysc._type.int import Int
+from apysc._type.string import String
+from apysc._type.number import Number
+from apysc._type.array import Array
+from apysc._type.dictionary import Dictionary
 from apysc._expression.indent_num import Indent
 
-T = TypeVar('T', ap.Int, ap.String, ap.Number)
+T = TypeVar('T', Int, String, Number)
 
 
 class For(Generic[T]):
@@ -19,14 +23,14 @@ class For(Generic[T]):
     A class to append for the (loop) expression.
     """
 
-    _arr_or_dict: Union[ap.Array, ap.Dictionary]
+    _arr_or_dict: Union[Array, Dictionary]
     _locals: Dict[str, Any]
     _globals: Dict[str, Any]
     _snapshot_name: str
     _indent: Indent
 
     def __init__(
-            self, arr_or_dict: Union[ap.Array, ap.Dictionary],
+            self, arr_or_dict: Union[Array, Dictionary],
             *,
             locals_: Optional[Dict[str, Any]] = None,
             globals_: Optional[Dict[str, Any]] = None) -> None:
@@ -52,6 +56,7 @@ class For(Generic[T]):
             when you don't want to update each variable by the
             implementation of the For scope.
         """
+        import apysc as ap
         with ap.DebugInfo(
                 callable_='__init__', locals_=locals(),
                 module_name=__name__, class_=For):
@@ -66,7 +71,7 @@ class For(Generic[T]):
             self._indent = Indent()
 
     def _validate_arr_or_dict_val_type(
-            self, *, arr_or_dict: Union[ap.Array, ap.Dictionary]) -> None:
+            self, *, arr_or_dict: Union[Array, Dictionary]) -> None:
         """
         Validate loop value type is Array of Dictionary.
 
@@ -80,7 +85,7 @@ class For(Generic[T]):
         TypeError
             If value type is neither Array nor Dictionary.
         """
-        if isinstance(arr_or_dict, (ap.Array, ap.Dictionary)):
+        if isinstance(arr_or_dict, (Array, Dictionary)):
             return
         raise TypeError(
             'Specified value type is neither Array nor Dictionary: '
@@ -95,6 +100,7 @@ class For(Generic[T]):
         i_or_key : Int or String
             Loop index or dictionary key.
         """
+        import apysc as ap
         with ap.DebugInfo(
                 callable_='__enter__', locals_=locals(),
                 module_name=__name__, class_=For):
@@ -122,6 +128,7 @@ class For(Generic[T]):
         """
         Method to be called when end of with statement.
         """
+        import apysc as ap
         with ap.DebugInfo(
                 callable_='__exit__', locals_=locals(),
                 module_name=__name__, class_=For):
@@ -137,7 +144,7 @@ class For(Generic[T]):
             ap.append_js_expression(expression='}')
             last_scope.set_last_scope(value=LastScope.FOR)
 
-    def _append_arr_enter_expression(self, *, i: ap.Int) -> None:
+    def _append_arr_enter_expression(self, *, i: Int) -> None:
         """
         Append for loop start expression (for Array value).
 
@@ -146,6 +153,7 @@ class For(Generic[T]):
         i : Int
             Loop index value.
         """
+        import apysc as ap
         with ap.DebugInfo(
                 callable_=self._append_arr_enter_expression, locals_=locals(),
                 module_name=__name__, class_=For):
@@ -156,7 +164,7 @@ class For(Generic[T]):
             )
             ap.append_js_expression(expression=expression)
 
-    def _append_dict_enter_expression(self, *, key: ap.String) -> None:
+    def _append_dict_enter_expression(self, *, key: String) -> None:
         """
         Append for loop start expression (for Dictionary value).
 
@@ -165,6 +173,7 @@ class For(Generic[T]):
         key : String
             Loop (dictionary) key value.
         """
+        import apysc as ap
         with ap.DebugInfo(
                 callable_=self._append_dict_enter_expression,
                 locals_=locals(),

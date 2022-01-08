ab7d9f1ecf85ca89eec7706d14e316f4e7d18db7
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-08 11:49:56 +0900

    #568 Append the `Examples` section to the `Number` class construct docstring.

--- apysc/_type/number.py
+++ apysc/_type/number.py
@@ -49,6 +49,26 @@ class Number(NumberValueInterface[float, 'Number']):
             - https://bit.ly/3evzcVj
         - Int and Number common comparison operations document
             - https://bit.ly/3zolw6T
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> number: ap.Number = ap.Number(10.5)
+        >>> number
+        Number(10.5)
+
+        >>> number == 10.5
+        Boolean(True)
+
+        >>> number == ap.Number(10.5)
+        Boolean(True)
+
+        >>> number >= 10.5
+        Boolean(True)
+
+        >>> number += 10.3
+        >>> number
+        Number(20.8)
         """
         import apysc as ap
         with ap.DebugInfo(

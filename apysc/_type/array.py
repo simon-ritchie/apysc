5bbe6dba5d2919cfd1e69064da43c6ab2a44d5d1
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-08 20:05:10 +0900

    #568 Append the `Examples` section to the `Array` class `length` interface docstring.

--- apysc/_type/array.py
+++ apysc/_type/array.py
@@ -1091,6 +1091,13 @@ class Array(
         ----------
         - Array class length interface document
             - https://simon-ritchie.github.io/apysc/array_length.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> arr: ap.Array = ap.Array([1, 2, 3])
+        >>> arr.length
+        Int(3)
         """
         import apysc as ap
         with ap.DebugInfo(

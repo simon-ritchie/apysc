a4a81d99106169bb29769bd2bc3070107d78a3b8
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-09 16:11:00 +0900

    #568 Append the `Examples` sections to the `CyInterface` class interfaces.

--- apysc/_display/cy_interface.py
+++ apysc/_display/cy_interface.py
@@ -46,6 +46,17 @@ class CyInterface(
         -------
         y : Int
             Center y-coordinate.
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af', alpha=0.5)
+        >>> circle: ap.Circle = sprite.graphics.draw_circle(
+        ...     x=100, y=100, radius=50)
+        >>> circle.y = ap.Int(120)
+        >>> circle.y
         """
         import apysc as ap
         with ap.DebugInfo(

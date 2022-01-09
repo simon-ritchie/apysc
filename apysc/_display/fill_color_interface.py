48a2866e9bef6dfc8dd31e8599a8e7a055a5b2d8
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-09 16:57:59 +0900

    #568 Append the `Examples` sections to the `FillColorInterface` class interfaces.

--- apysc/_display/fill_color_interface.py
+++ apysc/_display/fill_color_interface.py
@@ -31,6 +31,18 @@ class FillColorInterface(
         ----------
         - Graphics fill_color interface document
             - https://simon-ritchie.github.io/apysc/graphics_fill_color.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> rectangle.fill_color = ap.String('#f0a')
+        >>> rectangle.fill_color
+        String('#ff00aa')
         """
         import apysc as ap
         with ap.DebugInfo(

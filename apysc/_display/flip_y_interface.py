a9f88f1e040d69fc8c65ffd072920514e77f4770
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-09 17:16:15 +0900

    #568 Append the `Examples` sections to the `FlipYInterface` class interfaces.

--- apysc/_display/flip_y_interface.py
+++ apysc/_display/flip_y_interface.py
@@ -53,6 +53,22 @@ class FlipYInterface(
         ----------
         - GraphicsBase flip_x and flip_y interfaces document
             - https://bit.ly/31F9lHv
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> polygon: ap.Polygon = sprite.graphics.draw_polygon(
+        ...     points=[
+        ...         ap.Point2D(x=0, y=0),
+        ...         ap.Point2D(x=50, y=0),
+        ...         ap.Point2D(x=25, y=50),
+        ...     ])
+        >>> polygon.flip_y = ap.Boolean(True)
+        >>> polygon.flip_y
+        Boolean(True)
         """
         import apysc as ap
         with ap.DebugInfo(

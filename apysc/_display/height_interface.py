5de10c51e31fd8257ffaea5a7d8eea50286e43b1
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-10 10:19:02 +0900

    #568 Append the `Examples` sections to the `HeightInterface` class interfaces.

--- apysc/_display/height_interface.py
+++ apysc/_display/height_interface.py
@@ -49,6 +49,18 @@ class HeightInterface(
         -------
         height : Int
             This instance's height.
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> rectangle.height = ap.Int(100)
+        >>> rectangle.height
+        Int(100)
         """
         import apysc as ap
         with ap.DebugInfo(

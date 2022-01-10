3e03723030b75df3c65789ade5b4e7eb5f926c69
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-10 10:59:50 +0900

    #568 Append the `Examples` sections to the `LineDashSetting` class interfaces.

--- apysc/_display/line_dash_setting.py
+++ apysc/_display/line_dash_setting.py
@@ -34,6 +34,22 @@ class LineDashSetting(Dictionary[str, Int]):
         ----------
         - Graphics line_style interface document
             - https://bit.ly/3zauILT
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.line_style(color='#fff', thickness=10)
+        >>> line: ap.Line = sprite.graphics.draw_line(
+        ...     x_start=50, y_start=50, x_end=150, y_end=50)
+        >>> line.line_dash_setting = ap.LineDashSetting(
+        ...     dash_size=5, space_size=2)
+        >>> line.line_dash_setting.dash_size
+        Int(5)
+
+        >>> line.line_dash_setting.space_size
+        Int(2)
         """
         import apysc as ap
         with ap.DebugInfo(

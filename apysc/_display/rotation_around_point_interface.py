5d8756e8159ee501a722d3f72673e43857f5471a
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-10 15:59:50 +0900

    #568 Append the `Examples` sections to the `RotationAroundPointInterface` class interfaces.

--- apysc/_display/rotation_around_point_interface.py
+++ apysc/_display/rotation_around_point_interface.py
@@ -45,6 +45,21 @@ class RotationAroundPointInterface(
         ----------
         - GraphicsBase rotate_around_point interfaces document
             - https://bit.ly/37TDwKs
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> x: ap.Int = ap.Int(100)
+        >>> y: ap.Int = ap.Int(100)
+        >>> rectangle.set_rotation_around_point(
+        ...     rotation=ap.Int(45), x=x, y=y)
+        >>> rectangle.get_rotation_around_point(x=x, y=y)
+        Int(45)
         """
         import apysc as ap
         with ap.DebugInfo(

3f5123d8edbe7bbe55d4d313fa4ae96033fc9279
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-11 21:25:48 +0900

    #568 Append the `Examples` sections to the `PathBezier2D` class interfaces.

--- apysc/_geom/path_bezier_2d.py
+++ apysc/_geom/path_bezier_2d.py
@@ -44,6 +44,20 @@ class PathBezier2D(
         relative : bool or Boolean, default False
             The boolean value indicating whether the path
             coordinates are relative or not (absolute).
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.line_style(color='#fff', thickness=3)
+        >>> path: ap.Path = sprite.graphics.draw_path(
+        ...     path_data_list=[
+        ...         ap.PathMoveTo(x=0, y=50),
+        ...         ap.PathBezier2D(
+        ...             control_x=50, control_y=0,
+        ...             dest_x=100, dest_y=50),
+        ...     ])
         """
         import apysc as ap
         with ap.DebugInfo(
@@ -119,6 +133,27 @@ class PathBezier2D(
         relative : bool or Boolean, default False
             The boolean value indicating whether the path
             coordinates are relative or not (absolute).
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> bezier_2d: ap.PathBezier2D = ap.PathBezier2D(
+        ...     control_x=50, control_y=0,
+        ...     dest_x=100, dest_y=50)
+        >>> bezier_2d.update_path_data(
+        ...     control_x=150, control_y=100,
+        ...     dest_x=200, dest_y=150)
+        >>> bezier_2d.control_x
+        Int(150)
+
+        >>> bezier_2d.control_y
+        Int(100)
+
+        >>> bezier_2d.dest_x
+        Int(200)
+
+        >>> bezier_2d.dest_y
+        Int(150)
         """
         import apysc as ap
         with ap.DebugInfo(

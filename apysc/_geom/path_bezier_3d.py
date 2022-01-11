2cd371a04033445c38198491684db17e86a17a09
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-11 21:48:30 +0900

    #568 Append the `Examples` sections to the `PathBezier3D` class interfaces.

--- apysc/_geom/path_bezier_3d.py
+++ apysc/_geom/path_bezier_3d.py
@@ -53,6 +53,24 @@ class PathBezier3D(
         relative : bool or Boolean, default False
             The boolean value indicating whether the path
             coordinates are relative or not (absolute).
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.line_style(color='#fff', thickness=3)
+        >>> path: ap.Path = sprite.graphics.draw_path(
+        ...     path_data_list=[
+        ...         ap.PathMoveTo(x=0, y=50),
+        ...         ap.PathBezier3D(
+        ...             control_x1=0, control_y1=0,
+        ...             control_x2=50, control_y2=0,
+        ...             dest_x=50, dest_y=50),
+        ...         ap.PathBezier3DContinual(
+        ...             control_x=100, control_y=100,
+        ...             dest_x=100, dest_y=50),
+        ...     ])
         """
         import apysc as ap
         with ap.DebugInfo(
@@ -142,6 +160,35 @@ class PathBezier3D(
         relative : bool or Boolean, default False
             The boolean value indicating whether the path
             coordinates are relative or not (absolute).
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> bezier_3d_continual = ap.PathBezier3D(
+        ...     control_x1=0, control_y1=0,
+        ...     control_x2=50, control_y2=0,
+        ...     dest_x=50, dest_y=50)
+        >>> bezier_3d_continual.update_path_data(
+        ...     control_x1=100, control_y1=100,
+        ...     control_x2=150, control_y2=100,
+        ...     dest_x=150, dest_y=150)
+        >>> bezier_3d_continual.control_x1
+        Int(100)
+
+        >>> bezier_3d_continual.control_y1
+        Int(100)
+
+        >>> bezier_3d_continual.control_x2
+        Int(150)
+
+        >>> bezier_3d_continual.control_y2
+        Int(100)
+
+        >>> bezier_3d_continual.dest_x
+        Int(150)
+
+        >>> bezier_3d_continual.dest_y
+        Int(150)
         """
         import apysc as ap
         with ap.DebugInfo(

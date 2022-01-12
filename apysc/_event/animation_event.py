c8ee6110778c2ae617ae1c496c5b9001c7d40b89
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-12 21:44:11 +0900

    #568 Append the `Examples` sections to the `AnimationEvent` class interfaces.

--- apysc/_event/animation_event.py
+++ apysc/_event/animation_event.py
@@ -28,6 +28,23 @@ class AnimationEvent(Event, Generic[_T]):
         ----------
         this : AnimationBase
             Animation setting instance.
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> def on_animation_complete(
+        ...         e: ap.AnimationEvent[ap.Rectangle],
+        ...         options: dict) -> None:
+        ...     rectangle: ap.Rectangle = e.this.target
+        ...     ...
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_x(
+        ...     x=100).animation_complete(on_animation_complete)
+
         """
         import apysc as ap
         with ap.DebugInfo(
@@ -46,5 +63,21 @@ class AnimationEvent(Event, Generic[_T]):
         -------
         this : AnimationBase
             Instance that linstening this event.
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> def on_animation_complete(
+        ...         e: ap.AnimationEvent[ap.Rectangle],
+        ...         options: dict) -> None:
+        ...     rectangle: ap.Rectangle = e.this.target
+        ...     ...
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_x(
+        ...     x=100).animation_complete(on_animation_complete)
         """
         return self._this

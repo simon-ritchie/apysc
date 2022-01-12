263abdb0ba2a954e615117de17ac77885a9de7f1
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-12 21:19:30 +0900

    #568 Append the `Examples` sections to the `PathVertical` class interfaces.

--- apysc/_geom/path_vertical.py
+++ apysc/_geom/path_vertical.py
@@ -28,6 +28,18 @@ class PathVertical(PathDataBase, PathYInterface):
         relative : bool or Boolean, default False
             The boolean value indicating whether the path
             coordinates are relative or not (absolute).
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.line_style(color='#fff', thickness=3)
+        >>> path: ap.Path = sprite.graphics.draw_path(
+        ...     path_data_list=[
+        ...         ap.PathMoveTo(x=0, y=50),
+        ...         ap.PathVertical(y=100),
+        ...     ])
         """
         import apysc as ap
         with ap.DebugInfo(
@@ -77,6 +89,17 @@ class PathVertical(PathDataBase, PathYInterface):
         relative : bool or Boolean
             The boolean value indicating whether the path
             coordinates are relative or not (absolute).
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.line_style(color='#fff', thickness=3)
+        >>> path_vertical: ap.PathVertical = ap.PathVertical(y=50)
+        >>> path_vertical.update_path_data(y=100)
+        >>> path_vertical.y
+        Int(100)
         """
         import apysc as ap
         with ap.DebugInfo(

d747febf08e48298e1001c754fb167eb9d985549
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-12 20:08:43 +0900

    #568 Append the `Examples` sections to the `PathClose` class interfaces.

--- apysc/_geom/path_close.py
+++ apysc/_geom/path_close.py
@@ -13,6 +13,20 @@ class PathClose(PathDataBase):
     def __init__(self) -> None:
         """
         Path data class for the svg's `close path` (Z).
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.line_style(color='#fff', thickness=3)
+        >>> path: ap.Path = sprite.graphics.draw_path(
+        ...     path_data_list=[
+        ...         ap.PathMoveTo(x=0, y=00),
+        ...         ap.PathLineTo(x=50, y=0),
+        ...         ap.PathLineTo(x=50, y=50),
+        ...         ap.PathClose(),
+        ...     ])
         """
         import apysc as ap
         with ap.DebugInfo(

5b7d45f2afd1d21f73ef65a9e04e81549664d45f
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-13 21:23:22 +0900

    #568 Append the `Examples` sections to the `PathBezier3D` class docstring.

--- apysc/_geom/path_bezier_3d.py
+++ apysc/_geom/path_bezier_3d.py
@@ -21,6 +21,24 @@ class PathBezier3D(
         PathControlX2Interface, PathControlY2Interface):
     """
     Path data class for the svg's `3D bezier curve` (C).
+
+    Examples
+    --------
+    >>> import apysc as ap
+    >>> stage: ap.Stage = ap.Stage()
+    >>> sprite: ap.Sprite = ap.Sprite()
+    >>> sprite.graphics.line_style(color='#fff', thickness=3)
+    >>> path: ap.Path = sprite.graphics.draw_path(
+    ...     path_data_list=[
+    ...         ap.PathMoveTo(x=0, y=50),
+    ...         ap.PathBezier3D(
+    ...             control_x1=0, control_y1=0,
+    ...             control_x2=50, control_y2=0,
+    ...             dest_x=50, dest_y=50),
+    ...         ap.PathBezier3DContinual(
+    ...             control_x=100, control_y=100,
+    ...             dest_x=100, dest_y=50),
+    ...     ])
     """
 
     def __init__(

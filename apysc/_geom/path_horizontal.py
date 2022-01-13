6d330962945a33b7ed68cffecf9e389234bdd472
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-13 21:25:45 +0900

    #568 Append the `Examples` sections to the `PathHorizontal` class docstring.

--- apysc/_geom/path_horizontal.py
+++ apysc/_geom/path_horizontal.py
@@ -13,6 +13,18 @@ from apysc._type.string import String
 class PathHorizontal(PathDataBase, PathXInterface):
     """
     Path data class for the svg's `horizontal line` (H).
+
+    Examples
+    --------
+    >>> import apysc as ap
+    >>> stage: ap.Stage = ap.Stage()
+    >>> sprite: ap.Sprite = ap.Sprite()
+    >>> sprite.graphics.line_style(color='#fff', thickness=3)
+    >>> path: ap.Path = sprite.graphics.draw_path(
+    ...     path_data_list=[
+    ...         ap.PathMoveTo(x=0, y=50),
+    ...         ap.PathHorizontal(x=50),
+    ...     ])
     """
 
     def __init__(

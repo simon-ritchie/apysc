e061801f1fbdcc9613f6be4d448710c0764a1139
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-14 21:16:06 +0900

    #568 Append the `Examples` sections to the `MouseDownInterface` class interfaces.

--- apysc/_event/mouse_down_interface.py
+++ apysc/_event/mouse_down_interface.py
@@ -42,6 +42,20 @@ class MouseDownInterface(MouseEventInterfaceBase):
             - https://bit.ly/3zgjOnF
         - About the handler options’ type document
             - https://bit.ly/39tnYxC
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> def on_mousedown(
+        ...         e: ap.MouseEvent[ap.Rectangle], options: dict) -> None:
+        ...     rectangle: ap.Rectangle = e.this
+        ...     rectangle.fill_color = ap.String('#f0a')
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.mousedown(on_mousedown)
         """
         import apysc as ap
         from apysc._validation.variable_name_validation import \
@@ -93,6 +107,21 @@ class MouseDownInterface(MouseEventInterfaceBase):
         ----------
         - Mousedown and mouseup interfaces document
             - https://bit.ly/3zgjOnF
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> def on_mousedown(
+        ...         e: ap.MouseEvent[ap.Rectangle], options: dict) -> None:
+        ...     rectangle: ap.Rectangle = e.this
+        ...     rectangle.fill_color = ap.String('#f0a')
+        ...     rectangle.unbind_mousedown(on_mousedown)
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.mousedown(on_mousedown)
         """
         import apysc as ap
         with ap.DebugInfo(
@@ -111,6 +140,21 @@ class MouseDownInterface(MouseEventInterfaceBase):
         ----------
         - Mousedown and mouseup interfaces document
             - https://bit.ly/3zgjOnF
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> def on_mousedown(
+        ...         e: ap.MouseEvent[ap.Rectangle], options: dict) -> None:
+        ...     rectangle: ap.Rectangle = e.this
+        ...     rectangle.fill_color = ap.String('#f0a')
+        ...     rectangle.unbind_mousedown_all()
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.mousedown(on_mousedown)
         """
         import apysc as ap
         with ap.DebugInfo(

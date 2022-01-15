63d486abef1dc1d396ad129506a2060d092361b5
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-15 20:35:19 +0900

    #568 Append the `Examples` section to the `AnimationLineAlphaInterface` class docstring.

--- apysc/_animation/animation_line_alpha_interface.py
+++ apysc/_animation/animation_line_alpha_interface.py
@@ -59,6 +59,22 @@ class AnimationLineAlphaInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> sprite.graphics.line_style(
+        ...     color='#fff', thickness=5, alpha=1.0)
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_line_alpha(
+        ...     alpha=0.0,
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
         """
         animation_line_alpha: AnimationLineAlpha = AnimationLineAlpha(
             target=self,

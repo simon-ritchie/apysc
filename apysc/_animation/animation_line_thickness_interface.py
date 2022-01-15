72a506f08f073c791b56ff97926b2781b2ea43e0
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-15 20:53:02 +0900

    #568 Append the `Examples` section to the `AnimationLineThicknessInterface` class docstring.

--- apysc/_animation/animation_line_thickness_interface.py
+++ apysc/_animation/animation_line_thickness_interface.py
@@ -58,6 +58,22 @@ class AnimationLineThicknessInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> sprite.graphics.line_style(
+        ...     color='#fff', thickness=1)
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_line_thickness(
+        ...     thickness=6,
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
         """
         animation_line_thickness: AnimationLineThickness = \
             AnimationLineThickness(

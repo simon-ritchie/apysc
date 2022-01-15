04bc0439acb69e4426815aeba3206d681602a14f
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-15 11:24:28 +0900

    #568 Append the `Examples` and `References` sections to the `PreventDefaultInterface` class interfaces.

--- apysc/_event/prevent_default_interface.py
+++ apysc/_event/prevent_default_interface.py
@@ -9,6 +9,27 @@ class PreventDefaultInterface(VariableNameInterface):
     def prevent_default(self) -> None:
         """
         Prevent event's default behavior.
+
+        Refenreces
+        ----------
+        - Event class prevent_default and stop_propagation interfaces document
+            - https://bit.ly/3qqd4lH
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> def on_click(
+        ...         e: ap.MouseEvent[ap.Rectangle], options: dict) -> None:
+        ...     e.prevent_default()
+        ...     rectangle: ap.Rectangle = e.this
+        ...     rectangle.fill_color = ap.String('#f0a')
+        ...     rectangle.unbind_mouseup_all()
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.click(on_click)
         """
         import apysc as ap
         with ap.DebugInfo(

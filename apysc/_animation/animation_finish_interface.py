80192a7f6ed3769cc07e1e632fabf73a59d00440
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-15 19:05:48 +0900

    #568 Append the `Examples` section to the `AnimationFinishInterface` class docstring.

--- apysc/_animation/animation_finish_interface.py
+++ apysc/_animation/animation_finish_interface.py
@@ -15,6 +15,30 @@ class AnimationFinishInterface(VariableNameInterface):
         ----------
         - animation_finish interface document
             - https://simon-ritchie.github.io/apysc/animation_finish.html
+
+        Examples
+        --------
+        >>> from typing_extensions import TypedDict
+        >>> import apysc as ap
+        >>> class RectOptions(TypedDict):
+        ...     rectangle: ap.Rectangle
+        >>> def on_timer(
+        ...         e: ap.TimerEvent,
+        ...         options: RectOptions) -> None:
+        ...     rectangle: ap.Rectangle = options['rectangle']
+        ...     rectangle.animation_finish()
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_x(
+        ...     x=100,
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
+        >>> options: RectOptions = {'rectangle': rectangle}
+        >>> ap.Timer(on_timer, delay=750, options=options).start()
         """
         import apysc as ap
         with ap.DebugInfo(

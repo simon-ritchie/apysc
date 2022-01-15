13b1d28d162c5e1afc8d0214c2678d12cecec7cf
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-15 20:43:12 +0900

    #568 Append the `Examples` section to the `AnimationLineColorInterface` class docstring.

--- apysc/_animation/animation_line_color_interface.py
+++ apysc/_animation/animation_line_color_interface.py
@@ -32,7 +32,7 @@ class AnimationLineColorInterface(AnimationInterfaceBase):
 
         Parameters
         ----------
-        line_color : str or string
+        line_color : str or String
             The final line color (hex color code) of the animation.
         duration : int or Int, default 3000
             Milliseconds before an animation ends.
@@ -62,6 +62,22 @@ class AnimationLineColorInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> sprite.graphics.line_style(
+        ...     color='#fff', thickness=5)
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_line_color(
+        ...     line_color='#0af',
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
         """
         animation_line_color: AnimationLineColor = AnimationLineColor(
             target=self,

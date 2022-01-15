21e375d658b9d49cb6c2c87d2a507a3be9ae5073
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-15 11:29:58 +0900

    #568 Append the `Examples` and `References` sections to the `StopPropagationInterface` class interfaces.

--- apysc/_event/stop_propagation_interface.py
+++ apysc/_event/stop_propagation_interface.py
@@ -9,6 +9,25 @@ class StopPropagationInterface(VariableNameInterface):
     def stop_propagation(self) -> None:
         """
         Stop event propagation.
+
+        Refenreces
+        ----------
+        - Event class prevent_default and stop_propagation interfaces document
+            - https://bit.ly/3qqd4lH
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> def on_click(e: ap.MouseEvent, options: dict) -> None:
+        ...     e.stop_propagation()
+        ...     ap.trace('Clicked!')
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = sprite.click(on_click)
+        >>> _ = rectangle.click(on_click)
         """
         import apysc as ap
         with ap.DebugInfo(

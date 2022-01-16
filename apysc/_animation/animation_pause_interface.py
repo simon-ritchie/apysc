9ce45c8582b70735f926243c30da0bb868568222
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 11:20:43 +0900

    #568 Append the `Examples` section to the `AnimationPauseInterface` class docstring.

--- apysc/_animation/animation_pause_interface.py
+++ apysc/_animation/animation_pause_interface.py
@@ -14,6 +14,30 @@ class AnimationPauseInterface(VariableNameInterface):
         ----------
         - animation_pause and animation_play interfaces document
             - https://bit.ly/3m2Xh8Y
+
+        Examples
+        --------
+        >>> from typing_extensions import TypedDict
+        >>> import apysc as ap
+        >>> class RectOptions(TypedDict):
+        ...     rectangle: ap.Rectangle
+        >>> def on_timer(
+        ...         e: ap.TimerEvent,
+        ...         options: RectOptions) -> None:
+        ...     rectangle: ap.Rectangle = options['rectangle']
+        ...     rectangle.animation_pause()
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_x(
+        ...     x=100,
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
+        >>> options: RectOptions = {'rectangle': rectangle}
+        >>> ap.Timer(on_timer, delay=750, options=options).start()
         """
         import apysc as ap
         with ap.DebugInfo(

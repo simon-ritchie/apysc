53a6e6388616ab0ddd9944b02f621d2cd9d06371
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 13:46:42 +0900

    #568 Append the `Examples` section to the `AnimationTimeInterface` class docstring.

--- apysc/_animation/animation_time_interface.py
+++ apysc/_animation/animation_time_interface.py
@@ -20,6 +20,33 @@ class AnimationTimeInterface(VariableNameInterface):
         ----------
         - animation_time interface document
             - https://simon-ritchie.github.io/apysc/animation_time.html
+
+        Examples
+        --------
+        >>> from typing_extensions import TypedDict
+        >>> import apysc as ap
+        >>> class RectOptions(TypedDict):
+        ...     rectangle: ap.Rectangle
+        >>> def on_timer(
+        ...         e: ap.TimerEvent,
+        ...         options: RectOptions) -> None:
+        ...     rectangle: ap.Rectangle = options['rectangle']
+        ...     animation_time: ap.Number = rectangle.animation_time()
+        ...     ap.trace('animation_time:', animation_time)
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_x(
+        ...     x=100,
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
+        >>> options: RectOptions = {'rectangle': rectangle}
+        >>> ap.Timer(
+        ...     on_timer, delay=ap.FPS.FPS_60,
+        ...     options=options).start()
         """
         import apysc as ap
         with ap.DebugInfo(

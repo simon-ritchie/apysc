2df12ce2ea2cbf29b010037a96c25545fbbb2092
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 12:05:56 +0900

    #568 Append the `Examples` section to the `AnimationRotationAroundPointInterface` class docstring.

--- apysc/_animation/animation_rotation_around_point_interface.py
+++ apysc/_animation/animation_rotation_around_point_interface.py
@@ -66,6 +66,22 @@ class AnimationRotationAroundPointInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_rotation_around_point(
+        ...     rotation_around_point=90,
+        ...     x=ap.Int(100),
+        ...     y=ap.Int(100),
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
         """
         animation_rotation_around_point: AnimationRotationAroundPoint = \
             AnimationRotationAroundPoint(

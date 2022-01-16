73b4440d25f13c8a3cf0234b6ec54519e25d12d1
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 12:38:00 +0900

    #568 Append the `Examples` section to the `AnimationScaleXFromCenter` class docstring.

--- apysc/_animation/animation_scale_x_from_center.py
+++ apysc/_animation/animation_scale_x_from_center.py
@@ -17,6 +17,22 @@ _T = TypeVar('_T', bound=VariableNameInterface)
 class AnimationScaleXFromCenter(AnimationBase[_T], Generic[_T]):
     """
     The animation class for a scale-x.
+
+    Examples
+    --------
+    >>> import apysc as ap
+    >>> stage: ap.Stage = ap.Stage()
+    >>> sprite: ap.Sprite = ap.Sprite()
+    >>> sprite.graphics.begin_fill(color='#0af')
+    >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+    ...     x=50, y=50, width=50, height=50)
+    >>> animation: ap.AnimationScaleXFromCenter
+    >>> animation = rectangle.animation_scale_x_from_center(
+    ...     scale_x_from_center=0.5,
+    ...     duration=1500,
+    ...     easing=ap.Easing.EASE_OUT_QUINT,
+    ... )
+    >>> _ = animation.start()
     """
 
     _scale_x_from_center: Number

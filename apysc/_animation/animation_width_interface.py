1cec7645b8f9da2db0763a1571391618faed0f95
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 14:02:59 +0900

    #568 Append the `Examples` section to the `AnimationWidthInterface` class docstring.

--- apysc/_animation/animation_width_interface.py
+++ apysc/_animation/animation_width_interface.py
@@ -58,6 +58,20 @@ class AnimationWidthInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_width(
+        ...     width=100,
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
         """
         animation_width: AnimationWidth = AnimationWidth(
             target=self,

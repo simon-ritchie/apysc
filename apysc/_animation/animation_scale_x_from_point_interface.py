03c928353ae148fc8a43270c95901946a5395949
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 12:41:52 +0900

    #568 Fix the target classes mistakes.

--- apysc/_animation/animation_scale_x_from_point_interface.py
+++ apysc/_animation/animation_scale_x_from_point_interface.py
@@ -64,20 +64,6 @@ class AnimationScaleXFromPointInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
-
-        Examples
-        --------
-        >>> import apysc as ap
-        >>> stage: ap.Stage = ap.Stage()
-        >>> sprite: ap.Sprite = ap.Sprite()
-        >>> sprite.graphics.begin_fill(color='#0af')
-        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
-        ...     x=50, y=50, width=50, height=50)
-        >>> _ = rectangle.animation_scale_y_from_center(
-        ...     scale_y_from_center=0.5,
-        ...     duration=1500,
-        ...     easing=ap.Easing.EASE_OUT_QUINT,
-        ... ).start()
         """
         animation_scale_x_from_point: AnimationScaleXFromPoint = \
             AnimationScaleXFromPoint(

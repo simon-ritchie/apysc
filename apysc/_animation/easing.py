04fc44c2624344c06e70320bb07c7a7b5812245c
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 14:36:00 +0900

    #568 Append the `Examples` section to the `Easing` class docstring.

--- apysc/_animation/easing.py
+++ apysc/_animation/easing.py
@@ -7,6 +7,20 @@ from enum import Enum
 class Easing(Enum):
     """
     Enum class for the easing setting.
+
+    Examples
+    --------
+    >>> import apysc as ap
+    >>> stage: ap.Stage = ap.Stage()
+    >>> sprite: ap.Sprite = ap.Sprite()
+    >>> sprite.graphics.begin_fill(color='#0af')
+    >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+    ...     x=50, y=50, width=50, height=50)
+    >>> _ = rectangle.animation_y(
+    ...     y=100,
+    ...     duration=1500,
+    ...     easing=ap.Easing.EASE_OUT_QUINT,
+    ... ).start()
     """
     LINEAR = 'function(x) {return x;}'
 

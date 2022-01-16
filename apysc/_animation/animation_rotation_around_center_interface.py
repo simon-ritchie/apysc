df9bee86472c3fa8e10eb85034904b96407f8779
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 11:53:58 +0900

    #568 Append the `Examples` section to the `AnimationRotationAroundCenterInterface` class docstring.

--- apysc/_animation/animation_rotation_around_center_interface.py
+++ apysc/_animation/animation_rotation_around_center_interface.py
@@ -60,6 +60,20 @@ class AnimationRotationAroundCenterInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_rotation_around_center(
+        ...     rotation_around_center=90,
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
         """
         animation_rotation_around_center: AnimationRotationAroundCenter = \
             AnimationRotationAroundCenter(

a34e7352ac93a1a27c357e753344e2892c5cd507
simon-ritchie <antisocial.sid2@gmail.com>
2022-01-16 12:49:34 +0900

    #568 Append the `Examples` section to the `AnimationScaleYFromPointInterface` class docstring.

--- apysc/_animation/animation_scale_y_from_point_interface.py
+++ apysc/_animation/animation_scale_y_from_point_interface.py
@@ -64,6 +64,21 @@ class AnimationScaleYFromPointInterface(AnimationInterfaceBase):
             - https://simon-ritchie.github.io/apysc/animation_parallel.html
         - Easing enum document
             - https://simon-ritchie.github.io/apysc/easing_enum.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage()
+        >>> sprite: ap.Sprite = ap.Sprite()
+        >>> sprite.graphics.begin_fill(color='#0af')
+        >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+        ...     x=50, y=50, width=50, height=50)
+        >>> _ = rectangle.animation_scale_y_from_point(
+        ...     scale_y_from_point=0.5,
+        ...     y=ap.Int(100),
+        ...     duration=1500,
+        ...     easing=ap.Easing.EASE_OUT_QUINT,
+        ... ).start()
         """
         animation_scale_y_from_point: AnimationScaleYFromPoint = \
             AnimationScaleYFromPoint(

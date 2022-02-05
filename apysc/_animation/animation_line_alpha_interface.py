33809c73b4328a5976d244bd6501cfb55f23eb09
simon-ritchie <antisocial.sid2@gmail.com>
2022-02-05 12:38:21 +0900

    #607 Append the `animation_line_alpha` API document.

--- apysc/_animation/animation_line_alpha_interface.py
+++ apysc/_animation/animation_line_alpha_interface.py
@@ -29,11 +29,11 @@ class AnimationLineAlphaInterface(AnimationInterfaceBase):
 
         Parameters
         ----------
-        alpha : float or Number
+        alpha : Number or float
             The final line alpha of the animation.
-        duration : int or Int, default 3000
+        duration : Int or int, default 3000
             Milliseconds before an animation ends.
-        delay : int or Int, default 0
+        delay : Int or int, default 0
             Milliseconds before an animation starts.
         easing : Easing, default Easing.LINEAR
             Easing setting.

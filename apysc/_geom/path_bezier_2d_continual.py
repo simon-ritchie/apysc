6aa952c3bb169228251d11dc71c82d18b4209652
simon-ritchie <antisocial.sid2@gmail.com>
2022-02-14 20:45:29 +0900

    #646 Switch each interface debug information setting to the decorator interface and apply each lint.

--- apysc/_geom/path_bezier_2d_continual.py
+++ apysc/_geom/path_bezier_2d_continual.py
@@ -8,6 +8,7 @@ from typing import Union
 from apysc._geom.path_data_base import PathDataBase
 from apysc._geom.path_x_interface import PathXInterface
 from apysc._geom.path_y_interface import PathYInterface
+from apysc._html.debug_mode import add_debug_info_setting
 from apysc._type.boolean import Boolean
 from apysc._type.int import Int
 from apysc._type.string import String
@@ -33,6 +34,8 @@ class PathBezier2DContinual(PathDataBase, PathXInterface, PathYInterface):
     ...     ])
     """
 
+    @add_debug_info_setting(  # type: ignore[misc]
+        module_name=__name__, class_name='PathBezier2DContinual')
     def __init__(
             self, x: Union[int, Int], y: Union[int, Int], *,
             relative: Union[bool, Boolean] = False) -> None:
@@ -64,19 +67,17 @@ class PathBezier2DContinual(PathDataBase, PathXInterface, PathYInterface):
         ...         ap.PathBezier2DContinual(x=150, y=50),
         ...     ])
         """
-        import apysc as ap
-        with ap.DebugInfo(
-                callable_='__init__', locals_=locals(),
-                module_name=__name__, class_=PathBezier2DContinual):
-            from apysc._converter.to_apysc_val_from_builtin import \
-                get_copied_int_from_builtin_val
-            from apysc._geom.path_label import PathLabel
-            super(PathBezier2DContinual, self).__init__(
-                path_label=PathLabel.BEZIER_2D_CONTINUAL,
-                relative=relative)
-            self.x = get_copied_int_from_builtin_val(integer=x)
-            self.y = get_copied_int_from_builtin_val(integer=y)
+        from apysc._converter.to_apysc_val_from_builtin import \
+            get_copied_int_from_builtin_val
+        from apysc._geom.path_label import PathLabel
+        super(PathBezier2DContinual, self).__init__(
+            path_label=PathLabel.BEZIER_2D_CONTINUAL,
+            relative=relative)
+        self.x = get_copied_int_from_builtin_val(integer=x)
+        self.y = get_copied_int_from_builtin_val(integer=y)
 
+    @add_debug_info_setting(  # type: ignore[misc]
+        module_name=__name__, class_name='PathBezier2DContinual')
     def _get_svg_str(self) -> str:
         """
         Get a path's SVG string created with the current setting.
@@ -86,22 +87,20 @@ class PathBezier2DContinual(PathDataBase, PathXInterface, PathYInterface):
         svg_str : str
             A path's SVG string created with the current setting.
         """
-        import apysc as ap
-        with ap.DebugInfo(
-                callable_=self._get_svg_str, locals_=locals(),
-                module_name=__name__, class_=PathBezier2DContinual):
-            from apysc._type import value_util
-            svg_char: String = self._get_svg_char()
-            svg_char_str: str = value_util.get_value_str_for_expression(
-                value=svg_char)
-            x_str: str = value_util.get_value_str_for_expression(
-                value=self._x)
-            y_str: str = value_util.get_value_str_for_expression(
-                value=self._y)
-            svg_str: str = (
-                f'{svg_char_str} + String({x_str}) + " " + String({y_str})')
-            return svg_str
+        from apysc._type import value_util
+        svg_char: String = self._get_svg_char()
+        svg_char_str: str = value_util.get_value_str_for_expression(
+            value=svg_char)
+        x_str: str = value_util.get_value_str_for_expression(
+            value=self._x)
+        y_str: str = value_util.get_value_str_for_expression(
+            value=self._y)
+        svg_str: str = (
+            f'{svg_char_str} + String({x_str}) + " " + String({y_str})')
+        return svg_str
 
+    @add_debug_info_setting(  # type: ignore[misc]
+        module_name=__name__, class_name='PathBezier2DContinual')
     def update_path_data(
             self, x: Union[int, Int], y: Union[int, Int],
             *,
@@ -130,19 +129,17 @@ class PathBezier2DContinual(PathDataBase, PathXInterface, PathYInterface):
         >>> bezier_2d_continual.y
         Int(100)
         """
-        import apysc as ap
-        with ap.DebugInfo(
-                callable_=self.update_path_data, locals_=locals(),
-                module_name=__name__, class_=PathBezier2DContinual):
-            from apysc._converter.to_apysc_val_from_builtin import \
-                get_copied_boolean_from_builtin_val
-            from apysc._converter.to_apysc_val_from_builtin import \
-                get_copied_int_from_builtin_val
-            self.x = get_copied_int_from_builtin_val(integer=x)
-            self.y = get_copied_int_from_builtin_val(integer=y)
-            self.relative = get_copied_boolean_from_builtin_val(
-                bool_val=relative)
+        from apysc._converter.to_apysc_val_from_builtin import \
+            get_copied_boolean_from_builtin_val
+        from apysc._converter.to_apysc_val_from_builtin import \
+            get_copied_int_from_builtin_val
+        self.x = get_copied_int_from_builtin_val(integer=x)
+        self.y = get_copied_int_from_builtin_val(integer=y)
+        self.relative = get_copied_boolean_from_builtin_val(
+            bool_val=relative)
 
+    @add_debug_info_setting(  # type: ignore[misc]
+        module_name=__name__, class_name='PathBezier2DContinual')
     def __eq__(self, other: Any) -> Any:
         """
         Equal comparison method.
@@ -158,17 +155,16 @@ class PathBezier2DContinual(PathDataBase, PathXInterface, PathYInterface):
             Comparison result.
         """
         import apysc as ap
-        with ap.DebugInfo(
-                callable_='__eq__', locals_=locals(),
-                module_name=__name__, class_=PathBezier2DContinual):
-            if not isinstance(other, PathBezier2DContinual):
-                result: ap.Boolean = ap.Boolean(False)
-                return result
-            return (
-                self.x == other.x
-                and self.y == other.y
-                and self.relative == other.relative)
+        if not isinstance(other, PathBezier2DContinual):
+            result: ap.Boolean = ap.Boolean(False)
+            return result
+        return (
+            self.x == other.x
+            and self.y == other.y
+            and self.relative == other.relative)
 
+    @add_debug_info_setting(  # type: ignore[misc]
+        module_name=__name__, class_name='PathBezier2DContinual')
     def __ne__(self, other: Any) -> Any:
         """
         Not equal comparison method.
@@ -184,9 +180,6 @@ class PathBezier2DContinual(PathDataBase, PathXInterface, PathYInterface):
             Comparison result.
         """
         import apysc as ap
-        with ap.DebugInfo(
-                callable_='__ne__', locals_=locals(),
-                module_name=__name__, class_=PathBezier2DContinual):
-            result: ap.Boolean = self == other
-            result = result.not_
-            return result
+        result: ap.Boolean = self == other
+        result = result.not_
+        return result

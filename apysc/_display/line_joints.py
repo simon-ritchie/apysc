9434158f0316a4b3c1547d19f746f7d737bfe65e
simon-ritchie <antisocial.sid2@gmail.com>
2022-02-15 21:24:03 +0900

    #650 Append the `Reference` section to the `LineJoints` class docstring.

--- apysc/_display/line_joints.py
+++ apysc/_display/line_joints.py
@@ -16,6 +16,11 @@ class LineJoints(Enum):
         Line vertices will be rounded.
     BEVEL
         Line vertices will be bevel joint (bevel vertices).
+
+    References
+    ----------
+    - Graphics line_style interface document
+        - https://simon-ritchie.github.io/apysc/graphics_line_style.html
     """
     MITER = 'miter'
     ROUND = 'round'
--- docstring_markdowns/apysc/_display/line_joints.md
+++ docstring_markdowns/apysc/_display/line_joints.md
@@ -6,4 +6,8 @@ Line joints enum's definitions.
 
 ## `LineJoints` class docstring
 
-The class for the line joints' style type definitions.
+The class for the line joints' style type definitions.<hr>
+
+**[References]**
+
+- [Graphics line_style interface document](https://simon-ritchie.github.io/apysc/graphics_line_style.html)

55811015142e739bcee481075a77a2a3bbc43bc8
simon-ritchie <antisocial.sid2@gmail.com>
2022-02-23 21:05:38 +0900

    #640 Fix the `size_validation` module docstring with the `Grammarly`.

--- apysc/_validation/size_validation.py
+++ apysc/_validation/size_validation.py
@@ -11,11 +11,11 @@ from apysc._type.int import Int
 def validate_size_is_int(
         size: Union[int, Int], err_msg: Optional[str] = None) -> None:
     """
-    Check that whether size is integer or not.
+    Check whether a specified size is an integer or not.
 
     Parameters
     ----------
-    size : int or Int
+    size : Int or int
         Target size (width or height) value.
     err_msg : str or None, default None
         The error message that display at error raised.
@@ -23,7 +23,7 @@ def validate_size_is_int(
     Raises
     ------
     ValueError
-        If not integer value is specified.
+        The error message which displays at error raised.
     """
     if isinstance(size, (int, Int)):
         return
@@ -37,19 +37,19 @@ def validate_size_is_int(
 def validate_size_is_gt_zero(
         size: Union[int, Int], err_msg: Optional[str] = None) -> None:
     """
-    Check that whether size is greater than zero or not.
+    Check whether a specified size is greater than zero or not.
 
     Parameters
     ----------
-    size : int or Int
+    size : Int or int
         Target size (width or height) value.
     err_msg : str or None, default None
-        The error message that display at error raised.
+        The error message which displays at error raised.
 
     Raises
     ------
     ValueError
-        If size is less than or equal to zero.
+        If a specified size is less than or equal to zero.
     """
     if size > 0:
         return
@@ -63,19 +63,20 @@ def validate_size_is_gt_zero(
 def validate_size_is_gte_zero(
         size: Union[int, Int], err_msg: Optional[str] = None) -> None:
     """
-    Check that whether size is greater than or equal to zero or not.
+    Check whether a specified size is greater than or equal
+    to zero or not.
 
     Parameters
     ----------
-    size : int or Int
+    size : Int or int
         Target size (width or height) value.
     err_msg : str or None, default None
-        The error message that display at error raised.
+        The error message which displays at error raised.
 
     Raises
     ------
     ValueError
-        If size is less than zero.
+        If a specified size is less than zero.
     """
     if size >= 0:
         return
--- docstring_markdowns/apysc/_validation/size_validation.md
+++ docstring_markdowns/apysc/_validation/size_validation.md
@@ -6,48 +6,48 @@ Size's (width or height) common validation implementation.
 
 ## `validate_size_is_gt_zero` function docstring
 
-Check that whether size is greater than zero or not.<hr>
+Check whether a specified size is greater than zero or not.<hr>
 
 **[Parameters]**
 
-- `size`: int or Int
+- `size`: Int or int
   - Target size (width or height) value.
 - `err_msg`: str or None, default None
-  - The error message that display at error raised.
+  - The error message which displays at error raised.
 
 <hr>
 
 **[Raises]**
 
-- ValueError: If size is less than or equal to zero.
+- ValueError: If a specified size is less than or equal to zero.
 
 ## `validate_size_is_gte_zero` function docstring
 
-Check that whether size is greater than or equal to zero or not.<hr>
+Check whether a specified size is greater than or equal to zero or not.<hr>
 
 **[Parameters]**
 
-- `size`: int or Int
+- `size`: Int or int
   - Target size (width or height) value.
 - `err_msg`: str or None, default None
-  - The error message that display at error raised.
+  - The error message which displays at error raised.
 
 <hr>
 
 **[Raises]**
 
-- ValueError: If size is less than zero.
+- ValueError: If a specified size is less than zero.
 
 ## `validate_size_is_int` function docstring
 
-Check that whether size is integer or not.<hr>
+Check whether a specified size is an integer or not.<hr>
 
 **[Parameters]**
 
-- `size`: int or Int
+- `size`: Int or int
   - Target size (width or height) value.
 - `err_msg`: str or None, default None
-  - The error message that display at error raised.
+  - The error message which displays at error raised.
 
 <hr>
 

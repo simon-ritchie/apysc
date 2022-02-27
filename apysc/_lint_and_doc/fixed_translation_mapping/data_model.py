70aa37feaa66ae0ab9b3e517332088165593d812
simon-ritchie <antisocial.sid2@gmail.com>
2022-02-27 21:05:09 +0900

    #648 Add the function to get a fixed-translation mappings settings module path of a specified language.

--- apysc/_lint_and_doc/fixed_translation_mapping/data_model.py
+++ apysc/_lint_and_doc/fixed_translation_mapping/data_model.py
@@ -2,7 +2,9 @@
 mapping settings.
 """
 
-from typing import List
+from typing import List, Optional
+
+from apysc._lint_and_doc.add_doc_translation_mapping_blank_data import Lang
 
 
 class Mapping:
@@ -68,3 +70,68 @@ class Mappings:
             A target mappings list.
         """
         self.mapping = mappings
+
+
+def get_fixed_translation_str_if_exists(
+        *, key: str, lang: Lang) -> str:
+    """
+    Get a fixed-translation string from a specified language key
+    string if there is a mapping setting.
+
+    Parameters
+    ----------
+    key : str
+        A target key string (source english string).
+    lang : Lang
+        A target language.
+
+    Returns
+    -------
+    translation_str : str
+        A translated string. If there is no fixed-translation
+        mapping setting, this interface returns a blank string.
+    """
+    mappings: Optional[Mappings] = _read_mappings(lang=lang)
+    pass
+
+
+def _read_mappings(*, lang) -> Optional[Mappings]:
+    """
+    Read a fixed-translation mappings settings if it exists.
+
+    Parameters
+    ----------
+    lang : Lang
+        A target language.
+
+    Returns
+    -------
+    mappings : Mappings
+        A read mappings settings. If there is no mappings
+        settings, this interface returns None.
+    """
+    module_path: str = _get_mappings_module_path_from_lang(lang=lang)
+    pass
+
+
+def _get_mappings_module_path_from_lang(*, lang: Lang) -> str:
+    """
+    Get a fixed-translation mappings settings module path
+    of a specified language.
+
+    Parameters
+    ----------
+    lang : Lang
+        A target language.
+
+    Returns
+    -------
+    module_path : str
+        A fixed-translation mappings settings module path
+        of a specified language.
+    """
+    module_path: str = (
+        './apysc/_lint_and_doc/fixed_translation_mapping/'
+        f'{lang.value}.py'
+    )
+    return module_path
--- tests/_lint_and_doc/fixed_translation_mapping/test_data_model.py
+++ tests/_lint_and_doc/fixed_translation_mapping/test_data_model.py
@@ -1,10 +1,13 @@
 from random import randint
 from typing import List
+import os
 
 from retrying import retry
 
 from apysc._lint_and_doc.fixed_translation_mapping.data_model import Mapping, Mappings
+from apysc._lint_and_doc.fixed_translation_mapping import data_model
 from tests.testing_helper import assert_attrs
+from apysc._lint_and_doc.add_doc_translation_mapping_blank_data import Lang
 
 
 class TestMapping:
@@ -45,3 +48,11 @@ class TestMappings:
             },
             any_obj=mappings,
         )
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__get_mappings_module_path_from_lang() -> None:
+    module_path: str = data_model._get_mappings_module_path_from_lang(
+        lang=Lang.JP)
+    assert module_path.endswith('jp.py')
+    assert os.path.isfile(module_path)

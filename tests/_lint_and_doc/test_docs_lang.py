8576a04dfeae418b2dc15c785a8e8df92b7cb5bb
simon-ritchie <antisocial.sid2@gmail.com>
2022-03-06 20:33:27 +0900

    #648 Add the test to check the Languages values are not duplicated.

---
+++ tests/_lint_and_doc/test_docs_lang.py
@@ -0,0 +1,11 @@
+from random import randint
+
+from retrying import retry
+
+from apysc._lint_and_doc.docs_lang import Lang
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_langs_are_not_duplicated() -> None:
+    values_set: set = set([lang.value for lang in Lang])
+    assert len(values_set) == len(Lang)

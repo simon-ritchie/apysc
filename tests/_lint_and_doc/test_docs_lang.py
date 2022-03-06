49e7ccb11142efb57f4ae709f151330c9a9808d8
simon-ritchie <antisocial.sid2@gmail.com>
2022-03-06 21:49:18 +0900

    #648 Add the test to check the language enum values' types are string.

--- tests/_lint_and_doc/test_docs_lang.py
+++ tests/_lint_and_doc/test_docs_lang.py
@@ -9,3 +9,9 @@ from apysc._lint_and_doc.docs_lang import Lang
 def test_langs_are_not_duplicated() -> None:
     values_set: set = set([lang.value for lang in Lang])
     assert len(values_set) == len(Lang)
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_langs_values_are_str_type() -> None:
+    for lang in Lang:
+        assert isinstance(lang.value, str)

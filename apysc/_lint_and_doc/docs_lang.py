c927aa69e7d246a4fb82a0eea446e3d3367242f2
simon-ritchie <antisocial.sid2@gmail.com>
2022-03-10 20:43:34 +0900

    #648 Add the common function to get a language enum value from a specified string value.

--- apysc/_lint_and_doc/docs_lang.py
+++ apysc/_lint_and_doc/docs_lang.py
@@ -8,3 +8,33 @@ class Lang(Enum):
     """Translation target languages definitions.
     """
     JP = 'jp'
+
+
+class _UndefinedStrValue(Exception):
+    pass
+
+
+def get_lang_from_str_value(*, str_value: str) -> Lang:
+    """
+    Get a language enum value from a specified string value.
+
+    Parameters
+    ----------
+    str_value : str
+        A target language string value.
+
+    Returns
+    -------
+    lang : Lang
+        A target language enum value.
+
+    Raises
+    ------
+    _UndefinedStrValue
+        If a specified string value is undefined.
+    """
+    for lang in Lang:
+        if lang.value == str_value:
+            return lang
+    raise _UndefinedStrValue(
+        f'A specified language string value is undefined: {str_value}')
--- tests/_lint_and_doc/test_docs_lang.py
+++ tests/_lint_and_doc/test_docs_lang.py
@@ -3,6 +3,7 @@ from random import randint
 from retrying import retry
 
 from apysc._lint_and_doc.docs_lang import Lang
+from apysc._lint_and_doc import docs_lang
 
 
 @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
@@ -15,3 +16,10 @@ def test_langs_are_not_duplicated() -> None:
 def test_langs_values_are_str_type() -> None:
     for lang in Lang:
         assert isinstance(lang.value, str)
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_get_lang_from_str_value() -> None:
+    lang: Lang = docs_lang.get_lang_from_str_value(
+        str_value=Lang.JP.value)
+    assert lang == Lang.JP

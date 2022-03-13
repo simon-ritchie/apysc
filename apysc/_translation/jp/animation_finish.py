c079e4eedbd559fc6926d9ab92daf6f60ee75aa2
simon-ritchie <antisocial.sid2@gmail.com>
2022-03-13 20:09:15 +0900

    #648 Add the translated document of the `animation_finish.md`.

--- apysc/_translation/jp/animation_finish.py
+++ apysc/_translation/jp/animation_finish.py
@@ -10,36 +10,42 @@ from typing import Dict
 MAPPING: Dict[str, str] = {
 
     '# animation_finish interface':
-    '',
+    '# animation_finish インターフェイス',
 
     'This page explains the `animation_finish` method interface.':
-    '',
+    'このページでは`animation_finish`メソッドのインターフェイスについて説明します。',
 
     '## What interface is this?':
-    '',
+    '## インターフェイス概要',
 
-    'The `animation_finish` interface finishes all running animations and sets the last attribute values of each animation setting.\n\nThis interface exists in the instances that have the animation interfaces (such as the `animation_x`\\, `animation_move`).':  # noqa
-    '',
+    'The `animation_finish` interface finishes all running animations and sets the last attribute values of each animation setting.':  # noqa
+    '`animation_finish`インターフェイスでは実行されている全てのアニメーションにアニメーションの各属性の最終値を設定しアニメーションを終了させます。',  # noqa
+
+    'This interface exists in the instances that have the animation interfaces (such as the `animation_x`\\, `animation_move`).':  # noqa
+    'このインターフェイスは`animation_x`や`animation_move`などのアニメーションのインターフェイスを持つ各クラスのインスタンスに存在します。',  # noqa
 
     '## Basic usage':
-    '',
+    '## 使い方例',
 
     'The following example sets the click event to the rectangle. If you click the rectangle, the x-coordinate animation starts. After 2 seconds have passed, the x-coordinate animation finishes and sets the last x-coordinate value of the animation.':  # noqa
-    '',
+    '以下のコード例では四角にクリックイベントを設定しています。四角をクリックするとX座標のアニメーションが開始するようになっています。アニメーション開始後に2秒経過したらアニメーションは終了し、最終値の座標が設定されます。',  # noqa
 
     '```py\n# runnable\nfrom typing_extensions import TypedDict\n\nimport apysc as ap\n\n\nclass _RectOptions(TypedDict):\n    rectangle: ap.Rectangle\n\n\ndef on_click(e: ap.MouseEvent[ap.Rectangle], options: dict) -> None:\n    """\n    The handler that the rectangle calls when clicked.\n\n    Parameters\n    ----------\n    e : ap.MouseEvent\n        Event instance.\n    options : dict\n        Optional arguments dictionary.\n    """\n    rectangle: ap.Rectangle = e.this\n    rectangle.animation_x(\n        x=300, duration=5000,\n    ).start()\n\n    options_: _RectOptions = {\'rectangle\': rectangle}\n    ap.Timer(\n        on_timer, delay=2000, repeat_count=1, options=options_,\n    ).start()\n\n\ndef on_timer(e: ap.TimerEvent, options: _RectOptions) -> None:\n    """\n    The handler that the timer calls when its ends.\n\n    Parameters\n    ----------\n    e : ap.TimerEvent\n        Event instance.\n    options : dict\n        Optional arguments dictionary.\n    """\n    rectangle: ap.Rectangle = options[\'rectangle\']\n    rectangle.animation_finish()\n\n\nap.Stage(\n    stage_width=400, stage_height=150, background_color=\'#333\',\n    stage_elem_id=\'stage\')\nsprite: ap.Sprite = ap.Sprite()\nsprite.graphics.begin_fill(color=\'#0af\')\n\nrectangle: ap.Rectangle = sprite.graphics.draw_rect(\n    x=50, y=50, width=50, height=50)\nrectangle.click(on_click)\n\nap.save_overall_html(\n    dest_dir_path=\'animation_finish_basic_usage/\')\n```':  # noqa
-    '',
-
-    '<iframe src="static/animation_finish_basic_usage/index.html" width="400" height="150"></iframe>':  # noqa
-    '',
+    '```py\n# runnable\nfrom typing_extensions import TypedDict\n\nimport apysc as ap\n\n\nclass _RectOptions(TypedDict):\n    rectangle: ap.Rectangle\n\n\ndef on_click(e: ap.MouseEvent[ap.Rectangle], options: dict) -> None:\n    """\n    The handler that the rectangle calls when clicked.\n\n    Parameters\n    ----------\n    e : ap.MouseEvent\n        Event instance.\n    options : dict\n        Optional arguments dictionary.\n    """\n    rectangle: ap.Rectangle = e.this\n    rectangle.animation_x(\n        x=300, duration=5000,\n    ).start()\n\n    options_: _RectOptions = {\'rectangle\': rectangle}\n    ap.Timer(\n        on_timer, delay=2000, repeat_count=1, options=options_,\n    ).start()\n\n\ndef on_timer(e: ap.TimerEvent, options: _RectOptions) -> None:\n    """\n    The handler that the timer calls when its ends.\n\n    Parameters\n    ----------\n    e : ap.TimerEvent\n        Event instance.\n    options : dict\n        Optional arguments dictionary.\n    """\n    rectangle: ap.Rectangle = options[\'rectangle\']\n    rectangle.animation_finish()\n\n\nap.Stage(\n    stage_width=400, stage_height=150, background_color=\'#333\',\n    stage_elem_id=\'stage\')\nsprite: ap.Sprite = ap.Sprite()\nsprite.graphics.begin_fill(color=\'#0af\')\n\nrectangle: ap.Rectangle = sprite.graphics.draw_rect(\n    x=50, y=50, width=50, height=50)\nrectangle.click(on_click)\n\nap.save_overall_html(\n    dest_dir_path=\'animation_finish_basic_usage/\')\n```',  # noqa
 
     '## animation_finish API':
-    '',
+    '## animation_finish API',
 
-    '<!-- Docstring: apysc._animation.animation_finish_interface.AnimationFinishInterface.animation_finish -->\n\n<span class="inconspicuous-txt">Note: the document build script generates and updates this API document section automatically. Maybe this section is duplicated compared with previous sections.</span>\n\n**[Interface signature]** `animation_finish(self) -> None`<hr>\n\n**[Interface summary]** Finish all animations (set the animation last value to each attribute).<hr>\n\n**[Examples]**':  # noqa
-    '',
+    '<span class="inconspicuous-txt">Note: the document build script generates and updates this API document section automatically. Maybe this section is duplicated compared with previous sections.</span>':  # noqa
+    '<span class="inconspicuous-txt">特記事項: このAPIドキュメントはドキュメントビルド用のスクリプトによって自動で生成・同期されています。そのためもしかしたらこの節の内容は前節までの内容と重複している場合があります。</span>',  # noqa
+
+    '**[Interface summary]** Finish all animations (set the animation last value to each attribute).<hr>':  # noqa
+    '**[インターフェイス概要]** 全てのアニメーションを終了し各属性にアニメーションの最終値の値を設定します。<hr>',
+
+    '**[Examples]**':
+    '**[コードサンプル]**',
 
     '```py\n>>> from typing_extensions import TypedDict\n>>> import apysc as ap\n>>> class RectOptions(TypedDict):\n...     rectangle: ap.Rectangle\n>>> def on_timer(\n...         e: ap.TimerEvent,\n...         options: RectOptions) -> None:\n...     rectangle: ap.Rectangle = options[\'rectangle\']\n...     rectangle.animation_finish()\n>>> stage: ap.Stage = ap.Stage()\n>>> sprite: ap.Sprite = ap.Sprite()\n>>> sprite.graphics.begin_fill(color=\'#0af\')\n>>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(\n...     x=50, y=50, width=50, height=50)\n>>> _ = rectangle.animation_x(\n...     x=100,\n...     duration=1500,\n...     easing=ap.Easing.EASE_OUT_QUINT,\n... ).start()\n>>> options: RectOptions = {\'rectangle\': rectangle}\n>>> ap.Timer(on_timer, delay=750, options=options).start()\n```':  # noqa
-    '',
+    '```py\n>>> from typing_extensions import TypedDict\n>>> import apysc as ap\n>>> class RectOptions(TypedDict):\n...     rectangle: ap.Rectangle\n>>> def on_timer(\n...         e: ap.TimerEvent,\n...         options: RectOptions) -> None:\n...     rectangle: ap.Rectangle = options[\'rectangle\']\n...     rectangle.animation_finish()\n>>> stage: ap.Stage = ap.Stage()\n>>> sprite: ap.Sprite = ap.Sprite()\n>>> sprite.graphics.begin_fill(color=\'#0af\')\n>>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(\n...     x=50, y=50, width=50, height=50)\n>>> _ = rectangle.animation_x(\n...     x=100,\n...     duration=1500,\n...     easing=ap.Easing.EASE_OUT_QUINT,\n... ).start()\n>>> options: RectOptions = {\'rectangle\': rectangle}\n>>> ap.Timer(on_timer, delay=750, options=options).start()\n```',  # noqa
 
 }
---
+++ docs_src/source/jp_animation_finish.md
@@ -0,0 +1,113 @@
+<span class="inconspicuous-txt">※この翻訳ドキュメントはスクリプトによって出力・同期されています。内容が怪しそうな場合は<a href="https://github.com/simon-ritchie/apysc/issues" target="_blank">GitHubにissue</a>を追加したり[英語の原文](animation_finish.md)の確認をお願いします。</span>
+
+# animation_finish インターフェイス
+
+このページでは`animation_finish`メソッドのインターフェイスについて説明します。
+
+## インターフェイス概要
+
+`animation_finish`インターフェイスでは実行されている全てのアニメーションにアニメーションの各属性の最終値を設定しアニメーションを終了させます。
+
+このインターフェイスは`animation_x`や`animation_move`などのアニメーションのインターフェイスを持つ各クラスのインスタンスに存在します。
+
+## 使い方例
+
+以下のコード例では四角にクリックイベントを設定しています。四角をクリックするとX座標のアニメーションが開始するようになっています。アニメーション開始後に2秒経過したらアニメーションは終了し、最終値の座標が設定されます。
+
+```py
+# runnable
+from typing_extensions import TypedDict
+
+import apysc as ap
+
+
+class _RectOptions(TypedDict):
+    rectangle: ap.Rectangle
+
+
+def on_click(e: ap.MouseEvent[ap.Rectangle], options: dict) -> None:
+    """
+    The handler that the rectangle calls when clicked.
+
+    Parameters
+    ----------
+    e : ap.MouseEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    rectangle: ap.Rectangle = e.this
+    rectangle.animation_x(
+        x=300, duration=5000,
+    ).start()
+
+    options_: _RectOptions = {'rectangle': rectangle}
+    ap.Timer(
+        on_timer, delay=2000, repeat_count=1, options=options_,
+    ).start()
+
+
+def on_timer(e: ap.TimerEvent, options: _RectOptions) -> None:
+    """
+    The handler that the timer calls when its ends.
+
+    Parameters
+    ----------
+    e : ap.TimerEvent
+        Event instance.
+    options : dict
+        Optional arguments dictionary.
+    """
+    rectangle: ap.Rectangle = options['rectangle']
+    rectangle.animation_finish()
+
+
+ap.Stage(
+    stage_width=400, stage_height=150, background_color='#333',
+    stage_elem_id='stage')
+sprite: ap.Sprite = ap.Sprite()
+sprite.graphics.begin_fill(color='#0af')
+
+rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+    x=50, y=50, width=50, height=50)
+rectangle.click(on_click)
+
+ap.save_overall_html(
+    dest_dir_path='animation_finish_basic_usage/')
+```
+
+<iframe src="static/animation_finish_basic_usage/index.html" width="400" height="150"></iframe>
+
+## animation_finish API
+
+<span class="inconspicuous-txt">特記事項: このAPIドキュメントはドキュメントビルド用のスクリプトによって自動で生成・同期されています。そのためもしかしたらこの節の内容は前節までの内容と重複している場合があります。</span>
+
+**[インターフェイスの構造]** `animation_finish(self) -> None`<hr>
+
+**[インターフェイス概要]** 全てのアニメーションを終了し各属性にアニメーションの最終値の値を設定します。<hr>
+
+**[コードサンプル]**
+
+```py
+>>> from typing_extensions import TypedDict
+>>> import apysc as ap
+>>> class RectOptions(TypedDict):
+...     rectangle: ap.Rectangle
+>>> def on_timer(
+...         e: ap.TimerEvent,
+...         options: RectOptions) -> None:
+...     rectangle: ap.Rectangle = options['rectangle']
+...     rectangle.animation_finish()
+>>> stage: ap.Stage = ap.Stage()
+>>> sprite: ap.Sprite = ap.Sprite()
+>>> sprite.graphics.begin_fill(color='#0af')
+>>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
+...     x=50, y=50, width=50, height=50)
+>>> _ = rectangle.animation_x(
+...     x=100,
+...     duration=1500,
+...     easing=ap.Easing.EASE_OUT_QUINT,
+... ).start()
+>>> options: RectOptions = {'rectangle': rectangle}
+>>> ap.Timer(on_timer, delay=750, options=options).start()
+```

930ffe34602544bf357418415732fe5d1dc16be8
simon-ritchie <antisocial.sid2@gmail.com>
2022-03-26 18:54:42 +0900

    #648 Add the translated document of the `dictionary_generic.md`.

--- apysc/_translation/jp/dictionary_generic.py
+++ apysc/_translation/jp/dictionary_generic.py
@@ -10,48 +10,48 @@ from typing import Dict
 MAPPING: Dict[str, str] = {
 
     '# Dictionary class generic type settings':
-    '',
+    '# Dictionary クラスのジェネリックの型アノテーション設定',
 
     'This page explains the `Dictionary` class key and value\'s generic type settings.':  # noqa
-    '',
+    'このページでは`Dictionary`クラスのキーと値のジェネリックの型アノテーション設定について説明します。',
 
     '## Basic usage':
-    '',
+    '## 基本的な使い方',
 
     'You can specify the key and value\'s type at the `Dictionary` type-annotation, as follows:':  # noqa
-    '',
+    '以下のコードのように、`Dictionary`クラスではキーと値に対してジェネリックの型アノテーションを行うことができます:',
 
     '```py\n# runnable\nimport apysc as ap\n\ndict_value: ap.Dictionary[str, int] = ap.Dictionary({\'a\': 10})\na_value: int = dict_value[\'a\']\n```':  # noqa
-    '',
+    '```py\n# runnable\nimport apysc as ap\n\ndict_value: ap.Dictionary[str, int] = ap.Dictionary({\'a\': 10})\na_value: int = dict_value[\'a\']\n```',  # noqa
 
-    'These generic type-annotations are sometimes helpful for checking with the mypy, Pylance, or other libraries and enhancing safety.\n\nFor example, the following code raises an error of value\'s type when checking with the Pylance:':  # noqa
-    '',
+    'These generic type-annotations are sometimes helpful for checking with the mypy, Pylance, or other libraries and enhancing safety.':  # noqa
+    'これらのジェネリックの型アノテーションはmypyやPylanceなどのライブラリによるチェックや安全面で役に立つことがあります。',
+
+    'For example, the following code raises an error of value\'s type when checking with the Pylance:':  # noqa
+    '例えば、以下のコード例では辞書の値の型に対するPylanceがエラーが発生します:',
 
     '```py\n# runnable\nimport apysc as ap\n\ndict_value: ap.Dictionary[str, int] = ap.Dictionary({\'a\': 10})\na_value: str = dict_value[\'a\']\n```':  # noqa
-    '',
-
-    '':
-    '',
+    '```py\n# runnable\nimport apysc as ap\n\ndict_value: ap.Dictionary[str, int] = ap.Dictionary({\'a\': 10})\na_value: str = dict_value[\'a\']\n```',  # noqa
 
     '```\nExpression of type "int" cannot be assigned to declared type "str"\n  "int" is incompatible with "str"\n```':  # noqa
-    '',
+    '```\nExpression of type "int" cannot be assigned to declared type "str"\n  "int" is incompatible with "str"\n```',  # noqa
 
     'Also, the following code raises an error of key\'s type (`str` is required but `int` is specified):':  # noqa
-    '',
+    '同じように、以下のコード例では辞書のキーの型でエラーが発生します（`str`が必要になっている一方で`int`が指定されています）。',
 
     '```py\n# runnable\nimport apysc as ap\n\ndict_value: ap.Dictionary[str, int] = ap.Dictionary({\'a\': 10})\na_value: int = dict_value[10]\n```':  # noqa
-    '',
+    '```py\n# runnable\nimport apysc as ap\n\ndict_value: ap.Dictionary[str, int] = ap.Dictionary({\'a\': 10})\na_value: int = dict_value[10]\n```',  # noqa
 
-    'If you need to use multiple types and type checking, then use the `Union`\\, as follows:\n\nNotes: Alternatively, use the `|` symbol, if you are using Python 3.10 or later) or `Any` type.':  # noqa
-    '',
+    'If you need to use multiple types and type checking, then use the `Union`\\, as follows:':  # noqa
+    'もし複数の型の指定が必要な場合、以下のコード例のように`Union`を使うこともできます。',
+
+    'Notes: Alternatively, use the `|` symbol, if you are using Python 3.10 or later) or `Any` type.':  # noqa
+    '特記事項: もしPython3.10以降をお使いの場合には`|`の記号などを代わりに使用することができます（もしくは`Any`の型を指定するなど）。',  # noqa
 
     '```py\n# runnable\nfrom typing import Union\n\nimport apysc as ap\n\n# Accepting the str and int key types.\ndict_value: ap.Dictionary[Union[int, str], int] = ap.Dictionary(\n    {\'a\': 10, 2: 20})\na_value: int = dict_value[\'a\']\nb_value: int = dict_value[2]\n```':  # noqa
-    '',
-
-    '':
-    '',
+    '```py\n# runnable\nfrom typing import Union\n\nimport apysc as ap\n\n# Accepting the str and int key types.\ndict_value: ap.Dictionary[Union[int, str], int] = ap.Dictionary(\n    {\'a\': 10, 2: 20})\na_value: int = dict_value[\'a\']\nb_value: int = dict_value[2]\n```',  # noqa
 
     '```py\n# runnable\nfrom typing import Any\n\nimport apysc as ap\n\n# Accepting all types by specifying the Any type.\ndict_value: ap.Dictionary[Any, Any] = ap.Dictionary(\n    {\'a\': 10, 2: \'b\'})\na_value: int = dict_value[\'a\']\nb_value: str = dict_value[2]\n```':  # noqa
-    '',
+    '```py\n# runnable\nfrom typing import Any\n\nimport apysc as ap\n\n# Accepting all types by specifying the Any type.\ndict_value: ap.Dictionary[Any, Any] = ap.Dictionary(\n    {\'a\': 10, 2: \'b\'})\na_value: int = dict_value[\'a\']\nb_value: str = dict_value[2]\n```',  # noqa
 
 }
---
+++ docs_src/source/jp_dictionary_generic.md
@@ -0,0 +1,74 @@
+<span class="inconspicuous-txt">※この翻訳ドキュメントはスクリプトによって出力・同期されています。内容が怪しそうな場合は<a href="https://github.com/simon-ritchie/apysc/issues" target="_blank">GitHubにissue</a>を追加したり[英語の原文](dictionary_generic.md)の確認をお願いします。</span>
+
+# Dictionary クラスのジェネリックの型アノテーション設定
+
+このページでは`Dictionary`クラスのキーと値のジェネリックの型アノテーション設定について説明します。
+
+## 基本的な使い方
+
+以下のコードのように、`Dictionary`クラスではキーと値に対してジェネリックの型アノテーションを行うことができます:
+
+```py
+# runnable
+import apysc as ap
+
+dict_value: ap.Dictionary[str, int] = ap.Dictionary({'a': 10})
+a_value: int = dict_value['a']
+```
+
+これらのジェネリックの型アノテーションはmypyやPylanceなどのライブラリによるチェックや安全面で役に立つことがあります。
+
+例えば、以下のコード例では辞書の値の型に対するPylanceがエラーが発生します:
+
+```py
+# runnable
+import apysc as ap
+
+dict_value: ap.Dictionary[str, int] = ap.Dictionary({'a': 10})
+a_value: str = dict_value['a']
+```
+
+```
+Expression of type "int" cannot be assigned to declared type "str"
+  "int" is incompatible with "str"
+```
+
+同じように、以下のコード例では辞書のキーの型でエラーが発生します（`str`が必要になっている一方で`int`が指定されています）。
+
+```py
+# runnable
+import apysc as ap
+
+dict_value: ap.Dictionary[str, int] = ap.Dictionary({'a': 10})
+a_value: int = dict_value[10]
+```
+
+もし複数の型の指定が必要な場合、以下のコード例のように`Union`を使うこともできます。
+
+特記事項: もしPython3.10以降をお使いの場合には`|`の記号などを代わりに使用することができます（もしくは`Any`の型を指定するなど）。
+
+```py
+# runnable
+from typing import Union
+
+import apysc as ap
+
+# Accepting the str and int key types.
+dict_value: ap.Dictionary[Union[int, str], int] = ap.Dictionary(
+    {'a': 10, 2: 20})
+a_value: int = dict_value['a']
+b_value: int = dict_value[2]
+```
+
+```py
+# runnable
+from typing import Any
+
+import apysc as ap
+
+# Accepting all types by specifying the Any type.
+dict_value: ap.Dictionary[Any, Any] = ap.Dictionary(
+    {'a': 10, 2: 'b'})
+a_value: int = dict_value['a']
+b_value: str = dict_value[2]
+```

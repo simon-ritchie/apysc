82b7e1568338b5ea7164fc786c4c37d9b16e1a9e
simon-ritchie <antisocial.sid2@gmail.com>
2022-04-02 19:42:32 +0900

    #660 Rename the `apply_toc_mapping_to_index_md` module to `apply_link_text_mapping_to_index_html`.

--- tests/test_apply_toc_mapping_to_index_md.py
+++ tests/test_apply_toc_mapping_to_index_md.py
@@ -4,12 +4,12 @@ import os
 
 from retrying import retry
 
-from scripts import apply_toc_mapping_to_index_md
+from scripts import apply_link_text_mapping_to_index_html
 
 
 @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
 def test__get_target_html_paths() -> None:
-    html_paths: List[str] = apply_toc_mapping_to_index_md.\
+    html_paths: List[str] = apply_link_text_mapping_to_index_html.\
         _get_target_html_paths()
     assert './docs/index.html' in html_paths
     if os.path.exists('./docs/jp_index.html'):

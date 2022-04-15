58e528ee8fa5b41aafdbca966aa7f4068aac3b42
simon-ritchie <antisocial.sid2@gmail.com>
2022-04-15 21:18:33 +0900

    #671 Replace the link-text mapping implementation by index.md's text extraction.

--- scripts/apply_link_text_mapping_to_index_html.py
+++ scripts/apply_link_text_mapping_to_index_html.py
@@ -8,96 +8,15 @@ $ python scripts/apply_link_text_mapping_to_index_html.py
 import os
 import sys
 from logging import Logger
-from typing import Dict
+from typing import Dict, Pattern, Optional, Match
 from typing import List
+import re
 
 sys.path.append('./')
 
 from apysc._console import loggers
 
 logger: Logger = loggers.get_info_logger()
-
-_MAPPINGS: Dict[str, str] = {
-    'save overall html': 'save_overall_html',
-    'display on jupyter': 'display_on_jupyter',
-    'display on google colaboratory': 'display_on_google_colaboratory',
-    'append js expression': 'append_js_expression',
-    'add child': 'add_child',
-    'remove child': 'remove_child',
-    'num children': 'num_children',
-    'get child at': 'get_child_at',
-    'insert at': 'insert_at',
-    'remove at': 'remove_at',
-    'index of': 'index_of',
-    'get css': 'get_css',
-    'set css': 'set_css',
-    'rotation around center': 'rotation_around_center',
-    'rotation around point': 'rotation_around_point',
-    'scale from center': 'scale_from_center',
-    'scale from point': 'scale_from_point',
-    'skew x': 'skew_x',
-    'skew y': 'skew_y',
-    'begin fill': 'begin_fill',
-    'line style': 'line_style',
-    'draw rect': 'draw_rect',
-    'draw round rect': 'draw_round_rect',
-    'draw circle': 'draw_circle',
-    'draw ellipse': 'draw_ellipse',
-    'move to': 'move_to',
-    'line to': 'line_to',
-    'draw line': 'draw_line',
-    'draw dotted line': 'draw_dotted_line',
-    'draw dashed line': 'draw_dashed_line',
-    'draw round dotted line': 'draw_round_dotted_line',
-    'draw dash dotted line': 'draw_dash_dotted_line',
-    'draw polygon': 'draw_polygon',
-    'fill color': 'fill_color',
-    'fill alpha': 'fill_alpha',
-    'line color': 'line_color',
-    'line alpha': 'line_alpha',
-    'line thickness': 'line_thickness',
-    'line dot setting': 'line_dot_setting',
-    'line dash setting': 'line_dash_setting',
-    'line round dot setting': 'line_round_dot_setting',
-    'line dash dot setting': 'line_dash_dot_setting',
-    'bind custom event': 'bind_custom_event',
-    'trigger custom event': 'trigger_custom_event',
-    'repeat count': 'repeat_count',
-    'timer complete': 'timer_complete',
-    'animation complete': 'animation_complete',
-    'animation time': 'animation_time',
-    'animation parallel': 'animation_parallel',
-    'animation move': 'animation_move',
-    'animation x': 'animation_x',
-    'animation y': 'animation_y',
-    'animation width': 'animation_width',
-    'animation height': 'animation_height',
-    'animation fill color': 'animation_fill_color',
-    'animation fill alpha': 'animation_fill_alpha',
-    'animation line color': 'animation_line_color',
-    'animation line alpha': 'animation_line_alpha',
-    'animation line thickness': 'animation_line_thickness',
-    'animation radius': 'animation_radius',
-    'animation rotation around center': 'animation_rotation_around_center',
-    'animation rotation around point': 'animation_rotation_around_point',
-    'animation scale x from center': 'animation_scale_x_from_center',
-    'animation scale y from center': 'animation_scale_y_from_center',
-    'animation scale x from point': 'animation_scale_x_from_point',
-    'animation scale y from point': 'animation_scale_y_from_point',
-    'animation skew x': 'animation_skew_x',
-    'set debug mode': 'set_debug_mode',
-    'unset debug mode': 'unset_debug_mode',
-    'assert equal': 'assert_equal',
-    'assert not equal': 'assert_not_equal',
-    'assert true': 'assert_true',
-    'assert false': 'assert_false',
-    'assert arrays equal': 'assert_arrays_equal',
-    'assert arrays not equal': 'assert_arrays_not_equal',
-    'assert dicts equal': 'assert_dicts_equal',
-    'assert dicts not equal': 'assert_dicts_not_equal',
-    'assert defined': 'assert_defined',
-    'assert undefined': 'assert_undefined',
-}
 
 
 def apply() -> None:
@@ -134,10 +53,41 @@ def _apply_mapping(*, html_path: str) -> None:
         A target HTML's file path.
     """
     from apysc._file import file_util
+    mappings: Dict[str, str] = _extract_mappings_from_index_md()
     html_txt: str = file_util.read_txt(file_path=html_path)
-    for mapping_key, mapping_value in _MAPPINGS.items():
+    for mapping_key, mapping_value in mappings.items():
         html_txt = html_txt.replace(mapping_key, mapping_value)
     file_util.save_plain_txt(txt=html_txt, file_path=html_path)
+
+
+def _extract_mappings_from_index_md() -> Dict[str, str]:
+    """
+    Extract mappings from the `index.md` file.
+
+    Returns
+    -------
+    mappings : dict
+        A dictionary for the mappings.
+        Each key has a no-underscore link text and
+        each value has a link text with underscores.
+    """
+    from apysc._file import file_util
+    index_md_txt: str = file_util.read_txt(
+        file_path='./docs_src/source/index.md')
+    pattern: Pattern = re.compile(pattern=r'^- \[(.*?\_.*?)\]')
+    lines: List[str] = index_md_txt.splitlines()
+    mappings: Dict[str, str] = {}
+    for line in lines:
+        match: Optional[Match] = pattern.search(string=line)
+        if match is None:
+            continue
+        link_txt: str = match.group(1)
+        words: List[str] = link_txt.split(' ')
+        for word in words:
+            if '_' not in word:
+                continue
+            mappings[word.replace('_', ' ')] = word
+    return mappings
 
 
 def _get_target_html_paths() -> List[str]:
--- tests/test_apply_link_text_mapping_to_index_html.py
+++ tests/test_apply_link_text_mapping_to_index_html.py
@@ -1,6 +1,6 @@
 import os
 from random import randint
-from typing import List
+from typing import Dict, List
 
 from retrying import retry
 
@@ -14,3 +14,17 @@ def test__get_target_html_paths() -> None:
     assert './docs/index.html' in html_paths
     if os.path.exists('./docs/jp_index.html'):
         assert './docs/jp_index.html' in html_paths
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__extract_mappings_from_index_md() -> None:
+    mappings: Dict[str, str] = apply_link_text_mapping_to_index_html.\
+        _extract_mappings_from_index_md()
+    print(mappings)
+    expectd_mappings: Dict[str, str] = {
+        'display on google colaboratory': 'display_on_google_colaboratory',
+        'append js expression': 'append_js_expression',
+        'add child': 'add_child',
+    }
+    for key, value in expectd_mappings.items():
+        assert mappings[key] == value

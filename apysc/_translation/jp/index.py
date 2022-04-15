f38f721034dded84c3741d65aaa3a1138886d84a
simon-ritchie <antisocial.sid2@gmail.com>
2022-04-15 21:32:25 +0900

    #671 Apply translation mapping updating to the `index.py`.

--- apysc/_translation/jp/index.py
+++ apysc/_translation/jp/index.py
@@ -72,16 +72,16 @@ MAPPING: Dict[str, str] = {
     'The HTML and JavaScript exporting interfaces.':
     '以下はHTMLとJavaScriptの出力処理関係の各インターフェイスです。',
 
-    '- [save overall html interface](save_overall_html.md)':
+    '- [save_overall_html interface](save_overall_html.md)':
     '',
 
-    '- [display on jupyter interface](display_on_jupyter.md)':
+    '- [display_on_jupyter interface](display_on_jupyter.md)':
     '',
 
-    '- [display on google colaboratory interface](display_on_colaboratory.md)':  # noqa
+    '- [display_on_google_colaboratory interface](display_on_colaboratory.md)':  # noqa
     '',
 
-    '- [append js expression interface](append_js_expression.md)':
+    '- [append_js_expression interface](append_js_expression.md)':
     '',
 
     '---':
@@ -93,17 +93,17 @@ MAPPING: Dict[str, str] = {
     'The parent class, such as the `Sprite` or `Stage` have the following interfaces:':  # noqa
     '',
 
-    '- [add child and remove child interfaces](add_child_and_remove_child.md)':  # noqa
-    '',
+    '- [add_child and remove_child interfaces](add_child_and_remove_child.md)':  # noqa
+    '- [add_child （子の追加）と remove_child （子の削除）のインターフェイス](jp_add_child_and_remove_child.md)',  # noqa
 
     '- [contains interface](contains.md)':
     '- [contains インターフェイス](jp_contains.md)',
 
-    '- [num children interface](num_children.md)':
-    '',
+    '- [num_children interface](num_children.md)':
+    '- [num_children （子の件数属性）のインターフェイス](jp_num_children.md)',
 
-    '- [get child at interface](get_child_at.md)':
-    '',
+    '- [get_child_at interface](get_child_at.md)':
+    '- [get_child_at （特定位置の子の取得処理）のインターフェイス](jp_get_child_at.md)',
 
     '---':
     '---',
@@ -147,14 +147,14 @@ MAPPING: Dict[str, str] = {
     '- [Array class extend and concat interfaces](array_extend_and_concat.md)':  # noqa
     '- [Array クラスの extend と concat のインターフェイス](jp_array_extend_and_concat.md)',  # noqa
 
-    '- [Array class insert and insert at interfaces](array_insert_and_insert_at.md)':  # noqa
-    '',
+    '- [Array class insert and insert_at interfaces](array_insert_and_insert_at.md)':  # noqa
+    '- [Array クラスの insert と insert_at のインターフェイス](jp_array_insert_and_insert_at.md)',  # noqa
 
     '- [Array class pop interface](array_pop.md)':
     '- [Array クラスの pop インターフェイス](jp_array_pop.md)',
 
-    '- [Array class remove and remove at interfaces](array_remove_and_remove_at.md)':  # noqa
-    '',
+    '- [Array class remove and remove_at interfaces](array_remove_and_remove_at.md)':  # noqa
+    '- [Array クラスの remove と remove_at のインターフェイス](jp_array_remove_and_remove_at.md)',  # noqa
 
     '- [Array class sort interface](array_sort.md)':
     '- [Array クラスの sort インターフェイス](jp_array_sort.md)',
@@ -171,8 +171,8 @@ MAPPING: Dict[str, str] = {
     '- [Array class join interface](array_join.md)':
     '- [Array クラスの join (値の連結文字列生成) のインターフェイス](jp_array_join.md)',
 
-    '- [Array class index of interface](array_index_of.md)':
-    '',
+    '- [Array class index_of interface](array_index_of.md)':
+    '- [Array クラスの index_of (値のインデックス取得) のインターフェイス](jp_array_index_of.md)',
 
     '- [Array class comparison interfaces](array_comparison.md)':
     '- [Array クラスの比較の各インターフェイス](jp_array_comparison.md)',
@@ -216,29 +216,29 @@ MAPPING: Dict[str, str] = {
     '- [DisplayObject class visible interface](display_object_visible.md)':
     '- [DisplayObject クラスの visible (表示・非表示) のインターフェイス](jp_display_object_visible.md)',  # noqa
 
-    '- [DisplayObject class get css and set css interfaces](display_object_get_and_set_css.md)':  # noqa
+    '- [DisplayObject class get_css and set_css interfaces](display_object_get_and_set_css.md)':  # noqa
     '',
 
     '- [DisplayObject class mouse event binding interfaces](display_object_mouse_event.md)':  # noqa
     '- [DisplayObject クラスのマウスイベント設定の各インターフェイス](jp_display_object_mouse_event.md)',  # noqa
 
-    '- [GraphicsBase class rotation around center interface](graphics_base_rotation_around_center.md)':  # noqa
-    '',
+    '- [GraphicsBase class rotation_around_center interface](graphics_base_rotation_around_center.md)':  # noqa
+    '- [GraphicsBase クラスの rotation_around_center (中央座標基準の回転) インターフェイス](jp_graphics_base_rotation_around_center.md)',  # noqa
 
-    '- [GraphicsBase class rotation around point interfaces](graphics_base_rotation_around_point.md)':  # noqa
-    '',
+    '- [GraphicsBase class rotation_around_point interfaces](graphics_base_rotation_around_point.md)':  # noqa
+    '- [GraphicsBase クラスの rotation_around_point (指定座標基準の回転) の各インターフェイス](jp_graphics_base_rotation_around_point.md)',  # noqa
 
-    '- [GraphicsBase class scale from center interfaces](graphics_base_scale_from_center.md)':  # noqa
-    '',
+    '- [GraphicsBase class scale_from_center interfaces](graphics_base_scale_from_center.md)':  # noqa
+    '- [GraphicsBase クラスの scale_from_center (中央座標基準の拡縮) の各インターフェイス](jp_graphics_base_scale_from_center.md)',  # noqa
 
-    '- [GraphicsBase class scale from point interfaces](graphics_base_scale_from_point.md)':  # noqa
-    '',
+    '- [GraphicsBase class scale_from_point interfaces](graphics_base_scale_from_point.md)':  # noqa
+    '- [GraphicsBase クラスの scale_from_point (指定座標基準の拡縮) の各インターフェイス](jp_graphics_base_scale_from_point.md)',  # noqa
 
     '- [GraphicsBase class flip x and flip y interfaces](graphics_base_flip_interfaces.md)':  # noqa
     '',
 
-    '- [GraphicsBase class skew x and skew y interfaces](graphics_base_skew.md)':  # noqa
-    '',
+    '- [GraphicsBase class skew_x and skew_y interfaces](graphics_base_skew.md)':  # noqa
+    '- [GraphicsBase クラスの skew_x (X軸の歪み) と skew_y (Y軸の歪み) のインターフェイス](jp_graphics_base_skew.md)',  # noqa
 
     '---':
     '---',
@@ -255,71 +255,71 @@ MAPPING: Dict[str, str] = {
     '- [Graphics class](graphics.md)':
     '- [Graphics クラス](jp_graphics.md)',
 
-    '- [Graphics class begin fill interface](graphics_begin_fill.md)':
-    '',
+    '- [Graphics class begin_fill interface](graphics_begin_fill.md)':
+    '- [Graphics クラスの begin_fill (塗りの設定)のインターフェイス](jp_graphics_begin_fill.md)',  # noqa
 
-    '- [Graphics class line style interface](graphics_line_style.md)':
-    '',
+    '- [Graphics class line_style interface](graphics_line_style.md)':
+    '- [Graphics クラスの line_style (線のスタイル設定)のインターフェイス](jp_graphics_line_style.md)',  # noqa
 
-    '- [Graphics class draw rect interface](graphics_draw_rect.md)':
-    '',
+    '- [Graphics class draw_rect interface](graphics_draw_rect.md)':
+    '- [Graphics クラスの draw_rect (四角の描画)のインターフェイス](jp_graphics_draw_rect.md)',  # noqa
 
-    '- [Graphics class draw round rect interface](graphics_draw_round_rect.md)':  # noqa
-    '',
+    '- [Graphics class draw_round_rect interface](graphics_draw_round_rect.md)':  # noqa
+    '- [Graphics クラスの draw_round_rect (角丸の四角の描画)のインターフェイス](jp_graphics_draw_round_rect.md)',  # noqa
 
-    '- [Graphics class draw circle interface](graphics_draw_circle.md)':
-    '',
+    '- [Graphics class draw_circle interface](graphics_draw_circle.md)':
+    '- [Graphics クラスの draw_circle (円の描画)のインターフェイス](jp_graphics_draw_circle.md)',  # noqa
 
-    '- [Graphics class draw ellipse interface](graphics_draw_ellipse.md)':
-    '',
+    '- [Graphics class draw_ellipse interface](graphics_draw_ellipse.md)':
+    '- [Graphics クラスの draw_ellipse (楕円描画) のインターフェイス](jp_graphics_draw_ellipse.md)',  # noqa
 
-    '- [Graphics class move to and line to interfaces](graphics_move_to_and_line_to.md)':  # noqa
-    '',
+    '- [Graphics class move_to and line_to interfaces](graphics_move_to_and_line_to.md)':  # noqa
+    '- [Graphics クラスの move_to (線の描画位置の変更)と line_to (指定座標への線の描画)のインターフェイス](jp_graphics_move_to_and_line_to.md)',  # noqa
 
-    '- [Graphics class draw line interface](graphics_draw_line.md)':
-    '',
+    '- [Graphics class draw_line interface](graphics_draw_line.md)':
+    '- [Graphics クラスの draw_line (線の描画)のインターフェイス](jp_graphics_draw_line.md)',
 
-    '- [Graphics class draw dotted line interface](graphics_draw_dotted_line.md)':  # noqa
-    '',
+    '- [Graphics class draw_dotted_line interface](graphics_draw_dotted_line.md)':  # noqa
+    '- [Graphics クラスの draw_dotted_line (点線の描画)のインターフェイス](jp_graphics_draw_dotted_line.md)',  # noqa
 
-    '- [Graphics class draw dashed line interface](graphics_draw_dashed_line.md)':  # noqa
-    '',
+    '- [Graphics class draw_dashed_line interface](graphics_draw_dashed_line.md)':  # noqa
+    '- [Graphics クラスの draw_dashed_line (破線の描画)のインターフェイス](jp_graphics_draw_dashed_line.md)',  # noqa
 
-    '- [Graphics class draw round dotted line interface](graphics_draw_round_dotted_line.md)':  # noqa
-    '',
+    '- [Graphics class draw_round_dotted_line interface](graphics_draw_round_dotted_line.md)':  # noqa
+    '- [Graphics クラスの draw_round_dotted_line (点線(丸)の描画)のインターフェイス](jp_graphics_draw_round_dotted_line.md)',  # noqa
 
-    '- [Graphics class draw dash dotted line interface](graphics_draw_dash_dotted_line.md)':  # noqa
-    '',
+    '- [Graphics class draw_dash_dotted_line interface](graphics_draw_dash_dotted_line.md)':  # noqa
+    '- [Graphics クラスの draw_dash_dotted_line (一点鎖線の描画)のインターフェイス](jp_graphics_draw_dash_dotted_line.md)',  # noqa
 
-    '- [Graphics class draw polygon interface](graphics_draw_polygon.md)':
-    '',
+    '- [Graphics class draw_polygon interface](graphics_draw_polygon.md)':
+    '- [Graphics クラスの draw_polygon (多角形描画)のインターフェイス](jp_graphics_draw_polygon.md)',  # noqa
 
-    '- [Graphics class fill color interface](graphics_fill_color.md)':
-    '',
+    '- [Graphics class fill_color interface](graphics_fill_color.md)':
+    '- [Graphics クラスの fill_color (塗り設定)のインターフェイス](jp_graphics_fill_color.md)',  # noqa
 
-    '- [Graphics class fill alpha interface](graphics_fill_alpha.md)':
-    '',
+    '- [Graphics class fill_alpha interface](graphics_fill_alpha.md)':
+    '- [Graphics クラスの fill_alpha (塗りの透明度設定)のインターフェイス](jp_graphics_fill_alpha.md)',  # noqa
 
-    '- [Graphics class line color interface](graphics_line_color.md)':
-    '',
+    '- [Graphics class line_color interface](graphics_line_color.md)':
+    '- [Graphics クラスの line_color (線の色設定)のインターフェイス](jp_graphics_line_color.md)',  # noqa
 
-    '- [Graphics class line alpha interface](graphics_line_alpha.md)':
-    '',
+    '- [Graphics class line_alpha interface](graphics_line_alpha.md)':
+    '- [Graphics クラスの line_color (線の透明度設定)のインターフェイス](jp_graphics_line_alpha.md)',  # noqa
 
-    '- [Graphics class line thickness interface](graphics_line_thickness.md)':  # noqa
-    '',
+    '- [Graphics class line_thickness interface](graphics_line_thickness.md)':  # noqa
+    '- [Graphics クラスの line_color (線幅設定)のインターフェイス](jp_graphics_line_thickness.md)',  # noqa
 
-    '- [Graphics class line dot setting interface](graphics_line_dot_setting.md)':  # noqa
-    '',
+    '- [Graphics class line_dot_setting interface](graphics_line_dot_setting.md)':  # noqa
+    '- [Graphics クラスの line_dot_setting (点線設定)のインターフェイス](jp_graphics_line_dot_setting.md)',  # noqa
 
-    '- [Graphics class line dash setting interface](graphics_line_dash_setting.md)':  # noqa
-    '',
+    '- [Graphics class line_dash_setting interface](graphics_line_dash_setting.md)':  # noqa
+    '- [Graphics クラスの line_dash_setting (破線設定)のインターフェイス](jp_graphics_line_dash_setting.md)',  # noqa
 
-    '- [Graphics class line round dot setting interface](graphics_line_round_dot_setting.md)':  # noqa
-    '',
+    '- [Graphics class line_round_dot_setting interface](graphics_line_round_dot_setting.md)':  # noqa
+    '- [Graphics クラスの line_round_dot_setting (点線(丸)設定)のインターフェイス](jp_graphics_line_round_dot_setting.md)',  # noqa
 
-    '- [Graphics class line dash dot setting interface](graphics_line_dash_dot_setting.md)':  # noqa
-    '',
+    '- [Graphics class line_dash_dot_setting interface](graphics_line_dash_dot_setting.md)':  # noqa
+    '- [Graphics クラスの line_dash_dot_setting (一点鎖線設定)のインターフェイス](jp_graphics_line_dash_dot_setting.md)',  # noqa
 
     '---':
     '---',
@@ -333,7 +333,7 @@ MAPPING: Dict[str, str] = {
     '- [Event class prevent default and stop propagation interfaces](event_prevent_default_and_stop_propagation.md)':  # noqa
     '',
 
-    '- [bind custom event and trigger custom event interfaces](bind_and_trigger_custom_event.md)':  # noqa
+    '- [bind_custom_event and trigger_custom_event interfaces](bind_and_trigger_custom_event.md)':  # noqa
     '',
 
     '---':
@@ -414,13 +414,13 @@ MAPPING: Dict[str, str] = {
     '- [FPS enum](fps.md)':
     '',
 
-    '- [Timer class repeat count setting](timer_repeat_count.md)':
+    '- [Timer class repeat_count setting](timer_repeat_count.md)':
     '',
 
     '- [Timer class start and stop interfaces](timer_start_and_stop.md)':
     '',
 
-    '- [Timer class timer complete interface](timer_complete.md)':
+    '- [Timer class timer_complete interface](timer_complete.md)':
     '',
 
     '- [Timer class reset interface](timer_reset.md)':
@@ -450,7 +450,7 @@ MAPPING: Dict[str, str] = {
     '- [AnimationBase class start interface](animation_base_start.md)':
     '',
 
-    '- [AnimationBase class animation complete interface](animation_complete.md)':  # noqa
+    '- [AnimationBase class animation_complete interface](animation_complete.md)':  # noqa
     '',
 
     '- [AnimationBase class interfaces method chaining](animation_method_chaining.md)':  # noqa
@@ -471,7 +471,7 @@ MAPPING: Dict[str, str] = {
     '- [Animation reverse interface](animation_reverse.md)':
     '',
 
-    '- [animation time interface](animation_time.md)':
+    '- [animation_time interface](animation_time.md)':
     '',
 
     '- [Easing enum](easing_enum.md)':
@@ -480,52 +480,52 @@ MAPPING: Dict[str, str] = {
     '- [Sequential animation setting](sequential_animation.md)':
     '',
 
-    '- [animation parallel interface](animation_parallel.md)':
+    '- [animation_parallel interface](animation_parallel.md)':
     '',
 
-    '- [animation move interface](animation_move.md)':
+    '- [animation_move interface](animation_move.md)':
     '',
 
-    '- [animation x interface](animation_x.md)':
+    '- [animation_x interface](animation_x.md)':
     '',
 
-    '- [animation y interface](animation_y.md)':
+    '- [animation_y interface](animation_y.md)':
     '',
 
-    '- [animation width and animation height interfaces](animation_width_and_height.md)':  # noqa
+    '- [animation_width and animation_height interfaces](animation_width_and_height.md)':  # noqa
     '',
 
-    '- [animation fill color interface](animation_fill_color.md)':
+    '- [animation fill_color interface](animation_fill_color.md)':
     '',
 
-    '- [animation fill alpha interface](animation_fill_alpha.md)':
+    '- [animation fill_alpha interface](animation_fill_alpha.md)':
     '',
 
-    '- [animation line color interface](animation_line_color.md)':
+    '- [animation line_color interface](animation_line_color.md)':
     '',
 
-    '- [animation line alpha interface](animation_line_alpha.md)':
+    '- [animation line_alpha interface](animation_line_alpha.md)':
     '',
 
-    '- [animation line thickness interface](animation_line_thickness.md)':
+    '- [animation line_thickness interface](animation_line_thickness.md)':
     '',
 
-    '- [animation radius interface](animation_radius.md)':
+    '- [animation_radius interface](animation_radius.md)':
     '',
 
-    '- [animation rotation around center interface](animation_rotation_around_center.md)':  # noqa
+    '- [animation rotation_around_center interface](animation_rotation_around_center.md)':  # noqa
     '',
 
-    '- [animation rotation around point interface](animation_rotation_around_point.md)':  # noqa
+    '- [animation rotation_around_point interface](animation_rotation_around_point.md)':  # noqa
     '',
 
-    '- [animation scale x from center and animation scale y from center interfaces](animation_scale_x_and_y_from_center.md)':  # noqa
+    '- [animation_scale_x_from_center and animation_scale_y_from_center interfaces](animation_scale_x_and_y_from_center.md)':  # noqa
     '',
 
-    '- [animation scale x from point and animation scale y from point interfaces](animation_scale_x_and_y_from_point.md)':  # noqa
+    '- [animation_scale_x_from_point and animation_scale_y_from_point interfaces](animation_scale_x_and_y_from_point.md)':  # noqa
     '',
 
-    '- [animation skew x interface](animation_skew_x.md)':
+    '- [animation skew_x interface](animation_skew_x.md)':
     '',
 
     '---':
@@ -537,10 +537,10 @@ MAPPING: Dict[str, str] = {
     '- [trace function interface](trace.md)':
     '',
 
-    '- [set debug mode interface](set_debug_mode.md)':
+    '- [set_debug_mode interface](set_debug_mode.md)':
     '',
 
-    '- [unset debug mode interface](unset_debug_mode.md)':
+    '- [unset_debug_mode interface](unset_debug_mode.md)':
     '',
 
     '---':
@@ -552,19 +552,19 @@ MAPPING: Dict[str, str] = {
     '- [JavaScript assertion interface basic behavior](assertion_basic_behavior.md)':  # noqa
     '- [JavaScriptの各アサーションのインターフェイスの基本的な挙動](jp_assertion_basic_behavior.md)',  # noqa
 
-    '- [assert equal and assert not equal interfaces](assert_equal_and_not_equal.md)':  # noqa
+    '- [assert_equal and assert_not_equal interfaces](assert_equal_and_not_equal.md)':  # noqa
     '',
 
-    '- [assert true and assert false interfaces](assert_true_and_false.md)':
+    '- [assert_true and assert_false interfaces](assert_true_and_false.md)':
     '',
 
-    '- [assert arrays equal and assert arrays not equal interfaces](assert_arrays_equal_and_arrays_not_equal.md)':  # noqa
+    '- [assert_arrays_equal and assert_arrays_not_equal interfaces](assert_arrays_equal_and_arrays_not_equal.md)':  # noqa
     '',
 
-    '- [assert dicts equal and assert dicts not equal interfaces](assert_dicts_equal_and_dicts_not_equal.md)':  # noqa
+    '- [assert_dicts_equal and assert_dicts_not_equal interfaces](assert_dicts_equal_and_dicts_not_equal.md)':  # noqa
     '',
 
-    '- [assert defined and assert undefined interfaces](assert_defined_and_undefined.md)':  # noqa
+    '- [assert_defined and assert_undefined interfaces](assert_defined_and_undefined.md)':  # noqa
     '',
 
     '---':

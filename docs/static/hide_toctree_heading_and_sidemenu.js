6420b44fd85e9cdaca5e663dc074bd466e20f8fe
simon-ritchie <antisocial.sid2@gmail.com>
2022-04-30 20:32:44 +0900

    #666 Change the documents directory paths.

--- docs/static/hide_toctree_heading_and_sidemenu.js
+++
@@ -1,8 +0,0 @@
-// The script to hide the headings and sidemenu's table
-// of contents list.
-
-$(document).ready(function() {
-    $("h3:contains(Table of contents)").css("display", "none");
-    $("div.sphinxsidebar a:contains(Table of contents)").parent().css(
-        "display", "none");
-});

d8f303525e329cb3bc5c3f12ed632b7ff6d6e722
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-02 16:55:13 +0900

    #683 Fix the setting mistake.

--- docs_src/source/conf_en/conf.py
+++ docs_src/source/conf_en/conf.py
@@ -47,7 +47,7 @@ author: Final[str] = conf_common.AUTHOR
 extensions: Final[List[str]] = conf_common.EXTENSIONS
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path: Final[List[str]] = conf_common.EXTENSIONS
+templates_path: Final[List[str]] = conf_common.TEMPLATES_PATH
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.

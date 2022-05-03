e9edecb493a54ec07d1093875e632c3289fe63c1
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-03 23:18:32 +0900

    #642 Adjust the E2E script to access to the local document file.

--- scripts/run_docs_e2e_tests.py
+++ scripts/run_docs_e2e_tests.py
@@ -4,6 +4,7 @@ Command example:
 $ python ./scripts/run_docs_e2e_tests.py
 """
 
+import os
 from playwright.sync_api import sync_playwright
 
 
@@ -13,8 +14,13 @@ def _main() -> None:
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
-        page.goto(url='https://simon-ritchie.github.io/apysc/en/index.html')
-        print('page:', page.title())
+        os.path.abspath('./')
+        file_path: str = os.path.join(
+            f'file://{os.path.abspath("./")}/',
+            'docs/en/index.html'
+        )
+        page.goto(url=file_path)
+        print('Page title:', page.title())
 
 
 if __name__ == '__main__':

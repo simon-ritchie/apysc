02d5fc80ae065338c0cd77d60dc95f0140fe3082
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-05 21:14:26 +0900

    #642 Adjust the script to use the helper interfaces.

--- scripts/run_docs_e2e_tests.py
+++ scripts/run_docs_e2e_tests.py
@@ -5,45 +5,38 @@ $ python ./scripts/run_docs_e2e_tests.py
 """
 
 import os
-from typing import List
+from typing import Dict, List, Optional
+import sys
 
 from playwright.sync_api import sync_playwright
 from playwright.sync_api import Playwright, Page, ConsoleMessage, Error
 
+sys.path.append('./')
+
+from apysc._tests import e2e_testing_helper
+from apysc._lint_and_doc.docs_lang import Lang
+
+_EXPECTED_ASSERTION_FAILED_MSGS: Dict[str, List[str]] = {
+    'assert_equal_and_not_equal':
+    ['Values are equal!', 'Values are not equal!'],
+}
 
 
 def _main() -> None:
     """Entry point of this script.
     """
-    p: Playwright
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        file_paths: List[str] = _get_file_paths()
-        for file_path in file_paths:
-            page: Page = browser.new_page()
-            page.on(event='console', f=_on_console)
-            page.on(event='pageerror', f=_on_pageerror)
-            page.goto(url=file_path)
-            print('Page title:', page.title())
-            print('Page url:', page.url)
-
-
-def _get_file_paths() -> List[str]:
-    file_path_suffix: str = f'file://{os.path.abspath("./")}/docs/'
-    file_paths: List[str] = [
-        f'{file_path_suffix}en/assert_equal_and_not_equal.html',
-        # f'{file_path_suffix}en/static/assert_equal_basic_usage/index.html',
+    file_names: List[str] = [
+        'index',
+        'assert_equal_and_not_equal',
     ]
-    return file_paths
-
-
-def _on_pageerror(err: Error) -> None:
-    print('Error msg:', err.message)
-
-
-def _on_console(message: ConsoleMessage) -> None:
-    print('console msg:', message.text)
-    print('message type:', message.type)
+    for file_name in file_names:
+        file_path: str = e2e_testing_helper.get_docs_local_file_path(
+            lang=Lang.EN, file_name=file_name)
+        expected_assertion_failed_msgs: Optional[List[str]] = \
+            _EXPECTED_ASSERTION_FAILED_MSGS.get(file_name)
+        e2e_testing_helper.assert_local_file_not_raises_error(
+            file_path=file_path,
+            expected_assertion_failed_msgs=expected_assertion_failed_msgs)
 
 
 if __name__ == '__main__':

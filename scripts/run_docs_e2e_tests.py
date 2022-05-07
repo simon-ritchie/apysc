272c5268dbefb99848d7599026513400f8f5cf71
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-07 12:29:43 +0900

    #642 Add the function to get expected assertion failure messages if exist.

--- scripts/run_docs_e2e_tests.py
+++ scripts/run_docs_e2e_tests.py
@@ -39,10 +39,36 @@ def _main() -> None:
         file_path: str = e2e_testing_helper.get_docs_local_file_path(
             lang=Lang.EN, file_name=file_name)
         expected_assertion_failed_msgs: Optional[List[str]] = \
-            _EXPECTED_ASSERTION_FAILED_MSGS.get(file_name)
+            _get_expected_assertion_failed_msgs(file_name=file_name)
         e2e_testing_helper.assert_local_file_not_raises_error(
             file_path=file_path,
             expected_assertion_failed_msgs=expected_assertion_failed_msgs)
+
+
+def _get_expected_assertion_failed_msgs(
+        *,
+        file_name: str) -> Optional[List[str]]:
+    """
+    Get expected assertion failure messages if exist.
+
+    Parameters
+    ----------
+    file_name : str
+        A target document file name.
+
+    Returns
+    -------
+    expected_assertion_failed_msgs : Optional[List[str]]
+        Expected assertion failure messages. If there is no
+        definition, this interface returns None.
+    """
+    for lang in Lang:
+        if file_name.startswith(f'{lang.value}_'):
+            file_name = file_name.replace(f'{lang.value}_', '', 1)
+            break
+    expected_assertion_failed_msgs: Optional[List[str]] = \
+        _EXPECTED_ASSERTION_FAILED_MSGS.get(file_name)
+    return expected_assertion_failed_msgs
 
 
 def _get_file_names() -> List[str]:
--- tests/test_run_docs_e2e_tests.py
+++ tests/test_run_docs_e2e_tests.py
@@ -1,5 +1,5 @@
 from random import randint
-from typing import List
+from typing import List, Optional
 
 from retrying import retry
 
@@ -17,3 +17,17 @@ def test__get_file_names() -> None:
     ]
     for expected_file_name in expected_file_names:
         assert expected_file_name in file_names
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__get_expected_assertion_failed_msgs() -> None:
+    expected_assertion_failed_msgs: Optional[List[str]] = run_docs_e2e_tests.\
+        _get_expected_assertion_failed_msgs(
+            file_name='jp_assert_equal_and_not_equal')
+    assert expected_assertion_failed_msgs == [
+        'Values are equal!', 'Values are not equal!']
+
+    expected_assertion_failed_msgs = run_docs_e2e_tests.\
+        _get_expected_assertion_failed_msgs(
+            file_name='sprite')
+    assert expected_assertion_failed_msgs is None

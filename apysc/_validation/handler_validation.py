50988a0964c4b78dd0632209a3e4bde8ac8cf9be
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-15 13:33:44 +0900

    #688 Add the function to validate specified handler's arguments number.

--- apysc/_validation/handler_validation.py
+++ apysc/_validation/handler_validation.py
@@ -2,7 +2,9 @@
 validation implementations.
 """
 
-from typing import Any
+import inspect
+from inspect import Signature
+from typing import Any, Callable, List
 
 
 def validate_options_type(*, options: Any) -> None:
@@ -26,3 +28,35 @@ def validate_options_type(*, options: Any) -> None:
     raise TypeError(
         f"Handler's options argument must be a dictionary: {type(options)}"
         f'\n{options}')
+
+
+def validate_handler_args_num(*, handler: Callable) -> None:
+    """
+    Validate specified handler's arguments number.
+
+    Parameters
+    ----------
+    handler : Callable
+        A target handler to validate.
+
+    Raises
+    ------
+    ValueError
+        - If handler's arguments number is not 2.
+    """
+    signature: Signature = inspect.signature(obj=handler)
+    args_num: int = 0
+    skipping_arg_names: List[str] = ['*', '**', 'self', 'cls']
+    arg_names: List[str] = []
+    for parameter in signature.parameters:
+        if parameter in skipping_arg_names:
+            continue
+        args_num += 1
+        arg_names.append(parameter)
+    if args_num != 2:
+        raise ValueError(
+            'A specified handler\'s arguments number must be 2 '
+            f'(actual: {args_num})'
+            f'\nTarget argument names: {arg_names}'
+            '\n\nThe first argument becomes event instance and the second '
+            'one becomes the handler\'s option parameters.')
--- tests/_validation/test_handler_validation.py
+++ tests/_validation/test_handler_validation.py
@@ -5,6 +5,7 @@ from typing_extensions import TypedDict
 
 from apysc._testing.testing_helper import assert_raises
 from apysc._validation import handler_validation
+import apysc as ap
 
 
 class _TestTypedDict(TypedDict):
@@ -26,3 +27,28 @@ def test_validate_options_type() -> None:
         func_or_method=handler_validation.validate_options_type,
         kwargs={'options': [10]},
         match="Handler's options argument must be a dictionary")
+
+
+def _test_handler_1(*, e: ap.Event) -> None:
+    ...
+
+
+def _test_handler_2(*, e: ap.Event, options: dict) -> None:
+    ...
+
+
+def _test_handler_3(self, *, e: ap.Event, options: dict) -> None:
+    ...
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_validate_handler_args_num() -> None:
+    assert_raises(
+        expected_error_class=ValueError,
+        func_or_method=handler_validation.validate_handler_args_num,
+        kwargs={'handler': _test_handler_1},
+        match=r'A specified handler\'s arguments number '
+        r'must be 2 \(actual: 1\)')
+
+    handler_validation.validate_handler_args_num(handler=_test_handler_2)
+    handler_validation.validate_handler_args_num(handler=_test_handler_3)

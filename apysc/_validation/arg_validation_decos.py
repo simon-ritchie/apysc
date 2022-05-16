e0138afb91f9ac3877144c093aa97060ff612a9b
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-16 20:56:08 +0900

    #688 Add the decorator function to set the validation to check specified handler argument's number.

--- apysc/_validation/arg_validation_decos.py
+++ apysc/_validation/arg_validation_decos.py
@@ -2,7 +2,7 @@
 """
 
 import functools
-from typing import Any
+from typing import Any, Dict, List
 from typing import Callable
 from typing import Optional
 from typing import TypeVar
@@ -10,6 +10,37 @@ from typing import Union
 
 # pyright: reportInvalidTypeVarUse=false
 _F = TypeVar('_F', bound=Callable)
+
+
+def _extract_arg_value(
+        *, args: Any, kwargs: Dict[str, Any],
+        arg_position_index: int, arg_name: str) -> Any:
+    """
+    Extract an argument value from a specified arguments' dictionary
+    or list.
+
+    Parameters
+    ----------
+    args : List[Any]
+        A specified positional arguments' list.
+    kwargs : Dict[str, Any]
+        A specified keyword arguments' dictionary.'
+    arg_position_index : int
+        A target argument position index.
+    arg_name : str
+        A target argument name to check.
+
+    Returns
+    -------
+    value : Any
+        An extracted any value.
+    """
+    value: Any = None
+    if arg_name in kwargs:
+        value = kwargs[arg_name]
+    elif len(args) - 1 >= arg_position_index:
+        value = args[arg_position_index]
+    return value
 
 
 def not_empty_string(
@@ -66,14 +97,89 @@ def not_empty_string(
             from apysc._type.string import String
             from apysc._validation.string_validation import \
                 validate_not_empty_string
-            string: Optional[Union[str, String]] = None
-            if arg_name in kwargs:
-                string = kwargs.get(arg_name, '')
-            elif len(args) - 1 >= arg_position_index:
-                string = args[arg_position_index]
+            string: Any = _extract_arg_value(
+                args=args, kwargs=kwargs,
+                arg_position_index=arg_position_index, arg_name=arg_name)
 
             if string is not None:
-                validate_not_empty_string(string=string)
+                validate_not_empty_string(
+                    string=string,
+                    additional_err_msg=(
+                        'An argument\'s string value must not be empty.'
+                        f'\nTarget callable name: {func.__name__}'
+                        f'\nTarget argument name: {arg_name}'
+                    ))
+            result: Any = func(*args, **kwargs)
+            return result
+
+        return inner_wrapped  # type: ignore
+
+    return wrapped  # type: ignore
+
+
+def handler_args_num(
+        *, arg_position_index: int, arg_name: str) -> _F:
+    """
+    Set the validation to check specified handler argument's
+    number.
+
+    Parameters
+    ----------
+    arg_position_index : int
+        A target argument position index.
+    arg_name : str
+        A target argument name to validate.
+
+    Returns
+    -------
+    _wrapped : Callable
+        Wrapped callable object.
+    """
+
+    def wrapped(func: _F) -> _F:
+        """
+        Wrapping function for a decorator setting.
+
+        Parameters
+        ----------
+        func : Callable
+            A target function or method to wrap.
+
+        Returns
+        -------
+        inner_wrapped : Callable
+            Wrapped callable object.
+        """
+
+        @functools.wraps(func)
+        def inner_wrapped(*args: Any, **kwargs: Any) -> Any:
+            """
+            Wrapping function for a decorator setting.
+
+            Parameters
+            ----------
+            *args : list
+                Target positional arguments.
+            **kwargs : dict
+                Target keyword arguments.
+
+            Returns
+            -------
+            result : Any
+                A return value(s) of a callable execution result.
+            """
+            from apysc._validation.handler_validation import \
+                validate_handler_args_num
+            handler: Any = _extract_arg_value(
+                args=args, kwargs=kwargs,
+                arg_position_index=arg_position_index,
+                arg_name=arg_name)
+            if handler is not None:
+                validate_handler_args_num(
+                    handler=handler,
+                    additional_err_msg=(
+                        f'Target Callable name: {func.__name__}'
+                    ))
             result: Any = func(*args, **kwargs)
             return result
 
--- tests/_validation/test_arg_validation_decos.py
+++ tests/_validation/test_arg_validation_decos.py
@@ -1,9 +1,11 @@
 from random import randint
+from typing import Any, Callable
 
 from retrying import retry
 
 from apysc._testing.testing_helper import assert_raises
 from apysc._validation import arg_validation_decos
+import apysc as ap
 
 
 @arg_validation_decos.not_empty_string(
@@ -25,12 +27,25 @@ class _TestClass1:
         ...
 
 
+@arg_validation_decos.handler_args_num(
+    arg_position_index=0, arg_name='handler')
+def _test_func_2(*, handler: Callable) -> None:
+    ...
+
+
+@arg_validation_decos.handler_args_num(
+    arg_position_index=0, arg_name='handler')
+def _test_func_3() -> None:
+    ...
+
+
 @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
 def test_not_empty_string() -> None:
     assert_raises(
         expected_error_class=ValueError,
         func_or_method=_test_func_1,
-        kwargs={'a': ''})
+        kwargs={'a': ''},
+        match='An argument\'s string value must not be empty.')
     assert_raises(
         expected_error_class=ValueError,
         func_or_method=_test_func_1,
@@ -43,12 +58,59 @@ def test_not_empty_string() -> None:
     assert_raises(
         expected_error_class=ValueError,
         func_or_method=test_instance._test_method_1,
-        kwargs={'a': ''})
+        kwargs={'a': ''},
+        match='An argument\'s string value must not be empty.')
     assert_raises(
         expected_error_class=ValueError,
         func_or_method=test_instance._test_method_2,
-        kwargs={'a': ''})
+        kwargs={'a': ''},
+        match='An argument\'s string value must not be empty.')
 
     test_instance._test_method_1(a='Hello')
     test_instance._test_method_2(a='Hello')
     test_instance._test_method_2('Hello')
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__extract_arg_value() -> None:
+    value: Any = arg_validation_decos._extract_arg_value(
+        args=[],
+        kwargs={'a': 'Test message 1'},
+        arg_position_index=0,
+        arg_name='a')
+    assert value == 'Test message 1'
+
+    value = arg_validation_decos._extract_arg_value(
+        args=['Test message 2'],
+        kwargs={},
+        arg_position_index=0,
+        arg_name='a')
+    assert value == 'Test message 2'
+
+    value = arg_validation_decos._extract_arg_value(
+        args=[],
+        kwargs={},
+        arg_position_index=0,
+        arg_name='a')
+    assert value is None
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_handler_args_num() -> None:
+
+    def _test_handler_1(e: ap.Event) -> None:
+        ...
+
+    assert_raises(
+        expected_error_class=ValueError,
+        func_or_method=_test_func_2,
+        kwargs={'handler': _test_handler_1},
+        match='Target Callable name: _test_func_2',
+    )
+
+    _test_func_3()
+
+    def _test_handler_2(e: ap.Event, options: dict) -> None:
+        ...
+
+    _test_func_2(handler=_test_handler_2)

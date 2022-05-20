ebfe411e52190fb65c366dcd2ee67bf0ef0b3405
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-20 21:35:39 +0900

    #688 Add the decorator settings to the `animation_fill_color` interface.

--- apysc/_animation/animation_fill_color_interface.py
+++ apysc/_animation/animation_fill_color_interface.py
@@ -9,12 +9,18 @@ from apysc._animation.animation_interface_base import AnimationInterfaceBase
 from apysc._animation.easing import Easing
 from apysc._type.int import Int
 from apysc._type.string import String
+from apysc._validation import arg_validation_decos
 
 StrOrString = TypeVar('StrOrString', str, String)
 
 
 class AnimationFillColorInterface(AnimationInterfaceBase):
 
+    @arg_validation_decos.is_hex_color_code_format(arg_position_index=1)
+    @arg_validation_decos.is_integer(arg_position_index=2)
+    @arg_validation_decos.num_is_gt_zero(arg_position_index=2)
+    @arg_validation_decos.is_integer(arg_position_index=3)
+    @arg_validation_decos.is_easing(arg_position_index=4)
     def animation_fill_color(
             self,
             *,

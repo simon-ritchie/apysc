25d582f8b6487eaa2157fcbe33faf7673dee66f3
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-23 20:23:56 +0900

    #688 Adjust the color validation to accept a blank string.

--- apysc/_validation/color_validation.py
+++ apysc/_validation/color_validation.py
@@ -20,7 +20,8 @@ def validate_hex_color_code_format(
     ----------
     hex_color_code : str
         Hexadecimal color code (not including '#').
-        e.g., 'ff0000', '666', '0'
+        e.g., 'ff0000', '666', '0'.
+        A blank string is also acceptable.
     additional_err_msg : str, optional
         An additional error message to display.
 
@@ -42,7 +43,7 @@ def validate_hex_color_code_format(
         value_ = hex_color_code
 
     char_len: int = len(value_)
-    expected_char_lengths: Tuple[int, int, int] = (1, 3, 6)
+    expected_char_lengths: Tuple[int, int, int, int] = (0, 1, 3, 6)
     if char_len not in expected_char_lengths:
         raise ValueError(
             'Not supported hex color code number of digits is specified.'
--- tests/_validation/test_color_validation.py
+++ tests/_validation/test_color_validation.py
@@ -27,6 +27,14 @@ def test_validate_hex_color_code_format() -> None:
         callable_=color_validation.validate_hex_color_code_format,
         hex_color_code='gggggg')
 
+    color_validation.validate_hex_color_code_format(
+        hex_color_code='')
+    color_validation.validate_hex_color_code_format(
+        hex_color_code='0')
     color_validation.validate_hex_color_code_format(hex_color_code='333')
     color_validation.validate_hex_color_code_format(
         hex_color_code=ap.String('333'))
+    color_validation.validate_hex_color_code_format(
+        hex_color_code=ap.String('0af'))
+    color_validation.validate_hex_color_code_format(
+        hex_color_code=ap.String('00aaff'))

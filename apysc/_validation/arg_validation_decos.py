d6519a3766975cc38b99014d3537f233a9f0353b
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-27 20:46:20 +0900

    #688 Add the decorator function to set the validation to check a specified argument's type is a line cap-related type.

--- apysc/_validation/arg_validation_decos.py
+++ apysc/_validation/arg_validation_decos.py
@@ -59,6 +59,9 @@ Mainly the following decorators exist.
 - is_path_data_list
     - Set the validation to check a specified argument's type
         is the list of `ap.PathDataBase`.
+- is_line_cap
+    - Set the validation to check a specified argument's type
+        is a line cap-related type.
 """
 
 import functools
@@ -1543,6 +1546,72 @@ def is_path_data_list(*, arg_position_index: int) -> _F:
                     'A value of path_data_list argument is not a '
                     'type of `ap.PathDataBase`\'s subclass: '
                     f'{type(path_data)}')
+
+            result: Any = callable_(*args, **kwargs)
+            return result
+
+        return inner_wrapped  # type: ignore
+
+    return wrapped  # type: ignore
+
+
+def is_line_cap(*, arg_position_index: int) -> _F:
+    """
+    Set the validation to check a specified argument's type
+    is a line cap-related type.
+
+    Parameters
+    ----------
+    arg_position_index : int
+        A target argument position index.
+
+    Returns
+    -------
+    wrapped : Callable
+        Wrapped callable object.
+    """
+
+    def wrapped(callable_: _F) -> _F:
+        """
+        Wrapping function for a decorator setting.
+
+        Parameters
+        ----------
+        callable_ : Callable
+            A target function or method to wrap.
+
+        Returns
+        -------
+        inner_wrapped : Callable
+            Wrapped callable object.
+        """
+
+        @functools.wraps(callable_)
+        def inner_wrapped(*args: Any, **kwargs: Any) -> Any:
+            """
+            Wrapping function for a decorator setting.
+
+            Parameters
+            ----------
+            *args : list
+                Target positional arguments.
+            **kwargs : dict
+                Target keyword arguments.
+
+            Returns
+            -------
+            result : Any
+                A return value(s) of a callable execution result.
+            """
+            from apysc._validation.display_validation import validate_line_cap
+            cap: Any = _extract_arg_value(
+                args=args, kwargs=kwargs,
+                arg_position_index=arg_position_index, callable_=callable_)
+
+            callable_and_arg_names_msg: str = _get_callable_and_arg_names_msg(
+                callable_=callable_, arg_position_index=arg_position_index)
+            validate_line_cap(
+                cap=cap, additional_err_msg=callable_and_arg_names_msg)
 
             result: Any = callable_(*args, **kwargs)
             return result

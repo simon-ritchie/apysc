7ef295b9a520c643de292b7a45eb02f1dc5a8848
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-28 12:08:40 +0900

    #688 Drop the unnecessary inner-function's validation.

--- apysc/_display/line_dash_setting_interface.py
+++ apysc/_display/line_dash_setting_interface.py
@@ -84,11 +84,8 @@ class LineDashSettingInterface(VariableNameInterface, RevertInterface):
                 callable_='line_dash_setting', args=[value], kwargs={},
                 module_name=__name__,
                 class_name=LineDashSettingInterface.__name__):
-            from apysc._validation import display_validation
             self._update_line_dash_setting_and_skip_appending_exp(value=value)
             self._append_line_dash_setting_update_expression()
-            display_validation.validate_multiple_line_settings_are_not_set(
-                any_instance=self)
 
     def _update_line_dash_setting_and_skip_appending_exp(
             self, *, value: Optional[LineDashSetting]) -> None:

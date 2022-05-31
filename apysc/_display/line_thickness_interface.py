355a9e4bade71cdeb8621d3e8aba2310b9339b36
simon-ritchie <antisocial.sid2@gmail.com>
2022-05-31 21:46:51 +0900

    #688 Add the decorator settings to the `line_thickness` property.

--- apysc/_display/line_thickness_interface.py
+++ apysc/_display/line_thickness_interface.py
@@ -10,6 +10,7 @@ from apysc._html.debug_mode import add_debug_info_setting
 from apysc._type.attr_linking_interface import AttrLinkingInterface
 from apysc._type.int import Int
 from apysc._type.revert_interface import RevertInterface
+from apysc._validation import arg_validation_decos
 
 
 class LineThicknessInterface(
@@ -74,6 +75,8 @@ class LineThicknessInterface(
         return value_util.get_copy(value=self._line_thickness)
 
     @line_thickness.setter
+    @arg_validation_decos.is_apysc_num(arg_position_index=1)
+    @arg_validation_decos.num_is_gte_zero(arg_position_index=1)
     def line_thickness(self, value: Int) -> None:
         """
         Update this instance's line thickness.

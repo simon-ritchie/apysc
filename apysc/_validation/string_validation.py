e963c00d3db3095f780f6b2ea7bb36cef89c9b36
simon-ritchie <antisocial.sid2@gmail.com>
2022-06-03 20:29:38 +0900

    #688 Add the function to validate a specified string's type is Python built-in's str.

--- apysc/_validation/string_validation.py
+++ apysc/_validation/string_validation.py
@@ -10,19 +10,19 @@ def validate_string_type(
         *, string: Union[str, String],
         additional_err_msg: str = '') -> None:
     """
-    Validate specified string's type is str.
+    Validate a specified string's type is str.
 
     Parameters
     ----------
     string : String or str
-        String to check.
+        A string to check.
     additional_err_msg : str, optional
         An additional error message to display.
 
     Raises
     ------
     ValueError
-        - If not string value is specified.
+        - If a non-string value is specified.
     """
     if isinstance(string, str):
         return
@@ -31,7 +31,33 @@ def validate_string_type(
     if additional_err_msg != '':
         additional_err_msg = f'\n{additional_err_msg}'
     raise ValueError(
-        f'Specified value is not str type: {type(string)}'
+        f'A specified value is not str type: {type(string)}'
+        f'{additional_err_msg}')
+
+
+def validate_builtin_string_type(
+        *, string: str, additional_err_msg: str = '') -> None:
+    """
+    Validate a specified string's type is Python built-in's str.
+
+    Parameters
+    ----------
+    string : str
+        A string to check.
+    additional_err_msg : str, optional
+        An additional error message to display.
+
+    Raises
+    ------
+    ValueError
+        - If a non-string value is specified.
+    """
+    if isinstance(string, str):
+        return
+    if additional_err_msg != '':
+        additional_err_msg = f'\n{additional_err_msg}'
+    raise ValueError(
+        f'A specified value is not str type: {type(string)}'
         f'{additional_err_msg}')
 
 
--- tests/_validation/test_string_validation.py
+++ tests/_validation/test_string_validation.py
@@ -35,3 +35,15 @@ def test_validate_not_empty_string() -> None:
         expected_error_class=ValueError,
         callable_=string_validation.validate_not_empty_string,
         string=ap.String(''))
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_validate_builtin_string_type() -> None:
+    string_validation.validate_builtin_string_type(string='Hello!')
+    testing_helper.assert_raises(
+        expected_error_class=ValueError,
+        callable_=string_validation.validate_builtin_string_type,
+        match='\nTest error!',
+        string=ap.String('Hello!'),
+        additional_err_msg='Test error!',
+    )

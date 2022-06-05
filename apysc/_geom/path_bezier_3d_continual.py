45132bc7cf23d24623b5ba6d87a1792790a56996
simon-ritchie <antisocial.sid2@gmail.com>
2022-06-05 15:48:30 +0900

    #688 Add the decorator settings to the `PathBezier3DContinual` class interfaces.

--- apysc/_geom/path_bezier_3d_continual.py
+++ apysc/_geom/path_bezier_3d_continual.py
@@ -14,6 +14,7 @@ from apysc._html.debug_mode import add_debug_info_setting
 from apysc._type.boolean import Boolean
 from apysc._type.int import Int
 from apysc._type.string import String
+from apysc._validation import arg_validation_decos
 
 
 class PathBezier3DContinual(
@@ -41,6 +42,11 @@ class PathBezier3DContinual(
     ...     ])
     """
 
+    @arg_validation_decos.is_integer(arg_position_index=1)
+    @arg_validation_decos.is_integer(arg_position_index=2)
+    @arg_validation_decos.is_integer(arg_position_index=3)
+    @arg_validation_decos.is_integer(arg_position_index=4)
+    @arg_validation_decos.is_boolean(arg_position_index=5)
     @add_debug_info_setting(
         module_name=__name__, class_name='PathBezier3DContinual')
     def __init__(
@@ -129,6 +135,11 @@ class PathBezier3DContinual(
         )
         return svg_str
 
+    @arg_validation_decos.is_integer(arg_position_index=1)
+    @arg_validation_decos.is_integer(arg_position_index=2)
+    @arg_validation_decos.is_integer(arg_position_index=3)
+    @arg_validation_decos.is_integer(arg_position_index=4)
+    @arg_validation_decos.is_boolean(arg_position_index=5)
     @add_debug_info_setting(
         module_name=__name__, class_name='PathBezier3DContinual')
     def update_path_data(

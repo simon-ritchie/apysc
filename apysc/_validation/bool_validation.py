9af305573e2ef2294d71155cde087795ae653322
simon-ritchie <antisocial.sid2@gmail.com>
2022-06-05 16:45:03 +0900

    #688 Add the function to validate whether a specified value is the built-in's `bool` type.

--- apysc/_validation/bool_validation.py
+++ apysc/_validation/bool_validation.py
@@ -10,19 +10,19 @@ def validate_bool(
         *, value: Union[bool, Boolean],
         additional_err_msg: str = '') -> None:
     """
-    Validate specified value is bool or Boolean type.
+    Validate whether a specified value is `bool` or `Boolean` type.
 
     Parameters
     ----------
     value : bool or Boolean
-        Boolean value to check.
+        A boolean value to check.
     additional_err_msg : str, optional
         An additional error message to display.
 
     Raises
     ------
     ValueError
-        If specified value isn't bool or Boolean type.
+        If a specified value isn't the `bool` or `Boolean` type.
     """
     from apysc._type import type_util
     is_bool: bool = type_util.is_bool(value=value)
@@ -31,5 +31,33 @@ def validate_bool(
     if additional_err_msg != '':
         additional_err_msg = f'\n{additional_err_msg}'
     raise ValueError(
-        f'Specified value is not bool or Boolean type: {type(value)}'
+        f'A specified value is not the bool or Boolean type: {type(value)}'
+        f'{additional_err_msg}')
+
+
+def validate_builtin_bool(
+        *, value: bool,
+        additional_err_msg: str = '') -> None:
+    """
+    Validate whether a specified value is the built-in's
+    `bool` type.
+
+    Parameters
+    ----------
+    value : bool
+        A boolean value to check.
+    additional_err_msg : str, optional
+        An additional error message to display.
+
+    Raises
+    ------
+    ValueError
+        If a specified value isn't the `bool` type.
+    """
+    if isinstance(value, bool):
+        return
+    if additional_err_msg != '':
+        additional_err_msg = f'\n{additional_err_msg}'
+    raise ValueError(
+        f'A specified value is not the bool type: {type(value)}'
         f'{additional_err_msg}')
--- tests/_validation/test_bool_validation.py
+++ tests/_validation/test_bool_validation.py
@@ -1,7 +1,13 @@
+from random import randint
+
+from retrying import retry
+
 from apysc._testing import testing_helper
 from apysc._validation import bool_validation
+import apysc as ap
 
 
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
 def test_validate_bool() -> None:
     bool_validation.validate_bool(value=True)
     testing_helper.assert_raises(
@@ -9,4 +15,16 @@ def test_validate_bool() -> None:
         callable_=bool_validation.validate_bool,
         match='\nTest error!',
         value=1,
+        additional_err_msg='Test error!')
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_validate_builtin_bool() -> None:
+    bool_validation.validate_builtin_bool(value=True)
+    bool_validation.validate_builtin_bool(value=False)
+    testing_helper.assert_raises(
+        expected_error_class=ValueError,
+        callable_=bool_validation.validate_builtin_bool,
+        match='\nTest error!',
+        value=ap.Boolean(True),
         additional_err_msg='Test error!')

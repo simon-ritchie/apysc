541080917a1b6ca13d7c10e6593f2e95d3301b3c
simon-ritchie <antisocial.sid2@gmail.com>
2022-06-10 20:51:37 +0900

    #713 Apply each lint and conversion.

--- docs/en/static/add_navigation_to_footer.js
+++ docs/en/static/add_navigation_to_footer.js
@@ -1,8 +1,24 @@
 // The script to add a navigation links to the footer.
 
 $(document).ready(function() {
-    let href = $("[title='next chapter']").attr("href");
-    let linkText = String($("[title='next chapter']").text());
+
+    /**
+     * Sanitise a specified text.
+     * @param {String} text A target text to sanitise.
+     * @returns {String} The sanitised text.
+     */
+    function sanitise(text) {
+        text = text
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/'/g, '&#039;')
+            .replace(/"/g, '&quot;');
+        return text;
+    }
+
+    let href = sanitise($("[title='next chapter']").attr("href"));
+    let linkText = sanitise(String($("[title='next chapter']").text()));
     if (linkText.trim() === "") {
         return;
     }

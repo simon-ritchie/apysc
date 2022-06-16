8470688c1a697a9b7f13f52600034248e95a3059
simon-ritchie <antisocial.sid2@gmail.com>
2022-06-16 18:31:20 +0900

    #589 Add the docstring to the `AnyDisplayObject` class.

--- apysc/_display/any_display_object.py
+++ apysc/_display/any_display_object.py
@@ -1,7 +1,14 @@
+"""This module is for the `DisplayObject` subclass implementation
+to avoid an abstract method error.
+"""
+
 from apysc._display.display_object import DisplayObject
 from apysc._display.x_interface import XInterface
 from apysc._display.y_interface import YInterface
 
 
 class AnyDisplayObject(DisplayObject, XInterface, YInterface):
+    """This class is for the `DisplayObject` subclass
+    implementation to avoid an abstract method error.
+    """
     ...

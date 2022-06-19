da457f52c4e723761aed81cf3e5519232686aae5
simon-ritchie <antisocial.sid2@gmail.com>
2022-06-19 12:00:07 +0900

    #724 Apply each lint.

--- apysc/_display/graphics_clear_interface.py
+++ apysc/_display/graphics_clear_interface.py
@@ -4,14 +4,12 @@
 
 from typing import Optional
 
-from apysc._display.display_object import DisplayObject
 from apysc._display.line_dash_dot_setting import LineDashDotSetting
 from apysc._display.line_dash_setting import LineDashSetting
 from apysc._display.line_dot_setting import LineDotSetting
 from apysc._display.line_round_dot_setting import LineRoundDotSetting
 from apysc._display.polyline import Polyline
 from apysc._html.debug_mode import add_debug_info_setting
-from apysc._type.array import Array
 from apysc._type.int import Int
 from apysc._type.number import Number
 from apysc._type.string import String

e9219901f964796d00098b8b336ba63a1284ce3b
simon-ritchie <antisocial.sid2@gmail.com>
2022-07-11 21:15:02 +0900

    #749 Add the test project for the `VariableNameSuffixInterface` class.

---
+++ test_projects/VariableNameSuffixInterface/main.py
@@ -0,0 +1,44 @@
+"""The test project for the VariableNameSuffixInterface class.
+
+Command examples:
+$ python test_projects/VariableNameSuffixInterface/main.py
+"""
+
+import sys
+
+sys.path.append('./')
+
+import os
+from types import ModuleType
+
+import apysc as ap
+from apysc._file import file_util
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module),
+    'test_output/'
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    ap.Stage(
+        background_color='#333',
+        stage_width=1000, stage_height=500, stage_elem_id='stage')
+
+    int_1: ap.Int = ap.Int(10)
+    int_1.variable_name_suffix = 'int_1'
+    int_2: ap.Int = int_1 * 3
+    int_2.variable_name_suffix = 'int_2'
+    ap.trace(int_2)
+
+    ap.save_overall_html(
+        dest_dir_path=_DEST_DIR_PATH, minify=False)
+
+
+if __name__ == '__main__':
+    main()

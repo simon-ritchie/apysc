84a35871fbbe3e2a493a315b3dea60b256c15096
simon-ritchie <antisocial.sid2@gmail.com>
2022-07-16 18:29:24 +0900

    #749 Add the `Ellipse` class's test project.

--- test_projects/EllipseVariableNameSuffixInterface/main.py
+++ test_projects/EllipseVariableNameSuffixInterface/main.py
@@ -1,7 +1,7 @@
-"""The test project for the Circle class's VariableNameSuffixInterface.
+"""The test project for the Ellipse class's VariableNameSuffixInterface.
 
 Command examples:
-$ python test_projects/CircleVariableNameSuffixInterface/main.py
+$ python test_projects/EllipseVariableNameSuffixInterface/main.py
 """
 
 import sys
@@ -31,21 +31,21 @@ def main() -> None:
         stage_width=1000, stage_height=500, stage_elem_id='stage',
         variable_name_suffix='stage')
 
-    ap.Circle(
-        x=100, y=100, radius=50, fill_color='#0af',
-        variable_name_suffix='circle_1')
+    ap.Ellipse(
+        x=100, y=100, width=150, height=100, fill_color='#0af',
+        variable_name_suffix='ellipse_1')
 
     sprite: ap.Sprite = ap.Sprite(variable_name_suffix='sprite_1')
     sprite.graphics.begin_fill(color='#f0a', alpha=0.5)
     sprite.graphics.line_style(
         color='#fff', thickness=3, alpha=0.7)
-    sprite.graphics.draw_circle(
-        x=300, y=100, radius=75, variable_name_suffix='circle_2')
+    sprite.graphics.draw_ellipse(
+        x=300, y=100, width=150, height=100,
+        variable_name_suffix='ellipse_2')
 
     ap.save_overall_html(
         dest_dir_path=_DEST_DIR_PATH,
         minify=False)
-
 
 if __name__ == '__main__':
     main()

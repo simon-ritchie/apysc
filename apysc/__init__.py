8c53fee19f07970d235289beb171aa8dfa04c9a0
simon-ritchie <antisocial.sid2@gmail.com>
2022-07-20 20:38:42 +0900

    #757 Revert the root package module.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -28,9 +28,7 @@ from apysc._display.line_caps import LineCaps  # isort:skip # noqa
 from apysc._display.line_joints import LineJoints  # isort:skip # noqa
 from apysc._display.line_dot_setting import LineDotSetting  # isort:skip # noqa
 from apysc._display.line_dash_setting import LineDashSetting  # isort:skip # noqa
-from apysc._display.line_round_dot_setting import (
-    LineRoundDotSetting,
-)  # isort:skip # noqa
+from apysc._display.line_round_dot_setting import LineRoundDotSetting  # isort:skip # noqa
 from apysc._display.line_dash_dot_setting import LineDashDotSetting  # isort:skip # noqa
 from apysc._display.path import Path  # isort:skip # noqa
 from apysc._geom.point2d import Point2D  # isort:skip # noqa
@@ -42,13 +40,9 @@ from apysc._geom.path_horizontal import PathHorizontal  # isort:skip # noqa
 from apysc._geom.path_vertical import PathVertical  # isort:skip # noqa
 from apysc._geom.path_close import PathClose  # isort:skip # noqa
 from apysc._geom.path_bezier_2d import PathBezier2D  # isort:skip # noqa
-from apysc._geom.path_bezier_2d_continual import (
-    PathBezier2DContinual,
-)  # isort:skip # noqa
+from apysc._geom.path_bezier_2d_continual import PathBezier2DContinual  # isort:skip # noqa
 from apysc._geom.path_bezier_3d import PathBezier3D  # isort:skip # noqa
-from apysc._geom.path_bezier_3d_continual import (
-    PathBezier3DContinual,
-)  # isort:skip # noqa
+from apysc._geom.path_bezier_3d_continual import PathBezier3DContinual  # isort:skip # noqa
 from apysc._geom.path_data import PathData  # isort:skip # noqa
 from apysc._event.event import Event  # isort:skip # noqa
 from apysc._event.mouse_event import MouseEvent  # isort:skip # noqa
@@ -67,19 +61,11 @@ from apysc._console.assertion import assert_dicts_equal  # isort:skip # noqa
 from apysc._console.assertion import assert_dicts_not_equal  # isort:skip # noqa
 from apysc._console.assertion import assert_defined  # isort:skip # noqa
 from apysc._console.assertion import assert_undefined  # isort:skip # noqa
-from apysc._event.document_mouse_wheel_interface import (
-    bind_wheel_event_to_document,
-)  # isort:skip # noqa
-from apysc._event.document_mouse_wheel_interface import (
-    unbind_wheel_event_all_from_document,
-)  # isort:skip # noqa
-from apysc._event.document_mouse_wheel_interface import (
-    unbind_wheel_event_from_document,
-)  # isort:skip # noqa
+from apysc._event.document_mouse_wheel_interface import bind_wheel_event_to_document  # isort:skip # noqa
+from apysc._event.document_mouse_wheel_interface import unbind_wheel_event_all_from_document  # isort:skip # noqa
+from apysc._event.document_mouse_wheel_interface import unbind_wheel_event_from_document  # isort:skip # noqa
 from apysc._html.exporter import save_overall_html  # isort:skip # noqa
-from apysc._expression.expression_data_util import (
-    append_js_expression,
-)  # isort:skip # noqa
+from apysc._expression.expression_data_util import append_js_expression  # isort:skip # noqa
 from apysc._jupyter.jupyter_util import display_on_jupyter  # isort:skip # noqa
 from apysc._jupyter.jupyter_util import display_on_colaboratory  # isort:skip # noqa
 from apysc._time.fps import FPS  # isort:skip # noqa
@@ -99,48 +85,22 @@ from apysc._animation.animation_cx import AnimationCx  # isort:skip # noqa
 from apysc._animation.animation_cy import AnimationCy  # isort:skip # noqa
 from apysc._animation.animation_width import AnimationWidth  # isort:skip # noqa
 from apysc._animation.animation_height import AnimationHeight  # isort:skip # noqa
-from apysc._animation.animation_width_for_ellipse import (
-    AnimationWidthForEllipse,
-)  # isort:skip # noqa
-from apysc._animation.animation_height_for_ellipse import (
-    AnimationHeightForEllipse,
-)  # isort:skip # noqa
+from apysc._animation.animation_width_for_ellipse import AnimationWidthForEllipse  # isort:skip # noqa
+from apysc._animation.animation_height_for_ellipse import AnimationHeightForEllipse  # isort:skip # noqa
 from apysc._animation.animation_radius import AnimationRadius  # isort:skip # noqa
-from apysc._animation.animation_fill_alpha import (
-    AnimationFillAlpha,
-)  # isort:skip # noqa
-from apysc._animation.animation_fill_color import (
-    AnimationFillColor,
-)  # isort:skip # noqa
-from apysc._animation.animation_line_color import (
-    AnimationLineColor,
-)  # isort:skip # noqa
-from apysc._animation.animation_line_alpha import (
-    AnimationLineAlpha,
-)  # isort:skip # noqa
-from apysc._animation.animation_line_thickness import (
-    AnimationLineThickness,
-)  # isort:skip # noqa
+from apysc._animation.animation_fill_alpha import AnimationFillAlpha  # isort:skip # noqa
+from apysc._animation.animation_fill_color import AnimationFillColor  # isort:skip # noqa
+from apysc._animation.animation_line_color import AnimationLineColor  # isort:skip # noqa
+from apysc._animation.animation_line_alpha import AnimationLineAlpha  # isort:skip # noqa
+from apysc._animation.animation_line_thickness import AnimationLineThickness  # isort:skip # noqa
 from apysc._animation.animation_skew_x import AnimationSkewX  # isort:skip # noqa
 from apysc._animation.animation_skew_y import AnimationSkewY  # isort:skip # noqa
-from apysc._animation.animation_rotation_around_center import (
-    AnimationRotationAroundCenter,
-)  # isort:skip # noqa
-from apysc._animation.animation_rotation_around_point import (
-    AnimationRotationAroundPoint,
-)  # isort:skip # noqa
-from apysc._animation.animation_scale_x_from_center import (
-    AnimationScaleXFromCenter,
-)  # isort:skip # noqa
-from apysc._animation.animation_scale_y_from_center import (
-    AnimationScaleYFromCenter,
-)  # isort:skip # noqa
-from apysc._animation.animation_scale_x_from_point import (
-    AnimationScaleXFromPoint,
-)  # isort:skip # noqa
-from apysc._animation.animation_scale_y_from_point import (
-    AnimationScaleYFromPoint,
-)  # isort:skip # noqa
+from apysc._animation.animation_rotation_around_center import AnimationRotationAroundCenter  # isort:skip # noqa
+from apysc._animation.animation_rotation_around_point import AnimationRotationAroundPoint  # isort:skip # noqa
+from apysc._animation.animation_scale_x_from_center import AnimationScaleXFromCenter  # isort:skip # noqa
+from apysc._animation.animation_scale_y_from_center import AnimationScaleYFromCenter  # isort:skip # noqa
+from apysc._animation.animation_scale_x_from_point import AnimationScaleXFromPoint  # isort:skip # noqa
+from apysc._animation.animation_scale_y_from_point import AnimationScaleYFromPoint  # isort:skip # noqa
 from apysc._animation.animation_parallel import AnimationParallel  # isort:skip # noqa
 
 __version__: str = "2.2.31"

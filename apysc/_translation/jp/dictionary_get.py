e96a272f52ccd1ad6ddd3cd2dff05b01c2ba2329
simon-ritchie <antisocial.sid2@gmail.com>
2022-07-23 15:17:27 +0900

    #758 Apply each lint.

--- apysc/_translation/jp/dictionary_get.py
+++ apysc/_translation/jp/dictionary_get.py
@@ -15,19 +15,19 @@ MAPPING: Dict[str, str] = {
     ##################################################
     "## What interface is this?": "## インターフェイス概要",
     ##################################################
-    "The `get` method returns the specified key\'s value. If that key does not exist in the dictionary, it returns the default value (not raising an exception).": "`get`メソッドは引数に指定されたキーの値を返却します。もし指定されたキーが辞書内に存在しなければデフォルトの値を返却します（キーが無くともエラーにはなりません）。",  # noqa
+    "The `get` method returns the specified key's value. If that key does not exist in the dictionary, it returns the default value (not raising an exception).": "`get`メソッドは引数に指定されたキーの値を返却します。もし指定されたキーが辞書内に存在しなければデフォルトの値を返却します（キーが無くともエラーにはなりません）。",  # noqa
     ##################################################
     "## Basic usage": "## 基本的な使い方",
     ##################################################
     "The `get` method requires the first argument, `key` (dictionary key). The second argument of the `default` is optional, and if not provided, it returns the `None` value.": "`get`メソッドは`key`（辞書のキー）の第一引数を必要とします。第二引数の`default`引数は省略可で、もし指定されなければ`None`がデフォルト値となります。",  # noqa
     ##################################################
-    "```py\n# runnable\nfrom typing import Any, Optional\n\nimport apysc as ap\n\ndict_val: ap.Dictionary = ap.Dictionary({\'a\': 10})\ngot_val_1: int = dict_val.get(key=\'a\', default=0)\nassert got_val_1 == 10\n\ngot_val_2: int = dict_val.get(key=\'b\', default=0)\nassert got_val_2 == 0\n\ngot_val_3: Optional[Any] = dict_val.get(key=\'b\')\nassert got_val_3 is None\n```": "```py\n# runnable\nfrom typing import Any, Optional\n\nimport apysc as ap\n\ndict_val: ap.Dictionary = ap.Dictionary({\'a\': 10})\ngot_val_1: int = dict_val.get(key=\'a\', default=0)\nassert got_val_1 == 10\n\ngot_val_2: int = dict_val.get(key=\'b\', default=0)\nassert got_val_2 == 0\n\ngot_val_3: Optional[Any] = dict_val.get(key=\'b\')\nassert got_val_3 is None\n```",  # noqa
+    "```py\n# runnable\nfrom typing import Any, Optional\n\nimport apysc as ap\n\ndict_val: ap.Dictionary = ap.Dictionary({'a': 10})\ngot_val_1: int = dict_val.get(key='a', default=0)\nassert got_val_1 == 10\n\ngot_val_2: int = dict_val.get(key='b', default=0)\nassert got_val_2 == 0\n\ngot_val_3: Optional[Any] = dict_val.get(key='b')\nassert got_val_3 is None\n```": "```py\n# runnable\nfrom typing import Any, Optional\n\nimport apysc as ap\n\ndict_val: ap.Dictionary = ap.Dictionary({'a': 10})\ngot_val_1: int = dict_val.get(key='a', default=0)\nassert got_val_1 == 10\n\ngot_val_2: int = dict_val.get(key='b', default=0)\nassert got_val_2 == 0\n\ngot_val_3: Optional[Any] = dict_val.get(key='b')\nassert got_val_3 is None\n```",  # noqa
     ##################################################
     "## get API": "## get API",
     ##################################################
-    "<span class=\"inconspicuous-txt\">Note: the document build script generates and updates this API document section automatically. Maybe this section is duplicated compared with previous sections.</span>": "<span class=\"inconspicuous-txt\">特記事項: このAPIドキュメントはドキュメントビルド用のスクリプトによって自動で生成・同期されています。そのためもしかしたらこの節の内容は前節までの内容と重複している場合があります。</span>",  # noqa
+    '<span class="inconspicuous-txt">Note: the document build script generates and updates this API document section automatically. Maybe this section is duplicated compared with previous sections.</span>': '<span class="inconspicuous-txt">特記事項: このAPIドキュメントはドキュメントビルド用のスクリプトによって自動で生成・同期されています。そのためもしかしたらこの節の内容は前節までの内容と重複している場合があります。</span>',  # noqa
     ##################################################
-    "**[Interface summary]** Get a specified key dictionary value. If this dictionary hasn\'t a specified key, this interface returns a default value.<hr>": "**[インターフェイス概要]** 指定されたキーの辞書の値を取得します。もし辞書が指定されたキーを持っていない場合はデフォルト値を返却します。<hr>",  # noqa
+    "**[Interface summary]** Get a specified key dictionary value. If this dictionary hasn't a specified key, this interface returns a default value.<hr>": "**[インターフェイス概要]** 指定されたキーの辞書の値を取得します。もし辞書が指定されたキーを持っていない場合はデフォルト値を返却します。<hr>",  # noqa
     ##################################################
     "**[Parameters]**": "**[引数]**",
     ##################################################
@@ -51,5 +51,5 @@ MAPPING: Dict[str, str] = {
     ##################################################
     "**[Examples]**": "**[コードサンプル]**",
     ##################################################
-    "```py\n>>> from typing import Optional\n>>> import apysc as ap\n>>> dictionary: ap.Dictionary = ap.Dictionary({\'a\': 10})\n>>> value_1: Optional[int] = dictionary.get(\'a\')\n>>> value_1\n10\n\n>>> value_2: Optional[int] = dictionary.get(\'b\')\n>>> print(value_2)\nNone\n\n>>> value_3: int = dictionary.get(\'c\', default=0)\n>>> value_3\n0\n```": "```py\n>>> from typing import Optional\n>>> import apysc as ap\n>>> dictionary: ap.Dictionary = ap.Dictionary({\'a\': 10})\n>>> value_1: Optional[int] = dictionary.get(\'a\')\n>>> value_1\n10\n\n>>> value_2: Optional[int] = dictionary.get(\'b\')\n>>> print(value_2)\nNone\n\n>>> value_3: int = dictionary.get(\'c\', default=0)\n>>> value_3\n0\n```",  # noqa
+    "```py\n>>> from typing import Optional\n>>> import apysc as ap\n>>> dictionary: ap.Dictionary = ap.Dictionary({'a': 10})\n>>> value_1: Optional[int] = dictionary.get('a')\n>>> value_1\n10\n\n>>> value_2: Optional[int] = dictionary.get('b')\n>>> print(value_2)\nNone\n\n>>> value_3: int = dictionary.get('c', default=0)\n>>> value_3\n0\n```": "```py\n>>> from typing import Optional\n>>> import apysc as ap\n>>> dictionary: ap.Dictionary = ap.Dictionary({'a': 10})\n>>> value_1: Optional[int] = dictionary.get('a')\n>>> value_1\n10\n\n>>> value_2: Optional[int] = dictionary.get('b')\n>>> print(value_2)\nNone\n\n>>> value_3: int = dictionary.get('c', default=0)\n>>> value_3\n0\n```",  # noqa
 }
--- apysc/_translation/jp/why_apysc_doesnt_use_python_builtin_data_type.py
+++ apysc/_translation/jp/why_apysc_doesnt_use_python_builtin_data_type.py
@@ -9,9 +9,9 @@ from typing import Dict
 
 MAPPING: Dict[str, str] = {
     ##################################################
-    "# Why the apysc library doesn\'t use the Python built-in data type": "# 何故 apysc ライブラリではPythonのビルトインのデータ型を使用していないのか",  # noqa
+    "# Why the apysc library doesn't use the Python built-in data type": "# 何故 apysc ライブラリではPythonのビルトインのデータ型を使用していないのか",  # noqa
     ##################################################
-    "This page explains why the apysc library doesn\'t use the Python built-in data type, such as `int`\\, `float`\\, `bool`\\, `list`\\. And why the apysc library uses the apysc data type like the `Int`\\, `Number`\\, `Array` instead.": "このページでは apysc ライブラリが何故`int`や`float`、`bool`、`list`などのPythonビルトインのデータ型を使用していないのかについて説明します。また、何故それらの代わりに`Int`や`Number`、`Array`などのデータの型を使用しているのかについても説明します。",  # noqa
+    "This page explains why the apysc library doesn't use the Python built-in data type, such as `int`\\, `float`\\, `bool`\\, `list`\\. And why the apysc library uses the apysc data type like the `Int`\\, `Number`\\, `Array` instead.": "このページでは apysc ライブラリが何故`int`や`float`、`bool`、`list`などのPythonビルトインのデータ型を使用していないのかについて説明します。また、何故それらの代わりに`Int`や`Number`、`Array`などのデータの型を使用しているのかについても説明します。",  # noqa
     ##################################################
     "## apysc needs to convert Python to JavaScript and track variables change": "## apysc ではPythonをJavaScriptへと変換する必要があり、変数の変化を追う必要があります",  # noqa
     ##################################################
@@ -19,7 +19,7 @@ MAPPING: Dict[str, str] = {
     ##################################################
     "Occasionally, these are unnecessary to create HTML. Still, these types become essential when you use the asynchronous function, such as the event handler.": "場合によってはHTMLの生成処理でこれらの型の利用が不要な場合もありますが、イベントハンドラなどの非同期な処理を使う場合などには利用が必要になってきます。",  # noqa
     ##################################################
-    "The apysc library automatically sets each variable\'s names and uses them when exporting the HTML and JavaScript files. It also tracks variables creation and updating and applies them to the exported JavaScript.": "apysc ライブラリではそれらの型の内部で自動的に変数名を割り振り、HTMLやJavaScriptのファイルを出力する際にそれらの変数名を使用します。また、変数の生成や更新などの内容も出力されるJavaScriptのコードに反映されます。",  # noqa
+    "The apysc library automatically sets each variable's names and uses them when exporting the HTML and JavaScript files. It also tracks variables creation and updating and applies them to the exported JavaScript.": "apysc ライブラリではそれらの型の内部で自動的に変数名を割り振り、HTMLやJavaScriptのファイルを出力する際にそれらの変数名を使用します。また、変数の生成や更新などの内容も出力されるJavaScriptのコードに反映されます。",  # noqa
     ##################################################
-    "Using the Python built-in data type, these variables\' values become fixed (the apysc doesn\'t apply asynchronous function\'s changes).": "もしPythonビルトインの型を使った場合、これらの値JavaScript上では固定値で設定されます（apysc上では非同期の関数などでの変数の変更が反映されなくなります）。",  # noqa
+    "Using the Python built-in data type, these variables' values become fixed (the apysc doesn't apply asynchronous function's changes).": "もしPythonビルトインの型を使った場合、これらの値JavaScript上では固定値で設定されます（apysc上では非同期の関数などでの変数の変更が反映されなくなります）。",  # noqa
 }

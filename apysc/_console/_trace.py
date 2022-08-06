cb07fc526439be3616f200b0f017598280290de0
simon-ritchie <antisocial.sid2@gmail.com>
2022-08-06 12:07:17 +0900

    #756 Add the function to get a function caller's information.

--- apysc/_console/_trace.py
+++ apysc/_console/_trace.py
@@ -1,8 +1,11 @@
 """`trace` (console.log expression) interface implementations
 """
 
-from typing import Any
+from typing import Any, Optional
 from typing import List
+from types import FrameType
+import inspect
+from inspect import FrameInfo
 
 from apysc._html.debug_mode import add_debug_info_setting
 
@@ -42,5 +45,37 @@ def trace(*args: Any) -> None:
         arg = string_util.escape_str(string=str(arg))
         arg_strs.append(f'"{arg}"')
     expression += ", ".join(arg_strs)
+
+    func_caller_info: str = _get_func_callers_info()
+    expression +=f', "{func_caller_info}"'
+
     expression += ");"
     ap.append_js_expression(expression=expression)
+
+
+def _get_func_callers_info() -> str:
+    """
+    Get a function caller's information.
+
+    Returns
+    -------
+    func_caller_info : str
+        A function caller's information, such as the caller's name,
+        module name, and line number.
+    """
+    # Index count: this function + trace + decorator + caller = 3
+    OUTER_FRAMES_INDEX: int = 3
+
+    current_frame: Optional[FrameType] = inspect.currentframe()
+    outer_frames: List[FrameInfo] = inspect.getouterframes(frame=current_frame)
+    file_name: str = outer_frames[OUTER_FRAMES_INDEX].filename
+    file_name = file_name.rsplit('/', maxsplit=1)[-1]
+    lineno: int = outer_frames[OUTER_FRAMES_INDEX].lineno
+    function: str = outer_frames[OUTER_FRAMES_INDEX].function
+    if function != "<module>":
+        func_caller_info: str = (
+            f"\nCalled from: {function}, file name: {file_name}, line number: {lineno}"
+        )
+    else:
+        func_caller_info = f"\nCalled from: {file_name}, line number: {lineno}"
+    return func_caller_info
--- tests/_console/test__trace.py
+++ tests/_console/test__trace.py
@@ -4,6 +4,8 @@ from retrying import retry
 
 import apysc as ap
 from apysc._expression import expression_data_util
+from apysc._html.debug_mode import add_debug_info_setting
+from apysc._console import _trace
 
 
 @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
@@ -11,5 +13,39 @@ def test_trace() -> None:
     stage: ap.Stage = ap.Stage()
     ap.trace(stage, 100, "Hello!")
     expression: str = expression_data_util.get_current_expression()
-    expected: str = f'console.log({stage.variable_name}, "100", "Hello!");'
+    expected: str = (
+        f'console.log({stage.variable_name}, "100", "Hello!", "\nCalled from: '
+        'test_trace, file name: test__trace.py, line number: '
+    )
     assert expected in expression
+    assert '");' in expression
+
+
+@add_debug_info_setting(module_name=__name__)
+def _dummy_trace() -> str:
+    """
+    Dummy function of the `trace`.
+
+    Returns
+    -------
+    func_callers_info : str
+        A function caller's information.
+    """
+    func_callers_info: str = _trace._get_func_callers_info()
+    return func_callers_info
+
+
+_TOP_LEVEL_SCOPE_FUNC_CALLERS_INFO: str = _dummy_trace()
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test__get_func_callers_info() -> None:
+    print(_TOP_LEVEL_SCOPE_FUNC_CALLERS_INFO)
+    assert _TOP_LEVEL_SCOPE_FUNC_CALLERS_INFO.startswith(
+        "\nCalled from: test__trace"
+    )
+
+    func_callers_info: str = _dummy_trace()
+    assert func_callers_info.startswith(
+        "\nCalled from: test__get_func_callers_info, file name: test__trace"
+    )

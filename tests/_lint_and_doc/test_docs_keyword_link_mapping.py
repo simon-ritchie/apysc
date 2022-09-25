cdfe0672b99b391e3665ce5ad3f0dcea9f79c289
simon-ritchie <antisocial.sid2@gmail.com>
2022-09-25 12:19:40 +0900

    #779 Add the test to check whether the mappings are not duplicated.

---
+++ tests/_lint_and_doc/test_docs_keyword_link_mapping.py
@@ -0,0 +1,13 @@
+from random import randint
+from typing import List
+
+from retrying import retry
+
+from apysc._lint_and_doc.docs_keyword_link_mapping import MAPPINGS
+
+
+@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+def test_check_mappings_are_not_duplicated() -> None:
+    keys: List[str] = list(MAPPINGS.keys())
+    for key in keys:
+        assert keys.count(key) == 1, key

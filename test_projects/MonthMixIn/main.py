4ea9ceeed6a2c9d01c099a231a1976252e447f7e
simon-ritchie <antisocial.sid2@gmail.com>
2022-11-23 20:20:07 +0900

    #803 Add the test project for the `MonthMixIn`.

---
+++ test_projects/MonthMixIn/main.py
@@ -0,0 +1,42 @@
+"""The test project for the MonthMixIn class.
+
+Command examples:
+$ python test_projects/MonthMixIn/main.py
+"""
+
+import sys
+
+sys.path.append("./")
+
+import os
+from types import ModuleType
+
+import apysc as ap
+from apysc._file import file_util
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module), "test_output/"
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    ap.Stage(
+        background_color="#333",
+        stage_width=1000,
+        stage_height=500,
+        stage_elem_id="stage",
+    )
+    datetime_: ap.DateTime = ap.DateTime(year=2022, month=11, day=23)
+    month: ap.Int = datetime_.month
+    ap.assert_equal(month, 11)
+
+    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH)
+
+
+if __name__ == "__main__":
+    main()

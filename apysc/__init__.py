00189fad7425a0cd21fadade77d11265a7193a89
simon-ritchie <antisocial.sid2@gmail.com>
2022-12-05 20:03:29 +0900

    #796 Drop each line's `noqa` comment.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -1,107 +1,109 @@
-from apysc._type.int import Int  # isort:skip # noqa
-from apysc._type.number import Number  # isort:skip # noqa
-from apysc._type.number import Number as Float  # isort:skip # noqa
-from apysc._type.boolean import Boolean  # isort:skip # noqa
-from apysc._type.boolean import Boolean as Bool  # isort:skip # noqa
-from apysc._type.string import String  # isort:skip # noqa
-from apysc._type.array import Array  # isort:skip # noqa
-from apysc._type.dictionary import Dictionary  # isort:skip # noqa
-from apysc._type.any_value import AnyValue  # isort:skip # noqa
-from apysc._branch._if import If  # isort:skip # noqa
-from apysc._branch._elif import Elif  # isort:skip # noqa
-from apysc._branch._else import Else  # isort:skip # noqa
-from apysc._loop._for import For  # isort:skip # noqa
-from apysc._loop._continue import Continue  # isort:skip # noqa
-from apysc._display.display_object import DisplayObject  # isort:skip # noqa
-from apysc._display._document import document  # isort:skip # noqa
-from apysc._display.sprite import Sprite  # isort:skip # noqa
-from apysc._display.graphics import Graphics  # isort:skip # noqa
-from apysc._display.stage import Stage  # isort:skip # noqa
-from apysc._display.stage import get_stage  # isort:skip # noqa
-from apysc._display.rectangle import Rectangle  # isort:skip # noqa
-from apysc._display.circle import Circle  # isort:skip # noqa
-from apysc._display.ellipse import Ellipse  # isort:skip # noqa
-from apysc._display.line import Line  # isort:skip # noqa
-from apysc._display.polyline import Polyline  # isort:skip # noqa
-from apysc._display.polygon import Polygon  # isort:skip # noqa
-from apysc._display.line_caps import LineCaps  # isort:skip # noqa
-from apysc._display.line_joints import LineJoints  # isort:skip # noqa
-from apysc._display.line_dot_setting import LineDotSetting  # isort:skip # noqa
-from apysc._display.line_dash_setting import LineDashSetting  # isort:skip # noqa
-from apysc._display.line_round_dot_setting import LineRoundDotSetting  # isort:skip # noqa
-from apysc._display.line_dash_dot_setting import LineDashDotSetting  # isort:skip # noqa
-from apysc._display.path import Path  # isort:skip # noqa
-from apysc._geom.point2d import Point2D  # isort:skip # noqa
-from apysc._geom.path_label import PathLabel  # isort:skip # noqa
-from apysc._geom.path_data_base import PathDataBase  # isort:skip # noqa
-from apysc._geom.path_move_to import PathMoveTo  # isort:skip # noqa
-from apysc._geom.path_line_to import PathLineTo  # isort:skip # noqa
-from apysc._geom.path_horizontal import PathHorizontal  # isort:skip # noqa
-from apysc._geom.path_vertical import PathVertical  # isort:skip # noqa
-from apysc._geom.path_close import PathClose  # isort:skip # noqa
-from apysc._geom.path_bezier_2d import PathBezier2D  # isort:skip # noqa
-from apysc._geom.path_bezier_2d_continual import PathBezier2DContinual  # isort:skip # noqa
-from apysc._geom.path_bezier_3d import PathBezier3D  # isort:skip # noqa
-from apysc._geom.path_bezier_3d_continual import PathBezier3DContinual  # isort:skip # noqa
-from apysc._geom.path_data import PathData  # isort:skip # noqa
-from apysc._event.event import Event  # isort:skip # noqa
-from apysc._event.mouse_event import MouseEvent  # isort:skip # noqa
-from apysc._event.wheel_event import WheelEvent  # isort:skip # noqa
-from apysc._event.timer_event import TimerEvent  # isort:skip # noqa
-from apysc._event.animation_event import AnimationEvent  # isort:skip # noqa
-from apysc._event.enter_frame_event import EnterFrameEvent  # isort:skip # noqa
-from apysc._event.mouse_event_type import MouseEventType  # isort:skip # noqa
-from apysc._console._trace import trace  # isort:skip # noqa
-from apysc._console._trace import trace as print  # isort:skip # noqa
-from apysc._console.assertion import assert_equal  # isort:skip # noqa
-from apysc._console.assertion import assert_not_equal  # isort:skip # noqa
-from apysc._console.assertion import assert_true  # isort:skip # noqa
-from apysc._console.assertion import assert_false  # isort:skip # noqa
-from apysc._console.assertion import assert_arrays_equal  # isort:skip # noqa
-from apysc._console.assertion import assert_arrays_not_equal  # isort:skip # noqa
-from apysc._console.assertion import assert_dicts_equal  # isort:skip # noqa
-from apysc._console.assertion import assert_dicts_not_equal  # isort:skip # noqa
-from apysc._console.assertion import assert_defined  # isort:skip # noqa
-from apysc._console.assertion import assert_undefined  # isort:skip # noqa
-from apysc._event.document_mouse_wheel_func import bind_wheel_event_to_document  # isort:skip # noqa
-from apysc._event.document_mouse_wheel_func import unbind_wheel_event_all_from_document  # isort:skip # noqa
-from apysc._event.document_mouse_wheel_func import unbind_wheel_event_from_document  # isort:skip # noqa
-from apysc._html.exporter import save_overall_html  # isort:skip # noqa
-from apysc._expression.expression_data_util import append_js_expression  # isort:skip # noqa
-from apysc._jupyter.jupyter_util import display_on_jupyter  # isort:skip # noqa
-from apysc._jupyter.jupyter_util import display_on_colaboratory  # isort:skip # noqa
-from apysc._time.fps import FPS  # isort:skip # noqa
-from apysc._time.timer import Timer  # isort:skip # noqa
-from apysc._time.datetime_ import DateTime  # isort:skip # noqa
-from apysc._html.debug_mode import set_debug_mode  # isort:skip # noqa
-from apysc._html.debug_mode import unset_debug_mode  # isort:skip # noqa
-from apysc._html.debug_mode import is_debug_mode  # isort:skip # noqa
-from apysc._html.debug_mode import add_debug_info_setting  # isort:skip # noqa
-from apysc._type._return import Return  # isort:skip # noqa
-from apysc._type._delete import delete  # isort:skip # noqa
-from apysc._animation.easing import Easing  # isort:skip # noqa
-from apysc._animation.animation_base import AnimationBase  # isort:skip # noqa
-from apysc._animation.animation_move import AnimationMove  # isort:skip # noqa
-from apysc._animation.animation_x import AnimationX  # isort:skip # noqa
-from apysc._animation.animation_y import AnimationY  # isort:skip # noqa
-from apysc._animation.animation_cx import AnimationCx  # isort:skip # noqa
-from apysc._animation.animation_cy import AnimationCy  # isort:skip # noqa
-from apysc._animation.animation_width import AnimationWidth  # isort:skip # noqa
-from apysc._animation.animation_height import AnimationHeight  # isort:skip # noqa
-from apysc._animation.animation_width_for_ellipse import AnimationWidthForEllipse  # isort:skip # noqa
-from apysc._animation.animation_height_for_ellipse import AnimationHeightForEllipse  # isort:skip # noqa
-from apysc._animation.animation_radius import AnimationRadius  # isort:skip # noqa
-from apysc._animation.animation_fill_alpha import AnimationFillAlpha  # isort:skip # noqa
-from apysc._animation.animation_fill_color import AnimationFillColor  # isort:skip # noqa
-from apysc._animation.animation_line_color import AnimationLineColor  # isort:skip # noqa
-from apysc._animation.animation_line_alpha import AnimationLineAlpha  # isort:skip # noqa
-from apysc._animation.animation_line_thickness import AnimationLineThickness  # isort:skip # noqa
-from apysc._animation.animation_rotation_around_center import AnimationRotationAroundCenter  # isort:skip # noqa
-from apysc._animation.animation_rotation_around_point import AnimationRotationAroundPoint  # isort:skip # noqa
-from apysc._animation.animation_scale_x_from_center import AnimationScaleXFromCenter  # isort:skip # noqa
-from apysc._animation.animation_scale_y_from_center import AnimationScaleYFromCenter  # isort:skip # noqa
-from apysc._animation.animation_scale_x_from_point import AnimationScaleXFromPoint  # isort:skip # noqa
-from apysc._animation.animation_scale_y_from_point import AnimationScaleYFromPoint  # isort:skip # noqa
-from apysc._animation.animation_parallel import AnimationParallel  # isort:skip # noqa
+# flake8: noqa
+
+from apysc._type.int import Int  # isort:skip
+from apysc._type.number import Number  # isort:skip
+from apysc._type.number import Number as Float  # isort:skip
+from apysc._type.boolean import Boolean  # isort:skip
+from apysc._type.boolean import Boolean as Bool  # isort:skip
+from apysc._type.string import String  # isort:skip
+from apysc._type.array import Array  # isort:skip
+from apysc._type.dictionary import Dictionary  # isort:skip
+from apysc._type.any_value import AnyValue  # isort:skip
+from apysc._branch._if import If  # isort:skip
+from apysc._branch._elif import Elif  # isort:skip
+from apysc._branch._else import Else  # isort:skip
+from apysc._loop._for import For  # isort:skip
+from apysc._loop._continue import Continue  # isort:skip
+from apysc._display.display_object import DisplayObject  # isort:skip
+from apysc._display._document import document  # isort:skip
+from apysc._display.sprite import Sprite  # isort:skip
+from apysc._display.graphics import Graphics  # isort:skip
+from apysc._display.stage import Stage  # isort:skip
+from apysc._display.stage import get_stage  # isort:skip
+from apysc._display.rectangle import Rectangle  # isort:skip
+from apysc._display.circle import Circle  # isort:skip
+from apysc._display.ellipse import Ellipse  # isort:skip
+from apysc._display.line import Line  # isort:skip
+from apysc._display.polyline import Polyline  # isort:skip
+from apysc._display.polygon import Polygon  # isort:skip
+from apysc._display.line_caps import LineCaps  # isort:skip
+from apysc._display.line_joints import LineJoints  # isort:skip
+from apysc._display.line_dot_setting import LineDotSetting  # isort:skip
+from apysc._display.line_dash_setting import LineDashSetting  # isort:skip
+from apysc._display.line_round_dot_setting import LineRoundDotSetting  # isort:skip
+from apysc._display.line_dash_dot_setting import LineDashDotSetting  # isort:skip
+from apysc._display.path import Path  # isort:skip
+from apysc._geom.point2d import Point2D  # isort:skip
+from apysc._geom.path_label import PathLabel  # isort:skip
+from apysc._geom.path_data_base import PathDataBase  # isort:skip
+from apysc._geom.path_move_to import PathMoveTo  # isort:skip
+from apysc._geom.path_line_to import PathLineTo  # isort:skip
+from apysc._geom.path_horizontal import PathHorizontal  # isort:skip
+from apysc._geom.path_vertical import PathVertical  # isort:skip
+from apysc._geom.path_close import PathClose  # isort:skip
+from apysc._geom.path_bezier_2d import PathBezier2D  # isort:skip
+from apysc._geom.path_bezier_2d_continual import PathBezier2DContinual  # isort:skip
+from apysc._geom.path_bezier_3d import PathBezier3D  # isort:skip
+from apysc._geom.path_bezier_3d_continual import PathBezier3DContinual  # isort:skip
+from apysc._geom.path_data import PathData  # isort:skip
+from apysc._event.event import Event  # isort:skip
+from apysc._event.mouse_event import MouseEvent  # isort:skip
+from apysc._event.wheel_event import WheelEvent  # isort:skip
+from apysc._event.timer_event import TimerEvent  # isort:skip
+from apysc._event.animation_event import AnimationEvent  # isort:skip
+from apysc._event.enter_frame_event import EnterFrameEvent  # isort:skip
+from apysc._event.mouse_event_type import MouseEventType  # isort:skip
+from apysc._console._trace import trace  # isort:skip
+from apysc._console._trace import trace as print  # isort:skip
+from apysc._console.assertion import assert_equal  # isort:skip
+from apysc._console.assertion import assert_not_equal  # isort:skip
+from apysc._console.assertion import assert_true  # isort:skip
+from apysc._console.assertion import assert_false  # isort:skip
+from apysc._console.assertion import assert_arrays_equal  # isort:skip
+from apysc._console.assertion import assert_arrays_not_equal  # isort:skip
+from apysc._console.assertion import assert_dicts_equal  # isort:skip
+from apysc._console.assertion import assert_dicts_not_equal  # isort:skip
+from apysc._console.assertion import assert_defined  # isort:skip
+from apysc._console.assertion import assert_undefined  # isort:skip
+from apysc._event.document_mouse_wheel_func import bind_wheel_event_to_document  # isort:skip
+from apysc._event.document_mouse_wheel_func import unbind_wheel_event_all_from_document  # isort:skip
+from apysc._event.document_mouse_wheel_func import unbind_wheel_event_from_document  # isort:skip
+from apysc._html.exporter import save_overall_html  # isort:skip
+from apysc._expression.expression_data_util import append_js_expression  # isort:skip
+from apysc._jupyter.jupyter_util import display_on_jupyter  # isort:skip
+from apysc._jupyter.jupyter_util import display_on_colaboratory  # isort:skip
+from apysc._time.fps import FPS  # isort:skip
+from apysc._time.timer import Timer  # isort:skip
+from apysc._time.datetime_ import DateTime  # isort:skip
+from apysc._html.debug_mode import set_debug_mode  # isort:skip
+from apysc._html.debug_mode import unset_debug_mode  # isort:skip
+from apysc._html.debug_mode import is_debug_mode  # isort:skip
+from apysc._html.debug_mode import add_debug_info_setting  # isort:skip
+from apysc._type._return import Return  # isort:skip
+from apysc._type._delete import delete  # isort:skip
+from apysc._animation.easing import Easing  # isort:skip
+from apysc._animation.animation_base import AnimationBase  # isort:skip
+from apysc._animation.animation_move import AnimationMove  # isort:skip
+from apysc._animation.animation_x import AnimationX  # isort:skip
+from apysc._animation.animation_y import AnimationY  # isort:skip
+from apysc._animation.animation_cx import AnimationCx  # isort:skip
+from apysc._animation.animation_cy import AnimationCy  # isort:skip
+from apysc._animation.animation_width import AnimationWidth  # isort:skip
+from apysc._animation.animation_height import AnimationHeight  # isort:skip
+from apysc._animation.animation_width_for_ellipse import AnimationWidthForEllipse  # isort:skip
+from apysc._animation.animation_height_for_ellipse import AnimationHeightForEllipse  # isort:skip
+from apysc._animation.animation_radius import AnimationRadius  # isort:skip
+from apysc._animation.animation_fill_alpha import AnimationFillAlpha  # isort:skip
+from apysc._animation.animation_fill_color import AnimationFillColor  # isort:skip
+from apysc._animation.animation_line_color import AnimationLineColor  # isort:skip
+from apysc._animation.animation_line_alpha import AnimationLineAlpha  # isort:skip
+from apysc._animation.animation_line_thickness import AnimationLineThickness  # isort:skip
+from apysc._animation.animation_rotation_around_center import AnimationRotationAroundCenter  # isort:skip
+from apysc._animation.animation_rotation_around_point import AnimationRotationAroundPoint  # isort:skip
+from apysc._animation.animation_scale_x_from_center import AnimationScaleXFromCenter  # isort:skip
+from apysc._animation.animation_scale_y_from_center import AnimationScaleYFromCenter  # isort:skip
+from apysc._animation.animation_scale_x_from_point import AnimationScaleXFromPoint  # isort:skip
+from apysc._animation.animation_scale_y_from_point import AnimationScaleYFromPoint  # isort:skip
+from apysc._animation.animation_parallel import AnimationParallel  # isort:skip
 
 __version__: str = "2.5.55"

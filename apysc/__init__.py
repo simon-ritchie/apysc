390eab593a7f092b74b04f20d87d70b080802f10
simon-ritchie <antisocial.sid2@gmail.com>
2022-12-07 16:38:56 +0900

    #796 Increment the patch version to 2.5.57.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -108,4 +108,4 @@ from apysc._animation.animation_scale_x_from_point import AnimationScaleXFromPoi
 from apysc._animation.animation_scale_y_from_point import AnimationScaleYFromPoint
 from apysc._animation.animation_parallel import AnimationParallel
 
-__version__: str = "2.5.56"
+__version__: str = "2.5.57"

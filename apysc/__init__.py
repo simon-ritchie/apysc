86f1db19305e0afc773d22a060b59cc377791c3a
simon-ritchie <simon-ritchie+simonritchie@users.noreply.github.com>
2023-01-03 18:45:27 +0900

    #570 Increment the patch version to 2.5.78.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -115,4 +115,4 @@ from apysc._animation.animation_scale_y_from_point import AnimationScaleYFromPoi
 from apysc._animation.animation_parallel import AnimationParallel
 from apysc._math.math import Math
 
-__version__: str = "2.5.77"
+__version__: str = "2.5.78"

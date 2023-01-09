66f34e2ce682647f4332403a531ee5ffe4ac1140
simon-ritchie <antisocial.sid2@gmail.com>
2023-01-09 17:22:22 +0900

    #577 Add the `x1` setter interface.

--- apysc/_display/polygon_x1_mixin.py
+++ apysc/_display/polygon_x1_mixin.py
@@ -6,9 +6,10 @@ from typing_extensions import final
 from apysc._html.debug_mode import add_debug_info_setting
 from apysc._type.int import Int
 from apysc._display.points_2d_mixin import Points2DMixIn
+from apysc._display.polygon_apply_current_points_mixin import PolygonApplyCurrentPointsMixIn
 
 
-class PolygonX1MixIn(Points2DMixIn):
+class PolygonX1MixIn(Points2DMixIn, PolygonApplyCurrentPointsMixIn):
 
     _x1: Int
 
@@ -25,3 +26,18 @@ class PolygonX1MixIn(Points2DMixIn):
         """
         x1: Int = self._points[0].x
         return x1
+
+    @x1.setter
+    @add_debug_info_setting(module_name=__name__)
+    def x1(self, value: Int) -> None:
+        """
+        Set a first x-coordinate.
+
+        Parameters
+        ----------
+        value : Int
+            X-coordinate to set.
+        """
+        self._x1._value = value._value
+        self._points[0].x.value = value._copy()
+        self._apply_current_points()
--- tests/_display/test_polygon_x1_mixin.py
+++ tests/_display/test_polygon_x1_mixin.py
@@ -3,11 +3,13 @@ from random import randint
 from retrying import retry
 
 import apysc as ap
+from apysc._expression import expression_data_util
 
 
 class TestPolygonX1MixIn:
     @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
     def test_x1(self) -> None:
+        expression_data_util.empty_expression()
         ap.Stage()
         triangle: ap.Triangle = ap.Triangle(
             x1=100,
@@ -20,3 +22,9 @@ class TestPolygonX1MixIn:
         x1: ap.Int = triangle.x1
         assert isinstance(x1, ap.Int)
         assert x1 == ap.Int(100)
+
+        triangle.x1 = ap.Int(110)
+        assert triangle.x1 == ap.Int(110)
+        assert triangle._points[0].x == ap.Int(110)
+        expression: str = expression_data_util.get_current_expression()
+        assert ".plot" in expression

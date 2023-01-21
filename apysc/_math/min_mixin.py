730d7d4c719eb5f4ad322fe12170ee35709f80bc
simon-ritchie <antisocial.sid2@gmail.com>
2023-01-21 20:40:39 +0900

    #840 Add the class method to get a minimum number from a specified array's values.

--- apysc/_math/min_mixin.py
+++ apysc/_math/min_mixin.py
@@ -10,6 +10,7 @@ from apysc._type.int import Int
 from apysc._type.number import Number
 from apysc._type.array import Array
 from apysc._type.variable_name_suffix_mixin import VariableNameSuffixMixIn
+from apysc._validation import arg_validation_decos
 
 
 class MinMixIn:
@@ -18,8 +19,8 @@ class MinMixIn:
     @add_debug_info_setting(module_name=__name__)
     def min(
         cls,
-        values: Array[Union[Int, Number, int, float]],
-    ) -> Union[Int, Number]:
+        values: Array,
+    ) -> Number:
         """
         Get a minimum number from a specified array's values.
 
@@ -30,19 +31,20 @@ class MinMixIn:
 
         Returns
         -------
-        min_value : Union[Int, Number]
+        min_value : Number
             Minimum number in an array.
         """
-        all_values_are_int: bool = _all_values_are_int(values=values)
-        if all_values_are_int:
-            min_value: Union[Int, Number] = Int(0)
-            min_int_value: int = _get_min_int_value(values=values)
-            min_value._value = min_int_value
-        else:
-            min_value = Number(0)
-            min_float_value: float = _get_min_float_value(values=values)
-            min_value._value = min_float_value
-        pass
+        import apysc as ap
+
+        min_value = Number(0)
+        min_float_value: float = _get_min_float_value(values=values)
+        min_value._value = min_float_value
+        expression: str = (
+            f"{min_value.variable_name} = {values.variable_name}.reduce("
+            "function (a, b) {return Math.max(a, b)});"
+        )
+        ap.append_js_expression(expression=expression)
+        return min_value
 
 
 def _get_min_float_value(*, values: Array[Union[Int, Number, int, float]]) -> float:
@@ -67,47 +69,3 @@ def _get_min_float_value(*, values: Array[Union[Int, Number, int, float]]) -> fl
         values_.append(float(value))
     min_value: float = min(values_)
     return min_value
-
-
-def _get_min_int_value(*, values: Array[Union[Int, Number, int, float]]) -> int:
-    """
-    Get a minimum integer value from a specified array.
-
-    Parameters
-    ----------
-    values : Array[Union[Int, Number, int, float]]
-        An array of numbers.
-
-    Returns
-    -------
-    min_value : int
-        A minimum integer value.
-    """
-    values_: List[int] = []
-    for value in values._value:
-        if isinstance(value, (Int, Number)):
-            values_.append(int(value._value))
-            continue
-        values_.append(int(value))
-    min_value: int = min(values_)
-    return min_value
-
-
-def _all_values_are_int(*, values: Array[Union[Int, Number, int, float]]) -> bool:
-    """
-    Get a boolean value indicating whether all values in an array are int.
-
-    Parameters
-    ----------
-    values : Array[Union[Int, Number, int, float]]
-        An array of numbers.
-
-    Returns
-    -------
-    result : bool
-        If values are all int, this interface returns True.
-    """
-    for value in values._value:
-        if not isinstance(value, (int, Int)):
-            return False
-    return True
--- tests/_math/test_min_mixin.py
+++ tests/_math/test_min_mixin.py
@@ -4,38 +4,9 @@ from typing import Union
 from retrying import retry
 
 import apysc as ap
+from apysc._expression import expression_data_util
 from apysc._math import min_mixin
-
-
-@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
-def test__all_values_are_int() -> None:
-    result: bool = min_mixin._all_values_are_int(
-        values=ap.Array([10, ap.Int(20), 30])
-    )
-    assert result
-
-    result = min_mixin._all_values_are_int(
-        values=ap.Array([10, ap.Int(20), 10.5, 30])
-    )
-    assert not result
-
-    result = min_mixin._all_values_are_int(
-        values=ap.Array([10, ap.Int(20), 10.5, 30])
-    )
-    assert not result
-
-    result = min_mixin._all_values_are_int(
-        values=ap.Array([10, ap.Number(20.5), 10.5, 30])
-    )
-    assert not result
-
-
-@retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
-def test__get_min_int_value() -> None:
-    min_value: int = min_mixin._get_min_int_value(
-        values=ap.Array([10, 10.5, ap.Int(9), ap.Number(8.5)])
-    )
-    assert min_value == 8
+from apysc._math.min_mixin import MinMixIn
 
 
 @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
@@ -44,3 +15,20 @@ def test__get_min_float_value() -> None:
         values=ap.Array([10, 10.5, ap.Int(9), ap.Number(8.5)])
     )
     assert min_value == 8.5
+
+
+class TestMinMixIn:
+    @retry(stop_max_attempt_number=15, wait_fixed=randint(10, 3000))
+    def test_min(self) -> None:
+        expression_data_util.empty_expression()
+        values: ap.Array = ap.Array([10, 9.5, ap.Int(9), ap.Number(9.5)])
+        min_value: ap.Number = MinMixIn.min(
+            values=values,
+        )
+        assert min_value == ap.Number(9)
+        expression: str = expression_data_util.get_current_expression()
+        expected: str = (
+            f"{min_value.variable_name} = {values.variable_name}.reduce("
+            "function (a, b) {return Math.max(a, b)});"
+        )
+        assert expected in expression

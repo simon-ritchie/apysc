9a5c399e84599642aa8d902e74025e84423445ff
simon-ritchie <antisocial.sid2@gmail.com>
2023-01-25 20:17:28 +0900

    #848 Add the doctest to the `Math.trunc` interface's docstring.

--- apysc/_math/trunc_mixin.py
+++ apysc/_math/trunc_mixin.py
@@ -37,6 +37,29 @@ class TruncMixIn:
         ----------
         - Math max interface
             - https://simon-ritchie.github.io/apysc/en/math_trunc.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> result_int: ap.Int = ap.Math.trunc(value=ap.Int(10))
+        >>> result_int
+        Int(10)
+
+        >>> result_int = ap.Math.trunc(value=ap.Number(8.5))
+        >>> result_int
+        Int(8)
+
+        >>> result_int = ap.Math.trunc(value=ap.String("7.6"))
+        >>> result_int
+        Int(7)
+
+        >>> result_int = ap.Math.trunc(value=ap.Boolean(True))
+        >>> result_int
+        Int(1)
+
+        >>> result_int = ap.Math.trunc(value=ap.Boolean(False))
+        >>> result_int
+        Int(0)
         """
         import apysc as ap
 

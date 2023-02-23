7292161de52f3ffc72b5f70944358a9c89ecb9ea
simon-ritchie <antisocial.sid2@gmail.com>
2023-02-23 15:19:08 +0900

    #124 Add the `SVGTextAlign` import to the root package path.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -38,6 +38,7 @@ from apysc._display.line_round_dot_setting import LineRoundDotSetting
 from apysc._display.line_dash_dot_setting import LineDashDotSetting
 from apysc._display.path import Path
 from apysc._display.svg_text import SVGText
+from apysc._display.svg_text_align_mixin import SVGTextAlign
 from apysc._geom.point2d import Point2D
 from apysc._geom.path_label import PathLabel
 from apysc._geom.path_data_base import PathDataBase

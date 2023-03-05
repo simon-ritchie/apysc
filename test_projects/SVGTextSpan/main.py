825f2d3bd9c4554932a7e6b8c6ce4ebbecd9afc8
simon-ritchie <antisocial.sid2@gmail.com>
2023-03-05 20:29:35 +0900

    #860 Add the test project for the `SVGTextSpan` class.

---
+++ test_projects/SVGTextSpan/main.py
@@ -0,0 +1,55 @@
+"""The test project for the `SVGTextSpan` class.
+
+Command examples:
+$ python test_projects/SVGTextSpan/main.py
+"""
+
+import sys
+
+sys.path.append("./")
+
+import os
+from types import ModuleType
+
+import apysc as ap
+from apysc._file import file_util
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module), "test_output/"
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    ap.Stage(
+        background_color="#333",
+        stage_width=1000,
+        stage_height=1000,
+        stage_elem_id="stage",
+    )
+
+    svg_text: ap.SVGText = ap.SVGText.create_with_svg_text_spans(
+        text_spans=[
+            ap.SVGTextSpan(text="SVGTextSpan test 1 "),
+            ap.SVGTextSpan(
+                text="SVGTextSpan test 2 ",
+                font_size=20,
+                fill_color="#aaa",
+                font_family=["Arial"],
+            ),
+            ap.SVGTextSpan(text="SVGTextSpan test 3"),
+        ],
+        x=50,
+        y=50,
+        font_family=["Impact"],
+    )
+
+    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH, minify=False)
+
+
+if __name__ == "__main__":
+    main()

6a9dd0d75258242d7d16fc1b26b183bf45fc5e51
simon-ritchie <antisocial.sid2@gmail.com>
2023-03-05 20:34:14 +0900

    #860 Split the fill-color expression appending implementation to the mix-in class.

---
+++ apysc/_display/append_fill_color_expression_mixin.py
@@ -0,0 +1,59 @@
+"""The mix-in class implementation for the
+`_append_fill_color_attr_expression` method.
+"""
+
+from typing_extensions import final
+
+from apysc._html.debug_mode import add_debug_info_setting
+
+
+class AppendFillColorAttrExpressionMixIn:
+    @final
+    @add_debug_info_setting(module_name=__name__)
+    def _append_fill_color_attr_expression(
+        self,
+        *,
+        expression: str,
+        indent_num: int,
+        skip_appending: bool = False,
+    ) -> str:
+        """
+        Append a fill-color attribute expression to a specified
+        expression string.
+
+        Parameters
+        ----------
+        expression : str
+            A target expression string.
+        indent_num : int
+            An indentation number.
+        skip_appending : bool, optional
+            A boolean, whether to skip appending.
+
+        Returns
+        -------
+        expression : str
+            After appending expression string.
+
+        Raises
+        ------
+        TypeError
+            If this is not a `FillColorMixIn` instance.
+        """
+        from apysc._display.fill_color_mixin import FillColorMixIn
+        from apysc._display import graphics_expression
+
+        if skip_appending:
+            return expression
+        if not isinstance(self, FillColorMixIn):
+            raise TypeError(
+                f"This instance is not a `{FillColorMixIn.__name__}` instance: "
+                f"{type(self)}"
+            )
+        self._initialize_fill_color_if_not_initialized()
+        expression = graphics_expression.append_fill_expression(
+            fill_color=self._fill_color,
+            expression=expression,
+            indent_num=indent_num,
+        )
+        return expression
---
+++ tests/_display/test_append_fill_color_expression_mixin.py
@@ -0,0 +1,44 @@
+import apysc as ap
+from tests._display.test_graphics_expression import (
+    assert_fill_attr_expression_exists
+)
+from apysc._testing.testing_helper import apply_test_settings, assert_raises
+from apysc._display.append_fill_color_expression_mixin import (
+    AppendFillColorAttrExpressionMixIn
+)
+from apysc._display.fill_color_mixin import FillColorMixIn
+
+
+class _TestMixIn(AppendFillColorAttrExpressionMixIn, FillColorMixIn):
+    pass
+
+
+class TestAppendFillColorAttrExpressionMixIn:
+    @apply_test_settings()
+    def test__append_fill_color_attr_expression(self) -> None:
+        mixin_1: AppendFillColorAttrExpressionMixIn
+        mixin_1 = AppendFillColorAttrExpressionMixIn()
+        assert_raises(
+            expected_error_class=TypeError,
+            callable_=mixin_1._append_fill_color_attr_expression,
+            match="This instance is not a ",
+            expression=".attr({\n",
+            indent_num=2,
+            skip_appending=False,
+        )
+
+        mixin_2: _TestMixIn = _TestMixIn()
+        mixin_2.fill_color = ap.String("#0af")
+        expression = mixin_2._append_fill_color_attr_expression(
+            expression=".attr({\n",
+            indent_num=2,
+            skip_appending=True,
+        )
+        assert expression == ".attr({\n"
+
+        expression = mixin_2._append_fill_color_attr_expression(
+            expression=".attr({\n",
+            indent_num=2,
+            skip_appending=False,
+        )
+        assert_fill_attr_expression_exists(expression=expression)

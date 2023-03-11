bee693edb9ca6a408f7d37dab31aad0a85a23f79
simon-ritchie <antisocial.sid2@gmail.com>
2023-03-11 16:55:51 +0900

    #860 Add the test pattern for the `fill-color` setting.

--- tests/_display/test_svg_text_span.py
+++ tests/_display/test_svg_text_span.py
@@ -4,13 +4,17 @@ from apysc._display.svg_text_singleton_for_text_span import SVGTextSingletonForT
 from apysc._expression import expression_data_util
 from apysc._testing.testing_helper import apply_test_settings
 from apysc._display import svg_text_span
+from tests._display.test_graphics_expression import assert_fill_attr_expression_exists
 
 
 class TestSVGTextSpan:
     @apply_test_settings()
     def test__append_constructor_expression(self) -> None:
         ap.Stage()
-        svg_text_span: ap.SVGTextSpan = ap.SVGTextSpan(text="test_text_span")
+        svg_text_span: ap.SVGTextSpan = ap.SVGTextSpan(
+            text="test_text_span",
+            fill_color="#0af",
+        )
         svg_text: ap.SVGText = SVGTextSingletonForTextSpan.get_instance()
         expression: str = expression_data_util.get_current_expression()
         assert (
@@ -19,6 +23,7 @@ class TestSVGTextSpan:
         )
         assert "\n  .attr({" in expression
         assert "\n});" in expression
+        assert_fill_attr_expression_exists(expression=expression)
 
     @apply_test_settings()
     def test___repr__(self) -> None:

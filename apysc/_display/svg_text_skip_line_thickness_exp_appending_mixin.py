ef11a4468225f9bf68b60045e1442069aa78b427
simon-ritchie <antisocial.sid2@gmail.com>
2023-03-14 20:39:24 +0900

    #860 Add the mix-in class implementation for the `SVGText`'s `_skip_line_thickness_expression_appending` attribute.

---
+++ apysc/_display/svg_text_skip_line_thickness_exp_appending_mixin.py
@@ -0,0 +1,36 @@
+"""The mix-in class implementation for the `SVGText`'s
+`_skip_line_thickness_expression_appending` attribute.
+"""
+
+from typing import Union, Optional
+
+from typing_extensions import final
+
+from apysc._html.debug_mode import add_debug_info_setting
+from apysc._type.int import Int
+
+
+class SVGTextSkipLineThicknessExpAppendingMixIn:
+
+    _skip_line_thickness_expression_appending: bool = False
+
+    @final
+    @add_debug_info_setting(module_name=__name__)
+    def _set_line_thickness_expression_skipping_attr(
+        self,
+        *,
+        line_thickness: Optional[Union[int, Int]],
+    ) -> None:
+        """
+        Set a boolean attribute, whether to skip a line-thickness expression's
+        appending or not.
+
+        Parameters
+        ----------
+        line_thickness : Optional[Union[int, Int]]
+            A line-thickness setting.
+        """
+        if line_thickness is None:
+            self._skip_line_thickness_expression_appending = True
+            return
+        self._skip_line_thickness_expression_appending = False
---
+++ tests/_display/test_svg_text_skip_line_thickness_exp_appending_mixin.py
@@ -0,0 +1,15 @@
+from apysc._testing.testing_helper import apply_test_settings
+from apysc._display.svg_text_skip_line_thickness_exp_appending_mixin import (
+    SVGTextSkipLineThicknessExpAppendingMixIn
+)
+
+
+class TestSVGTextSkipLineThicknessExpAppendingMixIn:
+    @apply_test_settings()
+    def test__set_line_thickness_expression_skipping_attr(self) -> None:
+        mixin: SVGTextSkipLineThicknessExpAppendingMixIn
+        mixin = SVGTextSkipLineThicknessExpAppendingMixIn()
+        mixin._set_line_thickness_expression_skipping_attr(line_thickness=3)
+        assert not mixin._skip_line_thickness_expression_appending
+        mixin._set_line_thickness_expression_skipping_attr(line_thickness=None)
+        assert mixin._skip_line_thickness_expression_appending

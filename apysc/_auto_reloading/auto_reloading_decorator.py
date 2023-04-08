e25757d075b02664036c2391782c8e5b08f84d20
simon-ritchie <antisocial.sid2@gmail.com>
2023-04-08 19:30:40 +0900

    #878 Add the function to set an auto-reloading setting as a decorator.

--- apysc/_auto_reloading/auto_reloading_decorator.py
+++ apysc/_auto_reloading/auto_reloading_decorator.py
@@ -8,7 +8,7 @@ from typing import Any, List, Optional
 from typing import Callable
 from typing import Dict
 from typing import TypeVar
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 import os
 
@@ -16,7 +16,11 @@ import os
 _Callable = TypeVar("_Callable", bound=Callable)
 
 
-def set_auto_reloading(*, checking_dir_paths: List[str]) -> _Callable:
+def set_auto_reloading(
+    *,
+    checking_dir_paths: List[str],
+    max_checking_num: Optional[int] = None,
+) -> _Callable:
     """
     Set an auto-reloading setting as a decorator.
 
@@ -24,6 +28,9 @@ def set_auto_reloading(*, checking_dir_paths: List[str]) -> _Callable:
     ----------
     checking_dir_paths : List[str]
         Directory paths to check for files' changes.
+    max_checking_num : Optional[int], optional
+        Maximum checking attempts number.
+        Mainly this interface uses this setting for the testing.
 
     Returns
     -------
@@ -35,25 +42,41 @@ def set_auto_reloading(*, checking_dir_paths: List[str]) -> _Callable:
         @functools.wraps(callable_)
         def inner_wrapped(*args: Any, **kwargs: Any) -> Any:
             result: Any = callable_(*args, **kwargs)
-            last_checked_time: datetime = datetime.now()
+            last_executed_time: datetime = datetime.now()
+            time.sleep(1.0)
 
+            checking_count: int = 0
             while True:
+                if (
+                    max_checking_num is not None
+                    and checking_count >= max_checking_num
+                ):
+                    break
                 if not _are_files_updated(
-                    last_checked_time=last_checked_time,
+                    last_executed_time=last_executed_time,
                     checking_dir_paths=checking_dir_paths,
                 ):
-                    time.sleep(0.5)
+                    time.sleep(1.0)
+                    checking_count += 1
                     continue
                 try:
-                    result: Any = callable_(*args, **kwargs)
+                    result = callable_(*args, **kwargs)
+                    last_executed_time = datetime.now()
+                    time.sleep(1.0)
                 except:
-                    continue
-    pass
+                    pass
+                finally:
+                    checking_count += 1
+            return result
+
+        return inner_wrapped  # type: ignore
+
+    return wrapped  # type: ignore
 
 
 def _are_files_updated(
     *,
-    last_checked_time: datetime,
+    last_executed_time: datetime,
     checking_dir_paths: List[str],
 ) -> bool:
     """
@@ -61,8 +84,8 @@ def _are_files_updated(
 
     Parameters
     ----------
-    last_checked_time : datetime
-        A last checked time.
+    last_executed_time : datetime
+        A last execution time.
     checking_dir_paths : List[str]
         Directory paths to check for files' changes.
 
@@ -71,13 +94,14 @@ def _are_files_updated(
     result : bool
         If any updated files exist, this interface returns True.
     """
+    timedelta_: timedelta = timedelta(seconds=1)
     for dire_path in checking_dir_paths:
         file_or_dir_names: List[str] = os.listdir(dire_path)
         for file_or_dir_name in file_or_dir_names:
             file_or_dir_path: str = os.path.join(dire_path, file_or_dir_name)
             if os.path.isdir(file_or_dir_path):
                 result: bool = _are_files_updated(
-                    last_checked_time=last_checked_time,
+                    last_executed_time=last_executed_time,
                     checking_dir_paths=[file_or_dir_path],
                 )
                 if result:
@@ -87,6 +111,7 @@ def _are_files_updated(
             last_modified: datetime = datetime.fromtimestamp(
                 os.stat(file_or_dir_path).st_mtime,
             )
-            if last_modified > last_checked_time:
+            print(last_modified, last_executed_time)
+            if last_modified >= last_executed_time - timedelta_:
                 return True
     return False
--- tests/_auto_reloading/test_auto_reloading_decorator.py
+++ tests/_auto_reloading/test_auto_reloading_decorator.py
@@ -2,6 +2,7 @@ import os
 import apysc as ap
 import shutil
 from datetime import datetime, timedelta
+import time
 
 from apysc._testing import testing_helper
 from apysc._testing.testing_helper import apply_test_settings, assert_raises
@@ -12,7 +13,7 @@ from apysc._file import file_util
 @apply_test_settings()
 def test__are_files_updated() -> None:
     test_root_dir_path: str = os.path.join(
-        "tmp/test_auto_reloading_decorator/"
+        "tmp/test_auto_reloading_decorator_1/"
     )
     shutil.rmtree(test_root_dir_path, ignore_errors=True)
     test_subdir_path: str = os.path.join(test_root_dir_path, "subdir/")
@@ -29,25 +30,60 @@ def test__are_files_updated() -> None:
     )
 
     now: datetime = datetime.now()
-    last_checked_time: datetime = now + timedelta(minutes=3)
+    last_executed_time: datetime = now + timedelta(minutes=3)
     result: bool = auto_reloading_decorator._are_files_updated(
-        last_checked_time=last_checked_time,
+        last_executed_time=last_executed_time,
         checking_dir_paths=[test_root_dir_path],
     )
     assert not result
 
-    last_checked_time: datetime = now + timedelta(minutes=-3)
+    last_executed_time: datetime = now + timedelta(minutes=-3)
     result = auto_reloading_decorator._are_files_updated(
-        last_checked_time=last_checked_time,
+        last_executed_time=last_executed_time,
         checking_dir_paths=[test_root_dir_path],
     )
     assert result
 
     file_util.delete_file_if_exists(file_path=file_path_1)
     result = auto_reloading_decorator._are_files_updated(
-        last_checked_time=last_checked_time,
+        last_executed_time=last_executed_time,
         checking_dir_paths=[test_root_dir_path],
     )
     assert result
+
+    shutil.rmtree(test_root_dir_path, ignore_errors=True)
+
+
+_count: int = 0
+
+
+@apply_test_settings()
+def test_set_auto_reloading() -> None:
+    global _count
+    _count = 0
+    test_root_dir_path: str = os.path.join(
+        "tmp/test_auto_reloading_decorator_2/"
+    )
+    shutil.rmtree(test_root_dir_path, ignore_errors=True)
+    test_file_path: str = os.path.join(test_root_dir_path, "test_file.py")
+    os.makedirs(os.path.dirname(test_root_dir_path), exist_ok=True)
+
+    @auto_reloading_decorator.set_auto_reloading(
+        checking_dir_paths=[test_root_dir_path],
+        max_checking_num=5,
+    )
+    def _test_func(*, a: int) -> int:
+        global _count
+        _count += 1
+
+        if _count <= 3:
+            file_util.save_plain_txt(txt="print(10)", file_path=test_file_path)
+        if _count == 2:
+            raise Exception()
+        return 200
+
+    result: int = _test_func(a=10)
+    assert result == 200
+    assert _count == 4
 
     shutil.rmtree(test_root_dir_path, ignore_errors=True)

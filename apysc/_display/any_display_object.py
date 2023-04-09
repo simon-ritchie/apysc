3f65155fde9e17d94024327815fbe496086b0bf5
simon-ritchie <antisocial.sid2@gmail.com>
2023-04-09 15:48:37 +0900

    #884 Add the `SetOverflowVisibleSettingMixIn` inheritance to the `AnyDisplayObject`.

--- apysc/_display/any_display_object.py
+++ apysc/_display/any_display_object.py
@@ -6,9 +6,18 @@ from apysc._display.display_object import DisplayObject
 from apysc._display.x_mixin import XMixIn
 from apysc._display.y_mixin import YMixIn
 from apysc._display.css_mixin import CssMixIn
+from apysc._display.set_overflow_visible_setting_mixin import (
+    SetOverflowVisibleSettingMixIn
+)
 
 
-class AnyDisplayObject(CssMixIn, DisplayObject, XMixIn, YMixIn):
+class AnyDisplayObject(
+    SetOverflowVisibleSettingMixIn,
+    CssMixIn,
+    DisplayObject,
+    XMixIn,
+    YMixIn,
+):
     """This class is for the `DisplayObject` subclass
     implementation to avoid an abstract method error.
     """

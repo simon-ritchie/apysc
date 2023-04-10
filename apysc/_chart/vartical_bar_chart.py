d580efd319921f2ebcd31b79058a59e8ea717503
simon-ritchie <antisocial.sid2@gmail.com>
2023-04-10 20:03:16 +0900

    #882 Add the class implementation partially for the vertical bar chart.

--- apysc/_chart/vartical_bar_chart.py
+++ apysc/_chart/vartical_bar_chart.py
@@ -1,2 +1,25 @@
-"""Implementation for the vertical bar chart.
+"""Class implementation for the vertical bar chart.
 """
+
+from typing import Union
+
+from apysc._display.sprite import Sprite
+from apysc._type.array import Array
+from apysc._type.dictionary import Dictionary
+from apysc._type.int import Int
+from apysc._type.number import Number
+from apysc._type.string import String
+from apysc._chart.x_axis_settings import XAxisSettings
+
+
+class VarticalBarChart:
+
+    _container: Sprite
+
+    def __init__(
+        self,
+        *,
+        data: Array[Dictionary[String, Union[Int, Number, String]]],
+        x_axis_settings: XAxisSettings,
+    ) -> None:
+        pass

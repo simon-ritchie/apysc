5207d6a82b46f087c4b12af8305d674985e948f0
simon-ritchie <antisocial.sid2@gmail.com>
2023-06-13 23:24:03 +0900

    #891 Add the test for the `_get_last_scope` method.

--- tests/_loop/test_for_dict_keys.py
+++ tests/_loop/test_for_dict_keys.py
@@ -2,6 +2,7 @@ import apysc as ap
 from apysc._expression import expression_data_util
 from apysc._testing.testing_helper import apply_test_settings
 from apysc._expression.indent_num import Indent
+from apysc._expression.last_scope import LastScope
 
 
 class TestForDictKeys:
@@ -20,3 +21,13 @@ class TestForDictKeys:
         assert for_dict_keys._dict == dict_
         assert for_dict_keys._variable_name_suffix == "test_suffix"
         assert isinstance(for_dict_keys._indent, Indent)
+
+    @apply_test_settings()
+    def test__get_last_scope(self) -> None:
+        dict_: ap.Dictionary[int, str] = ap.Dictionary({10: "test"})
+        for_dict_keys: ap.ForDictKeys = ap.ForDictKeys(
+            dict_=dict_,
+            dict_key_type=int,
+        )
+        last_scope: LastScope = for_dict_keys._get_last_scope()
+        assert last_scope == LastScope.FOR_DICT_KEYS

332d1bb6efd4149503d23d3fe38ebf50cf42c345
simon-ritchie <antisocial.sid2@gmail.com>
2023-07-19 23:46:13 +0900

    #942 Add the files to handle commit omissions.

---
+++ tests/_loop/test_initialize_with_base_value_interface.py
@@ -0,0 +1,16 @@
+from apysc._loop.initialize_with_base_value_interface import (
+    InitializeWithBaseValueInterface,
+)
+from apysc._testing.testing_helper import apply_test_settings
+from apysc._testing.testing_helper import assert_raises
+
+
+class TestInitializeWithBaseValueInterface:
+    @apply_test_settings()
+    def test__initialize_for_loop_key_or_value(self) -> None:
+        assert_raises(
+            expected_error_class=NotImplementedError,
+            callable_=(
+                InitializeWithBaseValueInterface._initialize_for_loop_key_or_value
+            ),
+        )

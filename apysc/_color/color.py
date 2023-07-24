e5f4e7afd3d1bfef0f9ee0a66ab90f1c2bbba868
simon-ritchie <antisocial.sid2@gmail.com>
2023-07-24 22:44:36 +0900

    #947 Adjust the inheritance of `Color` class.

--- apysc/_color/color.py
+++ apysc/_color/color.py
@@ -1,25 +1,18 @@
 """The color class implementation.
 """
 
-from typing import Union, TypeVar
+from typing import TypeVar
 from typing_extensions import final
 
-from apysc._type.copy_mixin import CopyMixIn
-from apysc._type.revert_mixin import RevertMixIn
 from apysc._event.custom_event_mixin import CustomEventMixIn
 from apysc._type.string import String
 from apysc._validation import arg_validation_decos
-from apysc._html.debug_mode import add_debug_info_setting
-from apysc._type.initial_substitution_exp_mixin import InitialSubstitutionExpMixIn
 
 _StrOrString = TypeVar("_StrOrString", str, String)
 
 
 class Color(
-    CopyMixIn,
-    RevertMixIn,
     CustomEventMixIn,
-    InitialSubstitutionExpMixIn,
 ):
     _value: String
 
@@ -29,7 +22,6 @@ class Color(
         value: _StrOrString,
         *,
         variable_name_suffix: str = "",
-        skip_init_substitution_expression_appending: bool = False,
     ) -> None:
         """
         The color class implementation.
@@ -41,9 +33,6 @@ class Color(
         variable_name_suffix : str, default ''
             A JavaScript variable name suffix string.
             This setting is sometimes useful for JavaScript debugging.
-        skip_init_substitution_expression_appending : bool, default False
-            A boolean indicates whether to skip an initial substitution
-            expression or not. This class uses this option internally.
         """
         from apysc._expression.event_handler_scope import TemporaryNotHandlerScope
         from apysc._color import color_util
@@ -54,24 +43,3 @@ class Color(
                 value=value,
                 variable_name_suffix=variable_name_suffix,
             )
-
-        self._append_initial_substitution_expression_if_in_handler_scope(
-            skip_appending=skip_init_substitution_expression_appending,
-        )
-
-    @final
-    @add_debug_info_setting(module_name=__name__)
-    def _append_constructor_expression(self) -> None:
-        pass
-
-    @final
-    def _create_initial_substitution_expression(self) -> str:
-        """
-        Create an initial value's substitution expression string.
-
-        Returns
-        -------
-        expression : str
-            Created expression string.
-        """
-        return ""

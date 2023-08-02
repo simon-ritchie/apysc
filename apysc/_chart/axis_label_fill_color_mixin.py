c47578c8f9c5fd274e5b77dbca6c8460df12b64d
simon-ritchie <antisocial.sid2@gmail.com>
2023-08-03 00:28:43 +0900

    #947 Replace the `AxisLabelFillColorMixIn` color type with the `Color` class.

--- apysc/_chart/axis_label_fill_color_mixin.py
+++ apysc/_chart/axis_label_fill_color_mixin.py
@@ -8,43 +8,26 @@ from typing_extensions import final
 from apysc._html.debug_mode import add_debug_info_setting
 from apysc._type.string import String
 from apysc._validation import arg_validation_decos
-
-_StrOrString = TypeVar("_StrOrString", str, String)
+from apysc._color.color import Color
 
 
 class AxisLabelFillColorMixIn:
-    _axis_label_fill_color: String
+    _axis_label_fill_color: Color
 
     @final
-    @arg_validation_decos.is_hex_color_code_format(arg_position_index=1, optional=False)
+    @arg_validation_decos.is_color(arg_position_index=1, optional=False)
     @add_debug_info_setting(module_name=__name__)
     def _set_initial_axis_label_fill_color(
         self,
         *,
-        axis_label_fill_color: _StrOrString,
-        variable_name_suffix: str = "",
+        axis_label_fill_color: Color,
     ) -> None:
         """
         Set an initial axis label fill-color setting.
 
         Parameters
         ----------
-        axis_label_fill_color : _StrOrString
+        axis_label_fill_color : Color
             An axis label fill-color setting.
-        variable_name_suffix : str, optional
-            A JavaScript variable name suffix string.
-            This setting is sometimes useful for JavaScript debugging.
         """
-        from apysc._color import color_util
-
-        axis_label_fill_color = color_util.complement_hex_color(
-            hex_color_code=axis_label_fill_color
-        )
-        if not isinstance(axis_label_fill_color, String):
-            axis_label_fill_color_: String = String(
-                axis_label_fill_color,
-                variable_name_suffix=variable_name_suffix,
-            )
-        else:
-            axis_label_fill_color_ = axis_label_fill_color
-        self._axis_label_fill_color = axis_label_fill_color_
+        self._axis_label_fill_color = axis_label_fill_color
--- tests/_chart/test_axis_label_fill_color_mixin.py
+++ tests/_chart/test_axis_label_fill_color_mixin.py
@@ -8,16 +8,6 @@ class TestAxisLabelFillColorMixIn:
     def test__set_initial_axis_label_fill_color(self) -> None:
         mixin: AxisLabelFillColorMixIn = AxisLabelFillColorMixIn()
         mixin._set_initial_axis_label_fill_color(
-            axis_label_fill_color="#0af",
-            variable_name_suffix="test_suffix_1",
+            axis_label_fill_color=ap.Color("#0af"),
         )
-        assert mixin._axis_label_fill_color == ap.String("#00aaff")
-        assert mixin._axis_label_fill_color._variable_name_suffix == "test_suffix_1"
-
-        mixin._set_initial_axis_label_fill_color(
-            axis_label_fill_color=ap.String(
-                "#00aaff", variable_name_suffix="test_suffix_2"
-            )
-        )
-        assert mixin._axis_label_fill_color == ap.String("#00aaff")
-        assert mixin._axis_label_fill_color._variable_name_suffix == "test_suffix_2"
+        assert mixin._axis_label_fill_color == ap.Color("#00aaff")

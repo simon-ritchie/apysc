a4621a470556fddd94b47c1d8cae077cf96faeaf
simon-ritchie <antisocial.sid2@gmail.com>
2023-08-05 22:09:02 +0900

    #947 Add the color constants class.

---
+++ apysc/_color/colors.py
@@ -0,0 +1,56 @@
+"""Definitions for color constants.
+"""
+
+from apysc._color.color import Color
+
+_000000: Color = Color("#000000", variable_name_suffix="color_000000")
+_111111: Color = Color("#111111", variable_name_suffix="color_111111")
+_222222: Color = Color("#222222", variable_name_suffix="color_222222")
+_333333: Color = Color("#333333", variable_name_suffix="color_333333")
+_444444: Color = Color("#444444", variable_name_suffix="color_444444")
+_555555: Color = Color("#555555", variable_name_suffix="color_555555")
+_666666: Color = Color("#666666", variable_name_suffix="color_666666")
+_777777: Color = Color("#777777", variable_name_suffix="color_777777")
+_888888: Color = Color("#888888", variable_name_suffix="color_888888")
+_999999: Color = Color("#999999", variable_name_suffix="color_999999")
+_AAAAAA: Color = Color("#aaaaaa", variable_name_suffix="color_aaaaaa")
+_BBBBBB: Color = Color("#bbbbbb", variable_name_suffix="color_bbbbbb")
+_CCCCCC: Color = Color("#cccccc", variable_name_suffix="color_cccccc")
+_DDDDDD: Color = Color("#dddddd", variable_name_suffix="color_dddddd")
+_EEEEEE: Color = Color("#eeeeee", variable_name_suffix="color_eeeeee")
+_FFFFFF: Color = Color("#ffffff", variable_name_suffix="color_ffffff")
+_FF0000: Color = Color("#ff0000", variable_name_suffix="color_ff0000")
+_00FF00: Color = Color("#00ffff", variable_name_suffix="color_00ff00")
+_0000FF: Color = Color("#0000ff", variable_name_suffix="color_0000ff")
+_FFFF00: Color = Color("#ffff00", variable_name_suffix="color_ffff00")
+_00FFFF: Color = Color("#00ffff", variable_name_suffix="color_00ffff")
+_FF00FF: Color = Color("#ff00ff", variable_name_suffix="color_ff00ff")
+
+
+class Colors:
+    """
+    Color constants class.
+    """
+
+    BLACK_000000: Color = _000000
+    BLACK_111111: Color = _111111
+    BLACK_222222: Color = _222222
+    GRAY_333333: Color = _333333
+    GRAY_444444: Color = _444444
+    GRAY_555555: Color = _555555
+    GRAY_666666: Color = _666666
+    GRAY_777777: Color = _777777
+    GRAY_888888: Color = _888888
+    GRAY_999999: Color = _999999
+    GRAY_AAAAAA: Color = _AAAAAA
+    GRAY_BBBBBB: Color = _BBBBBB
+    GRAY_CCCCCC: Color = _CCCCCC
+    GRAY_DDDDDD: Color = _DDDDDD
+    WHITE_EEEEEE: Color = _EEEEEE
+    WHITE_FFFFFF: Color = _FFFFFF
+    RED_FF0000: Color = _FF0000
+    GREEN_00FF00: Color = _00FF00
+    BLUE_0000FF: Color = _0000FF
+    YELLOW_FFFF00: Color = _FFFF00
+    CYAN_00FFFF: Color = _00FFFF
+    MAGENTA_FF00FF: Color = _FF00FF

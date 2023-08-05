b53a04692d54413ea2dfc04579f393eb0bce3de9
simon-ritchie <antisocial.sid2@gmail.com>
2023-08-05 16:34:43 +0900

    #947 Replace the `AnimationLineColor` color type with the `Color` class.

--- apysc/_animation/animation_line_color.py
+++ apysc/_animation/animation_line_color.py
@@ -14,9 +14,9 @@ from apysc._html.debug_mode import add_debug_info_setting
 from apysc._type.int import Int
 from apysc._type.string import String
 from apysc._type.variable_name_mixin import VariableNameMixIn
+from apysc._color.color import Color
 
 _Target = TypeVar("_Target", bound=VariableNameMixIn)
-StrOrString = TypeVar("StrOrString", str, String)
 
 
 class AnimationLineColor(AnimationBase[_Target], Generic[_Target]):
@@ -45,20 +45,20 @@ class AnimationLineColor(AnimationBase[_Target], Generic[_Target]):
     >>> import apysc as ap
     >>> stage: ap.Stage = ap.Stage()
     >>> sprite: ap.Sprite = ap.Sprite()
-    >>> sprite.graphics.begin_fill(color="#0af")
-    >>> sprite.graphics.line_style(color="#fff", thickness=5)
+    >>> sprite.graphics.begin_fill(color=ap.Color("#0af"))
+    >>> sprite.graphics.line_style(color=ap.Color("#fff"), thickness=5)
     >>> rectangle: ap.Rectangle = sprite.graphics.draw_rect(
     ...     x=50, y=50, width=50, height=50
     ... )
     >>> animation: ap.AnimationLineColor = rectangle.animation_line_color(
-    ...     line_color="#0af",
+    ...     line_color=ap.Color("#0af"),
     ...     duration=1500,
     ...     easing=ap.Easing.EASE_OUT_QUINT,
     ... )
     >>> _ = animation.start()
     """
 
-    _line_color: String
+    _line_color: Color
 
     @final
     @add_debug_info_setting(module_name=__name__)
@@ -66,7 +66,7 @@ class AnimationLineColor(AnimationBase[_Target], Generic[_Target]):
         self,
         *,
         target: _Target,
-        line_color: StrOrString,
+        line_color: Color,
         duration: Union[int, Int] = 3000,
         delay: Union[int, Int] = 0,
         easing: Easing = Easing.LINEAR,
@@ -79,8 +79,8 @@ class AnimationLineColor(AnimationBase[_Target], Generic[_Target]):
         target : VariableNameMixIn
             A target instance of the animation target
             (e.g., `Rectangle` instance).
-        line_color : str or String
-            The final color (hex color code) of the animation.
+        line_color : Color
+            The final color of the animation.
         duration : int or Int, default 3000
             Milliseconds before an animation ends.
         delay : int or Int, default 0
@@ -88,17 +88,13 @@ class AnimationLineColor(AnimationBase[_Target], Generic[_Target]):
         easing : Easing, default Easing.LINEAR
             Easing setting.
         """
-        from apysc._converter import to_apysc_val_from_builtin
         from apysc._expression import expression_variables_util
         from apysc._expression import var_names
 
         variable_name: str = expression_variables_util.get_next_variable_name(
             type_name=var_names.ANIMATION_LINE_COLOR
         )
-        line_color = color_util.complement_hex_color(hex_color_code=line_color)
-        self._line_color = to_apysc_val_from_builtin.get_copied_string_from_builtin_val(
-            string=line_color
-        )
+        self._line_color = line_color
         self._set_basic_animation_settings(
             target=target, duration=duration, delay=delay, easing=easing
         )
@@ -114,12 +110,7 @@ class AnimationLineColor(AnimationBase[_Target], Generic[_Target]):
         expression : str
             Animation function expression.
         """
-        from apysc._type import value_util
-
-        line_color_str: str = value_util.get_value_str_for_expression(
-            value=self._line_color
-        )
-        return f"\n  .stroke({line_color_str});"
+        return f"\n  .stroke({self._line_color._value.variable_name});"
 
     @final
     def _get_complete_event_in_handler_head_expression(self) -> str:
@@ -139,7 +130,7 @@ class AnimationLineColor(AnimationBase[_Target], Generic[_Target]):
         if isinstance(self._target, LineColorMixIn):
             self._target._initialize_line_color_if_not_initialized()
             expression = (
-                f"{self._target._line_color.variable_name} = "
-                f"{self._line_color.variable_name};"
+                f"{self._target._line_color._value.variable_name} = "
+                f"{self._line_color._value.variable_name};"
             )
         return expression
--- tests/_animation/test_animation_line_color.py
+++ tests/_animation/test_animation_line_color.py
@@ -12,7 +12,7 @@ class TestAnimationLineColor:
         target.variable_name = "test_animation_line_color"
         animation: ap.AnimationLineColor = ap.AnimationLineColor(
             target=target,
-            line_color="0af",
+            line_color=ap.Color("0af"),
             duration=1000,
             delay=500,
             easing=ap.Easing.EASE_OUT_QUINT,
@@ -21,7 +21,7 @@ class TestAnimationLineColor:
         assert_attrs(
             expected_attrs={
                 "_target": target,
-                "_line_color": "#00aaff",
+                "_line_color": ap.Color("#00aaff"),
                 "_duration": 1000,
                 "_delay": 500,
                 "_easing": ap.Easing.EASE_OUT_QUINT,
@@ -35,10 +35,12 @@ class TestAnimationLineColor:
         target.variable_name = "test_animation_line_color"
         animation: ap.AnimationLineColor = ap.AnimationLineColor(
             target=target,
-            line_color="0af",
+            line_color=ap.Color("0af"),
         )
         expression: str = animation._get_animation_func_expression()
-        assert expression == (f"\n  .stroke({animation._line_color.variable_name});")
+        assert expression == (
+            f"\n  .stroke({animation._line_color._value.variable_name});"
+        )
 
     @apply_test_settings()
     def test__get_complete_event_in_handler_head_expression(self) -> None:
@@ -46,10 +48,10 @@ class TestAnimationLineColor:
         target.variable_name = "test_animation_line_color"
         animation: ap.AnimationLineColor = ap.AnimationLineColor(
             target=target,
-            line_color="0af",
+            line_color=ap.Color("0af"),
         )
         expression: str = animation._get_complete_event_in_handler_head_expression()
         assert expression == (
-            f"{target._line_color.variable_name} = "
-            f"{animation._line_color.variable_name};"
+            f"{target._line_color._value.variable_name} = "
+            f"{animation._line_color._value.variable_name};"
         )

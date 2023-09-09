3c9511ca4b3916a4a33924d1019ac9aaaf7cfa76
simon-ritchie <antisocial.sid2@gmail.com>
2023-09-09 10:10:27 +0900

    #984 Rebuild docs.

--- apysc/_translation/jp/datetime_minute.py
+++ apysc/_translation/jp/datetime_minute.py
@@ -11,7 +11,7 @@ MAPPING: Dict[str, str] = {
     ##################################################
     "# DateTime class minute property": "# DateTime クラスの minute 属性",
     ##################################################
-    "This page explains the `DateTime` class's `minute` property interface.": "このページでは`DateTime`クラスの`minute`属性のインターフェイスについて説明します。",  # noqa
+    "This page explains the `DateTime` class\'s `minute` property interface.": "このページでは`DateTime`クラスの`minute`属性のインターフェイスについて説明します。",  # noqa
     ##################################################
     "## What interface is this?": "## インターフェイス概要",
     ##################################################
@@ -21,11 +21,11 @@ MAPPING: Dict[str, str] = {
     ##################################################
     "A `DateTime` instance has its property interface.": "`DateTime`クラスのインスタンスがその属性のインターフェイスを持っています。",  # noqa
     ##################################################
-    "Its getter interface returns a minute's `Int` value.": "そのインターフェイスのgetterでは`Int`型の分の値を返却します。",  # noqa
+    "Its getter interface returns a minute\'s `Int` value.": "そのインターフェイスのgetterでは`Int`型の分の値を返却します。",  # noqa
     ##################################################
     "```py\n# runnable\nimport apysc as ap\n\nap.Stage()\ndatetime_: ap.DateTime = ap.DateTime(year=2022, month=12, day=5, minute=30)\nminute: ap.Int = datetime_.minute\nassert minute == 30\n```": "```py\n# runnable\nimport apysc as ap\n\nap.Stage()\ndatetime_: ap.DateTime = ap.DateTime(year=2022, month=12, day=5, minute=30)\nminute: ap.Int = datetime_.minute\nassert minute == 30\n```",  # noqa
     ##################################################
-    "Also, its setter interface accepts a minute's `Int` value.": "また、setter側のインターフェイスでは同様に`Int`型の分の値を受け付けます。",  # noqa
+    "Also, its setter interface accepts a minute\'s `Int` value.": "また、setter側のインターフェイスでは同様に`Int`型の分の値を受け付けます。",  # noqa
     ##################################################
     "0-59 integer is acceptable.": "0～59の整数を受け付けることができます。",
     ##################################################
@@ -33,11 +33,11 @@ MAPPING: Dict[str, str] = {
     ##################################################
     "## minute property API": "## minute 属性のAPI",
     ##################################################
-    '<span class="inconspicuous-txt">Note: the document build script generates and updates this API document section automatically. Maybe this section is duplicated compared with previous sections.</span>': '<span class="inconspicuous-txt">特記事項: このAPIドキュメントはドキュメントビルド用のスクリプトによって自動で生成・同期されています。そのためもしかしたらこの節の内容は前節までの内容と重複している場合があります。</span>',  # noqa
+    "<span class=\"inconspicuous-txt\">Note: the document build script generates and updates this API document section automatically. Maybe this section is duplicated compared with previous sections.</span>": "<span class=\"inconspicuous-txt\">特記事項: このAPIドキュメントはドキュメントビルド用のスクリプトによって自動で生成・同期されています。そのためもしかしたらこの節の内容は前節までの内容と重複している場合があります。</span>",  # noqa
     ##################################################
     "**[Interface summary]**": "**[インターフェイス概要]**",
     ##################################################
-    "Get a current minute's value.<hr>": "現在の分の値を取得します。<hr>",
+    "Get a current minute\'s value.<hr>": "現在の分の値を取得します。<hr>",
     ##################################################
     "**[Returns]**": "**[返却値]**",
     ##################################################

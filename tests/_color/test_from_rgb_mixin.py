cc0dc149ffd7eca1936bb1d7e5cc5a947f088825
simon-ritchie <antisocial.sid2@gmail.com>
2023-09-22 22:49:49 +0900

    #973 Add the test for `from_rgb` method.

--- tests/_color/test_from_rgb_mixin.py
+++ tests/_color/test_from_rgb_mixin.py
@@ -1,6 +1,9 @@
 import apysc as ap
 from apysc._color import from_rgb_mixin
+from apysc._expression import expression_data_util
 from apysc._testing.testing_helper import apply_test_settings
+import re
+from typing import Optional, Match
 
 
 @apply_test_settings()
@@ -25,3 +28,24 @@ def test__get_py_str_from_rgb() -> None:
         blue=ap.Int(2),
     )
     assert py_str == "#000102"
+
+
+class TestFromRgbMixIn:
+    @apply_test_settings()
+    def test_from_rgb(self) -> None:
+        color: ap.Color = ap.Color.from_rgb(
+            red=0, green=1, blue=2, variable_name_suffix="test_suffix"
+        )
+        assert color._value._value == "#000102"
+        assert color._variable_name_suffix == "test_suffix"
+        expression: str = expression_data_util.get_current_expression()
+        assert expression.count("toString(16);") >= 3
+        assert expression.count(".toUpperCase();") >= 3
+        match_: Optional[Match] = re.search(
+            pattern=rf'{color.variable_name} = "#" \+ .+? \+ .+? \+ .+?\;',
+            string=expression,
+        )
+        assert match_ is not None
+
+        color = ap.Color.from_rgb(red=255, green=255, blue=255)
+        assert color._value._value == "#FFFFFF"

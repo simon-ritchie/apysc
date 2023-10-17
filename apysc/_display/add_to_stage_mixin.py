cceb9958fcbf6ff56576c0b834c266daf48ea9e7
simon-ritchie <antisocial.sid2@gmail.com>
2023-10-17 22:52:36 +0900

    #1003 Add the mix-in class for the `_add_to_stage` method.

---
+++ apysc/_display/add_to_stage_mixin.py
@@ -0,0 +1,25 @@
+"""The mix-in class for the `_add_to_stage` method.
+"""
+
+from typing_extensions import final
+from apysc._html.debug_mode import add_debug_info_setting
+
+
+class AddToStageMixIn:
+    @final
+    @add_debug_info_setting(module_name=__name__)
+    def _add_to_stage(self) -> None:
+        """
+        Add this instance to a stage instance.
+        """
+        from apysc._display.stage import Stage
+        from apysc._display.stage import get_stage
+        from apysc._display.display_object import DisplayObject
+
+        if not isinstance(self, DisplayObject):
+            raise TypeError(
+                "This method is only available for `DisplayObject` subclasses."
+            )
+
+        stage: Stage = get_stage()
+        stage.add_child(child=self)
---
+++ tests/_display/test_add_to_stage_mixin.py
@@ -0,0 +1,40 @@
+import apysc as ap
+from apysc._testing.testing_helper import apply_test_settings, assert_raises
+from apysc._display.add_to_stage_mixin import AddToStageMixIn
+from apysc._type.variable_name_mixin import VariableNameMixIn
+from apysc._display.css_mixin import CssMixIn
+
+
+class _TestValidObject(
+    CssMixIn,
+    ap.DisplayObject,
+    AddToStageMixIn,
+    VariableNameMixIn,
+):
+    def __init__(self) -> None:
+        """
+        A class for a valid test pattern.
+        """
+        super().__init__(variable_name="test_object")
+
+
+class _TestInvalidObject(AddToStageMixIn, VariableNameMixIn):
+    def __init__(self) -> None:
+        """
+        A class for an invalid test pattern.
+        """
+
+
+class TestAddToStageMixIn:
+    @apply_test_settings()
+    def test__add_to_stage(self) -> None:
+        stage: ap.Stage = ap.get_stage()
+        test_valid_object: _TestValidObject = _TestValidObject()
+        test_valid_object._add_to_stage()
+        assert test_valid_object.parent == stage
+
+        test_invalid_object: _TestInvalidObject = _TestInvalidObject()
+        assert_raises(
+            expected_error_class=TypeError,
+            callable_=test_invalid_object._add_to_stage,
+        )

ee628a83b1e4f46780c28d89e58b527f0764893f
simon-ritchie <antisocial.sid2@gmail.com>
2023-10-20 22:49:10 +0900

    #1003 Add a type validation and adding implementation.

--- apysc/_display/svg_foreign_object_child_mixin.py
+++ apysc/_display/svg_foreign_object_child_mixin.py
@@ -26,7 +26,17 @@ class SVGForeignObjectChildMixIn:
             A JavaScript variable name suffix string.
             This setting is sometimes useful for JavaScript debugging.
         """
+        from apysc._display.add_foreign_object_child_mixin import (
+            AddForeignObjectChildMixIn
+        )
+
+        if not isinstance(self, AddForeignObjectChildMixIn):
+            raise TypeError(
+                "This method can only be called by an "
+                "`AddForeignObjectChildMixIn`'s instance."
+            )
         self._svg_foreign_object_child = SVGForeignObjectChild(
             html_str=html_str,
             variable_name_suffix=variable_name_suffix,
         )
+        self._add_foreign_object_child(child=self._svg_foreign_object_child)
--- tests/_display/test_svg_foreign_object_child_mixin.py
+++ tests/_display/test_svg_foreign_object_child_mixin.py
@@ -1,12 +1,58 @@
 from apysc._display.svg_foreign_object_child_mixin import SVGForeignObjectChildMixIn
+from apysc._display.add_foreign_object_child_mixin import (
+    AddForeignObjectChildMixIn
+)
+from apysc._expression import expression_data_util
+from apysc._type.variable_name_mixin import VariableNameMixIn
+from apysc._testing.testing_helper import apply_test_settings, assert_raises
+
+
+class _TestValidObject(
+    SVGForeignObjectChildMixIn,
+    AddForeignObjectChildMixIn,
+    VariableNameMixIn,
+):
+    def __init__(self) -> None:
+        """
+        The test class for the valid case.
+        """
+        self.variable_name = "test_valid_object"
+
+
+class _TestInvalidObject(
+    SVGForeignObjectChildMixIn,
+    VariableNameMixIn,
+):
+    def __init__(self) -> None:
+        """
+        The test class for the invalid case.
+        """
+        self.variable_name = "test_invalid_object"
 
 
 class TestSVGForeignObjectChildMixIn:
+    @apply_test_settings()
     def test__initialize_svg_foreign_object_child(self) -> None:
-        mixin: SVGForeignObjectChildMixIn = SVGForeignObjectChildMixIn()
-        mixin._initialize_svg_foreign_object_child(
+        valid_object: _TestValidObject = _TestValidObject()
+        valid_object._initialize_svg_foreign_object_child(
             html_str='<p>test</p>',
             variable_name_suffix='test_suffix',
         )
-        assert mixin._svg_foreign_object_child._html_str == '<p>test</p>'
-        assert mixin._svg_foreign_object_child._variable_name_suffix == 'test_suffix'
+        assert valid_object._svg_foreign_object_child._html_str == '<p>test</p>'
+        assert (
+            valid_object._svg_foreign_object_child._variable_name_suffix
+            == 'test_suffix'
+        )
+        expression: str = expression_data_util.get_current_expression()
+        expected: str = (
+            f"{valid_object.variable_name}"
+            f".add({valid_object._svg_foreign_object_child.variable_name});"
+        )
+        assert expected in expression
+
+        invalid_object: _TestInvalidObject = _TestInvalidObject()
+        assert_raises(
+            expected_error_class=TypeError,
+            callable_=invalid_object._initialize_svg_foreign_object_child,
+            html_str='<p>test</p>',
+        )

339da667c613f645d8369ad1d36dae17c23cab96
simon-ritchie <antisocial.sid2@gmail.com>
2023-10-20 22:16:51 +0900

    #1003 Add a validation decorator to the `_initialize_text` method.

--- apysc/_display/svg_foreign_object_text_mixin.py
+++ apysc/_display/svg_foreign_object_text_mixin.py
@@ -5,12 +5,14 @@ from typing import Union
 from apysc._type.string import String
 from typing_extensions import final
 from apysc._html.debug_mode import add_debug_info_setting
+from apysc._validation import arg_validation_decos
 
 
 class SVGForeignObjectTextMixIn:
     _text: String
 
     @final
+    @arg_validation_decos.is_string(arg_position_index=1, optional=False)
     @add_debug_info_setting(module_name=__name__)
     def _initialize_text(self, *, text: Union[str, String]) -> None:
         """

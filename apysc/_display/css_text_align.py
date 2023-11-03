8264ff54148fca4504636761f5fedc6b637c2425
simon-ritchie <antisocial.sid2@gmail.com>
2023-11-03 22:06:37 +0900

    #1003 Add the enum definitions for the text-align CSS property.

---
+++ apysc/_display/css_text_align.py
@@ -0,0 +1,14 @@
+"""The enum definitions for the text-align CSS property.
+"""
+
+from enum import Enum
+
+
+class CssTextAlign(Enum):
+    """
+    The enum definitions for the text-align CSS property.
+    """
+    LEFT = "left"
+    CENTER = "center"
+    RIGHT = "right"
+    JUSTIFY = "justify"

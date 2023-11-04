fc908b6a20845d9566c6dfdccc520bd4c9b84cd6
simon-ritchie <antisocial.sid2@gmail.com>
2023-11-04 22:16:40 +0900

    #1003 The enum definitions for the last line's text align CSS property (`text-align-last`).

---
+++ apysc/_display/css_text_align_last.py
@@ -0,0 +1,17 @@
+"""The enum definitions for the last line's text align CSS
+property (`text-align-last`).
+"""
+
+from enum import Enum
+
+
+class CssTextAlignLast(Enum):
+    """
+    The enum definitions for the last line's text align CSS
+    property (`text-align-last`).
+    """
+
+    LEFT = "left"
+    CENTER = "center"
+    RIGHT = "right"
+    JUSTIFY = "justify"

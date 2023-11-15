37cc35df0899a3b86afa0b2d57eeb63d3c447ecc
simon-ritchie <antisocial.sid2@gmail.com>
2023-11-16 00:12:16 +0900

    #1072 Add the doctest to the `MultiLineText`'s constructor.

--- apysc/_display/multi_line_text.py
+++ apysc/_display/multi_line_text.py
@@ -173,6 +173,28 @@ class MultiLineText(
             - https://simon-ritchie.github.io/apysc/en/multi_line_text.html
         - Text fill_color property
             - https://simon-ritchie.github.io/apysc/en/text_fill_color.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> stage: ap.Stage = ap.Stage(
+        ...     background_color=ap.Color("#333"),
+        ...     stage_width=300,
+        ...     stage_height=100,
+        ...     stage_elem_id="stage",
+        ... )
+        >>> multi_line_text: ap.MultiLineText = ap.MultiLineText(
+        ...     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+        ...     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        ...     "Ut enim ad minim veniam",
+        ...     width=300,
+        ...     font_size=16,
+        ...     fill_color=ap.Color("#00aaff"),
+        ...     x=20,
+        ...     y=20,
+        ... )
+        >>> multi_line_text.fill_color
+        Color("#00aaff")
         """
         from apysc._converter import to_apysc_val_from_builtin
         from apysc._expression import expression_variables_util

e8a1ec4d329b83ff5ea9779722947b429dafd86a
simon-ritchie <antisocial.sid2@gmail.com>
2023-11-29 22:03:49 +0900

    #1077 Add the doctests to the `text_align_last` property docstrings.

--- apysc/_display/text_align_last_css_mixin.py
+++ apysc/_display/text_align_last_css_mixin.py
@@ -22,6 +22,30 @@ class TextAlignLastCssMixIn:
         ----------
         - text_align_last property
             - https://simon-ritchie.github.io/apysc/en/text_align_last.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+
+        >>> stage: ap.Stage = ap.Stage(
+        ...     background_color=ap.Color("#333"),
+        ...     stage_width=350,
+        ...     stage_height=170,
+        ...     stage_elem_id="stage",
+        ... )
+        >>> multi_line_text: ap.MultiLineText = ap.MultiLineText(
+        ...     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+        ...     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        ...     "Ut enim ad minim veniam",
+        ...     width=300,
+        ...     font_size=16,
+        ...     fill_color=ap.Color("#00aaff"),
+        ...     x=25,
+        ...     y=25,
+        ... )
+        >>> multi_line_text.text_align = ap.CssTextAlign.JUSTIFY
+        >>> multi_line_text.text_align_last = ap.CssTextAlignLast.RIGHT
+        >>> assert multi_line_text.text_align_last == ap.CssTextAlignLast.RIGHT
         """
         return self._css_text_align_last
 
@@ -40,6 +64,30 @@ class TextAlignLastCssMixIn:
         ----------
         - text_align_last property
             - https://simon-ritchie.github.io/apysc/en/text_align_last.html
+
+        Examples
+        --------
+        >>> import apysc as ap
+
+        >>> stage: ap.Stage = ap.Stage(
+        ...     background_color=ap.Color("#333"),
+        ...     stage_width=350,
+        ...     stage_height=170,
+        ...     stage_elem_id="stage",
+        ... )
+        >>> multi_line_text: ap.MultiLineText = ap.MultiLineText(
+        ...     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+        ...     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        ...     "Ut enim ad minim veniam",
+        ...     width=300,
+        ...     font_size=16,
+        ...     fill_color=ap.Color("#00aaff"),
+        ...     x=25,
+        ...     y=25,
+        ... )
+        >>> multi_line_text.text_align = ap.CssTextAlign.JUSTIFY
+        >>> multi_line_text.text_align_last = ap.CssTextAlignLast.RIGHT
+        >>> assert multi_line_text.text_align_last == ap.CssTextAlignLast.RIGHT
         """
         from apysc._display.css_interface import CssInterface
         from apysc._validation import display_validation

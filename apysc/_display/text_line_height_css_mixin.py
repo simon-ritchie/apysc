10489dcbb373522e543baa2ef716fe7c44a39d64
simon-ritchie <antisocial.sid2@gmail.com>
2023-12-20 23:07:42 +0900

    #1105 Add the doctests to the `line_height` property.

--- apysc/_display/text_line_height_css_mixin.py
+++ apysc/_display/text_line_height_css_mixin.py
@@ -28,6 +28,30 @@ class TextLineHeightCssMixIn(
         -------
         line_height : Number
             A line-height value.
+
+        Examples
+        --------
+        >>> import apysc as ap
+
+        >>> stage: ap.Stage = ap.Stage(
+        ...     background_color=ap.Color("#333"),
+        ...     stage_width=300,
+        ...     stage_height=250,
+        ...     stage_elem_id="stage",
+        ... )
+
+        >>> multi_line_text: ap.MultiLineText = ap.MultiLineText(
+        ...     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+        ...     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        ...     "Ut enim ad minim veniam",
+        ...     width=250,
+        ...     fill_color=ap.Colors.GRAY_AAAAAA,
+        ...     x=25,
+        ...     y=25,
+        ... )
+        >>> multi_line_text.line_height = ap.Number(2.0)
+        >>> multi_line_text.line_height
+        Number(2.0)
         """
         self._initialize_line_height()
         return self._line_height
@@ -42,6 +66,30 @@ class TextLineHeightCssMixIn(
         ----------
         value : Number
             A line-height value.
+
+        Examples
+        --------
+        >>> import apysc as ap
+
+        >>> stage: ap.Stage = ap.Stage(
+        ...     background_color=ap.Color("#333"),
+        ...     stage_width=300,
+        ...     stage_height=250,
+        ...     stage_elem_id="stage",
+        ... )
+
+        >>> multi_line_text: ap.MultiLineText = ap.MultiLineText(
+        ...     text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+        ...     "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        ...     "Ut enim ad minim veniam",
+        ...     width=250,
+        ...     fill_color=ap.Colors.GRAY_AAAAAA,
+        ...     x=25,
+        ...     y=25,
+        ... )
+        >>> multi_line_text.line_height = ap.Number(2.0)
+        >>> multi_line_text.line_height
+        Number(2.0)
         """
         from apysc._display.css_interface import CssInterface
         from apysc._validation import display_validation

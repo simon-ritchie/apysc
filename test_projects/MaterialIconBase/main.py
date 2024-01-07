1c6e99bbc50e25019322f9440619b89deaf49344
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-07 22:32:47 +0900

    #1107 Add the test project for the `MaterialIconBase`.

---
+++ test_projects/MaterialIconBase/main.py
@@ -0,0 +1,45 @@
+"""The test project for the `MaterialIconBase` class.
+
+Command examples:
+$ python test_projects/MaterialIconBase/main.py
+"""
+
+import sys
+
+sys.path.append("./")
+
+import os
+from types import ModuleType
+
+import apysc as ap
+from apysc._file import file_util
+
+this_module: ModuleType = sys.modules[__name__]
+
+_DEST_DIR_PATH: str = os.path.join(
+    file_util.get_abs_module_dir_path(module=this_module), "test_output/"
+)
+
+
+def main() -> None:
+    """
+    Entry point of this test project.
+    """
+    _: ap.Stage = ap.Stage(
+        background_color=ap.Color("#333"),
+        stage_width=1200,
+        stage_height=900,
+    )
+    ap.MaterialIconBase(
+        svg_path_value="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z",  # noqa
+        fill_color=ap.Colors.CYAN_00AAFF,
+        fill_alpha=0.5,
+        width=24,
+        height=24,
+    )
+
+    ap.save_overall_html(dest_dir_path=_DEST_DIR_PATH, minify=False)
+
+
+if __name__ == "__main__":
+    main()

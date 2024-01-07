1e062d508c0b71846606ae838be894ce70a1ef82
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-07 22:47:00 +0900

    #1107 Add the license comment.

--- test_projects/MaterialIconBase/main.py
+++ test_projects/MaterialIconBase/main.py
@@ -30,6 +30,8 @@ def main() -> None:
         stage_width=1200,
         stage_height=900,
     )
+    # Material Icon: https://fonts.google.com/icons?selected=Material+Icons:search:
+    # License: https://www.apache.org/licenses/LICENSE-2.0.html
     ap.MaterialIconBase(
         svg_path_value="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z",  # noqa
         fill_color=ap.Colors.CYAN_00AAFF,

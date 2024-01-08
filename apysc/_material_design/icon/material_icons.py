4f52e7e50951539831aca7131b8067322017f900
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-08 20:46:23 +0900

    #1112 Add the `MaterialSearchIcon` and `MaterialIcons` classes.

---
+++ apysc/_material_design/icon/material_icons.py
@@ -0,0 +1,12 @@
+"""The class implementation for the Material Icons.
+"""
+
+from typing import Type
+from apysc._material_design.icon.material_search_icon import MaterialSearchIcon
+
+
+class MaterialIcons:
+    """
+    The class implementation for the Material Icons.
+    """
+    Search: Type[MaterialSearchIcon] = MaterialSearchIcon
---
+++ tests/_material_design/icon/test_material_search_icon.py
@@ -0,0 +1,25 @@
+import apysc as ap
+from apysc._expression import var_names
+from apysc._testing.testing_helper import apply_test_settings
+
+
+class TestMaterialSearchIcon:
+    @apply_test_settings()
+    def test___init__(self) -> None:
+        icon: ap.MaterialIcons.Search = ap.MaterialIcons.Search(
+            fill_color=ap.Colors.WHITE_FFFFFF,
+            fill_alpha=0.5,
+            x=100,
+            y=150,
+            width=40,
+            height=50,
+            variable_name_suffix="test_suffix",
+        )
+        assert icon._fill_color == ap.Colors.WHITE_FFFFFF
+        assert icon._fill_alpha._value == 0.5
+        assert icon._x._value == 100
+        assert icon._y._value == 150
+        assert icon._width._value == 40
+        assert icon._height._value == 50
+        assert var_names.MATERIAL_SEARCH_ICON in icon.variable_name
+        assert "test_suffix" in icon._variable_name_suffix

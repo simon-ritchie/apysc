3df1f10a1a6321546445f6987e7882dd205639ce
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-15 22:58:12 +0900

    #1112 Add the `MaterialCalendarTodayIcon` class implementation.

---
+++ apysc/_material_design/icon/material_calendar_today_icon.py
@@ -0,0 +1,43 @@
+"""Material icons' calendar today icon class implementation.
+"""
+
+from apysc._material_design.icon.path_and_var_name_setting_base import (
+    PathAndVarNameSettingBase,
+)
+
+
+class MaterialCalendarTodayIcon(PathAndVarNameSettingBase):
+    """
+    Material icons' calendar today icon class implementation.
+
+    References
+    ----------
+    - Material icons
+        - https://fonts.google.com/icons?selected=Material+Icons:search:
+    - APACHE LICENSE, VERSION 2.0
+        - https://www.apache.org/licenses/LICENSE-2.0.html
+    """
+
+    def _get_svg_path_value(self) -> str:
+        """
+        Get this icon's SVG path value.
+
+        Returns
+        -------
+        svg_path_value : str
+            This icon's SVG path value.
+        """
+        return "M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"  # noqa
+
+    def _get_icon_variable_name(self) -> str:
+        """
+        Get this icon variable name's constant value.
+
+        Returns
+        -------
+        icon_variable_name : str
+            This icon variable name's constant value.
+        """
+        from apysc._expression import var_names
+
+        return var_names.MATERIAL_CALENDAR_TODAY_ICON

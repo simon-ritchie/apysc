804cca7a70011cea20247042645f2fce0e9f4333
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-23 23:22:26 +0900

    #1107 Add the `MaterialDeleteForeverIcon` class implementation.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -188,6 +188,7 @@ from apysc._material_design.icon.material_work_icon import MaterialWorkIcon
 from apysc._material_design.icon.material_print_icon import MaterialPrintIcon
 from apysc._material_design.icon.material_analytics_icon import MaterialAnalyticsIcon
 from apysc._material_design.icon.material_calendar_month_icon import MaterialCalendarMonthIcon
+from apysc._material_design.icon.material_delete_forever_icon import MaterialDeleteForeverIcon
 
 
 True_: __True = __True()
---
+++ apysc/_material_design/icon/material_delete_forever_icon.py
@@ -0,0 +1,43 @@
+"""Material icons' delete forever icon class implementations.
+"""
+
+from apysc._material_design.icon.path_and_var_name_setting_base import (
+    PathAndVarNameSettingBase,
+)
+
+
+class MaterialDeleteForeverIcon(PathAndVarNameSettingBase):
+    """
+    Material icons' delete forever icon class implementations.
+
+    References
+    ----------
+    - Material icons
+        - https://fonts.google.com/icons?selected=Material+Icons:search:
+    - APACHE LICENSE, VERSION 2.0
+        - https://www.apache.org/licenses/LICENSE-2.0.html
+    """
+
+    def _get_svg_path_value(self) -> str:
+        """
+        Get this icon's SVG path value.
+
+        Returns
+        -------
+        svg_path_value : str
+            This icon's SVG path value.
+        """
+        return "M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"  # noqa
+
+    def _get_icon_variable_name(self) -> str:
+        """
+        Get this icon variable name's constant value.
+
+        Returns
+        -------
+        icon_variable_name : str
+            This icon variable name's constant value.
+        """
+        from apysc._expression import var_names
+
+        return var_names.MATERIAL_DELETE_FOREVER_ICON

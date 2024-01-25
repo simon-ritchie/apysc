947f26cb49d4144f50065755e2d0ee8018fb521a
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-25 22:46:15 +0900

    #1107 Add the `MaterialPetsIcon` class implementation.

---
+++ apysc/_material_design/icon/material_pets_icon.py
@@ -0,0 +1,43 @@
+"""Material icons' pets icon class implementations.
+"""
+
+from apysc._material_design.icon.path_and_var_name_setting_base import (
+    PathAndVarNameSettingBase,
+)
+
+
+class MaterialPetsIcon(PathAndVarNameSettingBase):
+    """
+    Material icons' pets icon class implementations.
+
+    References
+    ----------
+    - Material icons
+        - https://fonts.google.com/icons?selected=Material+Icons:search:
+    - APACHE LICENSE, VERSION 2.0
+        - https://www.apache.org/licenses/LICENSE-2.0.html
+    """
+
+    def _get_svg_path_value(self) -> str:
+        """
+        Get this icon's SVG path value.
+
+        Returns
+        -------
+        svg_path_value : str
+            This icon's SVG path value.
+        """
+        return "M17.34 14.86c-.87-1.02-1.6-1.89-2.48-2.91-.46-.54-1.05-1.08-1.75-1.32-.11-.04-.22-.07-.33-.09-.25-.04-.52-.04-.78-.04s-.53 0-.79.05c-.11.02-.22.05-.33.09-.7.24-1.28.78-1.75 1.32-.87 1.02-1.6 1.89-2.48 2.91-1.31 1.31-2.92 2.76-2.62 4.79.29 1.02 1.02 2.03 2.33 2.32.73.15 3.06-.44 5.54-.44h.18c2.48 0 4.81.58 5.54.44 1.31-.29 2.04-1.31 2.33-2.32.31-2.04-1.3-3.49-2.61-4.8z"  # noqa
+
+    def _get_icon_variable_name(self) -> str:
+        """
+        Get this icon variable name's constant value.
+
+        Returns
+        -------
+        icon_variable_name : str
+            This icon variable name's constant value.
+        """
+        from apysc._expression import var_names
+
+        return var_names.MATERIAL_PETS_ICON

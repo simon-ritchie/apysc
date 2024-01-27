30d2c73dbc43ce7afca669018c4d7367e22518e1
simon-ritchie <antisocial.sid2@gmail.com>
2024-01-27 16:30:34 +0900

    #1007 Add the `MaterialReorderIcon` class implementation.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -203,6 +203,7 @@ from apysc._material_design.icon.material_explore_icon import MaterialExploreIco
 from apysc._material_design.icon.material_bookmark_icon import MaterialBookmarkIcon
 from apysc._material_design.icon.material_account_box_icon import MaterialAccountBoxIcon
 from apysc._material_design.icon.material_note_add_icon import MaterialNoteAddIcon
+from apysc._material_design.icon.material_reorder_icon import MaterialReorderIcon
 
 True_: __True = __True()
 False_: __False = __False()
---
+++ apysc/_material_design/icon/material_reorder_icon.py
@@ -0,0 +1,43 @@
+"""Material icons' reorder icon class implementation.
+"""
+
+from apysc._material_design.icon.path_and_var_name_setting_base import (
+    PathAndVarNameSettingBase,
+)
+
+
+class MaterialReorderIcon(PathAndVarNameSettingBase):
+    """
+    Material icons' reorder icon class implementation.
+
+    References
+    ----------
+    - Material icons
+        - https://fonts.google.com/icons?selected=Material+Icons:search:
+    - APACHE LICENSE, VERSION 2.0
+        - https://www.apache.org/licenses/LICENSE-2.0.html
+    """
+
+    def _get_svg_path_value(self) -> str:
+        """
+        Get this icon's SVG path value.
+
+        Returns
+        -------
+        svg_path_value : str
+            This icon's SVG path value.
+        """
+        return "M3 15h18v-2H3v2zm0 4h18v-2H3v2zm0-8h18V9H3v2zm0-6v2h18V5H3z"
+
+    def _get_icon_variable_name(self) -> str:
+        """
+        Get this icon variable name's constant value.
+
+        Returns
+        -------
+        icon_variable_name : str
+            This icon variable name's constant value.
+        """
+        from apysc._expression import var_names
+
+        return var_names.MATERIAL_REORDER_ICON

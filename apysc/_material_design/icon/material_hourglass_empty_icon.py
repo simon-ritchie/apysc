b76057a98f3e676c7e702845072507d83040e93b
simon-ritchie <antisocial.sid2@gmail.com>
2024-02-06 20:16:51 +0900

    #1107 Add the `MaterialHourglassEmptyIcon` class implementation.

---
+++ apysc/_material_design/icon/material_hourglass_empty_icon.py
@@ -0,0 +1,43 @@
+"""Material icons' hourglass empty icon class implementation.
+"""
+
+from apysc._material_design.icon.path_and_var_name_setting_base import (
+    PathAndVarNameSettingBase,
+)
+
+
+class MaterialHourglassEmptyIcon(PathAndVarNameSettingBase):
+    """
+    Material icons' hourglass empty icon class implementation.
+
+    References
+    ----------
+    - Material icons
+        - https://fonts.google.com/icons?selected=Material+Icons:search:
+    - APACHE LICENSE, VERSION 2.0
+        - https://www.apache.org/licenses/LICENSE-2.0.html
+    """
+
+    def _get_svg_path_value(self) -> str:
+        """
+        Get this icon's SVG path value.
+
+        Returns
+        -------
+        svg_path_value : str
+            This icon's SVG path value.
+        """
+        return "M6 2v6h.01L6 8.01 10 12l-4 4 .01.01H6V22h12v-5.99h-.01L18 16l-4-4 4-3.99-.01-.01H18V2H6zm10 14.5V20H8v-3.5l4-4 4 4zm-4-5l-4-4V4h8v3.5l-4 4z"  # noqa
+
+    def _get_icon_variable_name(self) -> str:
+        """
+        Get this icon variable name's constant value.
+
+        Returns
+        -------
+        icon_variable_name : str
+            This icon variable name's constant value.
+        """
+        from apysc._expression import var_names
+
+        return var_names.MATERIAL_HOURGLASS_EMPTY_ICON

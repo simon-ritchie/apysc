c64a91c56f60e7bd042a6ae753da43c1fbed3c24
simon-ritchie <antisocial.sid2@gmail.com>
2024-02-12 21:21:07 +0900

    #1107 Add the `MaterialCopyrightIcon` class implementation.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -275,6 +275,7 @@ from apysc._material_design.icon.material_toc_icon import MaterialTocIcon
 from apysc._material_design.icon.material_bookmarks_icon import MaterialBookmarksIcon
 from apysc._material_design.icon.material_circle_up_icon import MaterialCircleUpIcon
 from apysc._material_design.icon.material_track_changes_icon import MaterialTrackChangesIcon
+from apysc._material_design.icon.material_copyright_icon import MaterialCopyrightIcon
 
 True_: __True = __True()
 False_: __False = __False()

ccb7e9151dc140ee197698bf252373558fb2721b
simon-ritchie <antisocial.sid2@gmail.com>
2024-02-12 13:22:39 +0900

    #1107 Add the `MaterialHttpsIcon` class implementation.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -270,6 +270,7 @@ from apysc._material_design.icon.material_table_view_icon import MaterialTableVi
 from apysc._material_design.icon.material_privacy_tip_icon import MaterialPrivacyTipIcon
 from apysc._material_design.icon.material_nightlight_round_icon import MaterialNightlightRoundIcon
 from apysc._material_design.icon.material_currency_exchange_icon import MaterialCurrencyExchangeIcon
+from apysc._material_design.icon.material_https_icon import MaterialHttpsIcon
 
 True_: __True = __True()
 False_: __False = __False()

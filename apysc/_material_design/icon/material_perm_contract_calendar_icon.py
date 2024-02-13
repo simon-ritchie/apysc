87836a08ede7f45fe2467b9fcc477849fb5846ba
simon-ritchie <antisocial.sid2@gmail.com>
2024-02-13 22:32:03 +0900

    #1107 Add the `MaterialPermContractCalendarIcon` class implementation.

---
+++ apysc/_material_design/icon/material_perm_contract_calendar_icon.py
@@ -0,0 +1,43 @@
+"""Material icons' perm contract calendar icon class implementation.
+"""
+
+from apysc._material_design.icon.path_and_var_name_setting_base import (
+    PathAndVarNameSettingBase,
+)
+
+
+class MaterialPermContractCalendarIcon(PathAndVarNameSettingBase):
+    """
+    Material icons' perm contract calendar icon class implementation.
+
+    References
+    ----------
+    - Material icons
+        - https://fonts.google.com/icons?selected=Material+Icons:search:
+    - APACHE LICENSE, VERSION 2.0
+        - https://www.apache.org/licenses/LICENSE-2.0.html
+    """
+
+    def _get_svg_path_value(self) -> str:
+        """
+        Get this icon's SVG path value.
+
+        Returns
+        -------
+        svg_path_value : str
+            This icon's SVG path value.
+        """
+        return "M19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm6 12H6v-1c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1z"  # noqa
+
+    def _get_icon_variable_name(self) -> str:
+        """
+        Get this icon variable name's constant value.
+
+        Returns
+        -------
+        icon_variable_name : str
+            This icon variable name's constant value.
+        """
+        from apysc._expression import var_names
+
+        return var_names.MATERIAL_PERM_CONTRACT_CALENDAR_ICON

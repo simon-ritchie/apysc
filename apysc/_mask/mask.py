2a8b3e85fd0769c7ee51b03102a68b176786cc2a
simon-ritchie <antisocial.sid2@gmail.com>
2024-05-30 00:03:44 +0900

    #1186 Add the `add_masking_object` method to the `Mask` class.

--- apysc/_mask/mask.py
+++ apysc/_mask/mask.py
@@ -1,10 +1,13 @@
 """Implementation for the mask class.
 """
 
+from typing import Union
 from apysc._display.css_mixin import CssMixIn
+from apysc._display.fill_color_mixin import FillColorMixIn
 from apysc._display.set_overflow_visible_setting_mixin import (
     SetOverflowVisibleSettingMixIn,
 )
+from apysc._type.number import Number
 
 
 class Mask(
@@ -50,4 +53,46 @@ class Mask(
 
         stage: Stage = get_stage()
         expression: str = f"var {self.variable_name} = {stage.variable_name}.mask();"
+        expression_data_util.append_js_expression(expression=expression)
+
+    def add_masking_object(
+        self,
+        *,
+        masking_object: FillColorMixIn,
+        alpha: Union[float, Number] = 1.0,
+    ) -> None:
+        """
+        Add a masking object to this mask. This instance uses its masking object
+        to mask other display objects.
+
+        It is possble to add multiple masking objects to a mask.
+
+        Notes
+        -----
+        This method updates the `masking_object` argument's fill color.
+
+        Parameters
+        ----------
+        masking_object : FillColorMixIn
+            The masking object to add.
+        alpha : float or Number, default 1.0
+            The alpha value for masking.
+            1.0 means fully visible, and 0.0 means fully invisible.
+        """
+        from apysc._converter import to_apysc_val_from_builtin
+        from apysc._math.math import Math
+        from apysc._color.color import Color
+        from apysc._type.int import Int
+        from apysc._expression import expression_data_util
+
+        alpha_: Number = to_apysc_val_from_builtin.get_copied_number_from_builtin_val(
+            float_or_num=alpha
+        )
+        alpha_ = Math.clamp(value=alpha_, min_=Number(0.0), max_=Number(1.0))
+        color_value: Int = Math.trunc(alpha_ * 255)
+        mask_color: Color = Color.from_rgb(
+            red=color_value, green=color_value, blue=color_value
+        )
+        masking_object.fill_color = mask_color
+        expression: str = f"{self.variable_name}.add({masking_object.variable_name});"
         expression_data_util.append_js_expression(expression=expression)
--- tests/_mask/test_mask.py
+++ tests/_mask/test_mask.py
@@ -20,3 +20,26 @@ class TestMask:
         mask: ap.Mask = ap.Mask(variable_name_suffix="test_suffix")
         assert var_names.MASK in mask.variable_name
         assert mask._variable_name_suffix == "test_suffix"
+
+    @apply_test_settings(retrying_max_attempts_num=0)
+    def test_add_masking_object(self) -> None:
+        rectangle_1: ap.Rectangle = ap.Rectangle(
+            x=50, y=50, width=50, height=50, fill_color=ap.Color("#0af")
+        )
+        mask: ap.Mask = ap.Mask()
+        mask.add_masking_object(masking_object=rectangle_1)
+        expression: str = expression_data_util.get_current_expression()
+        expected: str = f"{mask.variable_name}.add({rectangle_1.variable_name});"
+        assert expected in expression
+        assert (
+            rectangle_1._fill_color._value.lower() == ap.Color("#FFFFFF")._value.lower()
+        )
+
+        rectangle_2: ap.Rectangle = ap.Rectangle(
+            x=150, y=50, width=50, height=50, fill_color=ap.Color("#f0a")
+        )
+        mask.add_masking_object(masking_object=rectangle_2, alpha=0.0)
+        assert (
+            rectangle_2._fill_color._value.lower() == ap.Color("#000000")._value.lower()
+        )
+

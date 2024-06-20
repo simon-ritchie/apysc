1a61062d5a8b284700e955204fee5362f1ead6a4
simon-ritchie <antisocial.sid2@gmail.com>
2024-06-20 23:02:50 +0900

    #1203 Add the enum definition for the material design brightness (light or dark).

---
+++ apysc/_material_design/color/material_brightness.py
@@ -0,0 +1,11 @@
+"""The enum definition for the material design brightness (light or dark).
+"""
+
+from enum import Enum
+
+
+class MaterialBrightness(Enum):
+    """The enum class for the material design brightness (light or dark).
+    """
+    LIGHT = "light"
+    DARK = "dark"

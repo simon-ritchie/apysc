8c9b23b686ab92ef75dad9e6a5ee611989692ec3
simon-ritchie <antisocial.sid2@gmail.com>
2024-06-29 19:41:01 +0900

    #1209 Add the class method to create a dark color scheme sample setting.

---
+++ apysc/_material_design/color/material_color_scheme_samples.py
@@ -0,0 +1,54 @@
+"""The class implementation for material design's color scheme samples.
+"""
+
+from apysc._material_design.color.material_color_scheme import MaterialColorScheme
+
+
+class MaterialColorSchemeSamples:
+    @classmethod
+    def create_dark_color_schema_sample_brown_1(cls) -> MaterialColorScheme:
+        """
+        Create a dark color scheme sample setting.
+
+        Returns
+        -------
+        color_scheme : MaterialColorScheme
+            A created color scheme sample setting.
+
+        Notes
+        -----
+        This setting is created using the material-theme-builder, Apache-2.0 license.
+
+        References
+        ----------
+        - material-theme-builder
+            - https://material-foundation.github.io/material-theme-builder/
+        - material-theme-builder GitHub
+            - https://github.com/material-foundation/material-theme-builder
+        - material-theme-builder license
+            - https://github.com/material-foundation/material-theme-builder?tab=Apache-2.0-1-ov-file#readme  # noqa
+        """
+        from apysc._color.color import Color
+
+        return MaterialColorScheme(
+            primary=Color("#8F4C38"),
+            on_primary=Color("#FFFFFF"),
+            secondary=Color("#77574E"),
+            on_secondary=Color("#FFFFFF"),
+            error=Color("#BA1A1A"),
+            on_error=Color("#FFFFFF"),
+            surface=Color("#FFF8F6"),
+            on_surface=Color("#231917"),
+            primary_container=Color("#FFDBD1"),
+            on_primary_container=Color("#3A0B01"),
+            secondary_container=Color("#FFDBD1"),
+            on_secondary_container=Color("#2C150F"),
+            tertiary=Color("#6C5D2F"),
+            on_tertiary=Color("#FFFFFF"),
+            tertiary_container=Color("#F5E1A7"),
+            on_tertiary_container=Color("#231B00"),
+            error_container=Color("#FFDAD6"),
+            on_error_container=Color("#410002"),
+            outline=Color("#85736E"),
+            outline_variant=Color("#D8C2BC"),
+        )
---
+++ tests/_material_design/color/test_material_color_scheme_samples.py
@@ -0,0 +1,29 @@
+import apysc as ap
+from apysc._testing.testing_helper import apply_test_settings
+
+
+
+class TestMaterialColorSchemeSamples:
+    @apply_test_settings()
+    def test_create_dark_color_schema_sample_brown_1(self) -> None:
+        color_scheme: ap.MaterialColorScheme = ap.MaterialColorSchemeSamples.create_dark_color_schema_sample_brown_1()
+        assert color_scheme.primary == ap.Color("#8F4C38")
+        assert color_scheme.on_primary == ap.Color("#FFFFFF")
+        assert color_scheme.secondary == ap.Color("#77574E")
+        assert color_scheme.on_secondary == ap.Color("#FFFFFF")
+        assert color_scheme.error == ap.Color("#BA1A1A")
+        assert color_scheme.on_error == ap.Color("#FFFFFF")
+        assert color_scheme.surface == ap.Color("#FFF8F6")
+        assert color_scheme.on_surface == ap.Color("#231917")
+        assert color_scheme.primary_container == ap.Color("#FFDBD1")
+        assert color_scheme.on_primary_container == ap.Color("#3A0B01")
+        assert color_scheme.secondary_container == ap.Color("#FFDBD1")
+        assert color_scheme.on_secondary_container == ap.Color("#2C150F")
+        assert color_scheme.tertiary == ap.Color("#6C5D2F")
+        assert color_scheme.on_tertiary == ap.Color("#FFFFFF")
+        assert color_scheme.tertiary_container == ap.Color("#F5E1A7")
+        assert color_scheme.on_tertiary_container == ap.Color("#231B00")
+        assert color_scheme.error_container == ap.Color("#FFDAD6")
+        assert color_scheme.on_error_container == ap.Color("#410002")
+        assert color_scheme.outline == ap.Color("#85736E")
+        assert color_scheme.outline_variant == ap.Color("#D8C2BC")

88e94bdf5d8f289e187f46ecda337088e2e46723
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-02 21:33:24 +0900

    #1213 Bump patch version to 4.11.5.

--- apysc/__init__.py
+++ apysc/__init__.py
@@ -1337,6 +1337,6 @@ from apysc._material_design.setting.material_settings import MaterialSettings
 True_: __True = __True()
 False_: __False = __False()
 
-__version__: str = "4.11.4"
+__version__: str = "4.11.5"
 
 _logger.info("apysc packages imported successfully!")

4864d844ada455774220c178af13a54cedcb5be7
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-10 23:38:45 +0900

    #1106 Add the utility function to get a boolean value whether to skip importing Material icons or not.

--- apysc/_setting/env_var_util.py
+++ apysc/_setting/env_var_util.py
@@ -14,3 +14,20 @@ def apply_material_icons_import_skipping_setting() -> None:
     (i.e., `import apysc as ap`).
     """
     os.environ["APYSC_SKIP_MATERIAL_ICONS"] = "1"
+
+
+def skip_material_icons_importing() -> bool:
+    """
+    Get a boolean value whether to skip importing Material icons or not.
+
+    Returns
+    -------
+    result : bool
+        If the environment variable is set to skip importing Material icons,
+        then this function returns True.
+    """
+    skip_material_icons: str = os.environ.get("APYSC_SKIP_MATERIAL_ICONS", "")
+    try:
+        return bool(int(skip_material_icons))
+    except ValueError:
+        return False
--- tests/_setting/test_env_var_util.py
+++ tests/_setting/test_env_var_util.py
@@ -10,3 +10,19 @@ def test_apply_material_icons_import_skipping_setting() -> None:
     env_var_util.apply_material_icons_import_skipping_setting()
     assert os.environ["APYSC_SKIP_MATERIAL_ICONS"] == "1"
     os.environ["APYSC_SKIP_MATERIAL_ICONS"] = ""
+
+
+@apply_test_settings()
+def test_skip_material_icons_importing() -> None:
+    os.environ["APYSC_SKIP_MATERIAL_ICONS"] = ""
+    result: bool = env_var_util.skip_material_icons_importing()
+    assert not result
+
+    os.environ["APYSC_SKIP_MATERIAL_ICONS"] = "test"
+    result = env_var_util.skip_material_icons_importing()
+    assert not result
+
+    env_var_util.apply_material_icons_import_skipping_setting()
+    result = env_var_util.skip_material_icons_importing()
+    assert result
+    os.environ["APYSC_SKIP_MATERIAL_ICONS"] = ""

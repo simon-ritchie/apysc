2216df689e52d62c7ce648763317454cc1340f29
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-12 22:11:52 +0900

    #1106 Add the material color names definitions.

---
+++ apysc/_material_design/color/material_color_scheme_names.py
@@ -0,0 +1,27 @@
+"""The material color names definitions.
+"""
+
+from typing import Literal
+
+MaterialColorNames = Literal[
+    "primary",
+    "on_primary",
+    "secondary",
+    "on_secondary",
+    "error",
+    "on_error",
+    "surface",
+    "on_surface",
+    "primary_container",
+    "on_primary_container",
+    "secondary_container",
+    "on_secondary_container",
+    "tertiary",
+    "on_tertiary",
+    "tertiary_container",
+    "on_tertiary_container",
+    "error_container",
+    "on_error_container",
+    "outline",
+    "outline_variant",
+]

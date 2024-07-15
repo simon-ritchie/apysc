b0af344574abe64472118662e46c6101591310e1
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-15 17:31:49 +0900

    #1106 Add the class method to get a target color and add expressions of the specified color name.

--- apysc/_material_design/setting/material_settings_utils.py
+++ apysc/_material_design/setting/material_settings_utils.py
@@ -2,9 +2,10 @@
 """
 
 
-from typing import Optional
+from typing import Optional, Literal
 
 from apysc._color.color import Color
+from apysc._material_design.color.material_color_scheme_names import MaterialColorNames
 from apysc._material_design.color.material_color_scheme_samples import (
     MaterialColorSchemeSamples
 )
@@ -30,6 +31,118 @@ class MaterialSettingsUtils:
         )
 
     @classmethod
+    def _set_color_scheme_value_and_append_expression(
+        cls,
+        *,
+        color_scheme: Optional[MaterialColorScheme],
+        color_name: MaterialColorNames,
+        target_color: Color,
+    ) -> None:
+        """
+        Set a color scheme value to the target color and append
+        color-related expressions.
+
+        Notes
+        -----
+        If the specified `color_scheme` is `None`, then this method
+        will do nothing.
+
+        Parameters
+        ----------
+        color_scheme : Optional[MaterialColorScheme]
+            A target color scheme.
+        color_name : MaterialColorNames
+            A target color name (e.g., "primary").
+        target_color : Color
+            A target color.
+        """
+        from apysc._expression import expression_data_util
+
+        if color_scheme is None:
+            return
+
+        schgeme_color: Color = getattr(color_scheme, color_name)
+        expression_data_util.append_js_expression(
+            expression=f"{target_color.variable_name} = "
+            f"{schgeme_color.variable_name};"
+        )
+        target_color._value = schgeme_color._value
+
+    @classmethod
+    def _get_target_color_and_add_expressions_by_color_name(
+        cls,
+        *,
+        color_name: MaterialColorNames,
+        argument_color: Optional[Color],
+    ) -> Color:
+        """
+        Get a target color and add expressions of the specified
+        color name.
+
+        Parameters
+        ----------
+        color_name : MaterialColorNames
+            A target color name (e.g., "primary").
+        argument_color : Optional[Color]
+            A specified argument color.
+
+        Returns
+        -------
+        target_color : Color
+            A target primary color.
+            This value becomes according to the following priorities:
+            1. If the `argument_color` is not the `None`
+            2. If a color scheme is set in the `MaterialSettings`
+            3. A fixed color value.
+        """
+        from apysc._material_design.setting.material_settings import MaterialSettings
+        from apysc._branch._if import If
+        from apysc._material_design.color.material_color_scheme import (
+            MaterialColorScheme
+        )
+        from apysc._branch._elif import Elif
+        from apysc._type.boolean import Boolean
+        from apysc._branch._else import Else
+
+        cls._initialize_fixed_color_scheme_if_not_initialized()
+
+        if argument_color is not None:
+            return argument_color
+
+        target_color: Color = Color("#000000")
+        current_color_scheme_is_light_color_scheme: Boolean = (
+            MaterialSettings.current_color_scheme_is_light_color_scheme()
+        )
+        current_color_scheme_is_dark_color_scheme: Boolean = (
+            MaterialSettings.current_color_scheme_is_dark_color_scheme()
+        )
+        with If(MaterialSettings.color_scheme_setting_is_enabled()):
+            with If(current_color_scheme_is_light_color_scheme):
+                color_scheme: Optional[MaterialColorScheme] = (
+                    MaterialSettings.get_light_color_scheme()
+                )
+                cls._set_color_scheme_value_and_append_expression(
+                    color_scheme=color_scheme,
+                    color_name=color_name,
+                    target_color=target_color,
+                )
+
+            with Elif(current_color_scheme_is_dark_color_scheme):
+                color_scheme = MaterialSettings.get_dark_color_scheme()
+                cls._set_color_scheme_value_and_append_expression(
+                    color_scheme=color_scheme,
+                    color_name=color_name,
+                    target_color=target_color,
+                )
+        with Else():
+            cls._set_color_scheme_value_and_append_expression(
+                color_scheme=cls._fixed_color_scheme,
+                color_name=color_name,
+                target_color=target_color,
+            )
+        return target_color
+
+    @classmethod
     def get_primary_color(cls, *, argument_color: Optional[Color]) -> Color:
         """
         Get a primary color setting from setting.
@@ -48,28 +161,7 @@ class MaterialSettingsUtils:
             2. If a color scheme is set in the `MaterialSettings`
             3. A fixed color value.
         """
-        from apysc._material_design.setting.material_settings import MaterialSettings
-        from apysc._branch._if import If
-        from apysc._material_design.color.material_color_scheme import (
-            MaterialColorScheme
+        return cls._get_target_color_and_add_expressions_by_color_name(
+            color_name="primary",
+            argument_color=argument_color,
         )
-        from apysc._branch._elif import Elif
-
-        if argument_color is not None:
-            return argument_color
-
-        with If(MaterialSettings.color_scheme_setting_is_enabled()):
-            with If(MaterialSettings.current_color_scheme_is_light_color_scheme()):
-                color_scheme: Optional[MaterialColorScheme] = (
-                    MaterialSettings.get_light_color_scheme()
-                )
-                if color_scheme is not None:
-                    return color_scheme.primary
-
-            with Elif(MaterialSettings.current_color_scheme_is_dark_color_scheme()):
-                color_scheme = MaterialSettings.get_dark_color_scheme()
-                if color_scheme is not None:
-                    return color_scheme.primary
-
-        cls._initialize_fixed_color_scheme_if_not_initialized()
-        return cls._fixed_color_scheme.primary
--- tests/_material_design/setting/test_material_settings_utils.py
+++ tests/_material_design/setting/test_material_settings_utils.py
@@ -1,4 +1,5 @@
 import apysc as ap
+from apysc._expression import expression_data_util
 from apysc._testing.testing_helper import apply_test_settings
 
 
@@ -13,3 +14,48 @@ class TestMaterialSettingsUtils:
         assert isinstance(
             ap.MaterialSettingsUtils._fixed_color_scheme, ap.MaterialColorScheme
         )
+
+    @apply_test_settings()
+    def test__set_color_scheme_value_and_append_expression(self) -> None:
+        target_color: ap.Color = ap.Colors.RED_DIRT_7F5217.copy()
+        ap.MaterialSettingsUtils._set_color_scheme_value_and_append_expression(
+            color_scheme=None,
+            color_name="primary",
+            target_color=target_color,
+        )
+        assert target_color == ap.Colors.RED_DIRT_7F5217
+
+        color_scheme: ap.MaterialColorScheme = (
+            ap.MaterialColorSchemeSamples.create_dark_color_scheme_sample_brown_1()
+        )
+        ap.MaterialSettingsUtils._set_color_scheme_value_and_append_expression(
+            color_scheme=color_scheme,
+            color_name="primary",
+            target_color=target_color,
+        )
+        assert target_color == color_scheme.primary
+        expression: str = expression_data_util.get_current_expression()
+        assert f"{target_color.variable_name} = " in expression
+
+    @apply_test_settings()
+    def test__get_target_color_and_add_expressions_by_color_name(self) -> None:
+        target_color: ap.Color = ap.Colors.RED_DIRT_7F5217.copy()
+        target_color = (
+            ap.MaterialSettingsUtils._get_target_color_and_add_expressions_by_color_name(  # noqa
+                color_name="primary",
+                argument_color=target_color,
+            )
+        )
+        assert target_color == ap.Colors.RED_DIRT_7F5217
+
+        target_color = (
+            ap.MaterialSettingsUtils._get_target_color_and_add_expressions_by_color_name(  # noqa
+                color_name="primary",
+                argument_color=None,
+            )
+        )
+        assert isinstance(target_color, ap.Color)
+        expression: str = expression_data_util.get_current_expression()
+        assert expression.count("if (") == 3
+        assert expression.count("else if (") == 1
+        assert expression.count("else") == 2

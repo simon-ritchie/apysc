3dc9beb778347c321a3a00ced2b6f354c8971a4d
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-18 19:55:54 +0900

    #1106 Add the class method to get an `on_primary_container` color setting from setting.

--- tests/_material_design/setting/test_material_settings_utils.py
+++ tests/_material_design/setting/test_material_settings_utils.py
@@ -122,3 +122,14 @@ class TestMaterialSettingsUtils:
         assert primary_container == (
             ap.MaterialSettingsUtils._fixed_color_scheme.primary_container
         )
+
+    @apply_test_settings()
+    def test_get_on_primary_container_color(self) -> None:
+        on_primary_container: ap.Color = (
+            ap.MaterialSettingsUtils.get_on_primary_container_color(
+                argument_color=None
+            )
+        )
+        assert on_primary_container == (
+            ap.MaterialSettingsUtils._fixed_color_scheme.on_primary_container
+        )

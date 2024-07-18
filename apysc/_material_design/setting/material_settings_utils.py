86baeb50adc1dc01b6040acd4b40baacd214be0b
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-18 14:10:40 +0900

    #1106 Add the class method to get an error color setting from setting.

--- apysc/_material_design/setting/material_settings_utils.py
+++ apysc/_material_design/setting/material_settings_utils.py
@@ -234,3 +234,27 @@ class MaterialSettingsUtils:
             color_name="on_secondary",
             argument_color=argument_color,
         )
+
+    @classmethod
+    def get_error(cls, *, argument_color: Optional[Color]) -> Color:
+        """
+        Get an error color setting from setting.
+
+        Parameters
+        ----------
+        argument_color : Optional[Color]
+            A specified argument color.
+
+        Returns
+        -------
+        target_color : Color
+            A target `error` color.
+            This value becomes according to the following priorities:
+            1. If the `argument_color` is not the `None`
+            2. If a color scheme is set in the `MaterialSettings`
+            3. A fixed color value.
+        """
+        return cls._get_target_color_and_add_expressions_by_color_name(
+            color_name="error",
+            argument_color=argument_color,
+        )
--- tests/_material_design/setting/test_material_settings_utils.py
+++ tests/_material_design/setting/test_material_settings_utils.py
@@ -83,3 +83,10 @@ class TestMaterialSettingsUtils:
             argument_color=None
         )
         assert target_color == ap.MaterialSettingsUtils._fixed_color_scheme.on_secondary
+
+    @apply_test_settings()
+    def test_get_error(self) -> None:
+        target_color: ap.Color = ap.MaterialSettingsUtils.get_error(
+            argument_color=None
+        )
+        assert target_color == ap.MaterialSettingsUtils._fixed_color_scheme.error

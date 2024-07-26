86c3d19264f7f248fc9d44d69b682f410cb1ffec
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-26 22:18:59 +0900

    #1106 Add the test for the `_locate_icons` method.

---
+++ tests/_material_design/button/test_material_button_icon_mixin.py
@@ -0,0 +1,28 @@
+from apysc._expression import expression_data_util
+from apysc._material_design.button.material_button_icon_mixin import MaterialButtonIconMixIn
+from apysc._testing.testing_helper import apply_test_settings
+import apysc as ap
+from apysc._material_design.icon.material_home_icon import MaterialHomeIcon
+from apysc._material_design.icon.material_add_box_icon import MaterialAddBoxIcon
+
+
+class TestMaterialButtonIconMixIn:
+    @apply_test_settings()
+    def test__locate_icons(self) -> None:
+        prefix_icon: MaterialHomeIcon = MaterialHomeIcon()
+        suffix_icon: MaterialAddBoxIcon = MaterialAddBoxIcon()
+        ap.MaterialFilledButton(
+            label="Test button",
+            prefix_icon=prefix_icon,
+            suffix_icon=suffix_icon,
+            variable_name_suffix="test_button"
+        )
+        expression: str = expression_data_util.get_current_expression()
+        assert "prefix_icon_x" in expression
+        assert "prefix_icon_y" in expression
+        assert "suffix_icon_x" in expression
+        assert "suffix_icon_y" in expression
+        assert prefix_icon.x != 0
+        assert prefix_icon.y != 0
+        assert suffix_icon.x != 0
+        assert suffix_icon.y != 0

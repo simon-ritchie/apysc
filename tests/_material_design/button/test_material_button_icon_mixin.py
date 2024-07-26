be5ba3156f9cec68c8fbbc82c8511b4c462ef904
simon-ritchie <antisocial.sid2@gmail.com>
2024-07-26 23:56:26 +0900

    #1106 Add a test for the `_resize_icon_size` method.

--- tests/_material_design/button/test_material_button_icon_mixin.py
+++ tests/_material_design/button/test_material_button_icon_mixin.py
@@ -1,4 +1,5 @@
 from apysc._expression import expression_data_util
+from apysc._material_design.button.material_button_const import ICON_SIZE
 from apysc._material_design.button.material_button_icon_mixin import MaterialButtonIconMixIn
 from apysc._testing.testing_helper import apply_test_settings
 import apysc as ap
@@ -39,3 +40,23 @@ class TestMaterialButtonIconMixIn:
         )
         assert prefix_icon.parent == button
         assert suffix_icon.parent == button
+
+    @apply_test_settings()
+    def test__resize_icon_size(self) -> None:
+        prefix_icon: MaterialHomeIcon = MaterialHomeIcon()
+        suffix_icon: MaterialAddBoxIcon = MaterialAddBoxIcon()
+        ap.MaterialFilledButton(
+            label="Test button",
+            prefix_icon=prefix_icon,
+            suffix_icon=suffix_icon,
+            variable_name_suffix="test_button"
+        )
+        expression: str = expression_data_util.get_current_expression()
+        assert "prefix_icon_width" in expression
+        assert "prefix_icon_height" in expression
+        assert "suffix_icon_width" in expression
+        assert "suffix_icon_height" in expression
+        assert prefix_icon.width == ap.Int(ICON_SIZE)
+        assert prefix_icon.height == ap.Int(ICON_SIZE)
+        assert suffix_icon.width == ap.Int(ICON_SIZE)
+        assert suffix_icon.height == ap.Int(ICON_SIZE)
